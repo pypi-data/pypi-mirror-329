c3d70b90bbfdf7f8b58119b99fb82079cd5ddc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:03:51 +0000

    Add package plume-python/1.3.1/plume_python-1.3.1-py3-none-any.whl

---
+++ packages/plume-python/plume_python-1.3.1-py3-none-any.whl/plume/proxy/common/rect.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Rect:
+    x: float = 0.0
+    y: float = 0.0
+    width: float = 0.0
+    height: float = 0.0
---
+++ packages/plume-python/plume_python-1.3.1-py3-none-any.whl/plume/proxy/common/vector4.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+import numpy as np
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Vector4:
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
+    w: float = 0.0
+
+    def numpy(self) -> np.ndarray:
+        return np.array([self.x, self.y, self.z, self.w], dtype=np.float32)
+
+    def __array__(self) -> np.ndarray:
+        return self.numpy()
