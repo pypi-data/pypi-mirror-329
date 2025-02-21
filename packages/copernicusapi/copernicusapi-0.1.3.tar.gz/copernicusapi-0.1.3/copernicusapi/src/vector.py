46797516aec5b921cd93de1040970b8def1e5277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:03:36 +0000

    Add package copernicusapi/0.1.3/copernicusapi-0.1.3.tar.gz

---
+++ packages/copernicusapi/copernicusapi-0.1.3.tar.gz/copernicusapi-0.1.3/copernicusapi/src/vector.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Vector
+#
+# (c) 2024 Panopterra UG (haftungsbeschraenkt)
+#
+# This file is part of the copernicusapi package and the copernicus-api repository
+# (https://github.com/panopterra/copernicus-api). It is released under the Apache
+# License Version 2.0. See the README.md file in the repository root directory or
+# go to http://www.apache.org/licenses/ for full license details.
+#
+# ---
+#
+# This module contains utilities for operations on vector geometries.
+#
+# ---
+
+import pyproj
+import shapely
+from shapely.ops import transform
+
+
+def reproject_geometry(geom: shapely.geometry, source_epsg: int, target_epsg: int) -> shapely.geometry:
+    """
+    Projects a given vector geometry object from one CRS to another.
+
+    Parameters
+    ----------
+    geom : shapely.geometry object
+        The geometry to reproject.
+    source_epsg : int
+        The EPSG of the source projection of the geometry.
+    target_proj : str or int or float or pyproj.CRS
+        The EPSG of the target projection of the geometry.
+
+    Returns
+    -------
+    shapely.geometry object
+        Projected version of geometry.
+    """
+
+    # skip process if source and target CRS are identical
+    if source_epsg == target_epsg:
+        return geom
+
+    project = pyproj.Transformer.from_crs(source_epsg, target_epsg, always_xy=True).transform
+    reprojected_geom = transform(project, geom)
+    return reprojected_geom
---
+++ packages/copernicusapi/copernicusapi-0.1.3.tar.gz/copernicusapi-0.1.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+
+# load README.md as long description for PyPI
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+# run setup
+setup(name='copernicusapi',
+      version='0.1.3',
+      description='A package to facilitate interactive construction of queries to the Copernicus Data Space Ecosystem repository.',
+      package_dir={'copernicusapi': 'copernicusapi'},
+      packages=find_packages(),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/panopterra/copernicus-api',
+      author='Panopterra UG',
+      author_email='contact@panopterra.com',
+      license='Apache 2.0',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Topic :: Scientific/Engineering :: GIS',
+          'Topic :: Utilities'
+      ],
+      keywords='copernicus, data space, esa, satellite, download, remote sensing, earth observation',
+      install_requires=['geopandas>=0.14',
+                        'numpy>=1.26',
+                        'pandas>=2.0',
+                        'pyproj>=3.6',
+                        'shapely>=2.0',
+                        ],
+      extras_require={
+          'dev': ['black>=24.0',
+                  'flake>=7.0',
+                  'pytest-cov>=4.1',
+                  'twine>=5.1',
+                  'pytest>=8.0']
+                  },
+      python_requires='>=3.10'
+      )
