8d88586197aa4cfcac390ae1df1fa399f16e94d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:03:33 +0000

    Add package bookstack-file-exporter/1.4.1/bookstack_file_exporter-1.4.1.tar.gz

---
+++ packages/bookstack-file-exporter/bookstack_file_exporter-1.4.1.tar.gz/bookstack_file_exporter-1.4.1/bookstack_file_exporter/common/util.py
@@ -0,0 +1,44 @@
+import logging
+from typing import Dict
+# pylint: disable=import-error
+import requests
+# pylint: disable=import-error
+from requests.adapters import HTTPAdapter, Retry
+
+log = logging.getLogger(__name__)
+
+def http_get_request(url: str, headers: Dict[str, str],
+                     verify_ssl: bool, timeout: int = 30) -> requests.Response:
+    """make http requests and return response object"""
+    url_prefix = should_verify(url)
+    try:
+        with requests.Session() as session:
+            # {backoff factor} * (2 ** ({number of previous retries}))
+            # {raise_on_status} if status falls in status_forcelist range
+            #  and retries have been exhausted.
+            # {status_force_list} 413, 429, 503 defaults are overwritten with additional ones
+            retries = Retry(total=5,
+                            backoff_factor=0.5,
+                            raise_on_status=True,
+                            status_forcelist=[413, 429, 500, 502, 503, 504])
+            session.mount(url_prefix, HTTPAdapter(max_retries=retries))
+            response = session.get(url, headers=headers, verify=verify_ssl, timeout=timeout)
+    except Exception as req_err:
+        log.error("Failed to make request for %s", url)
+        raise req_err
+    try:
+        #raise_for_status() throws an exception on codes 400-599
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        # this means it either exceeded 50X retries in `http_get_request` handler
+        # or it returned a 40X which is not expected
+        log.error("Bookstack request failed with status code: %d on url: %s",
+                   response.status_code, url)
+        raise e
+    return response
+
+def should_verify(url: str) -> str:
+    """check if http or https"""
+    if url.startswith("https"):
+        return "https://"
+    return "http://"
---
+++ packages/bookstack-file-exporter/bookstack_file_exporter-1.4.1.tar.gz/bookstack_file_exporter-1.4.1/bookstack_file_exporter/run.py
@@ -0,0 +1,71 @@
+import argparse
+import sys
+import logging
+import time
+from typing import Dict
+
+from bookstack_file_exporter.config_helper.config_helper import ConfigNode
+from bookstack_file_exporter.exporter.node import Node
+from bookstack_file_exporter.exporter.exporter import NodeExporter
+from bookstack_file_exporter.archiver.archiver import Archiver
+
+log = logging.getLogger(__name__)
+
+def entrypoint(args: argparse.Namespace):
+    """entrypoint for export process"""
+    # get configuration from helper
+    config = ConfigNode(args)
+    if config.user_inputs.run_interval:
+        while True:
+            exporter(config)
+            log.info(f"Waiting {config.user_inputs.run_interval} seconds for next run")
+            # sleep process state
+            time.sleep(config.user_inputs.run_interval)
+    exporter(config)
+
+def exporter(config: ConfigNode):
+    """export bookstack nodes and archive locally and/or remotely"""
+
+    ## convenience vars
+    bookstack_headers = config.headers
+    api_urls = config.urls
+    unassigned_dir = config.unassigned_book_dir
+    verify_ssl = config.user_inputs.assets.verify_ssl
+
+    #### Export Data #####
+    # need to implement pagination for apis
+    log.info("Beginning run")
+
+    ## Use exporter class to get all the resources (pages, books, etc.) and their relationships
+    log.info("Building shelve/book/chapter/page relationships")
+    export_helper = NodeExporter(api_urls, bookstack_headers, verify_ssl)
+    ## shelves
+    shelve_nodes: Dict[int, Node] = export_helper.get_all_shelves()
+    ## books
+    book_nodes: Dict[int, Node] = export_helper.get_all_books(shelve_nodes, unassigned_dir)
+    ## pages
+    page_nodes: Dict[int, Node] = export_helper.get_all_pages(book_nodes)
+    if not page_nodes:
+        log.warning("No page data available from given Bookstack instance. Nothing to archive")
+        sys.exit(0)
+    log.info("Beginning archive")
+    ## start archive ##
+    archive: Archiver = Archiver(config)
+
+    # create export directory if not exists
+    archive.create_export_dir()
+
+    # get all page content for each page
+    archive.get_bookstack_exports(page_nodes)
+
+    # create tar if needed and gzip tar
+    archive.create_archive()
+
+    # archive to remote targets
+    archive.archive_remote()
+    # if remote target is specified and clean is true
+    # clean up the .tgz archive since it is already uploaded
+    archive.clean_up()
+
+    log.info("Created file archive: %s.tgz", archive.archive_dir)
+    log.info("Completed run")
