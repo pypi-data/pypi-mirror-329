a2ff52d39d6cdebe73cdc05b7d5a8239cb71b2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:28 +0000

    Add package keras-rs-nightly/0.0.1.dev2025022203/keras_rs_nightly-0.0.1.dev2025022203.tar.gz

---
+++ packages/keras-rs-nightly/keras_rs_nightly-0.0.1.dev2025022203.tar.gz/keras_rs_nightly-0.0.1.dev2025022203/keras_rs/src/utils/keras_utils.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+import keras
+
+
+def clone_initializer(
+    initializer: Union[str, keras.initializers.Initializer],
+) -> keras.initializers.Initializer:
+    """Clones an initializer to ensure a new seed.
+
+    Args:
+        initializer: The initializer to clone.
+
+    Returns:
+        A cloned initializer if it is clonable, otherwise the original one.
+
+    As of tensorflow 2.10, we need to clone user passed initializers when
+    invoking them twice to avoid creating the same randomized initialization.
+    """
+    if isinstance(initializer, keras.initializers.Initializer):
+        config = initializer.get_config()
+        initializer_class: type[keras.initializers.Initializer] = (
+            initializer.__class__
+        )
+        return initializer_class.from_config(config)
+    # If we get a string or dict, just return as we cannot and should not clone.
+    return initializer
