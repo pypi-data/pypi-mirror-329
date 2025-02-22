3154bda06b47a12df343c57fb67ba374bd918f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:35 +0000

    Add package apiverve-captchagenerator/1.1.8/apiverve_captchagenerator-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-captchagenerator/apiverve_captchagenerator-1.1.8-py3-none-any.whl/apiverve_captchagenerator/exceptions.py
@@ -0,0 +1,3 @@
+class CaptchageneratorCaptchaGeneratorAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-captchagenerator/apiverve_captchagenerator-1.1.8-py3-none-any.whl/apiverve_captchagenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_captchagenerator',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Captcha Generator is a simple tool for generating captchas. It returns a random string of characters that can be used as a captcha.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
