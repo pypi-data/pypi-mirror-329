699555e3b8c73ebba2c5f755754c028787a0d6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:05:45 +0000

    Add package create-lilypad-module/0.1.48/create_lilypad_module-0.1.48.tar.gz

---
+++ packages/create-lilypad-module/create_lilypad_module-0.1.48.tar.gz/create_lilypad_module-0.1.48/src/create_lilypad_module/templates/config/constants.py
@@ -0,0 +1,15 @@
+# TODO: Set the Docker Hub repository before pushing the image.
+# Example: "devlinrocha/lilypad-module-sentiment"
+DOCKER_REPO = ""
+
+# TODO: Set the tag for the Docker image.
+# Example: "latest", "v1.0", or a commit SHA
+DOCKER_TAG = "latest"
+
+# TODO: Set the GitHub repository URL where your module is stored.
+# Example: "github.com/devlinrocha/lilypad-module-sentiment".
+GITHUB_REPO = ""
+
+# TODO: Specify the GitHub tag, branch, or commit hash.
+# Example: "main" or "c3ed392c11060337cae010862b1af160cd805e67"
+GITHUB_TAG = "main"
