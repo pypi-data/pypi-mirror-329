bb574607c27ff1333183434c4a015506e6359cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 16:04:48 +0000

    Add package fastanime/2.8.8/fastanime-2.8.8.tar.gz

---
+++ packages/fastanime/fastanime-2.8.8.tar.gz/fastanime-2.8.8/flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "FastAnime Project Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+
+      python = pkgs.python312;
+      pythonPackages = python.pkgs;
+      fastanimeEnv = pythonPackages.buildPythonApplication {
+        pname = "fastanime";
+        version = "2.8.8";
+
+        src = ./.;
+
+        preBuild = ''
+          sed -i 's/rich>=13.9.2/rich>=13.8.1/' pyproject.toml
+          sed -i 's/pycryptodome>=3.21.0/pycryptodome>=3.20.0/' pyproject.toml
+        '';
+
+        # Add runtime dependencies
+        propagatedBuildInputs = with pythonPackages; [
+          click
+          inquirerpy
+          requests
+          rich
+          thefuzz
+          yt-dlp
+          dbus-python
+          hatchling
+          plyer
+          mpv
+          fastapi
+          pycryptodome
+          pypresence
+        ];
+
+        # Ensure compatibility with the pyproject.toml
+        format = "pyproject";
+      };
+
+    in
+    {
+      packages.default = fastanimeEnv;
+
+      # DevShell for development
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          fastanimeEnv
+          pythonPackages.hatchling
+          pkgs.mpv
+          pkgs.libmpv
+          pkgs.fzf
+          pkgs.rofi
+        ];
+      };
+    });
+}
