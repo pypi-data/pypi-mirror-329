e9fbdeae35a1317e2174cdef9b77f102bc2b33c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:04:31 +0000

    Add package omniscale/0.1/omniscale-0.1-py3-none-any.whl

---
+++ packages/omniscale/omniscale-0.1-py3-none-any.whl/omniscale/__init__.py
@@ -0,0 +1,13 @@
+from .module import (
+    celsius_to_fahrenheit,
+    fahrenheit_to_celsius,
+    meters_to_feet,
+    feet_to_meters
+)
+
+__all__ = [
+    "celsius_to_fahrenheit",
+    "fahrenheit_to_celsius",
+    "meters_to_feet",
+    "feet_to_meters"
+]
---
+++ packages/omniscale/omniscale-0.1-py3-none-any.whl/omniscale/module.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5/9
+
+def meters_to_feet(meters):
+    """Convert meters to feet."""
+    return meters * 3.28084
+
+def feet_to_meters(feet):
+    """Convert feet to meters."""
+    return feet / 3.28084
