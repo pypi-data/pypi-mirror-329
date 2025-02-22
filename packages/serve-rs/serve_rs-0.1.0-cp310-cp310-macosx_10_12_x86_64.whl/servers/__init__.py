8dbdb405af764d1a3d390dfadbf090c5d0244f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:03:54 +0000

    Add package serve-rs/0.1.0/serve_rs-0.1.0-cp310-cp310-macosx_10_12_x86_64.whl

---
+++ packages/serve-rs/serve_rs-0.1.0-cp310-cp310-macosx_10_12_x86_64.whl/servers/__init__.py
@@ -0,0 +1,12 @@
+from .servers import start
+from pathlib import Path
+import argparse
+import sys
+
+
+def serve():
+    sys.path.insert(0, str(Path.cwd()))
+    parser = argparse.ArgumentParser(description="Start the server.")
+    parser.add_argument("path", type=str, help="Path to the server directory")
+    args = parser.parse_args()
+    sys.exit(start(args.path))
