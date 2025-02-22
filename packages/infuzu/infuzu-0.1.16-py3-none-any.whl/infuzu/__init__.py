5a81de175f1e9616c79f68125eb06dd56eba4d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:58 +0000

    Add package infuzu/0.1.16/infuzu-0.1.16-py3-none-any.whl

---
+++ packages/infuzu/infuzu-0.1.16-py3-none-any.whl/infuzu/__init__.py
@@ -0,0 +1,7 @@
+from .api_client import create_chat_completion, ChatCompletionsHandlerRequestMessage, ChatCompletionsRequestContentPart
+
+__all__: list[str] = [
+    "create_chat_completion",
+    "ChatCompletionsRequestContentPart",
+    "ChatCompletionsHandlerRequestMessage"
+]
