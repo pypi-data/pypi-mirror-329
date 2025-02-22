1b0a7aa86df81127e0618cee2c57a507afa3ccdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:05:22 +0000

    Add package pyzohar/0.1.19/pyzohar-0.1.19.tar.gz

---
+++ packages/pyzohar/pyzohar-0.1.19.tar.gz/pyzohar-0.1.19/README.md
@@ -0,0 +1,50 @@
+# Pyzohar  
+
+### Introduction
+ - Pyzohar is a Python package for data manipulation.  
+
+### Main features  
+  - Basic data pre-processing between different types.  
+  - Pandas.DataFrame manipulating.  
+  - Data input and output between localDisc, mongodb, mysql, httpRequests, etc.  
+  - Fast modelling.  
+
+### Reference 
+ - See [package Reference here](https://zoharslong.com/#/pyzohar).  
+
+### Examples  
+Install
+```cmd
+> pip install pyzohar
+```
+
+Import package
+```python
+from pyzohar import dfz
+lst_mpt = [{'A':1,'B':2},{'A':3,'B':4}]
+dfz_tst = dfz(dts=lst_mpt, lcn={'fld':'./doc_smp','fls':'txt_tst.xlsx'})
+dfz_tst.lcl_xpt()  # send lst_mpt into path './doc_smp/txt_tst.xlsx'
+```
+> Use pyzohar.dfz to output data into hard disc.  
+
+### update 1.19
+  - Alter add_clm_nrm, show std and avg
+### update 1.18
+  - Alter for dfz.mng_nfo('dcm',prm='<columnName>',dct_qry={})
+### update 1.17
+  - Alter for fake_userAgent
+### update 1.16
+  - Alter lcl_mpt
+### update 1.15
+  - Update OpenpyxlWriter
+### update 1.14
+  - Support 'xlsm'
+### update 1.13
+  - Alter api_prx for auto userAgent changing
+  - Alter io export to excel method
+### update 1.12
+  - Alter spider
+### update 1.10
+  - Add new slot for generating pptx
+### update 1.08  
+  - Fixed bugs about misusing on pandas.Series.to_dict
---
+++ packages/pyzohar/pyzohar-0.1.19.tar.gz/pyzohar-0.1.19/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2021.03.18
+setup for package.
+@author: zoharslong
+"""
+from setuptools import setup, find_packages
+from os.path import join as os_join, abspath as os_abspath, dirname as os_dirname
+
+here = os_abspath(os_dirname(__file__))
+with open(os_join(here, 'README.md')) as f:
+    README = f.read()
+
+setup(
+    name="pyzohar",
+    version="0.1.19",
+    author="zoharslong",
+    author_email="zoharslong@hotmail.com",
+    description="a private package on data pre-processing.",
+    long_description=README,
+    url="https://zoharslong.com/#/pyzohar",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # {3:Alpha, 4:Beta, 5:Production/Stable}
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(),
+    keywords='data pre-processing',
+    python_requires='>=3',
+    install_requires=[
+        'numpy>=1.18.1',
+        'pandas>=1.0.1',
+        'pymongo>=3.9.0',
+        'pymysql>=0.9.3',
+        'fake-useragent>=0.1.11',
+        'requests>=2.22.0',
+        'openpyxl>=3.0.3',  # excel files resolving
+        'urllib3>=1.25.8',  # some error type of http requests
+        # 'matplotlib>=3.1.3',  # for sub_slt_mdl.mdz
+        # 'sklearn>=0.22.1',  # for sub_slt_mdl.mdz
+        # 'seaborn>=0.10.0',  # for sub_slt_mdl.mdz
+        # 'factor_analyzer>=0.3.2',  # for sub_slt_mdl.mdz
+        # 'joblib>=0.14.1',  # for sub_slt_mdl.mdz
+        # 'python-pptx>=0.6.19',  # for sub_slt_ppt.ppz
+    ],
+    package_data={'pyzohar': ['samples/*.*']},
+    include_package_data=True,
+)
