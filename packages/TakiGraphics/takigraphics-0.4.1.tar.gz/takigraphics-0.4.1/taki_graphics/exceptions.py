c5f466bef429af312bf7364e71918aff0a6c358c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:03:50 +0000

    Add package TakiGraphics/0.4.1/takigraphics-0.4.1.tar.gz

---
+++ packages/TakiGraphics/takigraphics-0.4.1.tar.gz/takigraphics-0.4.1/taki_graphics/exceptions.py
@@ -0,0 +1,11 @@
+class PlayerNotFoundError(Exception):
+    pass
+
+class InvalidCardError(Exception):
+    pass
+
+class MissingCardError(Exception):
+    pass
+
+class InvalidLocationError(Exception):
+    pass
