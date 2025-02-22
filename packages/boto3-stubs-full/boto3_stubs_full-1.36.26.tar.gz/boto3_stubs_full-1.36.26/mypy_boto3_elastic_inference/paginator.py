559800c252b3e3eea0d03060bfeb7083210ccc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:04:16 +0000

    Add package boto3-stubs-full/1.36.26/boto3_stubs_full-1.36.26.tar.gz

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.36.26.tar.gz/boto3_stubs_full-1.36.26/mypy_boto3_elastic_inference/paginator.py
@@ -0,0 +1,64 @@
+"""
+Type annotations for elastic-inference service client paginators.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_elastic_inference/paginators/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_elastic_inference.client import ElasticInferenceClient
+    from mypy_boto3_elastic_inference.paginator import (
+        DescribeAcceleratorsPaginator,
+    )
+
+    session = Session()
+    client: ElasticInferenceClient = session.client("elastic-inference")
+
+    describe_accelerators_paginator: DescribeAcceleratorsPaginator = client.get_paginator("describe_accelerators")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    DescribeAcceleratorsRequestPaginateTypeDef,
+    DescribeAcceleratorsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("DescribeAcceleratorsPaginator",)
+
+
+if TYPE_CHECKING:
+    _DescribeAcceleratorsPaginatorBase = Paginator[DescribeAcceleratorsResponseTypeDef]
+else:
+    _DescribeAcceleratorsPaginatorBase = Paginator  # type: ignore[assignment]
+
+
+class DescribeAcceleratorsPaginator(_DescribeAcceleratorsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elastic-inference/paginator/DescribeAccelerators.html#ElasticInference.Paginator.DescribeAccelerators)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_elastic_inference/paginators/#describeacceleratorspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeAcceleratorsRequestPaginateTypeDef]
+    ) -> PageIterator[DescribeAcceleratorsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elastic-inference/paginator/DescribeAccelerators.html#ElasticInference.Paginator.DescribeAccelerators.paginate)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_elastic_inference/paginators/#describeacceleratorspaginator)
+        """
