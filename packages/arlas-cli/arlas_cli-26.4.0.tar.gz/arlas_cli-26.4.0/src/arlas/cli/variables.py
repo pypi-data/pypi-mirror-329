cd51120e832ada928555af7d6fe68d53295f1e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:03:42 +0000

    Add package arlas-cli/26.4.0/arlas_cli-26.4.0.tar.gz

---
+++ packages/arlas-cli/arlas_cli-26.4.0.tar.gz/arlas_cli-26.4.0/src/arlas/cli/variables.py
@@ -0,0 +1,7 @@
+import os
+
+
+variables = {
+    "configuration_file": os.path.join(os.path.expanduser('~'), ".arlas", "cli", "configuration.yaml"),
+    "help_epilog": "See full arlas_cli documentation at https://gisaia.github.io/arlas_cli/"
+}
