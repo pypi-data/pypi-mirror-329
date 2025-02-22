91808e32e0d4f3f797a7b184720b2ecc1f4604ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:03:56 +0000

    Add package eolymp/0.12.24/eolymp-0.12.24-py3-none-any.whl

---
+++ packages/eolymp/eolymp-0.12.24-py3-none-any.whl/eolymp/mailing/__init__.py
@@ -0,0 +1,3 @@
+from .email_subscription_service_pb2 import *
+from .email_type_pb2 import *
+from .email_subscription_service_http import *
---
+++ packages/eolymp/eolymp-0.12.24-py3-none-any.whl/eolymp/newsletter/__init__.py
@@ -0,0 +1,6 @@
+from .recipient_pb2 import *
+from .recipient_service_http import *
+from .newsletter_service_pb2 import *
+from .recipient_service_pb2 import *
+from .newsletter_pb2 import *
+from .newsletter_service_http import *
