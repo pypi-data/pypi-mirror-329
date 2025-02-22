641ec93ecede657416e9a97ebe8da31b0c44aac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:03:53 +0000

    Add package asset-loader/0.0.1/asset_loader-0.0.1-py3-none-any.whl

---
+++ packages/asset-loader/asset_loader-0.0.1-py3-none-any.whl/asset_loader/__init__.py
@@ -0,0 +1,10 @@
+from ._main import (
+    LoadedAsset as LoadedAsset,
+    AssetLoader as AssetLoader,
+    PythonAsset as PythonAsset,
+    PythonAssetLoader as PythonAssetLoader,
+    YamlAsset as YamlAsset,
+    YamlAssetLoader as YamlAssetLoader,
+    JSONAsset as JSONAsset,
+    JSONAssetLoader as JSONAssetLoader
+)
