07b4ebbb17a0a4e0fb73b7c3b7cd6472a32091ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:03:40 +0000

    Add package turihub/0.0.1.dev0/turihub-0.0.1.dev0.tar.gz

---
+++ packages/turihub/turihub-0.0.1.dev0.tar.gz/turihub-0.0.1.dev0/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+from setuptools.command.install import install
+   
+setup(
+    name='turihub',
+    version='0.0.1-dev',
+    packages=['turihub'],
+    description='Example package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
