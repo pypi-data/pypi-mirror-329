4314aaa0fe0f9420b1f5a851af48d571dc8e3855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:44 +0000

    Add package task-scheduling/2.0.4/task_scheduling-2.0.4.tar.gz

---
+++ packages/task-scheduling/task_scheduling-2.0.4.tar.gz/task_scheduling-2.0.4/task_scheduling/utils/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .is_async_function import is_async_function
+from .sleep import interruptible_sleep
+from .worker_initializer import worker_initializer
+
+# Used for task tagging
+from .tag_added import FunctionRunner
