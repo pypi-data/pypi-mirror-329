8b751e947608daa97fc2225d8e27a5f93a4d660a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:29 +0000

    Add package yz-rpa-utils/0.1.20/yz_rpa_utils-0.1.20-py3-none-any.whl

---
+++ packages/yz-rpa-utils/yz_rpa_utils-0.1.20-py3-none-any.whl/yz_utils/web_api.py
@@ -0,0 +1,103 @@
+import traceback
+
+import requests, json, time, base64
+import threading
+from retrying import retry
+
+
+# 定义重试条件
+def retry_if_not_expected_code(response, expected_code):
+    """如果响应码不是期望的响应码，则重试"""
+    return response.status_code != expected_code
+
+
+class ApiClient:
+    def __init__(self, base_url, user_name, password, _print=print):
+        self.user_name = user_name
+        self.password = password
+        self.base_url = base_url
+        # 初始化当前令牌和刷新时间
+        self.token = None
+        self.token_refresh_time = 0
+        self.print = _print
+
+        if not self.base_url.startswith('http'):
+            raise Exception('请检查base_url格式是否正确')
+        if not self.user_name or not self.password:
+            raise Exception('请配置正确的用户名和密码')
+        # 创建的时候获取令牌
+        self.get_access_token()
+        # 创建定时器,每一分钟检测一次
+        self.token_thread = threading.Thread(target=self.token_thread_func)
+        self.token_thread_running = True
+        self.token_thread.start()
+
+    def token_thread_func(self):
+        while self.token_thread_running:
+            try:
+                self.get_access_token()
+            except Exception as ex:
+                self.print(traceback.format_exc())
+            finally:
+                time.sleep(30)
+
+    # 定义重试装饰器,最多30分钟还是失败就报错
+    @retry(
+        retry_on_result=lambda response: retry_if_not_expected_code(response, 200),  # 重试条件
+        stop_max_attempt_number=180,  # 最大重试次数
+        wait_fixed=10000,  # 每次重试间隔 30 秒
+    )
+    def retry_request(self, func):
+        try:
+            self.print("开始请求")
+            response = func()
+            self.print(f"请求结果:{response.text}")
+            return response
+        except Exception as ex:
+            self.print(traceback.format_exc())
+            raise Exception('请求异常:', ex)
+
+    def get_access_token(self):
+        if not self.token or int(time.time()) > self.token_refresh_time:
+            token_result = self.handle_response(self.retry_request(lambda: requests.post(url=f"{self.base_url}/api/v1/oauth2/token?grant_type=client_credentials",
+                                                                                         headers={
+                                                                                             'Authorization': 'Basic ' + base64.b64encode(f"{self.user_name}:{self.password}".encode('utf-8')).decode()
+                                                                                         })))
+            self.token = token_result.get('accessToken')
+            self.print(f"更新令牌:{self.token}")
+            # 减一分钟，防止网络延迟
+            self.token_refresh_time = int(time.time()) + int(token_result.get('expiresIn')) - 60
+
+    @staticmethod
+    def handle_response(response):
+        return response.json().get('data')
+
+    def get(self, request_path, request_body=None):
+        if request_body is None:
+            request_body = {}
+        response = self.retry_request(lambda: requests.get(url=f"{self.base_url}/api/v1{request_path}",
+                                                           headers={
+                                                               'Authorization': 'Bearer ' + self.token
+                                                           },
+                                                           params=request_body))
+        return self.handle_response(response)
+
+    def post(self, request_path, request_body=None):
+        if request_body is None:
+            raise Exception('请传入请求参数')
+        response = self.retry_request(lambda: requests.post(url=f"{self.base_url}/api/v1{request_path}",
+                                                            headers={'Authorization': 'Bearer ' + self.token},
+                                                            data=request_body))
+        return self.handle_response(response)
+
+    def post_json(self, request_path, request_body=None):
+        if request_body is None:
+            raise Exception('请传入请求参数')
+        response = self.retry_request(lambda: requests.post(url=f"{self.base_url}/api/v1{request_path}",
+                                                            headers={'Authorization': 'Bearer ' + self.token},
+                                                            json=request_body))
+        return self.handle_response(response)
+
+    def close(self):
+        self.token_thread_running = False
+        self.token_thread.join()
