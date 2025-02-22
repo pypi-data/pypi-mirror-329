562f8539d25040d645e48568caee2d433a0194a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:53 +0000

    Add package apiverve-antonymfinder/1.1.8/apiverve_antonymfinder-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-antonymfinder/apiverve_antonymfinder-1.1.8-py3-none-any.whl/apiverve_antonymfinder/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name='apiverve_antonymfinder',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    description='Antonym is a simple tool for getting the antonyms of a word. It returns a list of antonyms for the given word.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
