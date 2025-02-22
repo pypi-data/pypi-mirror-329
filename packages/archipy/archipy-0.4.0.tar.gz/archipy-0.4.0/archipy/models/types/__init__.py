ff72fff64523353b8faea652229e57c2479d2886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:03:46 +0000

    Add package archipy/0.4.0/archipy-0.4.0.tar.gz

---
+++ packages/archipy/archipy-0.4.0.tar.gz/archipy-0.4.0/archipy/models/types/__init__.py
@@ -0,0 +1,4 @@
+from archipy.models.types.base_types import FilterOperationType, SortOrderType
+from archipy.models.types.email_types import EmailAttachmentDispositionType, EmailAttachmentType
+
+__all__ = ["SortOrderType", "FilterOperationType", "EmailAttachmentType", "EmailAttachmentDispositionType"]
