a8d63c2800251d5aa9732dbebdb83347c2f5fcec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:40 +0000

    Add package apiverve-cocktailrecipe/1.1.8/apiverve_cocktailrecipe-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-cocktailrecipe/apiverve_cocktailrecipe-1.1.8-py3-none-any.whl/apiverve_cocktailrecipe/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_cocktailrecipe',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Cocktail is a simple tool for getting cocktail recipes. It returns the ingredients, instructions, and more of the cocktail.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
