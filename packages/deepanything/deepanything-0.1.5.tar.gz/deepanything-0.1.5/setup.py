0bd17194253b57b46b56a8c050660b1db4d3a187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 16:04:52 +0000

    Add package deepanything/0.1.5/deepanything-0.1.5.tar.gz

---
+++ packages/deepanything/deepanything-0.1.5.tar.gz/deepanything-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md",encoding='utf-8') as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+setup(
+    name="deepanything",
+    version="0.1.5",
+    author="Junity",
+    author_email="1727636624@qq.com",
+    description="DeepAnything is a project that provides DeepSeek R1's deep thinking capabilities for various large language models (LLMs).",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "deepanything=deepanything.__main__:main",
+        ],
+    },
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+    ],
+    install_requires=requirements
+)
