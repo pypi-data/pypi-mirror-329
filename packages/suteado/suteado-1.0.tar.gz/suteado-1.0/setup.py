f33705922243500263a96a2a2a53cad350b1af35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:04:10 +0000

    Add package suteado/1.0/suteado-1.0.tar.gz

---
+++ packages/suteado/suteado-1.0.tar.gz/suteado-1.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+DESCRIPTION = 'm.kuku.luで使える捨てアドぽいぽいの非公式API'
+NAME = 'suteado'
+AUTHOR = 'mily'
+AUTHOR_EMAIL = 'goshoahiru0323@gmail.com'
+URL = 'https://github.com/milypy/suteado/'
+LICENSE = 'MIT'
+DOWNLOAD_URL = URL
+VERSION = '1.0'
+PYTHON_REQUIRES = '>=3.1'
+INSTALL_REQUIRES = [
+    'requests'
+]
+PACKAGES = [
+    'suteado'
+]
+KEYWORDS = 'suteadopoipoi suteado'
+CLASSIFIERS=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.1'
+]
+with open('README.md', 'r', encoding='utf-8') as fp:
+    readme = fp.read()
+LONG_DESCRIPTION = readme
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=URL,
+    packages=PACKAGES,
+    classifiers=CLASSIFIERS,
+    license=LICENSE,
+    keywords=KEYWORDS,
+    install_requires=INSTALL_REQUIRES
+)
