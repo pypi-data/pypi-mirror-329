8138f699be7a644799043c0c80bcb812e325e965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 16:04:54 +0000

    Add package savecode/1.2.7/savecode-1.2.7-py3-none-any.whl

---
+++ packages/savecode/savecode-1.2.7-py3-none-any.whl/savecode/plugins/__init__.py
@@ -0,0 +1,7 @@
+"""
+savecode/plugins/__init__.py - Initialize the plugins package for savecode.
+"""
+
+# Import plugins to ensure they are registered.
+from . import gather
+from . import save
---
+++ packages/savecode/savecode-1.2.7-py3-none-any.whl/savecode/utils/__init__.py
@@ -0,0 +1,5 @@
+"""
+savecode/utils/__init__.py - Initializes the utils module for savecode.
+"""
+
+from .output_manager import configure_output_path
