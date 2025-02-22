2ce3a0de940de19dd47fe62acfe71da18b4e1d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:03:50 +0000

    Add package custom-sklearn/0.3.2/custom_sklearn-0.3.2.tar.gz

---
+++ packages/custom-sklearn/custom_sklearn-0.3.2.tar.gz/custom_sklearn-0.3.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="custom_sklearn",
+    version="0.3.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="WeareUs",
+    author_email="1customer11service1@gmail.com",
+    description="A simple example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
