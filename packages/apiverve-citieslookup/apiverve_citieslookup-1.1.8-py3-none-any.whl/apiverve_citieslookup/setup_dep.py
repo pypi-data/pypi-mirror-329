e41e0eaa5247439b328dc7e75c887433abff03d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:40 +0000

    Add package apiverve-citieslookup/1.1.8/apiverve_citieslookup-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-citieslookup/apiverve_citieslookup-1.1.8-py3-none-any.whl/apiverve_citieslookup/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_citieslookup',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Cities Lookup is a simple tool for looking up city data. It returns the city name, population, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
