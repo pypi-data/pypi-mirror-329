933201c23bc0a6ce2ce45c1f1aaab02fc031c130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:29 +0000

    Add package TMEImmune/1.1.1/tmeimmune-1.1.1.tar.gz

---
+++ packages/TMEImmune/tmeimmune-1.1.1.tar.gz/tmeimmune-1.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.1' 
+DESCRIPTION = 'Python package for calculating TME scores'
+
+
+# Setting up
+setup(
+        name="TMEImmune", 
+        version=VERSION,
+        author="Qilu Zhou",
+        author_email="<qiluzhou@umass.edu>",
+        url="https://github.com/qiluzhou/TMEImmune",
+        description=DESCRIPTION,
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        packages=find_packages(),
+        install_requires=["pandas>=1.5.0", "numpy>=1.23.5", "cmapPy", "rnanorm", "gseapy == 0.11.0",
+                          "inmoose", "lifelines", "scikit-learn", "matplotlib", "requests"], 
+        keywords=['python', 'TME score'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        python_requires=">=3.10"
+)
