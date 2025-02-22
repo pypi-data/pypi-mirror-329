ab473139b9d44f2213703a047539239864f7eb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:49 +0000

    Add package nonebot-plugin-luoguluck/0.1.2/nonebot_plugin_luoguluck-0.1.2.tar.gz

---
+++ packages/nonebot-plugin-luoguluck/nonebot_plugin_luoguluck-0.1.2.tar.gz/nonebot_plugin_luoguluck-0.1.2/src/nonebot_plugin_luoguluck/__init__.py
@@ -0,0 +1,11 @@
+from nonebot.plugin import PluginMetadata
+from . import image
+from . import event
+__plugin_meta__ = PluginMetadata(
+    name="LuoguLuck|洛谷运势",
+    description="洛谷同款的今日运势插件！",
+    usage="/luck",
+    type="application",
+)
+
+
