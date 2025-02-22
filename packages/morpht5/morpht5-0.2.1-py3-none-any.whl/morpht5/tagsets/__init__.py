91e76bfaee3b3fdd4e858e3f485a9a5c07f91ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:03:59 +0000

    Add package morpht5/0.2.1/morpht5-0.2.1-py3-none-any.whl

---
+++ packages/morpht5/morpht5-0.2.1-py3-none-any.whl/morpht5/tagsets/__init__.py
@@ -0,0 +1,4 @@
+from .biblehub import Tag as BibleHubTag
+from .oblubienica import Tag as OblubienicaTag
+
+__all__ = ["BibleHubTag", "OblubienicaTag"]
---
+++ packages/morpht5/morpht5-0.2.1-py3-none-any.whl/morpht5/tokenizer/__init__.py
@@ -0,0 +1,4 @@
+from .morph_t5_tokenizer import MorphT5Tokenizer
+from .morph_tokenizer import MorphTokenizer
+
+__all__ = ["MorphTokenizer", "MorphT5Tokenizer"]
