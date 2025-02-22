091bcf38595bac8c04226f2f7cbe2c87fcb0b453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:03:51 +0000

    Add package ProcessMeaningPatternsPython/0.11/processmeaningpatternspython-0.11.tar.gz

---
+++ packages/ProcessMeaningPatternsPython/processmeaningpatternspython-0.11.tar.gz/processmeaningpatternspython-0.11/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='ProcessMeaningPatternsPython',
+    version='0.11',
+    packages=['promean4py'],
+    description='An app to map event logs into ontology-based process knowledge and analyze the data through a library of knowledge patterns',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Riley Moher',
+    author_email='riley.moher@mail.utoronto.ca',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Natural Language :: English'
+    ],
+    url='https://github.com/riley-momo/ProcessMeaningPatternsPython',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pm4py',
+        'kglab',
+        'yatter',
+        'ruamel.yaml'
+        ],
+    extras_require={
+        'dev' : ['twine>=4.0.2']
+    },
+    python_requires='>=3.9',  
+)
