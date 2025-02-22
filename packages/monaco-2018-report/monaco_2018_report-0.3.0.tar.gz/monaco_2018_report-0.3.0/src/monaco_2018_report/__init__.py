eff40ff1a8f32de1f3ddac3ce74448f447d0356a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:03:53 +0000

    Add package monaco-2018-report/0.3.0/monaco_2018_report-0.3.0.tar.gz

---
+++ packages/monaco-2018-report/monaco_2018_report-0.3.0.tar.gz/monaco_2018_report-0.3.0/src/monaco_2018_report/__init__.py
@@ -0,0 +1,2 @@
+from .report import (parse_arguments, get_data, get_racers_profile, calculate_race_time, compute_racers_time,
+                     build_report, print_report, get_files, generate_report_data)
