3e96b5435fa84e5f183132bd572e91841f8dabd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:41 +0000

    Add package apiverve-colorconverter/1.1.8/apiverve_colorconverter-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-colorconverter/apiverve_colorconverter-1.1.8-py3-none-any.whl/apiverve_colorconverter/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_colorconverter',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Color Converter is a simple tool for converting color formats. It returns the color converted to the specified format.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
