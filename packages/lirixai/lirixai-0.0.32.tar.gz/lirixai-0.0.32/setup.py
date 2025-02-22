9006b1536f0421a28ccd32c0b656fefd2b9f0760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:51 +0000

    Add package lirixai/0.0.32/lirixai-0.0.32.tar.gz

---
+++ packages/lirixai/lirixai-0.0.32.tar.gz/lirixai-0.0.32/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'lirixai',
+    packages = find_packages(include = ["lirixai"]),
+    version = '0.0.32',
+    description = 'An Open Source framework that converts the tedious process of creating agentic-systems for backend applications seamless. Developed at the Center of Excellence, AI and Robotics (AIR) at VIT-AP University',
+    author = 'Tanishq and Abhiram',
+    install_requires=[
+        'pydantic>=2.10.6',
+        'pydantic-ai>=0.0.24',
+        'asyncio',  # Provides extended random functionalities if needed
+        'setuptools',
+    ],
+
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
