076c7935aef5158502c6f14498d811ce84ceb64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:04:34 +0000

    Add package sunholo/0.119.1/sunholo-0.119.1.tar.gz

---
+++ packages/sunholo/sunholo-0.119.1.tar.gz/sunholo-0.119.1/src/sunholo/discovery_engine/create_new.py
@@ -0,0 +1,30 @@
+from .discovery_engine_client import DiscoveryEngineClient
+from ..utils import ConfigManager
+from ..utils.gcp_project import get_gcp_project
+
+def create_new_discovery_engine(config:ConfigManager):
+
+    chunker_config = config.vacConfig("chunker")
+
+    chunk_size = 500
+    if chunker_config:
+        if "chunk_size" in chunker_config:
+            chunk_size = chunker_config["chunk_size"]    
+
+    gcp_config = config.vacConfig("gcp_config")
+    project_id = gcp_config.get("project_id") or get_gcp_project()
+    if not project_id:
+        raise ValueError("Could not find project_id in gcp_config")
+    
+    #location = gcp_config.get('location')
+
+    de = DiscoveryEngineClient(
+                    data_store_id=config.vector_name, 
+                    project_id=project_id,
+                    # location needs to be 'eu' or 'us' which doesn't work with other configurations
+                    #location=location
+                    )
+
+    new_store = de.create_data_store(chunk_size=chunk_size)
+
+    return new_store
