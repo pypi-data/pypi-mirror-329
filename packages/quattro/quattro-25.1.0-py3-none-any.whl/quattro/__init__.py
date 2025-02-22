66ef4eb659900c6a7c8b373d3c8f0d4182003535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:58 +0000

    Add package quattro/25.1.0/quattro-25.1.0-py3-none-any.whl

---
+++ packages/quattro/quattro-25.1.0-py3-none-any.whl/quattro/__init__.py
@@ -0,0 +1,33 @@
+"""Task control for asyncio."""
+
+from __future__ import annotations
+
+from ._gather import gather
+from .cancelscope import (
+    CancelScope,
+    cancel_stack,
+    fail_after,
+    fail_at,
+    move_on_after,
+    move_on_at,
+)
+from .taskgroup import TaskGroup
+
+__all__ = [
+    "CancelScope",
+    "TaskGroup",
+    "fail_after",
+    "fail_at",
+    "gather",
+    "get_current_effective_deadline",
+    "move_on_after",
+    "move_on_at",
+    "test",
+]
+
+
+def get_current_effective_deadline() -> float:
+    return min(
+        [cs._deadline for cs in cancel_stack.get() if cs._deadline is not None],
+        default=float("inf"),
+    )
