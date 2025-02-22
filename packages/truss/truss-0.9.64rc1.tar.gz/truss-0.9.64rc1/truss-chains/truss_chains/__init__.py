45760ffbb601c70e43de378db5cce08f2ce19296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:04:31 +0000

    Add package truss/0.9.64rc1/truss-0.9.64rc1.tar.gz

---
+++ packages/truss/truss-0.9.64rc1.tar.gz/truss-0.9.64rc1/truss-chains/truss_chains/__init__.py
@@ -0,0 +1,71 @@
+import sys
+
+if (sys.version_info.major, sys.version_info.minor) <= (3, 8):
+    raise RuntimeError(
+        "Python version 3.8 or older is not supported for `Truss-Chains`. Please"
+        "upgrade to Python 3.9 or newer. You can still use other Truss functionality."
+    )
+del sys
+import pydantic
+
+pydantic_major_version = int(pydantic.VERSION.split(".")[0])
+if pydantic_major_version < 2:
+    raise RuntimeError(
+        f"Pydantic version {pydantic.VERSION} is not supported for Truss-Chains."
+        "Please upgrade to pydantic v2. With v1, you can still use all 'classical' "
+        "(non-Chains) Truss features."
+    )
+
+del pydantic, pydantic_major_version
+
+
+from truss_chains.definitions import (
+    Assets,
+    BasetenImage,
+    ChainletOptions,
+    Compute,
+    CustomImage,
+    DeployedServiceDescriptor,
+    DeploymentContext,
+    DockerImage,
+    GenericRemoteException,
+    RemoteConfig,
+    RemoteErrorDetail,
+    RPCOptions,
+)
+from truss_chains.framework import ChainletBase, ModelBase
+from truss_chains.public_api import (
+    depends,
+    depends_context,
+    mark_entrypoint,
+    push,
+    run_local,
+)
+
+# TODO: make this optional (remove aiohttp, httpx and starlette deps).
+from truss_chains.remote_chainlet.stub import StubBase
+from truss_chains.utils import make_abs_path_here
+
+__all__ = [
+    "Assets",
+    "BasetenImage",
+    "ChainletBase",
+    "ModelBase",
+    "ChainletOptions",
+    "Compute",
+    "CustomImage",
+    "DeploymentContext",
+    "DockerImage",
+    "RPCOptions",
+    "GenericRemoteException",
+    "RemoteConfig",
+    "RemoteErrorDetail",
+    "DeployedServiceDescriptor",
+    "StubBase",
+    "depends",
+    "depends_context",
+    "make_abs_path_here",
+    "mark_entrypoint",
+    "push",
+    "run_local",
+]
---
+++ packages/truss/truss-0.9.64rc1.tar.gz/truss-0.9.64rc1/truss/remote/baseten/custom_types.py
@@ -0,0 +1,42 @@
+import pathlib
+from enum import Enum
+from typing import Optional
+
+import pydantic
+
+
+class DeployedChainlet(pydantic.BaseModel):
+    name: str
+    is_entrypoint: bool
+    is_draft: bool
+    status: str
+    logs_url: str
+    oracle_name: str
+
+
+class ChainletArtifact(pydantic.BaseModel):
+    truss_dir: pathlib.Path
+    display_name: str
+    name: str
+
+
+class ModelOrigin(Enum):
+    BASETEN = "BASETEN"
+    CHAINS = "CHAINS"
+
+
+class OracleData(pydantic.BaseModel):
+    class Config:
+        protected_namespaces = ()
+
+    model_name: str
+    s3_key: str
+    encoded_config_str: str
+    semver_bump: Optional[str] = "MINOR"
+    version_name: Optional[str] = None
+
+
+# This corresponds to `ChainletInputAtomicGraphene` in the backend.
+class ChainletDataAtomic(pydantic.BaseModel):
+    name: str
+    oracle: OracleData
