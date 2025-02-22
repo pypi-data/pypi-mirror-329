eeb308ae545f4b6de8da04cb3bdb4ac88ae794b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:04:44 +0000

    Add package dm-aiomodbus/0.3.0/dm_aiomodbus-0.3.0-py3-none-any.whl

---
+++ packages/dm-aiomodbus/dm_aiomodbus-0.3.0-py3-none-any.whl/dm_aiomodbus/aiomodbus_tcp_client.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from dm_logger import DMLogger
+from pymodbus.client import AsyncModbusTcpClient
+
+from .aiomodbus_base_client import DMAioModbusBaseClient, DMAioModbusBaseClientConfig
+
+
+@dataclass
+class DMAioModbusTcpClientConfig():
+    host: str
+    port: int = 502
+    disconnect_timeout_s: int = 20
+
+
+class DMAioModbusTcpClient(DMAioModbusBaseClient):
+    def __init__(self, config: DMAioModbusTcpClientConfig):
+        super().__init__(
+            config=DMAioModbusBaseClientConfig(
+                modbus_client=AsyncModbusTcpClient(
+                    host=config.host,
+                    port=config.port
+                ),
+                disconnect_timeout_s=config.disconnect_timeout_s
+            )
+        )
+        self._logger = DMLogger(f"{self.__class__.__name__}-{config.host}:{config.port}")
