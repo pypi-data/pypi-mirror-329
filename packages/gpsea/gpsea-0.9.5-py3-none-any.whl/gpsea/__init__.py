101bad60a5f4c3c33adb726d88ad95aae9658fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:04:13 +0000

    Add package gpsea/0.9.5/gpsea-0.9.5-py3-none-any.whl

---
+++ packages/gpsea/gpsea-0.9.5-py3-none-any.whl/gpsea/__init__.py
@@ -0,0 +1,12 @@
+"""
+GPSEA is a library for finding genotype-phenotype associations.
+"""
+
+__version__ = "0.9.5"
+
+_overwrite = False
+"""
+A private "hack" flag for regenerating documentation output (HTML fragments, figures).
+"""
+
+_BASE_URL = "https://monarch-initiative.github.io/gpsea/stable"
