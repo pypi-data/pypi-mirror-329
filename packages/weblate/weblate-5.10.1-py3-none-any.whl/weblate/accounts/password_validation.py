9d0d039cf180e97a99f4f73d578617f27468fa84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:05:25 +0000

    Add package weblate/5.10.1/weblate-5.10.1-py3-none-any.whl

---
+++ packages/weblate/weblate-5.10.1-py3-none-any.whl/weblate/accounts/password_validation.py
@@ -0,0 +1,62 @@
+# Copyright © Michal Čihař <michal@weblate.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext
+
+from weblate.accounts.models import AuditLog
+
+
+class CharsPasswordValidator:
+    """Validate whether the password is not only whitespace or single char."""
+
+    def validate(self, password, user=None) -> None:
+        if not password:
+            return
+
+        if not password.strip():
+            raise ValidationError(
+                gettext("This password consists of only whitespace."),
+                code="password_whitespace",
+            )
+        if not password.strip(password[0]):
+            raise ValidationError(
+                gettext("This password is only a single character."),
+                code="password_same_chars",
+            )
+
+    def get_help_text(self):
+        return gettext(
+            "Your password can't consist of a single character or only whitespace."
+        )
+
+
+class PastPasswordsValidator:
+    """Validate whether the password was not used before."""
+
+    def validate(self, password, user=None) -> None:
+        if user is not None:
+            passwords = []
+            if user.has_usable_password():
+                passwords.append(user.password)
+
+            if user.pk:
+                passwords.extend(
+                    log.params["password"]
+                    for log in AuditLog.objects.get_past_passwords(user=user)
+                    if "password" in log.params
+                )
+
+            for old in passwords:
+                if check_password(password, old):
+                    raise ValidationError(
+                        gettext("Can not reuse previously used password."),
+                        code="password-past",
+                    )
+
+    def get_help_text(self):
+        return gettext(
+            "Your password can't match a password you have used in the past."
+        )
