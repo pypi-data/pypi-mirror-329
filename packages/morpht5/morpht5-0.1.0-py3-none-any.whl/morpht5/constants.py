a1680cc687621c413408d901096e51fa51362cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:49 +0000

    Add package morpht5/0.1.0/morpht5-0.1.0-py3-none-any.whl

---
+++ packages/morpht5/morpht5-0.1.0-py3-none-any.whl/morpht5/constants.py
@@ -0,0 +1,6 @@
+# Sentinel token used to separate words in the source corpus
+SOURCE_BLOCK_SEPARATOR = "<extra_id_0>"
+# Sentinel token used to separate tags in the source corpus in the case of tags-within-text encoding
+TAG_BLOCK_SEPARATOR = "<extra_id_1>"
+# Sentinel token used to separate words in the target corpus
+TARGET_BLOCK_SEPARATOR = "<extra_id_2>"
