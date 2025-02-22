21388a8ea68054852891799701526a8ddd580e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:03:54 +0000

    Add package Httppool/1.0.7/httppool-1.0.7.tar.gz

---
+++ packages/Httppool/httppool-1.0.7.tar.gz/httppool-1.0.7/src/Httppool/deamon.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process
+import Httppool
+
+PROCESS_NAME = 'httppool-deamon-2.py'
+
+class HttppoolDeamon(object):
+
+    def __init__(self, name=PROCESS_NAME):
+        self.process = Process(name=name, daemon=True, target=Httppool.run_producer_thread())
+
+    def start(self):
+        self.process.start()
+
+    def stop(self):
+        self.process.terminate()
+
+
+if __name__ == '__main__':
+    p = HttppoolDeamon()
+    p.start()
