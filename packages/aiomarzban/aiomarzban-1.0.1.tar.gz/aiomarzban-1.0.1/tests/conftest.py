3b01c55b055c8a2550ced5295ebc1c1554edf769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:04:04 +0000

    Add package aiomarzban/1.0.1/aiomarzban-1.0.1.tar.gz

---
+++ packages/aiomarzban/aiomarzban-1.0.1.tar.gz/aiomarzban-1.0.1/tests/conftest.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+import time
+
+import pytest
+from dotenv import load_dotenv
+
+from aiomarzban import MarzbanAPI
+from tests.final import delete_all_data
+
+
+@pytest.fixture(autouse=True, scope="function")
+def wait_after_test():
+    """
+    Timeout between requests to server.
+    """
+    yield
+    time.sleep(1)
+
+
+@pytest.fixture(scope="module")
+def get_api_client():
+    load_dotenv()
+
+    PROD =  os.getenv("PROD") == "FALSE"
+
+    client = MarzbanAPI(
+        address=os.getenv("MARZBAN_ADDRESS"),
+        username=os.getenv("MARZBAN_USERNAME"),
+        password=os.getenv("MARZBAN_PASSWORD"),
+    )
+
+    yield client
+    if not PROD:
+        asyncio.run(delete_all_data(client))
