0006ac4f38d2c99eb099ccf023a8bdfd4c46f7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:13 +0000

    Add package cfengine/0.1.0/cfengine-0.1.0.tar.gz

---
+++ packages/cfengine/cfengine-0.1.0.tar.gz/cfengine-0.1.0/cfengine_cli/utils.py
@@ -0,0 +1,8 @@
+class UserError(Exception):
+    """Exception raised when a user has most likely made a mistake"""
+
+    def __init__(self, message):
+        super().__init__(message)
+
+    def __str__(self):
+        return f"Error: {self.message}"
