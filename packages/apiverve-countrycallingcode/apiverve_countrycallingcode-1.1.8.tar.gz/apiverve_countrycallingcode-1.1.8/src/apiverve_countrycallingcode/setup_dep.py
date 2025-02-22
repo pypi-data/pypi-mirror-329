996688d8734e124a5970b29cb975d13950a1d270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:35 +0000

    Add package apiverve-countrycallingcode/1.1.8/apiverve_countrycallingcode-1.1.8.tar.gz

---
+++ packages/apiverve-countrycallingcode/apiverve_countrycallingcode-1.1.8.tar.gz/apiverve_countrycallingcode-1.1.8/src/apiverve_countrycallingcode/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_countrycallingcode',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Country Calling Code is a simple tool for getting the country's international dialing codes. It returns the country's international dialing codes.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
