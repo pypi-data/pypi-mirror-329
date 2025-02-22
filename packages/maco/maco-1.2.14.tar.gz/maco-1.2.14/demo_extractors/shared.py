323ffb7bdc5cdecdf8aad1cea832ff77fbaf54fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:27 +0000

    Add package maco/1.2.14/maco-1.2.14.tar.gz

---
+++ packages/maco/maco-1.2.14.tar.gz/maco-1.2.14/demo_extractors/shared.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+import pydantic
+
+from maco import model
+
+
+class MyCustomModel(model.ExtractorModel):
+    class Other(pydantic.BaseModel):
+        key1: str
+        key2: bool
+        key3: int
+
+    # set a custom class here as valid for the 'other' property
+    other: Optional[Other] = None
---
+++ packages/maco/maco-1.2.14.tar.gz/maco-1.2.14/tests/test_extractor.py
@@ -0,0 +1,60 @@
+import unittest
+
+from maco import extractor
+
+
+class TestExtractor(unittest.TestCase):
+    def test_bad(self):
+        class Tmp(extractor.Extractor):
+            family = "smell_ya_later"
+            author = "me"
+            last_modified = "yeah"
+
+        Tmp()
+
+        class Tmp1(Tmp):
+            family = None
+
+        self.assertRaises(extractor.InvalidExtractor, Tmp1)
+
+        class Tmp1(extractor.Extractor):
+            author = None
+
+        self.assertRaises(extractor.InvalidExtractor, Tmp1)
+
+        class Tmp1(extractor.Extractor):
+            version = None
+
+        self.assertRaises(extractor.InvalidExtractor, Tmp1)
+
+        class Tmp1(Tmp):
+            yara_rule: str = "t"
+
+        self.assertRaises(extractor.InvalidExtractor, Tmp1)
+
+        class Tmp1(Tmp):
+            yara_rule = """
+                rule DifferentName
+                {
+                    condition:
+                        true
+                }
+            """
+
+        Tmp1()
+
+        class Tmp1(Tmp):
+            yara_rule = """
+                rule Tmp1
+                {
+                    condition:
+                        true
+                }
+                rule OtherName
+                {
+                    condition:
+                        true
+                }
+            """
+
+        Tmp1()
