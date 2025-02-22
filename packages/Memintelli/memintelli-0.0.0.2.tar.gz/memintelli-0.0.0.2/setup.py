abe936d1cc6f7a4cee6241cf38f39c47d4333816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 16:04:55 +0000

    Add package Memintelli/0.0.0.2/memintelli-0.0.0.2.tar.gz

---
+++ packages/Memintelli/memintelli-0.0.0.2.tar.gz/memintelli-0.0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "Memintelli",
+    version = "0.0.0.2",
+    author = "Odysseia",
+    author_email = "1548384176@qq.com",
+    description = "Memristive Intelligient Computing Simulator",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    license = "MIT",
+    license_files = "LICENSE.txt",
+    url = "https://github.com/zzzzzzzzzzw/Memintelli",
+    zip_safe = False,
+    packages=find_packages(include=["pimpy", "NN_layers", "NN_models", "utils"]),
+    #install_requires = requirements,
+)
