6f60309fd8185b13d398a12b3b1f1c58b6df4264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:05:50 +0000

    Add package resonate-sdk/0.4.12/resonate_sdk-0.4.12.tar.gz

---
+++ packages/resonate-sdk/resonate_sdk-0.4.12.tar.gz/resonate_sdk-0.4.12/src/resonate/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from resonate.context import Context
+from resonate.dataclasses import DurablePromise
+from resonate.handle import Handle
+from resonate.promise import Promise
+from resonate.resonate import Resonate
+
+__all__ = ["Context", "DurablePromise", "Handle", "Promise", "Resonate"]
