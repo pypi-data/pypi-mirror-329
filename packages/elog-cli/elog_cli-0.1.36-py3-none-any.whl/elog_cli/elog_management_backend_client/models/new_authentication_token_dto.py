e53e6f470b78b19ff6537d4cf1c13bfe7d43a994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:56 +0000

    Add package elog-cli/0.1.36/elog_cli-0.1.36-py3-none-any.whl

---
+++ packages/elog-cli/elog_cli-0.1.36-py3-none-any.whl/elog_cli/elog_management_backend_client/models/new_authentication_token_dto.py
@@ -0,0 +1,77 @@
+import datetime
+from typing import Any, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+from dateutil.parser import isoparse
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="NewAuthenticationTokenDTO")
+
+
+@_attrs_define
+class NewAuthenticationTokenDTO:
+    """Is the new authentication token to be created
+
+    Attributes:
+        name (Union[Unset, str]):
+        expiration (Union[Unset, datetime.date]):
+    """
+
+    name: Union[Unset, str] = UNSET
+    expiration: Union[Unset, datetime.date] = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        name = self.name
+
+        expiration: Union[Unset, str] = UNSET
+        if not isinstance(self.expiration, Unset):
+            expiration = self.expiration.isoformat()
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if name is not UNSET:
+            field_dict["name"] = name
+        if expiration is not UNSET:
+            field_dict["expiration"] = expiration
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        d = src_dict.copy()
+        name = d.pop("name", UNSET)
+
+        _expiration = d.pop("expiration", UNSET)
+        expiration: Union[Unset, datetime.date]
+        if isinstance(_expiration, Unset):
+            expiration = UNSET
+        else:
+            expiration = isoparse(_expiration).date()
+
+        new_authentication_token_dto = cls(
+            name=name,
+            expiration=expiration,
+        )
+
+        new_authentication_token_dto.additional_properties = d
+        return new_authentication_token_dto
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
