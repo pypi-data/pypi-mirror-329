cb23e66f11d5f0a09e6ba00bdaa9ea6e7b3b20b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:05:04 +0000

    Add package m2c2-datakit/0.1.5/m2c2_datakit-0.1.5.tar.gz

---
+++ packages/m2c2-datakit/m2c2_datakit-0.1.5.tar.gz/m2c2_datakit-0.1.5/m2c2_datakit/core/config.py
@@ -0,0 +1,10 @@
+# Approach #1: Simple Constants
+package_version = "0.1.0"
+standard_grouping_for_aggregation = ["participant_id", "session_uuid", "session_id"]
+standard_grouping_for_aggregation_metricwire = [
+    "userId",
+    "submissionSessionId",
+    "activityId",
+]
+default_plot_color = "steelblue"
+default_plot_dpi = 150
