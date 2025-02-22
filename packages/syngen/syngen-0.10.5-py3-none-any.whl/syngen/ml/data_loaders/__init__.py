161fc8e8449439f5d2d0003789a02d73713db1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:05:29 +0000

    Add package syngen/0.10.5/syngen-0.10.5-py3-none-any.whl

---
+++ packages/syngen/syngen-0.10.5-py3-none-any.whl/syngen/ml/data_loaders/__init__.py
@@ -0,0 +1,11 @@
+from syngen.ml.data_loaders.data_loaders import (  # noqa: F401;
+    CSVLoader,
+    AvroLoader,
+    DataLoader,
+    MetadataLoader,
+    YAMLLoader,
+    BaseDataLoader,
+    BinaryLoader,
+    ExcelLoader,
+)
+from syngen.ml.data_loaders.dataframe_fetcher import DataFrameFetcher  # noqa: F401
