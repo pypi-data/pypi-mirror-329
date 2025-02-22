6cac1ca4fa08036fa6a6971c0b6ebeff57dba8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:04:19 +0000

    Add package marimo-base/0.11.8/marimo_base-0.11.8-py3-none-any.whl

---
+++ packages/marimo-base/marimo_base-0.11.8-py3-none-any.whl/marimo/_server/models/completion.py
@@ -0,0 +1,46 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Any, List, Literal, Optional
+
+from marimo._ai._types import ChatMessage
+
+
+@dataclass
+class SchemaColumn:
+    name: str
+    type: str
+    sample_values: List[Any]
+
+
+@dataclass
+class SchemaTable:
+    name: str
+    columns: List[SchemaColumn]
+
+
+@dataclass
+class AiCompletionContext:
+    schema: List[SchemaTable] = field(default_factory=list)
+
+
+Language = Literal["python", "markdown", "sql"]
+
+
+@dataclass
+class AiCompletionRequest:
+    prompt: str
+    include_other_code: str
+    code: str
+    context: Optional[AiCompletionContext] = None
+    language: Language = "python"
+
+
+@dataclass
+class ChatRequest:
+    context: AiCompletionContext
+    include_other_code: str
+    messages: List[ChatMessage]
+    model: Optional[str] = None
+    variables: Optional[List[str]] = None
---
+++ packages/marimo-base/marimo_base-0.11.8-py3-none-any.whl/marimo/_server/models/export.py
@@ -0,0 +1,28 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
+class ExportAsHTMLRequest:
+    download: bool
+    files: List[str]
+    include_code: bool
+    asset_url: Optional[str] = None
+
+
+@dataclass
+class ExportAsScriptRequest:
+    download: bool
+
+
+@dataclass
+class ExportAsIPYNBRequest:
+    download: bool
+
+
+@dataclass
+class ExportAsMarkdownRequest:
+    download: bool
