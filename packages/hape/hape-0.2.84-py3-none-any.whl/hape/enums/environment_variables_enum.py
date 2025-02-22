866c27dfd008adbaa891bacc148b5decb6d115dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:27 +0000

    Add package hape/0.2.84/hape-0.2.84-py3-none-any.whl

---
+++ packages/hape/hape-0.2.84-py3-none-any.whl/hape/enums/environment_variables_enum.py
@@ -0,0 +1,6 @@
+import enum
+
+class EnvironmentVariablesEnum(enum.Enum):
+    HAPE_LOG_FILE = "HAPE_LOG_FILE"
+    HAPE_LOG_LEVEL = "HAPE_LOG_LEVEL"
+    HAPE_LOG_ROTATE_EVERY_RUN = "HAPE_LOG_ROTATE_EVERY_RUN"
