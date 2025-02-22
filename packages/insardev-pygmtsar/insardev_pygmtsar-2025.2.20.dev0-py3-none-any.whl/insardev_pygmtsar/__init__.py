8048e65a560e9627853bbf6b0cc2943e0d20c60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:05:14 +0000

    Add package insardev-pygmtsar/2025.2.20.dev0/insardev_pygmtsar-2025.2.20.dev0-py3-none-any.whl

---
+++ packages/insardev-pygmtsar/insardev_pygmtsar-2025.2.20.dev0-py3-none-any.whl/insardev_pygmtsar/__init__.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------------------------
+# PyGMTSAR
+# 
+# This file is part of the PyGMTSAR project: https://github.com/mobigroup/gmtsar
+# 
+# Copyright (c) 2025, Alexey Pechnikov
+# 
+# Licensed under the BSD 3-Clause License (see LICENSE for details)
+# ----------------------------------------------------------------------------
+__version__ = '2025.2.20.dev'
+
+# radar parameters management
+from .PRM import PRM
+# Sentinel-1 processing
+from .S1 import S1
