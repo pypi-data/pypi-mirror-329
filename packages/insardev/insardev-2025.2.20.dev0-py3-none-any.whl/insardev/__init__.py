26a3a94a3373ad0e87093cc887c8409ed89c18b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:05:14 +0000

    Add package insardev/2025.2.20.dev0/insardev-2025.2.20.dev0-py3-none-any.whl

---
+++ packages/insardev/insardev-2025.2.20.dev0-py3-none-any.whl/insardev/__init__.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------------------------
+# InSAR.dev
+# 
+# This file is part of the InSAR.dev project: https://InSAR.dev
+# 
+# Copyright (c) 2025, Alexey Pechnikov
+# 
+# Licensed under the BSD 3-Clause License (see LICENSE for details)
+# ----------------------------------------------------------------------------
+__version__ = '2025.2.20.dev'
+
+# processing functions
+from .Stack import Stack
+# morphology and other helper functions
+from .utils import utils
