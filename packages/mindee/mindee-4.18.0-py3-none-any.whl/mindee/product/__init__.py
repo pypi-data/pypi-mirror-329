91ed98c20645d8538d2276d4f84e23c9b3e64618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:06:27 +0000

    Add package mindee/4.18.0/mindee-4.18.0-py3-none-any.whl

---
+++ packages/mindee/mindee-4.18.0-py3-none-any.whl/mindee/product/__init__.py
@@ -0,0 +1,60 @@
+from mindee.product import eu, fr, ind, us
+from mindee.product.barcode_reader import BarcodeReaderV1, BarcodeReaderV1Document
+from mindee.product.bill_of_lading import (
+    BillOfLadingV1,
+    BillOfLadingV1Carrier,
+    BillOfLadingV1CarrierItem,
+    BillOfLadingV1Consignee,
+    BillOfLadingV1Document,
+    BillOfLadingV1NotifyParty,
+    BillOfLadingV1Shipper,
+)
+from mindee.product.business_card import BusinessCardV1, BusinessCardV1Document
+from mindee.product.cropper import CropperV1, CropperV1Document
+from mindee.product.custom import CustomV1, CustomV1Document, CustomV1Page
+from mindee.product.delivery_note import DeliveryNoteV1, DeliveryNoteV1Document
+from mindee.product.driver_license import DriverLicenseV1, DriverLicenseV1Document
+from mindee.product.financial_document import (
+    FinancialDocumentV1,
+    FinancialDocumentV1Document,
+    FinancialDocumentV1LineItem,
+)
+from mindee.product.generated import GeneratedV1, GeneratedV1Document, GeneratedV1Page
+from mindee.product.international_id import InternationalIdV2, InternationalIdV2Document
+from mindee.product.invoice import InvoiceV4, InvoiceV4Document, InvoiceV4LineItem
+from mindee.product.invoice_splitter import InvoiceSplitterV1, InvoiceSplitterV1Document
+from mindee.product.material_certificate import (
+    MaterialCertificateV1,
+    MaterialCertificateV1Document,
+)
+from mindee.product.multi_receipts_detector import (
+    MultiReceiptsDetectorV1,
+    MultiReceiptsDetectorV1Document,
+)
+from mindee.product.nutrition_facts_label import (
+    NutritionFactsLabelV1,
+    NutritionFactsLabelV1AddedSugar,
+    NutritionFactsLabelV1Calorie,
+    NutritionFactsLabelV1Cholesterol,
+    NutritionFactsLabelV1DietaryFiber,
+    NutritionFactsLabelV1Document,
+    NutritionFactsLabelV1Nutrient,
+    NutritionFactsLabelV1Protein,
+    NutritionFactsLabelV1SaturatedFat,
+    NutritionFactsLabelV1ServingSize,
+    NutritionFactsLabelV1Sodium,
+    NutritionFactsLabelV1TotalCarbohydrate,
+    NutritionFactsLabelV1TotalFat,
+    NutritionFactsLabelV1TotalSugar,
+    NutritionFactsLabelV1TransFat,
+)
+from mindee.product.passport import PassportV1, PassportV1Document
+from mindee.product.proof_of_address import ProofOfAddressV1, ProofOfAddressV1Document
+from mindee.product.receipt import (
+    ReceiptV4,
+    ReceiptV4Document,
+    ReceiptV5,
+    ReceiptV5Document,
+    ReceiptV5LineItem,
+)
+from mindee.product.resume import ResumeV1, ResumeV1Document
