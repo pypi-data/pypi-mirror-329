a1fa1e07ee3ad8c61fef0a8f86b81f0961863cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:05:04 +0000

    Add package nix-playground/0.1.2/nix_playground-0.1.2.tar.gz

---
+++ packages/nix-playground/nix_playground-0.1.2.tar.gz/nix_playground-0.1.2/nix_playground/main.py
@@ -0,0 +1,9 @@
+from .build import main as build  # noqa
+from .checkout import main as checkout  # noqa
+from .cli import cli
+from .patch import main as patch  # noqa
+
+__ALL__ = [cli]
+
+if __name__ == "__main__":
+    cli()
