ede6da5307f536f18e011cc859f8930ea64d1238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 16:04:48 +0000

    Add package multitools-common/0.1.2/multitools-common-0.1.2.tar.gz

---
+++ packages/multitools-common/multitools-common-0.1.2.tar.gz/multitools-common-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multitools-common',
+    version='0.1.2',
+    packages=find_packages(),
+    package_data={
+        'tools': ['*.pyd', '*.so'],  # 只包含 .pyd 文件
+    },
+    install_requires=[
+        'ruamel.yaml'
+        # 'tkinter',  # tkinter 通常是内置的
+    ],
+    author='hubosoft',
+    author_email='hubosoft@hotmail.com',
+    description='Multi-tools: log, config, serial, regedit, etc.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/yourusername/your_project',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+# 1. 打包： python setup.py sdist bdist_wheel
