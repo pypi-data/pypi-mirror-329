7743e9f6fcb7cadcd9bae3638a9a9e33cbcd2935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:05:26 +0000

    Add package gudhi/3.11.0/gudhi-3.11.0-cp39-cp39-win_amd64.whl

---
+++ packages/gudhi/gudhi-3.11.0-cp39-cp39-win_amd64.whl/gudhi/__init__.py
@@ -0,0 +1,49 @@
+# This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
+#  See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
+#  Author(s):       Vincent Rouvreau
+#
+# Copyright (C) 2016  Inria
+#
+# Modification(s):
+#   - YYYY/MM Author: Description of the modification
+
+from importlib import import_module
+
+__author__ = "GUDHI Editorial Board"
+__copyright__ = "Copyright (C) 2016 Inria"
+__license__ = "https://gudhi.inria.fr/licensing/"
+__version__ = "3.11.0"
+# This variable is used by doctest to find files
+__root_source_dir__ = "D:/a/gudhi-devel/gudhi-devel"
+__debug_info__ =     "Python version 3.9.13\n" \
+    "Pybind11 version 2.13.6 \n" \
+    "Cython version 3.0.12 \n" \
+    "Numpy version 2.0.2 \n" \
+    "Eigen3 version 3.4.0\n" \
+    "Boost version 1.86.0\n" \
+    "CGAL header only version 6.0.1\n" \
+    "GMP_LIBRARIES = C:/vcpkg/installed/x64-windows/lib/gmp.lib\n" \
+    "GMPXX_LIBRARIES = C:/vcpkg/installed/x64-windows/lib/gmpxx.lib\n" \
+    "MPFR_LIBRARIES = C:/vcpkg/installed/x64-windows/lib/mpfr.lib\n" \
+
+
+__all__ = ['bottleneck', 'off_utils', 'simplex_tree', 'rips_complex', 'cubical_complex', 'periodic_cubical_complex', 'persistence_graphical_tools', 'reader_utils', 'witness_complex', 'strong_witness_complex', 'nerve_gic', 'subsampling', 'tangential_complex', 'delaunay_complex', 'euclidean_witness_complex', 'euclidean_strong_witness_complex',  'hera', 'clustering', 'datasets', 'representations', 'sklearn', 'tensorflow', 'wasserstein', 'point_cloud', 'weighted_rips_complex', 'dtm_rips_complex', 'cover_complex', 'flag_filtration', ]
+
+__available_modules = ''
+__missing_modules = ''
+
+# Try to import * from gudhi.__module_name for default modules.
+# Extra modules require an explicit import by the user (mostly because of
+# unusual dependencies, but also to avoid cluttering namespace gudhi and
+# speed up the basic import)
+for __module_name in ['bottleneck', 'off_utils', 'simplex_tree', 'rips_complex', 'cubical_complex', 'periodic_cubical_complex', 'persistence_graphical_tools', 'reader_utils', 'witness_complex', 'strong_witness_complex', 'nerve_gic', 'subsampling', 'tangential_complex', 'delaunay_complex', 'euclidean_witness_complex', 'euclidean_strong_witness_complex', ]:
+    try:
+        __module = import_module('gudhi.' + __module_name)
+        try:
+            __to_import = __module.__all__
+        except AttributeError:
+            __to_import = [name for name in __module.__dict__ if not name.startswith('_')]
+        globals().update({name: __module.__dict__[name] for name in __to_import})
+        __available_modules += __module_name + ";"
+    except:
+        __missing_modules += __module_name + ";"
