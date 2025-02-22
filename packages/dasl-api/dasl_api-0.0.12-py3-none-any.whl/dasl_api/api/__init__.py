cee601f071f78fb3a5fb6037f67a2d38d213708a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:03:57 +0000

    Add package dasl-api/0.0.12/dasl_api-0.0.12-py3-none-any.whl

---
+++ packages/dasl-api/dasl_api-0.0.12-py3-none-any.whl/dasl_api/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from dasl_api.api.content_v1alpha1_api import ContentV1alpha1Api
+from dasl_api.api.core_v1alpha1_api import CoreV1alpha1Api
+from dasl_api.api.dbui_v1alpha1_api import DbuiV1alpha1Api
+from dasl_api.api.workspace_v1alpha1_api import WorkspaceV1alpha1Api
+
