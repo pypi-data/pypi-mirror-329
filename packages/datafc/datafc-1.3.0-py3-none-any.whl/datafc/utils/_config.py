314f79370c529a2c1dea4cfc4ba40bbf54492802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 16:04:36 +0000

    Add package datafc/1.3.0/datafc-1.3.0-py3-none-any.whl

---
+++ packages/datafc/datafc-1.3.0-py3-none-any.whl/datafc/utils/_config.py
@@ -0,0 +1,8 @@
+ALLOWED_SOURCES = {"sofavpn", "sofascore"}
+
+API_BASE_URLS = {
+    "sofavpn": "https://api.sofavpn.com",
+    "sofascore": "https://api.sofascore.com",
+    "sofavpn2": "https://www.sofavpn.com",
+    "sofascore2": "https://www.sofascore.com"
+}
