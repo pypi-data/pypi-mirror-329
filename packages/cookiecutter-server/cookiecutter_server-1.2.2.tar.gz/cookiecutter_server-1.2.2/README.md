b26009f526f09b8518be710314dae2ce9340fce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:05:27 +0000

    Add package cookiecutter-server/1.2.2/cookiecutter_server-1.2.2.tar.gz

---
+++ packages/cookiecutter-server/cookiecutter_server-1.2.2.tar.gz/cookiecutter_server-1.2.2/README.md
@@ -0,0 +1,105 @@
+# cookiecutter-server
+
+[![build](https://img.shields.io/github/actions/workflow/status/at-gmbh/cookiecutter-server/build.yml?branch=master)](https://github.com/at-gmbh/at-python-template/actions?query=branch%3Amaster+)
+[![PyPI](https://img.shields.io/pypi/v/cookiecutter-server)](https://pypi.org/project/cookiecutter-server/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/cookiecutter-server)](https://pypi.org/project/cookiecutter-server/)
+[![PyPI - License](https://img.shields.io/pypi/l/personio-py)](https://github.com/at-gmbh/cookiecutter-server/blob/master/LICENSE)
+
+A local development server to get live previews of [cookiecutter templates](https://github.com/cookiecutter/cookiecutter).
+
+Are there too many code paths in your cookiecutter templates? You need an easy way to check what your template looks like with different parameters? cookiecutter-server got you covered! It's a process that watches the source folder of your template and serves the rendered template in a different folder. Whenever you make a change in your template or select different parameters, the rendered template gets updated automatically.
+
+## Getting Started
+
+Install cookiecutter-server with
+
+    pip install cookiecutter-server
+
+This will make the `cc_server` command available on your terminal. Test it with
+
+    cc_server --help
+
+In case the command was not registered, you can use `python -m cc_server` instead.
+
+Now switch to your cookiecutter template folder and run
+
+    cc_server .
+
+This will launch cookiecutter-server in the current folder and render the template in a new folder named `serve` under the current folder. Of course, you can also provide custom paths, if you prefer:
+
+    cc_server path/to/cookiecutter -o path/to/output
+
+The server process will now watch your template folder for changes. Please make a change in one of your template files and save the file. On the terminal, cc_server will tell you
+
+> updating due to change in {changed file}
+
+Now open this file in the output folder; your changes have already been applied. If you don't see the changes, please re-open the file or use an editor that automatically updates files when they change on disk, like PyCharm or VSCode.
+
+How do you change your template parameters? In your cookiecutter template folder, a new file named `cookiecutter-server.yml` was created, which contains all the parameters of your template. Change the parameters to your liking and save the file, cookiecutter-server will immediately update the contents of the output folder.
+
+To stop the server, use Ctrl+C on your terminal.
+
+If you're having trouble with cookiecutter-server, please have a look at the [open issues](https://github.com/at-gmbh/cookiecutter-server/issues) and open a new one, if you can't find a solution.
+
+## Contributing
+
+To set up your local development environment, please use Poetry:
+
+    # Install Poetry (if not already installed)
+    curl -sSL https://install.python-poetry.org | python3 -
+    
+    # Install dependencies
+    poetry install --with dev
+    
+    # Activate the virtual environment
+    poetry shell
+    
+    # Launch the server
+    cc_server --help`
+
+We use `pytest` as the test framework. To execute the tests, please run:
+
+    poetry run pytest
+To build a distribution package (wheel), please use:
+
+    poetry build
+Before contributing code, please set up the `pre-commit` hooks to reduce errors and ensure consistency:
+
+    poetry run pre-commit install
+
+
+
+### PyPI Release
+
+This project is released on [PyPI](https://pypi.org/project/cookiecutter-server/). Most of the tedious steps that are required to test & publish your release are automated by [CI pipelines](https://github.com/at-gmbh/cookiecutter-server/actions). All you have to do is to write your code and when the time comes to make a release, please follow these steps:
+
+* Update the program version in pyproject.toml [`pyproject.toml`](pyproject.toml)
+* write a summary of your changes in [`CHANGELOG.md`](./CHANGELOG.md)
+* add a tag on the master branch with the new version number preceded by the letter `v`, e.g. for version 1.0.0 the tag would be `v1.0.0`. To tag the head of the current branch, use `git tag v1.0.0`
+* push your changes to GitHub and don't forget to push the tag with `git push origin v1.0.0`
+* now have a look at the [release pipeline](https://github.com/at-gmbh/cookiecutter-server/actions/workflows/release.yml); if it finishes without errors, you can find your release on [TestPyPI](https://test.pypi.org/project/cookiecutter-server/). Please verify that your release works as expected.
+* Now for the live deployment on PyPI. To avoid mistakes, this is only triggered, when a release is published on GitHub first. Please have a look at the [Releases](https://github.com/at-gmbh/cookiecutter-server/releases) now; there should be a draft release with your version number (this was created by the CI pipeline which also made the TestPyPI release). Edit the draft release, copy the text you added to [`CHANGELOG.md`](./CHANGELOG.md) into the description field and publish it.
+* After you publish the release, the [deploy pipeline](https://github.com/at-gmbh/cookiecutter-server/actions/workflows/deploy.yml) is triggered on GitHub. It will publish the release directly to [PyPI](https://pypi.org/project/cookiecutter-server/) where everyone can enjoy your latest features.
+
+## Contact
+
+Sebastian Straub (sebastian.straub [at] alexanderthamm.com)
+Christian Baur (christian.baur [at] alexanderthamm.com)
+
+Developed with ❤ at [Alexander Thamm GmbH](https://www.alexanderthamm.com/)
+
+## License
+
+    Copyright 2025 Alexander Thamm GmbH
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ packages/cookiecutter-server/cookiecutter_server-1.2.2.tar.gz/cookiecutter_server-1.2.2/src/cc_server/main.py
@@ -0,0 +1,255 @@
+import json
+import logging
+import os
+import time
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Dict, Optional, Union
+
+import typer
+import yaml
+from cookiecutter.main import cookiecutter
+from dirsync import sync
+from rich import print
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+from yaml import FullLoader
+
+from cc_server import __title__, __version__
+
+logging.basicConfig(format="%(asctime)s.%(msecs)03d [%(levelname)s]: %(message)s",
+                    level=logging.WARNING)
+logger = logging.getLogger('cc_server')
+logger.setLevel(logging.WARNING)
+
+
+app = typer.Typer(
+    name='cc_server',
+    help="A local development server to get live previews of cookiecutter templates")
+
+
+def version_callback(version: bool):
+    if version:
+        print(f"{__title__} {__version__}")
+        raise typer.Exit()
+
+
+TemplatePath = typer.Argument(
+    ...,
+    help="cookiecutter template source folder",
+    exists=True, file_okay=False, dir_okay=True, readable=True, resolve_path=True)
+OutputPath = typer.Option(
+    './serve/', '-o', '--output',
+    help="output folder (defaults to folder 'serve' under the current folder)",
+    exists=False, file_okay=False, dir_okay=True, resolve_path=True)
+Version = typer.Option(
+    None, '-v', '--version', callback=version_callback, is_eager=True,
+    help="print the program version and exit")
+
+
+@app.command()
+def main(template: Path = TemplatePath, output: Path = OutputPath, version: bool = Version):
+    """
+    A local development server to get live previews of cookiecutter templates
+    """
+    print(
+        f"starting [yellow]cookiecutter-server[/yellow] "
+        f"(template: '{template}', output: '{output}')")
+    server = CookiecutterServer(
+        template_dir=template,
+        output_dir=output)
+    server.serve()
+
+
+class CookiecutterServer:
+    """
+    cookiecutter server: get live previews of cookiecutter templates while you're working on them.
+
+    Creates an instance of the cookiecutter template in `template_dir` and writes it
+    to `output_dir`. The settings for the template are specified by `config_file`, if provided,
+    otherwise a file named `cookiecutter-server.yml` is generated in the template folder.
+
+    Cookiecutter server is watching for changes in the template server. When a file is changed,
+    the template will be rendered and all changed files will be updated in the output directory.
+    The same applies for the config file: when you change a config parameter, the rendered
+    template will be updated as well.
+
+    :param template_dir: the folder where the cookiecutter template is located
+           (must contain a cookiecutter.json or cookiecutter.yml)
+    :param output_dir: the folder where the live preview shall be served
+    :param config_file: settings for the cookiecutter template (default settings will be stored
+           in cookiecutter-server.yml, when this file is not provided)
+    :param min_delay: wait at least this many seconds, before the template will be rendered again
+           (prevents high disk & cpu load when changes are detected in quick succession)
+    """
+
+    default_config = 'cookiecutter-server.yml'
+    template_files = [
+        ('cookiecutter.json', json),
+        ('cookiecutter.yml', yaml),
+        ('cookiecutter.yaml', yaml),
+    ]
+
+    def __init__(self, template_dir: Path, output_dir: Path, config_file: Path = None,
+                 min_delay=5.0):
+        self.template_dir = template_dir.resolve()
+        self.output_dir = output_dir
+        self.config_file = self._init_config(template_dir, config_file)
+        self.observer = Observer()
+        self.handler = TemplateUpdate(
+            self.template_dir, self.output_dir, self.config_file, min_delay)
+        self.signal_stop = False
+
+    def serve(self):
+        # render the template for the first time, if the output directory does not exist
+        if not self.output_dir.is_dir():
+            self.handler.render_template(self.output_dir)
+        # start the watchdog
+        self.observer.schedule(self.handler, self.template_dir, recursive=True)
+        self.observer.start()
+        print("template is ready, watching for changes")
+        try:
+            while not self.signal_stop:
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.observer.stop()
+            self.observer.join(1.0)
+        print("[yellow]cookiecutter-server[/yellow] terminated")
+
+    @classmethod
+    def _init_config(cls, template_dir: Path, config_file: Path = None):
+        # check if a config file is defined
+        if config_file is None:
+            config_file = template_dir / cls.default_config
+        # validate the existing config or generate a new one
+        if config_file.is_file():
+            with config_file.open() as fp:
+                yaml.load(fp, Loader=FullLoader)
+        else:
+            cls._fill_config(template_dir, config_file)
+        logger.info(f"your template config is at {config_file}")
+        return config_file
+
+    @classmethod
+    def _fill_config(cls, template_dir: Path, config_file: Path):
+        for fname, parser in cls.template_files:
+            path = template_dir / fname
+            if path.exists():
+                with path.open('r') as fp:
+                    cc_config = parser.load(fp)
+                ccs_config = {k: v[0] if isinstance(v, list) else v for k, v in cc_config.items()}
+                with config_file.open('w') as fp:
+                    yaml.dump(ccs_config, fp, sort_keys=False)
+                return config_file
+        raise RuntimeError(f"No cookiecutter.json found in {template_dir}")
+
+
+class TemplateUpdate(FileSystemEventHandler):
+    """
+    ...
+
+    :param template_dir: the folder where the cookiecutter template is located
+    :param output_dir: the folder where the live preview shall be served
+    :param config_file: settings for the cookiecutter template
+    :param min_delay: wait at least this many seconds, before the template will be rendered again
+    """
+
+    def __init__(self, template_dir: Path, output_dir: Path, config_file: Path, min_delay=5.0):
+        super().__init__()
+        self.template_dir = template_dir
+        self.output_dir = output_dir
+        self.config_file = config_file
+        self.last_sync = time.time()
+        self.min_delay = min_delay
+        self.settings: Optional[Dict] = None
+
+    def on_any_event(self, event: FileSystemEvent):
+        path = Path(event.src_path)
+        if self.is_change_relevant(path):
+            logger.debug(f"change detected: {event}")
+            now = time.time()
+            # don't update too frequently
+            if now - self.last_sync > self.min_delay:
+                try:
+                    # Use relative_to if possible; fallback to relpath for cross-platform compatibility
+                    try:
+                        rel_path = path.relative_to(self.template_dir)
+                    except ValueError:
+                        rel_path = os.path.relpath(path.resolve(), self.template_dir)
+
+                    if str(rel_path).startswith('..'):
+                        logger.debug(f"Path {path} is outside the template directory")
+                        return
+
+                    print(f"[green]updating[/green] due to change in '{rel_path}'")
+                    self.settings = self.read_config()
+                    self.sync_output()
+                    self.last_sync = now
+                except ValueError as e:
+                    logger.debug(f"Skipping path due to error: {e}")
+
+    def is_change_relevant(self, path: Path) -> bool:
+        logger.debug(f"Checking relevance for path: {path}")
+
+        # Ignore temporary files
+        if path.name.endswith('~'):
+            logger.debug("ignoring temporary files (ending with ~)")
+            return False
+
+        # Ignore changes in the output directory
+        if self.path_is_relative_to(path, self.output_dir):
+            logger.debug("ignoring changes in the output directory")
+            return False
+
+        try:
+            # Use relative_to if possible; fallback to relpath for cross-platform compatibility
+            try:
+                rel_path = path.relative_to(self.template_dir)
+            except ValueError:
+                rel_path = os.path.relpath(path.resolve(), self.template_dir)
+
+            # Ignore dot-files in the template's root folder
+            if len(Path(rel_path).parts) > 0 and Path(rel_path).parts[0].startswith('.'):
+                logger.debug("ignoring changes to dot-files in the template's root folder")
+                return False
+
+        except ValueError as e:
+            logger.debug(f"Skipping path due to error: {e}")
+            return False
+        return True
+
+    def read_config(self) -> Optional[Dict]:
+        if self.config_file.is_file():
+            with self.config_file.open('r') as fp:
+                return yaml.load(fp, Loader=FullLoader)
+        else:
+            logger.warning(f"config file {self.config_file} was not found")
+
+    def sync_output(self):
+        logger.debug("Sync output triggered")
+        with TemporaryDirectory() as temp_dir:
+            self.render_template(temp_dir)
+            sync(temp_dir, self.output_dir, 'sync', content=True, purge=True,
+                 ignore=['.git'], logger=logger)
+        logger.debug("file sync complete")
+
+    def render_template(self, output_dir: Union[str, Path]):
+        cookiecutter(
+            str(self.template_dir),
+            extra_context=self.settings,
+            no_input=True,
+            output_dir=str(output_dir))
+
+    @staticmethod
+    def path_is_relative_to(path: Path, other: Path) -> bool:
+        try:
+            path.relative_to(other)
+            return True
+        except ValueError:
+            return False
+
+
+if __name__ == "__main__":
+    app()
