8486530d4ffccbb8f9dee360f758f62078f94552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:04:26 +0000

    Add package pubmed-downloader/0.0.3/pubmed_downloader-0.0.3.tar.gz

---
+++ packages/pubmed-downloader/pubmed_downloader-0.0.3.tar.gz/pubmed_downloader-0.0.3/src/pubmed_downloader/__init__.py
@@ -0,0 +1,43 @@
+"""Automate downloading and processing PubMed."""
+
+from .api import (
+    ISSN,
+    AbstractText,
+    Article,
+    Author,
+    Heading,
+    Journal,
+    Qualifier,
+    ensure_baselines,
+    ensure_updates,
+    iterate_ensure_articles,
+    iterate_ensure_baselines,
+    iterate_ensure_updates,
+    iterate_process_articles,
+    iterate_process_baselines,
+    iterate_process_updates,
+    process_articles,
+    process_baselines,
+    process_updates,
+)
+
+__all__ = [
+    "ISSN",
+    "AbstractText",
+    "Article",
+    "Author",
+    "Heading",
+    "Journal",
+    "Qualifier",
+    "ensure_baselines",
+    "ensure_updates",
+    "iterate_ensure_articles",
+    "iterate_ensure_baselines",
+    "iterate_ensure_updates",
+    "iterate_process_articles",
+    "iterate_process_baselines",
+    "iterate_process_updates",
+    "process_articles",
+    "process_baselines",
+    "process_updates",
+]
