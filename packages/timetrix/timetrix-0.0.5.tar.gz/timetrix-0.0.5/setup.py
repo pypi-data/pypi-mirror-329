d7a1755f8e3b6fe9132884f0509b79c88a7d1c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:39 +0000

    Add package timetrix/0.0.5/timetrix-0.0.5.tar.gz

---
+++ packages/timetrix/timetrix-0.0.5.tar.gz/timetrix-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="timetrix",
+    version="0.0.5",
+    entry_points={
+        "console_scripts": [
+            "timetrix=timetrix.cli:main",
+        ],
+    },
+    author="Maglovski Nenad",
+    author_email="maglovskin@gmail.com",
+    description="Lightweight and intuitive Python library designed to help developers,track, measure, and visualize time with ease.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/maglovskiNenad/timetrix",
+    packages=find_packages(),
+    install_requires=[
+        "datetime",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
