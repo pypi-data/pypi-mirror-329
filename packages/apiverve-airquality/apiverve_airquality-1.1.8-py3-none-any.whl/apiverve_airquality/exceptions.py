d05feb219619b5fcd976a3e16bf73f061b053492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:53 +0000

    Add package apiverve-airquality/1.1.8/apiverve_airquality-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-airquality/apiverve_airquality-1.1.8-py3-none-any.whl/apiverve_airquality/exceptions.py
@@ -0,0 +1,3 @@
+class AirqualityAirQualityAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-airquality/apiverve_airquality-1.1.8-py3-none-any.whl/apiverve_airquality/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_airquality',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Air Quality is a simple tool for getting air quality data. It returns the air quality index, o3, pm2, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
