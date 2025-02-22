713554f1855fa11e2e05baa0b3bc0b216fc1b9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:05:08 +0000

    Add package open-deep-research/0.0.5/open_deep_research-0.0.5-py3-none-any.whl

---
+++ packages/open-deep-research/open_deep_research-0.0.5-py3-none-any.whl/open_deep_research/configuration.py
@@ -0,0 +1,59 @@
+import os
+from enum import Enum
+from dataclasses import dataclass, fields
+from typing import Any, Optional
+
+from langchain_core.runnables import RunnableConfig
+from dataclasses import dataclass
+
+DEFAULT_REPORT_STRUCTURE = """Use this structure to create a report on the user-provided topic:
+
+1. Introduction (no research needed)
+   - Brief overview of the topic area
+
+2. Main Body Sections:
+   - Each section should focus on a sub-topic of the user-provided topic
+   
+3. Conclusion
+   - Aim for 1 structural element (either a list of table) that distills the main body sections 
+   - Provide a concise summary of the report"""
+
+class SearchAPI(Enum):
+    PERPLEXITY = "perplexity"
+    TAVILY = "tavily"
+
+class PlannerProvider(Enum):
+    OPENAI = "openai"
+    GROQ = "groq"
+
+class WriterProvider(Enum):
+    ANTHROPIC = "anthropic"
+    OPENAI = "openai"
+    GROQ = "groq"
+
+@dataclass(kw_only=True)
+class Configuration:
+    """The configurable fields for the chatbot."""
+    report_structure: str = DEFAULT_REPORT_STRUCTURE # Defaults to the default report structure
+    number_of_queries: int = 2 # Number of search queries to generate per iteration
+    max_search_depth: int = 2 # Maximum number of reflection + search iterations
+    planner_provider: PlannerProvider = PlannerProvider.OPENAI  # Defaults to OpenAI as provider
+    planner_model: str = "o3-mini" # Defaults to OpenAI o3-mini as planner model
+    writer_provider: WriterProvider = WriterProvider.ANTHROPIC # Defaults to Anthropic as provider
+    writer_model: str = "claude-3-5-sonnet-latest" # Defaults to Anthropic as provider
+    search_api: SearchAPI = SearchAPI.TAVILY # Default to TAVILY
+
+    @classmethod
+    def from_runnable_config(
+        cls, config: Optional[RunnableConfig] = None
+    ) -> "Configuration":
+        """Create a Configuration instance from a RunnableConfig."""
+        configurable = (
+            config["configurable"] if config and "configurable" in config else {}
+        )
+        values: dict[str, Any] = {
+            f.name: os.environ.get(f.name.upper(), configurable.get(f.name))
+            for f in fields(cls)
+            if f.init
+        }
+        return cls(**{k: v for k, v in values.items() if v})
