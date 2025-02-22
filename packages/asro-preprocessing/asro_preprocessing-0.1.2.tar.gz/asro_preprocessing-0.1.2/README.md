0d6a9669ac3fdc454a7993fcf6a691ef8e34d664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:28 +0000

    Add package asro-preprocessing/0.1.2/asro_preprocessing-0.1.2.tar.gz

---
+++ packages/asro-preprocessing/asro_preprocessing-0.1.2.tar.gz/asro_preprocessing-0.1.2/README.md
@@ -0,0 +1,20 @@
+# Asro Preprocessing
+
+Library untuk preprocessing teks bahasa Indonesia, mencakup:
+
+- Case folding (mengubah teks menjadi huruf kecil).
+- Pembersihan teks dari URL, tanda baca, angka, dan lainnya.
+- Tokenisasi teks menjadi daftar kata.
+- Penghapusan stopwords bawaan library.
+- Normalisasi kata tidak baku ke kata baku dengan kamus bawaan.
+- Penghapusan duplikat berdasarkan teks yang telah diproses.
+- Deteksi sumber teks, membedakan apakah dari media atau individu, dengan menggunakan daftar nama media bawaan.
+
+
+## Instalasi
+
+Instal library melalui PyPI dengan versi yang spesifik:
+
+```bash
+pip install asro_preprocessing==0.1.2
+```
---
+++ packages/asro-preprocessing/asro_preprocessing-0.1.2.tar.gz/asro_preprocessing-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asro_preprocessing",  # Nama library
+    version="0.1.2",  # Versi library yang diperbarui
+    description="Library untuk preprocessing teks bahasa Indonesia.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Asro",
+    author_email="asro@raharja.info",
+    url="https://github.com/asroharun6/asro_preprocessing",  # URL GitHub Anda
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "asro_preprocessing": [
+            "data/stopwords.txt",
+            "data/kamuskatabaku.xlsx",
+            "data/news_dictionary.txt"  # Tambahkan path file baru
+        ],
+    },
+    install_requires=[
+        "pandas",
+        "nltk",
+        "openpyxl"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
