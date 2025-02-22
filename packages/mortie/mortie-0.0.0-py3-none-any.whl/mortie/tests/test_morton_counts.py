2dc45dc0538bc5fed515c5fb31a1b13c7d726def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:03:38 +0000

    Add package mortie/0.0.0/mortie-0.0.0-py3-none-any.whl

---
+++ packages/mortie/mortie-0.0.0-py3-none-any.whl/mortie/tests/test_morton_counts.py
@@ -0,0 +1,10 @@
+import pytest
+import numpy as np
+import pandas as pd
+import healpy as hp
+
+def test_healpy_install():
+    nside = 2**6
+    b2idx = hp.ang2pix(nside,132.85, 77.32, lonlat=True) 
+    assert type(b2idx) == np.int64
+
