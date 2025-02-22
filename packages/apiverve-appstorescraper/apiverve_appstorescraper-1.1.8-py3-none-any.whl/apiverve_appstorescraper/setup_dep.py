7669fa7562b4a0ab1a31ae5f951ed42b5a783efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:54 +0000

    Add package apiverve-appstorescraper/1.1.8/apiverve_appstorescraper-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-appstorescraper/apiverve_appstorescraper-1.1.8-py3-none-any.whl/apiverve_appstorescraper/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_appstorescraper',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='App Store Scraper is a simple tool for scraping app store data. It returns the app name, description, price, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
