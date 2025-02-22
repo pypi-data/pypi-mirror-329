aed6e51e7cd9cbc083b36fb00da6d922bfcb43b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:05:47 +0000

    Add package DataConnect/0.2.3/DataConnect-0.2.3.tar.gz

---
+++ packages/DataConnect/DataConnect-0.2.3.tar.gz/DataConnect-0.2.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DataConnect",            # Your package name
+    version="0.2.3",              # Initial version
+    description="This package contains connectors for databricks and sql server as well as setting up a flask environment project",  # Description of the package
+    long_description=open("README.md").read(),  # Read from README.md
+    long_description_content_type="text/markdown",  # Optional, specifies markdown format
+    author="Ben Fuqua",          # Author's name
+    author_email="fuquac@slhs.org",  # Author's email
+    packages=find_packages(),    # Automatically find all packages in the directory
+    classifiers=[                # Classifiers to categorize the package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[           # List any dependencies
+                        "numpy",
+                        "Flask",
+                        "SQLAlchemy",
+                        "WTForms",
+                        "flask-wtf",
+                        "wtforms_sqlalchemy",
+                        "flask_sqlalchemy",
+                        "databricks-sql-connector[sqlalchemy]",
+                        "pyodbc",                    # Example dependency,
+                        "ldap3"
+    ],
+    python_requires=">=3.9, <=3.10",     # Minimum Python version
+)
