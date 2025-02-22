f0d0f8ce908fd79b33c1d04e2f6ba34deca39cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:39 +0000

    Add package taizmath/0.1/taizmath-0.1.tar.gz

---
+++ packages/taizmath/taizmath-0.1.tar.gz/taizmath-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="taizmath",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # No dependencies
+    author="Taizun",
+    description="An easy-to-use symbolic math library",
+    url="https://github.com/t4zn/taizmath",
+    license="MIT",
+)
