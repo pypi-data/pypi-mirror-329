09ffa606d71147a7b6a71cf3e1696181446dd79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:03:46 +0000

    Add package compute-horde/0.0.23/compute_horde-0.0.23.tar.gz

---
+++ packages/compute-horde/compute_horde-0.0.23.tar.gz/compute_horde-0.0.23/compute_horde/fv_protocol/facilitator_requests.py
@@ -0,0 +1,227 @@
+import base64
+import typing
+from typing import Annotated, Literal, Self
+
+import pydantic
+from pydantic import (
+    BaseModel,
+    JsonValue,
+    field_serializer,
+    field_validator,
+    model_validator,
+)
+
+from compute_horde.base.output_upload import MultiUpload, OutputUpload, ZipAndHttpPutUpload
+from compute_horde.base.volume import MultiVolume, Volume, ZipUrlVolume
+from compute_horde.executor_class import ExecutorClass
+
+
+class Error(BaseModel, extra="allow"):
+    msg: str
+    type: str
+    help: str = ""
+
+
+class Response(BaseModel, extra="forbid"):
+    """Message sent from facilitator to validator in response to AuthenticationRequest & JobStatusUpdate"""
+
+    status: Literal["error", "success"]
+    errors: list[Error] = []
+
+
+class Signature(BaseModel, extra="forbid"):
+    # has defaults to allow easy instantiation
+    signature_type: str = ""
+    signatory: str = (
+        ""  # identity of the signer (e.g. sa58 address if signature_type == "bittensor")
+    )
+    timestamp_ns: int = 0  # UNIX timestamp in nanoseconds
+    signature: bytes
+
+    @field_validator("signature")
+    @classmethod
+    def validate_signature(cls, signature: str) -> bytes:
+        return base64.b64decode(signature)
+
+    @field_serializer("signature")
+    def serialize_signature(self, signature: bytes) -> str:
+        return base64.b64encode(signature).decode("utf-8")
+
+
+class V0JobRequest(BaseModel, extra="forbid"):
+    """Message sent from facilitator to validator to request a job execution"""
+
+    # this points to a `ValidatorConsumer.job_new` handler (fuck you django-channels!)
+    type: Literal["job.new"] = "job.new"
+    message_type: Literal["V0JobRequest"] = "V0JobRequest"
+
+    uuid: str
+    miner_hotkey: str
+    executor_class: ExecutorClass
+    docker_image: str
+    raw_script: str
+    args: list[str]
+    env: dict[str, str]
+    use_gpu: bool
+    input_url: str
+    output_url: str
+
+    def get_args(self):
+        return self.args
+
+    @model_validator(mode="after")
+    def validate_at_least_docker_image_or_raw_script(self) -> Self:
+        if not (bool(self.docker_image) or bool(self.raw_script)):
+            raise ValueError("Expected at least one of `docker_image` or `raw_script`")
+        return self
+
+    @property
+    def volume(self) -> Volume | None:
+        if self.input_url:
+            return ZipUrlVolume(contents=self.input_url)
+        return None
+
+    @property
+    def output_upload(self) -> OutputUpload | None:
+        if self.output_url:
+            return ZipAndHttpPutUpload(url=self.output_url)
+        return None
+
+
+class V1JobRequest(BaseModel, extra="forbid"):
+    """Message sent from facilitator to validator to request a job execution"""
+
+    # this points to a `ValidatorConsumer.job_new` handler (fuck you django-channels!)
+    type: Literal["job.new"] = "job.new"
+    message_type: Literal["V1JobRequest"] = "V1JobRequest"
+    uuid: str
+    miner_hotkey: str
+    executor_class: ExecutorClass
+    docker_image: str
+    raw_script: str
+    args: list[str]
+    env: dict[str, str]
+    use_gpu: bool
+    volume: Volume | None = None
+    output_upload: OutputUpload | None = None
+
+    def get_args(self):
+        return self.args
+
+    @model_validator(mode="after")
+    def validate_at_least_docker_image_or_raw_script(self) -> Self:
+        if not (bool(self.docker_image) or bool(self.raw_script)):
+            raise ValueError("Expected at least one of `docker_image` or `raw_script`")
+        return self
+
+
+class SignedFields(BaseModel):
+    executor_class: str
+    docker_image: str
+    raw_script: str
+    args: list[str]
+    env: dict[str, str]
+    use_gpu: bool
+    artifacts_dir: str
+
+    volumes: list[JsonValue]
+    uploads: list[JsonValue]
+
+    @staticmethod
+    def from_facilitator_sdk_json(data: JsonValue):
+        data = typing.cast(dict[str, JsonValue], data)
+
+        signed_fields = SignedFields(
+            executor_class=str(data.get("executor_class")),
+            docker_image=str(data.get("docker_image", "")),
+            raw_script=str(data.get("raw_script", "")),
+            args=typing.cast(list[str], data.get("args", [])),
+            env=typing.cast(dict[str, str], data.get("env", None)),
+            use_gpu=typing.cast(bool, data.get("use_gpu")),
+            artifacts_dir=str(data.get("artifacts_dir", "")),
+            volumes=typing.cast(list[JsonValue], data.get("volumes", [])),
+            uploads=typing.cast(list[JsonValue], data.get("uploads", [])),
+        )
+        return signed_fields
+
+
+def to_json_array(data) -> list[JsonValue]:
+    return typing.cast(list[JsonValue], [x.model_dump() for x in data])
+
+
+class V2JobRequest(BaseModel, extra="forbid"):
+    """Message sent from facilitator to validator to request a job execution"""
+
+    # this points to a `ValidatorConsumer.job_new` handler (fuck you django-channels!)
+    type: Literal["job.new"] = "job.new"
+    message_type: Literal["V2JobRequest"] = "V2JobRequest"
+    signature: Signature | None = None
+
+    uuid: str
+
+    # !!! all fields below are included in the signed json payload
+    executor_class: ExecutorClass
+    docker_image: str
+    raw_script: str
+    args: list[str]
+    env: dict[str, str]
+    use_gpu: bool
+    volume: Volume | None = None
+    output_upload: OutputUpload | None = None
+    artifacts_dir: str | None = None
+    # !!! all fields above are included in the signed json payload
+
+    def get_args(self):
+        return self.args
+
+    def get_signed_fields(self) -> SignedFields:
+        volumes = (
+            to_json_array(
+                self.volume.volumes if isinstance(self.volume, MultiVolume) else [self.volume]
+            )
+            if self.volume
+            else []
+        )
+
+        uploads = (
+            to_json_array(
+                self.output_upload.uploads
+                if isinstance(self.output_upload, MultiUpload)
+                # TODO: fix consolidate faci output_upload types
+                else [self.output_upload]  # type: ignore
+            )
+            if self.output_upload
+            else []
+        )
+
+        signed_fields = SignedFields(
+            executor_class=self.executor_class,
+            docker_image=self.docker_image,
+            raw_script=self.raw_script,
+            args=self.args,
+            env=self.env,
+            use_gpu=self.use_gpu,
+            artifacts_dir=self.artifacts_dir or "",
+            volumes=volumes,
+            uploads=uploads,
+        )
+        return signed_fields
+
+    def json_for_signing(self) -> JsonValue:
+        payload = self.model_dump(mode="json")
+        del payload["type"]
+        del payload["message_type"]
+        del payload["signature"]
+        return payload
+
+    @model_validator(mode="after")
+    def validate_at_least_docker_image_or_raw_script(self) -> Self:
+        if not (bool(self.docker_image) or bool(self.raw_script)):
+            raise ValueError("Expected at least one of `docker_image` or `raw_script`")
+        return self
+
+
+JobRequest = Annotated[
+    V0JobRequest | V1JobRequest | V2JobRequest,
+    pydantic.Field(discriminator="message_type"),
+]
---
+++ packages/compute-horde/compute_horde-0.0.23.tar.gz/compute_horde-0.0.23/compute_horde/utils.py
@@ -0,0 +1,93 @@
+import datetime
+from typing import Any
+
+import bittensor
+import pydantic
+from bittensor.core.errors import SubstrateRequestException
+
+BAC_VALIDATOR_SS58_ADDRESS = "5HBVrFGy6oYhhh71m9fFGYD7zbKyAeHnWN8i8s9fJTBMCtEE"
+MIN_STAKE = 1000
+VALIDATORS_LIMIT = 24
+
+
+class MachineSpecs(pydantic.BaseModel):
+    specs: dict[Any, Any]
+
+    def __str__(self) -> str:
+        return str(self.specs)
+
+
+class ValidatorListError(Exception):
+    def __init__(self, reason: Exception):
+        self.reason = reason
+
+
+def get_validators(
+    metagraph: bittensor.Metagraph | None = None,
+    netuid=12,
+    network="finney",
+    block: int | None = None,
+) -> list[bittensor.NeuronInfo]:
+    """
+    Validators are top 24 neurons in terms of stake, only taking into account those that have at least 1000
+    and forcibly including BAC_VALIDATOR_SS58_ADDRESS.
+    The result is sorted.
+    """
+    if metagraph is None:
+        try:
+            subtensor = bittensor.subtensor(network=network)
+        except Exception as ex:
+            raise ValidatorListError(ex) from ex
+
+        try:
+            metagraph = subtensor.metagraph(netuid, block=block)
+        except SubstrateRequestException as ex:
+            raise ValidatorListError(ex) from ex
+
+    neurons = [
+        n
+        for n in metagraph.neurons
+        if (
+            n.hotkey
+            in (
+                BAC_VALIDATOR_SS58_ADDRESS,
+                "5FFApaS75bv5pJHfAp2FVLBj9ZaXuFDjEypsaBNc1wCfe52v",
+                "5F4tQyWrhfGVcNhoqeiNsR6KjD4wMZ2kfhLj4oHYuyHbZAc3",
+                "5F2CsUDVbRbVMXTh9fAzF9GacjVX7UapvRxidrxe7z8BYckQ",
+                "5HEo565WAy4Dbq3Sv271SAi7syBSofyfhhwRNjFNSM2gP9M2",
+            )
+            or n.stake.tao >= MIN_STAKE
+        )
+    ]
+    neurons = sorted(
+        neurons, key=lambda n: (n.hotkey == BAC_VALIDATOR_SS58_ADDRESS, n.stake), reverse=True
+    )
+    return neurons[:VALIDATORS_LIMIT]
+
+
+def json_dumps_default(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+
+    raise TypeError
+
+
+class Timer:
+    def __init__(self, timeout=None):
+        self.start_time = datetime.datetime.now()
+        self.timeout = timeout
+
+    def passed_time(self):
+        return (datetime.datetime.now() - self.start_time).total_seconds()
+
+    def time_left(self):
+        if self.timeout is None:
+            raise ValueError("timeout was not specified")
+        return self.timeout - self.passed_time()
+
+
+def sign_blob(kp: bittensor.Keypair, blob: str):
+    """
+    Signs a string blob with a bittensor keypair and returns the signature
+    """
+    return f"0x{kp.sign(blob).hex()}"
