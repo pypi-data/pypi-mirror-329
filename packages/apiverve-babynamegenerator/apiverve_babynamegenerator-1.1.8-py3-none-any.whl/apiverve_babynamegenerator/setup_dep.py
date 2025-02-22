12f471b0541791aa5af0c22ca2829072b609a3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:04:55 +0000

    Add package apiverve-babynamegenerator/1.1.8/apiverve_babynamegenerator-1.1.8-py3-none-any.whl

---
+++ packages/apiverve-babynamegenerator/apiverve_babynamegenerator-1.1.8-py3-none-any.whl/apiverve_babynamegenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_babynamegenerator',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Baby Name Generator is a simple tool for generating baby names. It returns a list of baby names.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
