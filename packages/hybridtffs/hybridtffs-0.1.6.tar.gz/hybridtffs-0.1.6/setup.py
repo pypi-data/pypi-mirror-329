178be6ee41fd0489b6b594a1e48e5ebdffa2b960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:05:20 +0000

    Add package hybridtffs/0.1.6/hybridtffs-0.1.6.tar.gz

---
+++ packages/hybridtffs/hybridtffs-0.1.6.tar.gz/hybridtffs-0.1.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hybridtffs",  # Tên package trên PyPI
+    version="0.1.6",  # Phiên bản đầu tiên
+    author="kieuanh",
+    author_email="vukieuanh.hnue@gmail.com",
+    description="Mo ta ngan",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/VuKieuAnh/hybrid-tffs.git",  # Link repo (nếu có)
+    packages=find_packages(),  # Tự động tìm package trong thư mục
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.21.0",
+        "pandas>=1.3.0",
+        "scikit-learn>=1.0.2",
+        "mlxtend>=0.19.0",
+        "tffs"
+    ],
+)
