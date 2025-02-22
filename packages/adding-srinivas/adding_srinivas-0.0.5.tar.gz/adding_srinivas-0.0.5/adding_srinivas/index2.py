b27eccb541d07a28437a18eca89a457e03f94999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 16:04:33 +0000

    Add package adding-srinivas/0.0.5/adding_srinivas-0.0.5.tar.gz

---
+++ packages/adding-srinivas/adding_srinivas-0.0.5.tar.gz/adding_srinivas-0.0.5/adding_srinivas/index2.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+def floor_divide(a,b):
+    return a//b
+def square_root(a):
+    return a**(1/2)
+def power(a,b):
+    return a**b
+
---
+++ packages/adding-srinivas/adding_srinivas-0.0.5.tar.gz/adding_srinivas-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup ,find_packages
+import codecs
+import os
+version="0.0.5"
+Description="add number package"
+
+setup(
+    name="adding_srinivas",
+    version=version,
+    author="srinivas",
+    description=Description,
+    packages=find_packages(),
+    install_require=['pandas'],
+    keywords=["python","video","videostream"],
+    classifiers=[
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Operating System :: Unix",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows"
+]
+    
+)
