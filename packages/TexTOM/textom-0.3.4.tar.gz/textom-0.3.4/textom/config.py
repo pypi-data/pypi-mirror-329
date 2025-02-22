22a964c55eb2fd70abc902c650e8986282d28886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 14:04:12 +0000

    Add package TexTOM/0.3.4/textom-0.3.4.tar.gz

---
+++ packages/TexTOM/textom-0.3.4.tar.gz/textom-0.3.4/textom/config.py
@@ -0,0 +1,12 @@
+import numpy as np # don't delete this line
+##################################################
+
+# Define how many cores you want to use 
+n_threads = 128 
+
+# Choose if you want to use a GPU for integration and alignment
+use_gpu = False
+
+# Choose your precision
+# recommended np.float64 for double or np.float32 for single precision
+data_type = np.float32
