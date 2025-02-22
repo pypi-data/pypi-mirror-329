e155265ee0d232630bc444dceccf2814cec3bc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 15:04:29 +0000

    Add package quick-ini/3.0/quick_ini-3.0.tar.gz

---
+++ packages/quick-ini/quick_ini-3.0.tar.gz/quick_ini-3.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quick_ini",
+    version="3.0",
+    description="A library for reading from and writting to .ini formatted files",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="OllieZ-Mods",
+    author_email="socksinthewash@gmail.com",
+    url="https://github.com/olliez-mods/Quick-Ini",
+    packages=find_packages()
+)
