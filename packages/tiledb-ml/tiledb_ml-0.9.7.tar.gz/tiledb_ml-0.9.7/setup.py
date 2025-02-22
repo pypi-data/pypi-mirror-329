d0361fffce7350ea798e6602ef01b6dd120179eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:03:21 +0000

    Add package tiledb-ml/0.9.7/tiledb_ml-0.9.7.tar.gz

---
+++ packages/tiledb-ml/tiledb_ml-0.9.7.tar.gz/tiledb_ml-0.9.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+tensorflow = ["tensorflow>=2.6, <2.16.0"]
+pytorch = ["torch>=1.11", "torchdata"]
+sklearn = ["scikit-learn>=1.0"]
+cloud = ["tiledb-cloud"]
+full = sorted({"torchvision", *tensorflow, *pytorch, *sklearn, *cloud})
+
+setuptools.setup(
+    setup_requires=["setuptools_scm"],
+    use_scm_version={
+        "version_scheme": "guess-next-dev",
+        "local_scheme": "dirty-tag",
+        "write_to": "tiledb/ml/version.py",
+    },
+    install_requires=["sparse", "tiledb>=0.14", "wrapt"],
+    extras_require={
+        "tensorflow": tensorflow,
+        "pytorch": pytorch,
+        "sklearn": sklearn,
+        "cloud": cloud,
+        "full": full,
+    },
+)
---
+++ packages/tiledb-ml/tiledb_ml-0.9.7.tar.gz/tiledb_ml-0.9.7/tiledb/ml/models/_cloud_utils.py
@@ -0,0 +1,45 @@
+import os
+from typing import Mapping, Optional
+
+try:
+    import tiledb.cloud
+except ImportError:
+    pass
+
+CLOUD_MODELS = "ml_models"
+FILETYPE_ML_MODEL = "ml_model"
+
+
+def get_s3_prefix(namespace: Optional[str]) -> Optional[str]:
+    """
+    Get S3 path from the user profile or organization profile.
+    :param namespace: User's namespace
+    :return: S3 prefix
+    """
+    if namespace is None:
+        return None
+    profile = tiledb.cloud.client.user_profile()
+    if namespace == profile.username:
+        s3_path = profile.default_s3_path
+    else:
+        s3_path = tiledb.cloud.client.organization(namespace).default_s3_path
+    return os.path.join(s3_path, CLOUD_MODELS) if s3_path is not None else None
+
+
+def update_file_properties(uri: str, file_properties: Mapping[str, str]) -> None:
+    tiledb.cloud.array.update_file_properties(
+        uri=uri,
+        file_type=FILETYPE_ML_MODEL,
+        file_properties=file_properties,
+    )
+
+
+def get_cloud_uri(uri: str, namespace: Optional[str]) -> str:
+    s3_prefix = get_s3_prefix(namespace)
+    if s3_prefix is None:
+        raise ValueError(
+            f"You must set the default s3 prefix path for ML models in "
+            f"{namespace} profile settings on TileDB-Cloud"
+        )
+
+    return f"tiledb://{namespace}/{os.path.join(s3_prefix, uri)}"
