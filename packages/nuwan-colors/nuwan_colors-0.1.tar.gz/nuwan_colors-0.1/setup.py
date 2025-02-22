521db04222c48cef27e6e7d791c0dab5b24f163e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:03:49 +0000

    Add package nuwan-colors/0.1/nuwan_colors-0.1.tar.gz

---
+++ packages/nuwan-colors/nuwan_colors-0.1.tar.gz/nuwan_colors-0.1/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="nuwan_colors",
+    version="0.1",
+    packages=find_packages(),
+    description="A lightweight package for adding customizable text coloring and styling to terminal output, making console applications more visually appealing and readable",
+    author="Shashika Nuwan (DevShashika)",
+    author_email="shashika90nuwan@gmail.com",
+    url="https://github.com/DevNuwancat",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+        
+)
