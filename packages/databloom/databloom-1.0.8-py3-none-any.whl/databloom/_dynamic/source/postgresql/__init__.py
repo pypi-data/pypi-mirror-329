542a41b815af0aab2fbd69e9d318e46ad3a6deca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:55 +0000

    Add package databloom/1.0.8/databloom-1.0.8-py3-none-any.whl

---
+++ packages/databloom/databloom-1.0.8-py3-none-any.whl/databloom/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,22 @@
+# --- render code block -----
+
+from .bumdata import bumdata
+from .marketing_source import marketing_source
+from .hr-t1 import hr-t1
+from .finance-t1 import finance-t1
+from .report-c-level import report-c-level
+
+__all__ = [
+    
+    "bumdata",
+    
+    "marketing_source",
+    
+    "hr-t1",
+    
+    "finance-t1",
+    
+    "report-c-level",
+    
+]
+# --- render code block -----
