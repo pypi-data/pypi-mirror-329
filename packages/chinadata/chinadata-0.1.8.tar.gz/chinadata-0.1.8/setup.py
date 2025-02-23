6d5d80402fd87b6dce02004ffbd66176a9482d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:33 +0000

    Add package chinadata/0.1.8/chinadata-0.1.8.tar.gz

---
+++ packages/chinadata/chinadata-0.1.8.tar.gz/chinadata-0.1.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='chinadata',
+    version='0.1.8',
+    author='xc',
+    author_email='utyu@gmail.com',
+    # license='MIT License',
+    platforms='any',
+    install_requires=["requests", "pandas",]
+)
