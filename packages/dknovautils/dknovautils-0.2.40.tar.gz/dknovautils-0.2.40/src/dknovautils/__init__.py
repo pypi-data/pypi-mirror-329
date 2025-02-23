7b55ce70f2da692b4ee3490f8f1eb91377d6f1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 14:04:18 +0000

    Add package dknovautils/0.2.40/dknovautils-0.2.40.tar.gz

---
+++ packages/dknovautils/dknovautils-0.2.40.tar.gz/dknovautils-0.2.40/src/dknovautils/__init__.py
@@ -0,0 +1,22 @@
+# 初始化代码写这里
+
+# import dknovautils.myadd
+# import dknovautils.mysubtract
+
+# from .myadd import m_add
+# import mysubtract
+
+
+from dknovautils.commons import *
+
+from dknovautils.dkat import *
+
+from dknovautils.dkipy import *
+
+from dknovautils.dkfiles import *
+
+from dknovautils.dkprocess import *
+
+from dknovautils.dknetwork import DkNetwork
+
+from dknovautils import dknetwork
