1843e6c756309b009b4696b2bb7a9f02b4ee8f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:02 +0000

    Add package evntaly-python/1.0.1/evntaly_python-1.0.1.tar.gz

---
+++ packages/evntaly-python/evntaly_python-1.0.1.tar.gz/evntaly_python-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="evntaly-python",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=["requests"],
+    description="A Python SDK for Evntaly event tracking platform",
+    author="Alameer Ashraf",
+    author_email="alameer@evntaly.com",
+    url="https://github.com/Evntaly/evntaly-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
