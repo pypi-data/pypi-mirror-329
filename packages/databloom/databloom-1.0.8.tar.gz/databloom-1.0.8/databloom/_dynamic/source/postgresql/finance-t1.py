a698eb137f1efa082cb88098e986b1add9035546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:55 +0000

    Add package databloom/1.0.8/databloom-1.0.8.tar.gz

---
+++ packages/databloom/databloom-1.0.8.tar.gz/databloom-1.0.8/databloom/_dynamic/source/postgresql/finance-t1.py
@@ -0,0 +1,8 @@
+# --- render code block -----
+from databloom._core.postgres_core import PostgresqlBase
+
+class finance-t1(PostgresqlBase):
+    def __init__(self, get_credential_from_server) -> None:
+        self.id = "67ba9118f83cc8e6de00477c"
+        self.credential = get_credential_from_server(self.id)
+# --- render code block -----
---
+++ packages/databloom/databloom-1.0.8.tar.gz/databloom-1.0.8/databloom/v1/ontology/__init__.py
@@ -0,0 +1,8 @@
+from databloom.v1.ontology.datasource import Mysql, Postgres, Datasource
+
+
+__all__ = [
+    "Mysql",
+    "Postgres",
+    "Datasource"
+]
