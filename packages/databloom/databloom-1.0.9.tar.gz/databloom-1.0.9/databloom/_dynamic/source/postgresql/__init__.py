55d8947b5820979a3ce3ecd93d876c0be2587359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:55 +0000

    Add package databloom/1.0.9/databloom-1.0.9.tar.gz

---
+++ packages/databloom/databloom-1.0.9.tar.gz/databloom-1.0.9/databloom/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,22 @@
+# --- render code block -----
+
+from .report-c-level import report-c-level
+from .bumdata import bumdata
+from .hr-t1 import hr-t1
+from .finance-t1 import finance-t1
+from .marketing_source2 import marketing_source2
+
+__all__ = [
+    
+    "report-c-level",
+    
+    "bumdata",
+    
+    "hr-t1",
+    
+    "finance-t1",
+    
+    "marketing_source2",
+    
+]
+# --- render code block -----
