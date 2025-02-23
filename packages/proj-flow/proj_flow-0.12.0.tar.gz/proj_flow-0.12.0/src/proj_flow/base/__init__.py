8c2a3aa790b22bac7637ad15f70bb44d759eca19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 14:04:31 +0000

    Add package proj-flow/0.12.0/proj_flow-0.12.0.tar.gz

---
+++ packages/proj-flow/proj_flow-0.12.0.tar.gz/proj_flow-0.12.0/src/proj_flow/base/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025 Marcin Zdun
+# This code is licensed under MIT license (see LICENSE for details)
+
+"""
+The **proj_flow.base** contains low-level tools for higher-level parts of the
+library.
+"""
+
+from . import cmd, matrix, plugins, registry, uname
+
+__all__ = ["cmd", "matrix", "plugins", "registry", "uname"]
