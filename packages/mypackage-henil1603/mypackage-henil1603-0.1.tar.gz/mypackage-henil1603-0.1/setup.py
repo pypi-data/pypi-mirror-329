1ae6943c45663101082e2241aa8f50d9eb1bfdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:03 +0000

    Add package mypackage-henil1603/0.1/mypackage-henil1603-0.1.tar.gz

---
+++ packages/mypackage-henil1603/mypackage-henil1603-0.1.tar.gz/mypackage-henil1603-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage-henil1603",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List dependencies here, e.g., "requests"
+    ],
+)
