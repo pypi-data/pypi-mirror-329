be35619078175a90dcdd2d08f907cc0a9af84a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:02 +0000

    Add package tccpy/0.14/tccpy-0.14.tar.gz

---
+++ packages/tccpy/tccpy-0.14.tar.gz/tccpy-0.14/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tccpy',
+    version=0.14,
+    description='A Python implementation of the Target Confusability Competition (TCC) memory model',
+    packages=find_packages(),
+    install_requirements=[
+        'numpy>=2.0.0'
+    ],
+    url='https://github.com/ilabsweden/tccpy'
+)
