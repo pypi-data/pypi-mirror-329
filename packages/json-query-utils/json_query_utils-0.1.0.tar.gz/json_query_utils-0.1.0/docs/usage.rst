d3d4b7f3c7d1487a3675a636b81727c7e215b2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 16:04:38 +0000

    Add package json-query-utils/0.1.0/json_query_utils-0.1.0.tar.gz

---
+++ packages/json-query-utils/json_query_utils-0.1.0.tar.gz/json_query_utils-0.1.0/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use JSON Query Utils in a project::
+
+    import json_query_utils
---
+++ packages/json-query-utils/json_query_utils-0.1.0.tar.gz/json_query_utils-0.1.0/tests/json_query_utils_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `json_query_utils` package."""
+
+
+import unittest
+
+from json_query_utils import json_query_utils
+
+
+class TestJson_query_utils(unittest.TestCase):
+    """Tests for `json_query_utils` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
