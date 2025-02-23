b1b3fb004c8d86ea300a76c944aa3918d0b55dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:55 +0000

    Add package databloom/1.0.6/databloom-1.0.6-py3-none-any.whl

---
+++ packages/databloom/databloom-1.0.6-py3-none-any.whl/databloom/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,22 @@
+# --- render code block -----
+
+from .databum import databum
+from .databummmm import databummmm
+from .bumdata import bumdata
+from .marketing_source import marketing_source
+from .marketing_2 import marketing_2
+
+__all__ = [
+    
+    "databum",
+    
+    "databummmm",
+    
+    "bumdata",
+    
+    "marketing_source",
+    
+    "marketing_2",
+    
+]
+# --- render code block -----
