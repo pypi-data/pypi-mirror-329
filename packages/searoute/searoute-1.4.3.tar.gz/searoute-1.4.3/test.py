c67c5c021ae0ce7890f6e3cc2865938a06054d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 14:04:26 +0000

    Add package searoute/1.4.3/searoute-1.4.3.tar.gz

---
+++ packages/searoute/searoute-1.4.3.tar.gz/searoute-1.4.3/test.py
@@ -0,0 +1,18 @@
+import searoute as sr
+
+#Define origin and destination points:
+origin = [21.1545, 55.6526]
+destination = [-118.2629, 33.7276]
+
+
+route = sr.searoute(origin, destination, append_orig_dest=True, restrictions=['northwest'], include_ports=True, port_params={'only_terminals':True, 'country_pol': '', 'country_pod' :'', 'country_restricted': False})
+# > Returns a GeoJSON LineString Feature
+# show route distance with unit
+print("{:.1f} {}".format(route.properties['length'], route.properties['units']))
+
+print(route)
+
+# Optionally, define the units for the length calculation included in the properties object.
+# Defaults to km, can be can be 'm' = meters 'mi = miles 'ft' = feets 'in' = inches 'deg' = degrees
+# 'cen' = centimeters 'rad' = radians 'naut' = nauticals 'yd' = yards
+routeMiles = sr.searoute(origin, destination, units="mi")
