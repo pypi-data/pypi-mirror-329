81a2b5113cfea0aabd39816c43464e61da4b8800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:38 +0000

    Add package funlbm/1.1.98/funlbm-1.1.98.tar.gz

---
+++ packages/funlbm/funlbm-1.1.98.tar.gz/funlbm-1.1.98/src/funlbm/particle/swarm.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import h5py
+from funlbm.particle import Particle, ParticleConfig
+from .base import create_particle
+
+
+class ParticleSwarm:
+    def __init__(self, configs=[], device="cpu"):
+        self.particles: List[Particle] = []
+        for config in configs:
+            self.particles.append(
+                create_particle(ParticleConfig(config_json=config), device=device)
+            )
+
+    def init(self, *args, **kwargs):
+        for particle in self.particles:
+            particle.init(*args, **kwargs)
+
+    def update(self, *args, **kwargs):
+        for particle in self.particles:
+            particle.update(*args, **kwargs)
+
+    def save_particle(self):
+        with h5py.File("./particle_lagrange.h5", "w") as fw:
+            for i, particle in enumerate(self.particles):
+                fw.create_dataset(f"particle{i}", particle._lagrange)
