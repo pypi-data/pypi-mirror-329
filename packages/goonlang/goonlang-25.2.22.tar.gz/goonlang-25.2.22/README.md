e1bd792f343f416ea48291f79c4e1e5de99847b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 16:04:22 +0000

    Add package goonlang/25.2.22/goonlang-25.2.22.tar.gz

---
+++ packages/goonlang/goonlang-25.2.22.tar.gz/goonlang-25.2.22/README.md
@@ -0,0 +1,5 @@
+# goonlang
+
+programming lnaguage made in python with lark
+
+there's only a few functions right now, you can assign variables, add, subtract, print, and i think a few others.
---
+++ packages/goonlang/goonlang-25.2.22.tar.gz/goonlang-25.2.22/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Natural Language :: English'
+]
+ 
+setup(
+    name='goonlang',
+    version='25.2.22',
+    description='A super simple programming language made with the lark package.',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/Touchcreator/goonlang',  
+    author='Touchcreator (Tochukwu Okolo)',
+    author_email='tochukwu.m.okolo@gmail.com',
+    license='MIT', 
+    classifiers=classifiers,
+    keywords=['lark','language','programming','python','goonlang'], 
+    packages=find_packages(),
+    install_requires=['lark'],
+    entry_points = {
+        'console_scripts': [
+            'goonlang = scripts.entry:main'
+        ]
+    },
+    package_data = {
+        'goonlang': ['grammar/*.lark']
+    },
+    include_package_data=True
+)
