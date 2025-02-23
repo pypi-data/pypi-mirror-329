7b91cb204623597578ac3ae4fba95bf625fffcd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 16:04:17 +0000

    Add package aqua-blue/0.0.18/aqua_blue-0.0.18-py3-none-any.whl

---
+++ packages/aqua-blue/aqua_blue-0.0.18-py3-none-any.whl/aqua_blue/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.0.18"
+__authors__ = [
+    "Jacob Jeffries",
+    "Ameen Mahmood",
+    "Avik Thumati",
+    "Samuel Josephs"
+]
+__author_emails__ = [
+    "jacob.jeffries@chicagoclubteam.org",
+    "ameen.mahmood@chicagoclubteam.org",
+    "avik.thumati@chicagoclubteam.org",
+    "samuel.joseph@chicagoclubteam.org"
+]
+__url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue"
+
+from .reservoir import EchoStateNetwork as EchoStateNetwork
+from .reservoir import InstabilityWarning as InstabilityWarning
+from .time_series import TimeSeries as TimeSeries
+from .time_series import ShapeChangedWarning as ShapeChangedWarning
