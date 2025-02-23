4a9c1f81d7a7ef1a3c4662385099ee303f73e5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:03 +0000

    Add package eclypse/0.6.19/eclypse-0.6.19-cp310-cp310-manylinux_2_35_x86_64.whl

---
+++ packages/eclypse/eclypse-0.6.19-cp310-cp310-manylinux_2_35_x86_64.whl/eclypse/graph/assets/space.py
@@ -0,0 +1,19 @@
+"""Module containing off-the-shelf asset spaces, together with the class to customise
+them.
+
+For the complete documentation, refer to :py:mod:`eclypse_core.graph.assets.space`.
+"""
+
+from eclypse_core.graph.assets.space import (
+    AssetSpace,
+    Choice,
+    Sample,
+    Uniform,
+)
+
+__all__ = [
+    "AssetSpace",
+    "Choice",
+    "Sample",
+    "Uniform",
+]
