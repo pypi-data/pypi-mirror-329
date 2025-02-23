2759fa25d46beac8f66e94e6bbbb505739795b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:41 +0000

    Add package siya-rama/1.2/siya_rama-1.2.tar.gz

---
+++ packages/siya-rama/siya_rama-1.2.tar.gz/siya_rama-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='siya_rama',
+    version='1.2',
+    description='The module have function to search for letters in string passed',
+    author='SnehaK',
+    py_modules=['siya_rama'],
+    )
