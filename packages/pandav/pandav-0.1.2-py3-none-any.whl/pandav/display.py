4fccef96677233218321b195487afffff0a5d51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 14:04:22 +0000

    Add package pandav/0.1.2/pandav-0.1.2-py3-none-any.whl

---
+++ packages/pandav/pandav-0.1.2-py3-none-any.whl/pandav/display.py
@@ -0,0 +1,33 @@
+import nbformat
+from nbconvert import HTMLExporter
+
+def display_1():
+    """Converts the notebook to HTML and prints it."""
+    with open("1.ipynb", "r", encoding="utf-8") as f:
+        notebook_data = nbformat.read(f, as_version=4)
+
+    # Convert notebook to HTML
+    html_exporter = HTMLExporter()
+    html_body, _ = html_exporter.from_notebook_node(notebook_data)
+
+    # Print HTML output
+    from IPython.core.display import display, HTML
+    display(HTML(html_body))
+
+display_1()
+
+
+def display_2():
+    """Converts the notebook to HTML and prints it."""
+    with open("2.ipynb", "r", encoding="utf-8") as f:
+        notebook_data = nbformat.read(f, as_version=4)
+
+    # Convert notebook to HTML
+    html_exporter = HTMLExporter()
+    html_body, _ = html_exporter.from_notebook_node(notebook_data)
+
+    # Print HTML output
+    from IPython.core.display import display, HTML
+    display(HTML(html_body))
+
+display_2()
