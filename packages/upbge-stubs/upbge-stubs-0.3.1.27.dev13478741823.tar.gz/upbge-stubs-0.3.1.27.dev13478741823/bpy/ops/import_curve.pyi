db0728413b23ebcf853c110ba94ca0f25ae7efff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:56 +0000

    Add package upbge-stubs/0.3.1.27.dev13478741823/upbge-stubs-0.3.1.27.dev13478741823.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev13478741823.tar.gz/upbge-stubs-0.3.1.27.dev13478741823/bpy/ops/import_curve.pyi
@@ -0,0 +1,21 @@
+"""
+
+
+Import Curve Operators
+**********************
+
+:func:`svg`
+
+"""
+
+import typing
+
+def svg(*args, filepath: str = '', filter_glob: str = '*args.svg') -> None:
+
+  """
+
+  Load a SVG file
+
+  """
+
+  ...
---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev13478741823.tar.gz/upbge-stubs-0.3.1.27.dev13478741823/object_type_items/__init__.pyi
@@ -0,0 +1,72 @@
+"""
+
+
+Object Type Items
+^^^^^^^^^^^^^^^^^
+
+:MESH:            
+  Mesh.
+
+:CURVE:           
+  Curve.
+
+:SURFACE:         
+  Surface.
+
+:META:            
+  Metaball.
+
+:FONT:            
+  Text.
+
+:CURVES:          
+  Hair Curves.
+
+:POINTCLOUD:      
+  Point Cloud.
+
+:VOLUME:          
+  Volume.
+
+:GPENCIL:         
+  Grease Pencil.
+
+:GREASEPENCIL:    
+  Grease Pencil v3.
+
+.. _rna-enum-object-type-items:
+
+======================================================================
+
+:ARMATURE:        
+  Armature.
+
+:LATTICE:         
+  Lattice.
+
+======================================================================
+
+:EMPTY:           
+  Empty.
+
+======================================================================
+
+:LIGHT:           
+  Light.
+
+:LIGHT_PROBE:     
+  Light Probe.
+
+======================================================================
+
+:CAMERA:          
+  Camera.
+
+======================================================================
+
+:SPEAKER:         
+  Speaker.
+
+"""
+
+import typing
