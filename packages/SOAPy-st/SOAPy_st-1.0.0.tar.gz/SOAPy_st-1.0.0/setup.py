bffe49d2ad9114cc78540919fe7c09935139899e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:59 +0000

    Add package SOAPy-st/1.0.0/SOAPy_st-1.0.0.tar.gz

---
+++ packages/SOAPy-st/SOAPy_st-1.0.0.tar.gz/SOAPy_st-1.0.0/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+from pathlib import Path
+
+with open("README.md", encoding="utf8") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='SOAPy_st',
+    version='1.0.0',
+    author='Cancer system biology lab',
+    author_email='wangheqi2021@sinh.ac.cn',
+    description='Spatial Omics Analysis in Python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/LiHongCSBLab/SOAPy',
+    python_requires='>=3.9',
+    include_package_data=True,
+    package_data={
+        "SOAPy_st": ['tl/datasets/Human-2020-Jin-LR-pairs.csv', 'tl/datasets/Mouse-2020-Jin-LR-pairs.csv'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3'],
+    keywords=sorted(
+        [
+            "bio-informatics",
+            "spatial omics",
+            "spatial transcriptomics",
+            "spatial data analysis",
+        ]),
+    packages=setuptools.find_packages(include=["SOAPy_st", "SOAPy_st.*"]),
+    install_requires=requirements
+)
