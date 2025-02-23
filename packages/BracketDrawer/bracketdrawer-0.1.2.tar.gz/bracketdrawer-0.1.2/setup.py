a7bc0e4eb1f9b9f263caac977f985f9254398a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:01 +0000

    Add package BracketDrawer/0.1.2/bracketdrawer-0.1.2.tar.gz

---
+++ packages/BracketDrawer/bracketdrawer-0.1.2.tar.gz/bracketdrawer-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BracketDrawer',
+    version='0.1.2',
+    author="Ben DeMouth",
+    author_email="bendemouth@gmail.com",
+    url="https://github.com/bendemouth/BracketDrawer",
+    description="A Python Package to easily create tournament brackets",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    install_requires=[
+        'matplotlib',
+        'pandas'
+    ]
+)
