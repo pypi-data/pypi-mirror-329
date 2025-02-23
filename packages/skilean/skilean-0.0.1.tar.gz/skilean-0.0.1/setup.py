21584df7553dfb752f052491c0511ffbcc67100b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 14:04:44 +0000

    Add package skilean/0.0.1/skilean-0.0.1.tar.gz

---
+++ packages/skilean/skilean-0.0.1.tar.gz/skilean-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="skilean",  # Change this to your package name
+    version="0.0.1",
+    packages=find_packages(),  # This ensures the folder gets installed
+    author="Gaussian Kernal",
+    author_email="gaussiankernal@gmail.com",
+    description="Python image processing library",
+    license="MIT",
+    url="",
+    python_requires=">=2.7",
+)
