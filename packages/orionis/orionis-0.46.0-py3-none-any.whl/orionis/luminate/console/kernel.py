2f5c0d423a89bc010416d5e4cf7762bcbf66ea13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 16:04:17 +0000

    Add package orionis/0.46.0/orionis-0.46.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.46.0-py3-none-any.whl/orionis/luminate/console/kernel.py
@@ -0,0 +1,32 @@
+from typing import Any
+from orionis.contracts.console.i_kernel import ICLIKernel
+from orionis.luminate.console.runner import CLIRunner
+
+class CLIKernel(ICLIKernel):
+    """
+    CLIKernel acts as a bridge between the CLI entry point and the command runner.
+
+    This class provides a static method to handle command execution via `CLIRunner`.
+
+    Methods
+    -------
+    handle(*args: tuple[Any, ...]) -> Any
+        Processes CLI arguments and delegates execution to `CLIRunner`.
+    """
+
+    @staticmethod
+    def handle(*args: tuple[Any, ...]) -> Any:
+        """
+        Handles CLI command execution by forwarding arguments to `CLIRunner`.
+
+        Parameters
+        ----------
+        *args : tuple[Any, ...]
+            A tuple containing CLI arguments passed from the command line.
+
+        Returns
+        -------
+        Any
+            The result of the executed command.
+        """
+        pass
