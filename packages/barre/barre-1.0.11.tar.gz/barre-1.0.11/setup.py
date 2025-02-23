5c3e0afe7fe297c092badf43e4bf2f05af4a9243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:38 +0000

    Add package barre/1.0.11/barre-1.0.11.tar.gz

---
+++ packages/barre/barre-1.0.11.tar.gz/barre-1.0.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+setup(
+    name="barre",
+    version="1.0.11",
+    py_modules=["barre"],
+    description="Minimal progress bar",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Fonk",
+    author_email="feelthefonk@gmail.com",
+    url="https://github.com/FeelTheFonk/barre",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Logging",
+    ],
+    python_requires=">=3.8",
+)
