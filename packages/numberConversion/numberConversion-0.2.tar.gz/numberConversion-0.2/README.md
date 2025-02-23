1061c9abbb12e6bcd8dc9f8284dd8211ac0fc107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:30 +0000

    Add package numberConversion/0.2/numberConversion-0.2.tar.gz

---
+++ packages/numberConversion/numberConversion-0.2.tar.gz/numberConversion-0.2/README.md
@@ -0,0 +1,5 @@
+# simplemath
+A simple python package that converts binary to decimal and vice versa
+
+## Installation
+pip install and use
---
+++ packages/numberConversion/numberConversion-0.2.tar.gz/numberConversion-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+VERSION = '0.0.2'
+DESCRIPTION = 'A simple package to convert binary to decimal and decimal to binary'
+LONG_DESCRIPTION = 'A simple package to convert binary to decimal and decimal to binary'
+
+setup(
+    name="numberConversion",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Prayush Shrestha",
+    author_email="prayushshrestha89@gmail.com",
+    description="A simple package to convert decimal to binary and binary to decimal",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/shresthaprayush2/numberconversion.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
