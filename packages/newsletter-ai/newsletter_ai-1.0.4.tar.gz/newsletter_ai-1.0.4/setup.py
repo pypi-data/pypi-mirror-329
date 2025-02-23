cc5de32b66f7d19aefeaa1075b1f41aea5117e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 16:04:16 +0000

    Add package newsletter-ai/1.0.4/newsletter_ai-1.0.4.tar.gz

---
+++ packages/newsletter-ai/newsletter_ai-1.0.4.tar.gz/newsletter_ai-1.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="newsletter-ai",  # Replace with your package name
+    version="1.0.4",
+    description="A powerful library for managing AI-driven prompt handling and response generation, featuring structured database management and seamless Node.js integration.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gaurav-Codetek/chatflow.git",  # Replace with your GitHub URL
+    author="Gaurav Patel (CodersTek)",
+    author_email="gauravpatel29@outlook.in",
+    install_requires=['google.genai', 'requests', 'beautifulsoup4'],
+    keywords=['python', 'chatbot', 'genai', 'generative ai', 'gemini api', 'chat ai'],
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
