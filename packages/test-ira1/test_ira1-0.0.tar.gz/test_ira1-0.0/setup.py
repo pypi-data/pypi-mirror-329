550ca70c3446fdf42668cff147d774b5af73ca5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:55 +0000

    Add package test-ira1/0.0/test_ira1-0.0.tar.gz

---
+++ packages/test-ira1/test_ira1-0.0.tar.gz/test_ira1-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='test_ira1',
+      version='0.0',
+      description='calculator',
+      packsges=['test_ira1'],
+      author='ikkyk',
+      author_email='klara.vysotskaya@yandex.ru')
