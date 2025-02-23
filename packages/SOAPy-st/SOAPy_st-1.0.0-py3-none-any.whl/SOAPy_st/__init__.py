f1c03f0381d252534e620646d8f64a280d0acf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:59 +0000

    Add package SOAPy-st/1.0.0/SOAPy_st-1.0.0-py3-none-any.whl

---
+++ packages/SOAPy-st/SOAPy_st-1.0.0-py3-none-any.whl/SOAPy_st/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from . import pp
+from . import tl
+from . import pl
+from .tl import other_package_without_pip
+
+__version__ = "v1.0.0"
+
+"""
+# Author: Lihong lab
+# File Name:SOAPy
+# github:https://github.com/LiHongCSBLab/SOAPy
+# Description:
+"""
+
+__author__ = "Cancer system biology lab"
+__email__ = [
+    "wangheqi2021@sinh.ac.cn"
+    "lijiarong2020@sinh.ac.cn"
+    "lihong01@sibs.ac.cn"
+]
+
+
+
---
+++ packages/SOAPy-st/SOAPy_st-1.0.0-py3-none-any.whl/SOAPy_st/tl/other_package_without_pip/__init__.py
@@ -0,0 +1,3 @@
+from . import STAGATE_pyG
+from . import GraphST
+from . import scanit
