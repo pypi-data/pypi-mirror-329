f0e66a6453c0f72a7b9d02aa3e5ed81c2396a5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:48 +0000

    Add package rag-pipeline/0.1.3/rag_pipeline-0.1.3.tar.gz

---
+++ packages/rag-pipeline/rag_pipeline-0.1.3.tar.gz/rag_pipeline-0.1.3/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="rag_pipeline",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=["openai", "faiss-cpu", "numpy", "pdfplumber"],
+    author="Salim Khazem",
+    author_email="salim.khazem@talan.com",
+    description="A library for Retrieval-Augmented Generation with Azure OpenAI",
+    url="https://github.com/salimkhazem/rag-pipeline",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
