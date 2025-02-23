c83dc1502b5be8af0b925cfced287a9461f97df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:36 +0000

    Add package pazok/0.1.3.7/pazok-0.1.3.7.tar.gz

---
+++ packages/pazok/pazok-0.1.3.7.tar.gz/pazok-0.1.3.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='pazok',
+    version='0.1.3.7',
+    author='b_azo',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+)
