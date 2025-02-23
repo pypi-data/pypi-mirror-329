695ac4daa7cd2e88e300415f9a35e5db7d7ce43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 14:04:02 +0000

    Add package dirscribe/0.1.3/dirscribe-0.1.3-py3-none-any.whl

---
+++ packages/dirscribe/dirscribe-0.1.3-py3-none-any.whl/dirscribe/__init__.py
@@ -0,0 +1,10 @@
+"""
+DirScribe
+
+A Python package that helps you export a directory structure and optionally
+the contents of files with certain extensions.
+"""
+
+__version__ = "0.1.1"
+
+from .core import export_directory_structure, main
