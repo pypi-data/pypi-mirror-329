82a27df9dd6d6f1b170eba7f74be96647b81213a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:48 +0000

    Add package bias-lens/0.0.1/bias_lens-0.0.1.tar.gz

---
+++ packages/bias-lens/bias_lens-0.0.1.tar.gz/bias_lens-0.0.1/bias_lens/__init__.py
@@ -0,0 +1,5 @@
+from .bias_model_explainer_factory import BiasModelExplainerFactory
+from .bias_ner_classifier import BiasNERClassifier
+
+
+__all__ = ["BiasModelExplainerFactory", "BiasNERClassifier"]
