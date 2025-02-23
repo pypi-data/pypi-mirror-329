98bdb759913b8594b65c63f32f80fde1b307d158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:37 +0000

    Add package nemo-library/1.2.46/nemo_library-1.2.46-py3-none-any.whl

---
+++ packages/nemo-library/nemo_library-1.2.46-py3-none-any.whl/nemo_library/model/defined_column.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field, asdict
+from typing import Optional
+
+
+@dataclass
+class DefinedColumn:
+    categorialType: bool
+    columnType: str
+    containsSensitiveData: bool
+    dataType: str
+    description: str
+    displayName: str
+    formula: str
+    groupByColumnInternalName: Optional[str]
+    importName: str
+    stringSize: int
+    unit: str
+    internalName: str
+    id: str
+    projectId: str
+    tenant: str
+
+    def to_dict(self):
+        return asdict(self)
