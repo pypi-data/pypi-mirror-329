324bec4c050744d902f90a82e3c6c2b251b02c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:03 +0000

    Add package py-spam-hunter-client/1.0.2/py_spam_hunter_client-1.0.2-py3-none-any.whl

---
+++ packages/py-spam-hunter-client/py_spam_hunter_client-1.0.2-py3-none-any.whl/py_spam_hunter_client/__init__.py
@@ -0,0 +1,5 @@
+from .async_spam_hunter import AsyncSpamHunter
+from .sync_spam_hunter import SyncSpamHunter
+from .messages import Message
+from .messages import CheckedMessage
+from .exceptions import CheckException
