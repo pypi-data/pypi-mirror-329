afcb1736973c50d18b86b882725abf9dc1346a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 16:04:42 +0000

    Add package json-validation-utils/0.1.1/json_validation_utils-0.1.1.tar.gz

---
+++ packages/json-validation-utils/json_validation_utils-0.1.1.tar.gz/json_validation_utils-0.1.1/json_validation_utils/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for JSON Validation Utils."""
+
+__author__ = """Jaideep Sundaram"""
+__email__ = "jai.python3@gmail.com"
+__version__ = "0.1.0"
+
+# from .json_validation_utils import Manager as JSONValidationManager
---
+++ packages/json-validation-utils/json_validation_utils-0.1.1.tar.gz/json_validation_utils-0.1.1/tests/json_validation_utils_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `json_validation_utils` package."""
+
+
+import unittest
+
+from json_validation_utils import json_validation_utils
+
+
+class TestJson_validation_utils(unittest.TestCase):
+    """Tests for `json_validation_utils` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
