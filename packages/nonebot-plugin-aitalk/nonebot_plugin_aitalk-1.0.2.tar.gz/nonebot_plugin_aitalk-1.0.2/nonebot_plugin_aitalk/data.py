e94b9876895439c1a2418c992ce096a1e30b8348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:58 +0000

    Add package nonebot-plugin-aitalk/1.0.2/nonebot_plugin_aitalk-1.0.2.tar.gz

---
+++ packages/nonebot-plugin-aitalk/nonebot_plugin_aitalk-1.0.2.tar.gz/nonebot_plugin_aitalk-1.0.2/nonebot_plugin_aitalk/data.py
@@ -0,0 +1,53 @@
+import nonebot_plugin_localstore as store
+from pathlib import Path
+import json
+from .config import default_available
+
+def get_config_file() -> Path:
+    file = store.get_config_file("nonebot-plugin-aitalk", "config.json")
+    if not file.exists(): file.write_text("{}")
+    return file
+
+def get_available_config_file() -> Path:
+    file = store.get_config_file("nonebot-plugin-aitalk", "available.json")
+    if not file.exists(): file.write_text("{}")
+    return file
+
+def write_data(key, value):
+    file = get_config_file()
+    data = json.loads(file.read_text())
+    data[key] = value
+    file.write_text(json.dumps(data))
+
+def write_all_data(data):
+    file = get_config_file()
+    file.write_text(json.dumps(data))
+
+def read_data(key):
+    file = get_config_file()
+    data = json.loads(file.read_text())
+    return data.get(key)
+
+def read_all_data():
+    file = get_config_file()
+    data = json.loads(file.read_text())
+    return data
+
+def enable(gid: int):
+    file = get_available_config_file()
+    data = json.loads(file.read_text())
+    data[str(gid)] = True
+    file.write_text(json.dumps(data))
+
+def disable(gid: int):
+    file = get_available_config_file()
+    data = json.loads(file.read_text())
+    data[str(gid)] = False
+    file.write_text(json.dumps(data))
+
+def is_available(gid: int):
+    file = get_available_config_file()
+    data = json.loads(file.read_text())
+    if str(gid) not in data:
+        return default_available
+    return data[str(gid)]
