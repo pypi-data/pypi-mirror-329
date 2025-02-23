11f111b1b2000d15cc1ec130094483edd3fdf615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:55 +0000

    Add package databloom/1.0.7/databloom-1.0.7-py3-none-any.whl

---
+++ packages/databloom/databloom-1.0.7-py3-none-any.whl/databloom/v1/ontology/datasource/postgresql.py
@@ -0,0 +1,22 @@
+import databloom._dynamic.source.postgresql as db
+from typing import Callable
+
+class Postgres:
+    """
+    data source type is postgresql
+    """
+    def __init__(self, get_credential_from_sdk: Callable) -> None:
+        ## ----render code block-----
+        
+        self.databummmm = db.databummmm(get_credential_from_sdk)
+        
+        self.bumdata = db.bumdata(get_credential_from_sdk)
+        
+        self.marketing_source = db.marketing_source(get_credential_from_sdk)
+        
+        self.hr-t1 = db.hr-t1(get_credential_from_sdk)
+        
+        self.finance-t1 = db.finance-t1(get_credential_from_sdk)
+        
+        ## ----render code block----
+        pass
