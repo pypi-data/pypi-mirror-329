1f31dfdc693ade97977306471b8b5a2c10a54066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 17:03:49 +0000

    Add package LlamaDocIndexer/0.3.1/llamadocindexer-0.3.1.tar.gz

---
+++ packages/LlamaDocIndexer/llamadocindexer-0.3.1.tar.gz/llamadocindexer-0.3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="LlamaDocIndexer",
+    version="0.3.1",
+    packages=find_packages(),
+    description="A tool to index and monitor changes in document directories",
+    long_description_content_type="text/markdown",
+    author="Jiayi Chen",
+    author_email="chenjiayi_344@hotmail.com",
+    url="https://github.com/chenjiayi8/LlamaDocIndexer",
+    download_url="https://pypi.org/project/LlamaDocIndexer/",
+    install_requires=required,
+)
