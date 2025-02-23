795b3b664a1718b62a4e11c58d542ff066e2648d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-23 15:04:01 +0000

    Add package deploy-llm/0.0.3/deploy_llm-0.0.3.tar.gz

---
+++ packages/deploy-llm/deploy_llm-0.0.3.tar.gz/deploy_llm-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deploy-llm",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "torch",
+        "transformers"
+    ],
+    entry_points={
+        "console_scripts": [
+            "llmdeploy=cli:cli",
+        ],
+    },
+    author="Ankit Gupta",
+    description="CLI tool for deploying and running models from Ollama and Hugging Face",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
