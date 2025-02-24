8f94d50ef87f41f6c965b2cb7b958259dd5133f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:46 +0000

    Add package databloom/1.0.24/databloom-1.0.24.tar.gz

---
+++ packages/databloom/databloom-1.0.24.tar.gz/databloom-1.0.24/databloom/_dynamic/source/postgresql/dep-2.py
@@ -0,0 +1,8 @@
+# --- render code block -----
+from databloom._core.postgres_core import PostgresqlBase
+
+class dep-2(PostgresqlBase):
+    def __init__(self, get_credential_from_server) -> None:
+        self.id = "67bc10b0f1ba9a2bf4a678ba"
+        self.credential = get_credential_from_server(self.id)
+# --- render code block -----
