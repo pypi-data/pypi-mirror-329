9ae5062403e689bf954b40f279777448e6bd6f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:04:58 +0000

    Add package social-scraper-tool/0.8.0/social_scraper_tool-0.8.0-py3-none-any.whl

---
+++ packages/social-scraper-tool/social_scraper_tool-0.8.0-py3-none-any.whl/social_scraper/test.py
@@ -0,0 +1,25 @@
+from reddit import RedditScraper
+from dotenv import load_dotenv
+
+from utils import posts_to_dataframe
+
+load_dotenv()
+
+reddit_scraper = RedditScraper()
+reddit_posts = reddit_scraper.get_posts(query="AI", subreddit="technology", time_filter="week", limit=1)
+
+df_reddit_posts = posts_to_dataframe(reddit_posts)
+
+print(df_reddit_posts.head())
+
+from twitter import TwitterScraper
+import asyncio
+
+async def fetch_twitter_posts():
+    twitter_scraper = TwitterScraper()
+    posts = await twitter_scraper.get_posts(query="AI technology", limit=1)
+    return posts
+twitter_posts = asyncio.run(fetch_twitter_posts())
+
+df_twitter_posts = posts_to_dataframe(twitter_posts)
+print(df_twitter_posts.head())
