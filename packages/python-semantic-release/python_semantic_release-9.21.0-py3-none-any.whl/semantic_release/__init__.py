98b653e3b865ca22cd85ca9fcfa9e8aca67df306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:05:05 +0000

    Add package python-semantic-release/9.21.0/python_semantic_release-9.21.0-py3-none-any.whl

---
+++ packages/python-semantic-release/python_semantic_release-9.21.0-py3-none-any.whl/semantic_release/__init__.py
@@ -0,0 +1,59 @@
+"""Python Semantic Release"""
+
+from __future__ import annotations
+
+from semantic_release.commit_parser import (
+    CommitParser,
+    ParsedCommit,
+    ParseError,
+    ParseResult,
+    ParseResultType,
+    ParserOptions,
+)
+from semantic_release.enums import LevelBump
+from semantic_release.errors import (
+    CommitParseError,
+    InvalidConfiguration,
+    InvalidVersion,
+    SemanticReleaseBaseError,
+)
+from semantic_release.version import (
+    Version,
+    VersionTranslator,
+    next_version,
+    tags_and_versions,
+)
+
+__version__ = "9.21.0"
+
+__all__ = [
+    "CommitParser",
+    "ParsedCommit",
+    "ParseError",
+    "ParseResult",
+    "ParseResultType",
+    "ParserOptions",
+    "LevelBump",
+    "SemanticReleaseBaseError",
+    "CommitParseError",
+    "InvalidConfiguration",
+    "InvalidVersion",
+    "Version",
+    "VersionTranslator",
+    "next_version",
+    "tags_and_versions",
+]
+
+
+def setup_hook(argv: list[str]) -> None:
+    """
+    A hook to be used in setup.py to enable `python setup.py publish`.
+
+    :param argv: sys.argv
+    """
+    if len(argv) > 1 and any(
+        cmd in argv for cmd in ["version", "publish", "changelog"]
+    ):
+        from semantic_release.cli.commands.main import main
+
+        main()
