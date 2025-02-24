5254d8ab98ca18e95622145a99797258cd17dbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:05 +0000

    Add package vehicle-dynamics/1.0.4/vehicle_dynamics-1.0.4.tar.gz

---
+++ packages/vehicle-dynamics/vehicle_dynamics-1.0.4.tar.gz/vehicle_dynamics-1.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.4' 
+DESCRIPTION = 'vehicle_dynamics'
+LONG_DESCRIPTION = 'package that calculates the vehicle dynamics'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="vehicle_dynamics", 
+    version=VERSION,
+    author="Maikol Funk Dreschler, Yuri Poledna",
+    author_email="<Maikol.FunkDreschler@thi.de>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        "pyyaml",
+        "tqdm",
+        "pandas",
+        "munch",
+        "urllib",
+    ],
+
+    keywords=['python', 'vehicle dynamics'],
+    classifiers= [
+        "Programming Language :: Python :: 3",
+    ]
+)
