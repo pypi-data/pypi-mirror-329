5ff0cf9539540992251fbe4e7c4c44b83cc92b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:05:02 +0000

    Add package marimo-agents/0.0.5/marimo_agents-0.0.5-py3-none-any.whl

---
+++ packages/marimo-agents/marimo_agents-0.0.5-py3-none-any.whl/marimo/_server/models/home.py
@@ -0,0 +1,55 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+from marimo._server.models.files import FileInfo
+from marimo._tutorials import Tutorial
+from marimo._types.ids import SessionId
+
+
+@dataclass
+class MarimoFile:
+    # Name of the file
+    name: str
+    # Absolute path to the file
+    path: str
+    # Last modified time of the file
+    last_modified: Optional[float] = None
+    # Session id
+    session_id: Optional[SessionId] = None
+    # Session initialization id
+    # This is the ID for when the session was initialized
+    initialization_id: Optional[str] = None
+
+
+@dataclass
+class RecentFilesResponse:
+    files: List[MarimoFile]
+
+
+@dataclass
+class RunningNotebooksResponse:
+    files: List[MarimoFile]
+
+
+@dataclass
+class OpenTutorialRequest:
+    tutorial_id: Tutorial
+
+
+@dataclass
+class WorkspaceFilesRequest:
+    include_markdown: bool = False
+
+
+@dataclass
+class WorkspaceFilesResponse:
+    root: str
+    files: List[FileInfo]
+
+
+@dataclass
+class ShutdownSessionRequest:
+    session_id: SessionId
---
+++ packages/marimo-agents/marimo_agents-0.0.5-py3-none-any.whl/marimo/_static/assets/channel-Ci_ZLzzp.js
@@ -0,0 +1,1 @@
+import{U as a,C as r}from"./mermaid-BAj7zJZ0.js";const s=(s,o)=>a.lang.round(r.parse(s)[o]);export{s as c};
