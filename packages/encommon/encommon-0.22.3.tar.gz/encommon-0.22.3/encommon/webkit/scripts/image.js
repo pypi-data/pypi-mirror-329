048e3e9f37b77bcb01ef8e0c004bae37948cc777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:52 +0000

    Add package encommon/0.22.3/encommon-0.22.3.tar.gz

---
+++ packages/encommon/encommon-0.22.3.tar.gz/encommon-0.22.3/encommon/webkit/scripts/image.js
@@ -0,0 +1,39 @@
+/*
+Functions and routines associated with Enasis Network Common Library.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+*/
+
+
+
+function svgicon(
+  image,
+  dimension=null,
+) {
+  // Return the simple construct for SVG based icon images.
+
+  assert(!isnull(image));
+
+
+  let element =
+    $('<div/>').addClass(
+      'encommon_svgicon');
+
+
+  element.attr(
+    'data-image',
+    image);
+
+  if (!isnull(dimension)) {
+
+    element.css(
+      'height',
+      dimension);
+
+    element.css(
+      'width',
+      dimension); }
+
+
+  return element; }
