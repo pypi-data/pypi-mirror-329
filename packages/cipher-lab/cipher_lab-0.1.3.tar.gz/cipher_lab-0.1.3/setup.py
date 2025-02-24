a5891cb7dceffb3318c66aba2e3e80a5e5be00f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 04:04:22 +0000

    Add package cipher-lab/0.1.3/cipher_lab-0.1.3.tar.gz

---
+++ packages/cipher-lab/cipher_lab-0.1.3.tar.gz/cipher_lab-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cipher-lab",  # Changed to desired package name
+    version="0.1.3",  # Increment version
+    packages=find_packages(),
+    package_data={
+        'cipher_package': ['cipher_code/*/*.py'],
+    },
+    entry_points={
+        'console_scripts': [
+ 'get_ciphers=cipher_package.cli:main',  # Changed to underscore   
+ ],
+    },
+    author="Your Name",
+    author_email="your@email.com",
+    description="Classic cipher implementations with export functionality",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/cipher-package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    keywords="ciphers cryptography encryption decryption",
+)
