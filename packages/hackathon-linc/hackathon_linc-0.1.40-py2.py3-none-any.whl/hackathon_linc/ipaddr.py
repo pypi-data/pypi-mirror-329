cc23778e24760ac2b6e4f73ab24a5adb99384974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 03:10:41 +0000

    Add package hackathon-linc/0.1.40/hackathon_linc-0.1.40-py2.py3-none-any.whl

---
+++ packages/hackathon-linc/hackathon_linc-0.1.40-py2.py3-none-any.whl/hackathon_linc/ipaddr.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 19:10:18 2021
+
+@author: yasse
+"""
+
+url = 'https://hackathonlincdemoapp.azurewebsites.net/api'
+#url = 'http://localhost'
+token = ''
+tickers = []
