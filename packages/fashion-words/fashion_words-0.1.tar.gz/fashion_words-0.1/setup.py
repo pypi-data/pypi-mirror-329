ff1ef25481f74bbaf7c9a5b2ee959b00394c374f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:04:38 +0000

    Add package fashion-words/0.1/fashion_words-0.1.tar.gz

---
+++ packages/fashion-words/fashion_words-0.1.tar.gz/fashion_words-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fashion_words",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    description="Um pacote de palavras relacionadas à moda para NLP",
+    author="Seu Nome",
+    install_requires=[],
+)
