aae414e09762afcfa7c4122412eb1f8cef3cadb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:51 +0000

    Add package response-pm-zov/0.4/response_pm_zov-0.4.tar.gz

---
+++ packages/response-pm-zov/response_pm_zov-0.4.tar.gz/response_pm_zov-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="response_pm_zov",
+    version="0.4",
+    packages=find_packages(),
+    description="zov",
+    author="rqiza",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/exam_library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
