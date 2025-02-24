621635b9a2ec406332211da3447110292ff67656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 03:10:50 +0000

    Add package mangokit/1.4.1/mangokit-1.4.1-py3-none-any.whl

---
+++ packages/mangokit/mangokit-1.4.1-py3-none-any.whl/mangokit/__init__.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2024-09-07 22:15
+# @Author : 毛鹏
+import platform
+
+import sys
+
+python_version = sys.version_info
+
+if f"{python_version.major}.{python_version.minor}" != "3.10":
+    raise Exception("必须使用Python3.10.N")
+
+if platform.system() == 'Windows':
+    from mangokit.mango import Mango
+elif platform.system() == 'Linux':
+    from mangokit.mango import Mango
+elif platform.system() == 'Darwin':
+    from mangokit.mango import Mango
+else:
+    raise ImportError("Unsupported operating system")
+
+from mangokit.tools.base_request import *
+from mangokit.tools.log_collector import set_log
+from mangokit.tools.data_processor import *
+from mangokit.tools.database import *
+from mangokit.models.models import *
+from mangokit.tools.decorator import *
+from mangokit.tools.notice import *
+from mangokit.enums.enums import *
+from mangokit.exceptions import MangoKitError
+
+__all__ = [
+    'DataProcessor',
+    'DataClean',
+    'ObtainRandomData',
+    'CacheTool',
+    'CodingTool',
+    'EncryptionTool',
+    'JsonTool',
+    'RandomCharacterInfoData',
+    'RandomNumberData',
+    'RandomStringData',
+    'RandomTimeData',
+
+    'MysqlConingModel',
+    'ResponseModel',
+    'EmailNoticeModel',
+    'TestReportModel',
+    'WeChatNoticeModel',
+    'FunctionModel',
+    'ClassMethodModel',
+
+    'CacheValueTypeEnum',
+    'NoticeEnum',
+
+    'MysqlConnect',
+    'SQLiteConnect',
+    'requests',
+    'async_requests',
+    'set_log',
+    'WeChatSend',
+    'EmailSend',
+
+    'singleton',
+    'convert_args',
+
+    'Mango',
+
+    'MangoKitError',
+]
