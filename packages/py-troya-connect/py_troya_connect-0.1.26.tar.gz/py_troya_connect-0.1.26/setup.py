da93959362de77ebe8a94d1f2af8390db6dc0d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:07 +0000

    Add package py-troya-connect/0.1.26/py_troya_connect-0.1.26.tar.gz

---
+++ packages/py-troya-connect/py_troya_connect-0.1.26.tar.gz/py_troya_connect-0.1.26/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-troya-connect",
+    version="0.1.26",
+    packages=find_packages(),
+    install_requires=[
+        "pywin32>=223",
+    ],
+    author="Tolga Kurtulus",
+    author_email="tolgakurtulus95@gmail.com",
+    description="A Python interface for Attachmate Extra Terminal sessions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tolgakurtuluss/py-troya-connect",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.6",
+)
