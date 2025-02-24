dcb27ca068db1e3053c0dc9460d6134f5d8f7b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 03:11:19 +0000

    Add package homematicip/1.1.7.post1.dev29/homematicip-1.1.7.post1.dev29-py3-none-any.whl

---
+++ packages/homematicip/homematicip-1.1.7.post1.dev29-py3-none-any.whl/homematicip/connection/rate_limited_rest_connection.py
@@ -0,0 +1,31 @@
+import json
+
+import httpx
+
+from homematicip.connection import RATE_LIMITER_FILL_RATE, RATE_LIMITER_TOKENS
+from homematicip.connection.buckets import Buckets
+from homematicip.connection.rest_connection import RestConnection, RestResult
+from homematicip.connection.connection_context import ConnectionContext
+
+
+class RateLimitedRestConnection(RestConnection):
+
+    def __init__(self,
+                 context: ConnectionContext,
+                 tokens: int = RATE_LIMITER_TOKENS,
+                 fill_rate: int = RATE_LIMITER_FILL_RATE,
+                 httpx_client_session: httpx.AsyncClient = None):
+        """Initialize the RateLimitedRestConnection with a token bucket algorithm.
+
+        :param context: The connection context.
+        :param tokens: The number of tokens in the bucket. Default is 10.
+        :param fill_rate: The fill rate of the bucket in tokens per second. Default is 8.
+        :param httpx_client_session: The httpx client session if you want to use a custom one.
+        """
+        super().__init__(context, httpx_client_session=httpx_client_session)
+        self._buckets = Buckets(tokens=tokens, fill_rate=fill_rate)
+
+    async def async_post(self, url: str, data: json = None, custom_header: dict = None) -> RestResult:
+        """Post data to the HomematicIP Cloud API."""
+        await self._buckets.wait_and_take()
+        return await super().async_post(url, data, custom_header)
