7c84ef7307df01421e12ad75b5747da7afd04dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 03:11:09 +0000

    Add package steganon/0.4/steganon-0.4-py3-none-any.whl

---
+++ packages/steganon/steganon-0.4-py3-none-any.whl/steganon/__init__.py
@@ -0,0 +1,12 @@
+from . import api
+from . import tools
+
+from .api import LSB_WS
+from .tools import pngify
+from .version import VERSION
+
+from PIL import Image
+
+
+__version__ = VERSION
+__author__ = 'thenonproton@pm.me'
