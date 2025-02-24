56f00aeb5b5ab6767481acb18d60d84bff845e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 04:04:37 +0000

    Add package a9x-webstatistics/1.5.1/a9x_webstatistics-1.5.1.tar.gz

---
+++ packages/a9x-webstatistics/a9x_webstatistics-1.5.1.tar.gz/a9x_webstatistics-1.5.1/src/a9x_webstatistics/sumNavDayV0001.py
@@ -0,0 +1,61 @@
+from copy import deepcopy
+from operator import itemgetter
+
+# accumulate navigation to the last processed day of the month
+def sumNavDayV0001(d):
+    tmpNav = {}
+    prevMonth = None
+    for x in d['v0001']['days']:
+        if prevMonth is None:
+            prevMonth = x[0:6]
+        if prevMonth != x[0:6]:
+            if len(tmpNav) > 1:
+                d['v0001']['days'][x]['user']['navigation'] = tmpNav
+                tmpNav = {}
+        if len(x) == 8 and 'navigation' in d['v0001']['days'][x]['user']:
+            for ck,cv in sorted(d['v0001']['days'][x]['user']['navigation'].items(), key=itemgetter(1),reverse=False):
+                if ck not in tmpNav:
+                    tmpNav[ck] = cv
+                else:
+                    tmpNav[ck] += cv
+                del d['v0001']['days'][x]['user']['navigation'][ck]
+            del d['v0001']['days'][x]['user']['navigation']
+
+    if len(tmpNav) > 1:
+        d['v0001']['days'][x]['user']['navigation'] = tmpNav
+
+
+    # nav: accumulate nav on the last day of month:
+    lastDay = None
+    for k, v in sorted(d['v0001']['days'].items(), key=itemgetter(0), reverse=True):
+        # consider only days:
+        if len(k) < 8:
+            continue
+
+        # change of month or first iteration:
+        if (lastDay is None 
+                or lastDay[0:6] != k[0:6]):
+            lastDay = k
+
+        # prepare last day and do not process it!
+        if lastDay == k:
+            if 'user' not in d['v0001']['days'][lastDay]:
+                d['v0001']['days'][lastDay]['user'] = {}
+            if 'nav' not in d['v0001']['days'][lastDay]['user']:
+                d['v0001']['days'][lastDay]['user']['nav'] = []
+            continue
+
+        # iterate though previous days
+        if 'nav' in d['v0001']['days'][k]['user']:
+            for e in d['v0001']['days'][k]['user']['nav']:
+                found = False
+                for f in d['v0001']['days'][lastDay]['user']['nav']:            
+                    if (e['s'] == f['s']
+                        and e['t'] == f['t']):
+                        e['c'] += f['c']
+                    found = True
+                    break
+                if found == False:
+                    d['v0001']['days'][lastDay]['user']['nav'].append(e)
+            del d['v0001']['days'][k]['user']['nav']
+    return d
