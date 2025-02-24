abf78e5163c6539f119033eab5615e7384c11d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:33 +0000

    Add package tag2ver/1.4.0/tag2ver-1.4.0.tar.gz

---
+++ packages/tag2ver/tag2ver-1.4.0.tar.gz/tag2ver-1.4.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+import tag2ver
+
+
+def read_text(file_name: str) -> str:
+    with open(file_name, "r") as fh:
+        return fh.read()
+
+
+setuptools.setup(
+    name="tag2ver",
+    version="1.4.0",
+    url=tag2ver.__repository__,
+    license="MIT License",  # Can only have one line `license`; setuptools bug.
+    author=tag2ver.__author__,
+    author_email="howard.lovatt@gmail.com",
+    description=tag2ver.HELP_TEXT,
+    long_description=read_text("README.rst"),
+    long_description_content_type="text/x-rst",
+    py_modules=["tag2ver"],
+    platforms=["any"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
