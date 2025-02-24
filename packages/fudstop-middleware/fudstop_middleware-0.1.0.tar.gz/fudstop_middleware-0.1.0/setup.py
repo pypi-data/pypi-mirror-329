5f7c0e86935c92727116b5eb16de563a6aa7b96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:33 +0000

    Add package fudstop-middleware/0.1.0/fudstop_middleware-0.1.0.tar.gz

---
+++ packages/fudstop-middleware/fudstop_middleware-0.1.0.tar.gz/fudstop_middleware-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fudstop_middleware",  # Package name on PyPI
+    version="0.1.0",  # Increment version for updates
+    author="Charlies Vids",
+    author_email="chuckdustin12@gmail.com",
+    description="A middleware for seamless integration of financial APIs like Polygon, Webull, OCC, and more.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/fudstop_middleware",  # Update with your GitHub repo
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "asyncpg",
+        "aiohttp",
+        "pandas",
+        "numpy",
+        "disnake",
+        "discord_webhook",
+        "fudstop2"
+    ],  # List your dependencies here
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",  # Minimum Python version
+)
