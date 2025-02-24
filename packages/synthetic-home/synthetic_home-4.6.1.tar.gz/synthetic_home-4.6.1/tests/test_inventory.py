2d91dd822180ea63e658850586dbc965b15ac63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:47 +0000

    Add package synthetic-home/4.6.1/synthetic_home-4.6.1.tar.gz

---
+++ packages/synthetic-home/synthetic_home-4.6.1.tar.gz/synthetic_home-4.6.1/tests/test_inventory.py
@@ -0,0 +1,29 @@
+"""Test for inventory."""
+
+from synthetic_home import inventory
+
+
+INVENTORY = """
+---
+areas:
+- name: Backyard
+  id: backyard
+  floor: Ground
+- name: Frontyard
+  id: frontyard
+  floor: Ground
+- name: Living Room
+  id: living_room
+  floor: Ground
+- name: Loft
+  id: loft
+  floor: Upstairs
+- name: Basement
+  id: basement
+"""
+
+def test_floors() -> None:
+    """Test floor logic"""
+    inv = inventory.decode_inventory(INVENTORY)
+    assert inv.floors == set({"Ground", "Upstairs"})
+    assert len(inv.areas) == 5
