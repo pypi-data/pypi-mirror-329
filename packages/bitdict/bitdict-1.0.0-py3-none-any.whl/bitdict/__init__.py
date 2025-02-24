c8fae7afdabd9dc198870a24a1fa8ea8179175ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:04:37 +0000

    Add package bitdict/1.0.0/bitdict-1.0.0-py3-none-any.whl

---
+++ packages/bitdict/bitdict-1.0.0-py3-none-any.whl/bitdict/__init__.py
@@ -0,0 +1,6 @@
+"""bitdict package."""
+
+from bitdict.bitdict import bitdict_factory
+from bitdict.markdown import generate_markdown_tables
+
+__all__ = ["bitdict_factory", "generate_markdown_tables"]
