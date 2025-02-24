7edfb6697c986ba8d97dd3371fcc2f396cbf816e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:59 +0000

    Add package rnet/1.0.1/rnet-1.0.1.tar.gz

---
+++ packages/rnet/rnet-1.0.1.tar.gz/rnet-1.0.1/src/dns.rs
@@ -0,0 +1,39 @@
+use crate::error::DNSResolverError;
+use rquest::dns::{HickoryDnsResolver, LookupIpStrategy};
+use std::sync::{Arc, OnceLock};
+
+/// Initializes and returns a DNS resolver with the specified strategy.
+///
+/// This function initializes a global DNS resolver using the provided lookup IP strategy.
+/// If the DNS resolver has already been initialized, it returns the existing instance.
+///
+/// # Returns
+///
+/// A `Result` containing an `Arc` to the `HickoryDnsResolver` instance, or an error if initialization fails.
+///
+/// # Errors
+///
+/// This function returns an error if the DNS resolver fails to initialize.
+pub fn get_or_try_init() -> crate::Result<Arc<HickoryDnsResolver>> {
+    static DNS_RESOLVER: OnceLock<Result<Arc<HickoryDnsResolver>, &'static str>> = OnceLock::new();
+
+    DNS_RESOLVER
+        .get_or_init(move || {
+            HickoryDnsResolver::new(LookupIpStrategy::Ipv4AndIpv6)
+                .map(Arc::new)
+                .map_err(|err| {
+                    #[cfg(feature = "logging")]
+                    {
+                        log::error!("failed to initialize the DNS resolver: {}", err);
+                    }
+                    #[cfg(not(feature = "logging"))]
+                    {
+                        eprintln!("failed to initialize the DNS resolver: {}", err);
+                    }
+                    "failed to initialize the DNS resolver"
+                })
+        })
+        .as_ref()
+        .map(Arc::clone)
+        .map_err(DNSResolverError::new_err)
+}
---
+++ packages/rnet/rnet-1.0.1.tar.gz/rnet-1.0.1/src/param/mod.rs
@@ -0,0 +1,7 @@
+mod client;
+mod request;
+mod websocket;
+
+pub use self::client::{ClientParams, UpdateClientParams};
+pub use self::request::RequestParams;
+pub use self::websocket::WebSocketParams;
