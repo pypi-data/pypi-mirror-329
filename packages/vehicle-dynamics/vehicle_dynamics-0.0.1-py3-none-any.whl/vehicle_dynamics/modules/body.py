64792a0604f346cfb0e22661efbed752c9881320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:02 +0000

    Add package vehicle-dynamics/0.0.1/vehicle_dynamics-0.0.1-py3-none-any.whl

---
+++ packages/vehicle-dynamics/vehicle_dynamics-0.0.1-py3-none-any.whl/vehicle_dynamics/modules/body.py
@@ -0,0 +1,279 @@
+"""
+Vehicle Dynamic Model - Body Class
+
+@author:   Maikol Funk Drechsler, Yuri Poledna
+
+Funded by the European Union (grant no. 101069576). Views and opinions expressed are however those of the author(s) only and do not necessarily reflect those of the European Union or the European Climate, Infrastructure and Environment Executive Agency (CINEA). Neither the European Union nor the granting authority can be held responsible for them.
+"""
+from vehicle_dynamics.utils.StaticParameters import StaticParameters
+from vehicle_dynamics.structures.StateVector import StateVector
+from vehicle_dynamics.structures.WheelHubForce import WheelHubForce
+from vehicle_dynamics.structures.Displacement import Displacement
+from vehicle_dynamics.utils.CurrentStates import CurrentStates
+from vehicle_dynamics.utils.import_data_CM import import_data_CM
+from vehicle_dynamics.utils.LocalLogger import LocalLogger
+from vehicle_dynamics.structures.OutputStates import OutputStates
+from vehicle_dynamics.structures.Manoeuvre import Manoeuvre
+from vehicle_dynamics.utils.plot_function import plot_function
+
+import numpy as np
+import logging
+from copy import copy
+import yaml
+
+
+class Body():
+    def __init__(self, static_parameters, logger: logging.Logger):
+        self.static_parameters = static_parameters
+        self.logger = logger
+
+    def access_z_road(self, x, y):
+        # TODO implement topography
+        z = 0.
+        return z
+
+    def road(self, current_state):
+        for k in range(4):
+            # displacement.zs[k][2] = self.access_z_road(self.displacement.zs[k][0],self.displacement.zs[k][1])
+            current_state.displacement.road[k] = 0.
+
+        return current_state 
+
+    def body(self, current_state: CurrentStates):
+        """
+         body is a function that calculates the current wheel loads (z)
+
+         Required current_state from static_parameters:
+             1. suspension_spring_rate
+             2. suspension_damping_rate
+
+         Required Arguments:
+             1. f_zr
+                 1.01 wheel_load_z
+            2.displacement
+                2.01 za    :  position of the connection point of the suspension with the dampner spring system
+                2.02 zs    :  Z velocity of the wheel
+                2.03 l_stat:  constant
+                2.04 za_dot:  velocity of the chassis point
+            3.vehicle_fixed2inertial_system
+         Returns:
+             1. f_zr.wheel_load_z
+
+         """
+        vx = current_state.x_a.vx
+        vy = current_state.x_a.vy
+        Ф = current_state.x_a.roll
+        θ = current_state.x_a.pitch
+        Ψ = current_state.x_a.yaw
+        sin_Ф = current_state.x_a.sin_roll
+        cos_Ф = current_state.x_a.cos_roll
+        sin_θ = current_state.x_a.sin_pitch
+        cos_θ = current_state.x_a.cos_pitch
+        sin_Ψ = current_state.x_a.sin_yaw
+        cos_Ψ = current_state.x_a.cos_yaw
+        θdot = current_state.x_a.wx
+        Фdot = current_state.x_a.wy
+        Ψdot = current_state.x_a.wz
+        θdotdot = current_state.x_a.wx_dot
+        Фdotdot = current_state.x_a.wy_dot
+        Ψdotdot = current_state.x_a.wz_dot
+        h = self.static_parameters.body.sz
+        m = self.static_parameters.body.mass
+
+        sum_f_wheel = np.sum(current_state.x_rf.wheel_forces_transformed_force2vehicle_sys, axis=1)
+
+        current_state = self.road(current_state)  # placeholder to consider road status
+
+        # x-position from Vehicle CoG to the front axle [m]
+        long_f = self.static_parameters.body.lf
+        # x-position from Vehicle Cog to the rear axle [m]
+        long_r = self.static_parameters.body.lr
+        # y-position from Vehicle CoG to the left chassis point [m]
+        lat_l = self.static_parameters.body.wl
+        # y-position from Vehicle CoG to the right chassis point  [m]
+        lat_r = self.static_parameters.body.wr
+
+        l = long_r + long_f
+        w = lat_l + lat_r
+
+
+        longitudinal_distance = np.array([long_f, -long_r, long_f, -long_r])
+        lateral_distance = np.array([lat_l, lat_l, - lat_r, - lat_r])
+
+        zCG = current_state.x_a.z
+        static_cg_height = current_state.reference_zCG
+
+        displacement_suspension = zCG - static_cg_height - (longitudinal_distance * sin_θ) + (lateral_distance * sin_Ф) - current_state.displacement.road
+
+        current_state.displacement.suspension_dot = (displacement_suspension - current_state.displacement.suspension) / self.static_parameters.time_step
+        current_state.displacement.suspension = displacement_suspension
+
+        sprung_mass = self.static_parameters.sprung_mass
+
+        damping_force = -(self.static_parameters.suspension.damping_rate * current_state.displacement.suspension_dot)
+        spring_force = -(self.static_parameters.suspension.spring_rate * displacement_suspension)
+
+        anti_roll_bar_force = self.static_parameters.suspension.roll_bar_stiffness * sin_Ф / (2 * lateral_distance)
+
+        suspension_force = spring_force + damping_force + anti_roll_bar_force
+
+        current_state.suspension_force = suspension_force
+
+        front_wheels_sum = sprung_mass[0] + sprung_mass[2]
+        rear_wheels_sum = sprung_mass[1] + sprung_mass[3]
+
+        static_force = self.static_parameters.wd * self.static_parameters.body.mass
+
+
+        ξ_lon = np.array([-1/2, 1/2, -1/2, 1/2])
+        ξ_lat = np.array([- long_r/w, - long_f/w, long_r/w, long_f/w])
+
+        lateral_force_transfer = ξ_lat * (m * current_state.x_a.acc_y * self.static_parameters.suspension.roll_centre_height / self.static_parameters.track_width)
+
+        longitudinal_force_transfer = ξ_lon * (m* current_state.x_a.acc_x * self.static_parameters.suspension.pitch_centre_height / self.static_parameters.wheel_base)
+
+        current_state.f_zr.wheel_load_z = (sprung_mass+self.static_parameters.suspension.unsprung_mass) * self.static_parameters.gravity + suspension_force + longitudinal_force_transfer + lateral_force_transfer
+
+        current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[2, :] = current_state.f_zr.wheel_load_z 
+
+
+        # CHASSIS
+
+        fy_fl = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 0]
+        fy_rl = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 1]
+        fy_fr = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 2]
+        fy_rr = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 3]
+
+        fx_fl = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 0]
+        fx_rl = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 1]
+        fx_fr = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 2]
+        fx_rr = current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 3]
+
+        #self.logger.info(sum_f_wheel[0])
+
+        current_state.x_a.acc_x = (sum_f_wheel[0] - 0.5 * self.static_parameters.aerodynamics_front * vx**2) / m 
+        current_state.x_a.vx += (current_state.x_a.acc_x + vy * Ψdot) * self.static_parameters.time_step
+        
+        if current_state.x_a.vx <= 0.0:
+            current_state.x_a.acc_x = 0.0
+            current_state.x_a.vx = 0.0
+        
+
+        current_state.x_a.acc_y = (sum_f_wheel[1]) / m  
+        current_state.x_a.acc_z = sum(suspension_force) / m
+
+        current_state.x_a.vy += (current_state.x_a.acc_y - vx * Ψdot) * self.static_parameters.time_step
+        current_state.x_a.vz += current_state.x_a.acc_z * self.static_parameters.time_step
+
+        vxx, vyx = vx * cos_Ψ, vy * -sin_Ψ
+        vxy, vyy = vx * sin_Ψ, vy * cos_Ψ
+
+        current_state.x_a.x += (vxx + vyx) * self.static_parameters.time_step  # (vxx + vyx)
+        current_state.x_a.y += (vxy + vyy) * self.static_parameters.time_step  # (vxy + vyy)
+        current_state.x_a.z += current_state.x_a.vz * self.static_parameters.time_step
+
+        # Chassis rotation
+
+        Ix = self.static_parameters.body.i_x_s
+        Iy = self.static_parameters.body.i_y_s
+        Iz = self.static_parameters.body.i_z
+        h = self.static_parameters.body.sz
+        l = self.static_parameters.body.wr    # halftrack
+
+        Fx = sum_f_wheel[0]
+        Fy = sum_f_wheel[1]
+
+        #Mz = ((fy_fl + fy_fr) * self.static_parameters.body.lf - (fy_rl + fy_rr) * self.static_parameters.body.lr + (fx_rr + fx_fr - fx_rl - fx_fl) * l)
+        Mz = ((fy_fl + fy_fr) * self.static_parameters.body.lf - (fy_rl + fy_rr) * self.static_parameters.body.lr + (fx_rr + fx_fr) * (self.static_parameters.body.wr) + (fx_rl + fx_fl) * (-self.static_parameters.body.wl))
+
+        hsr = self.static_parameters.suspension.roll_centre_height - h
+        hsp = self.static_parameters.suspension.pitch_centre_height - h
+        
+
+        current_state.x_a.wx_dot = (np.sum(lateral_distance * suspension_force) + np.sum(sprung_mass) * hsr * (current_state.x_a.acc_y + self.static_parameters.gravity * sin_Ф)) / Ix
+        current_state.x_a.wy_dot = ((-1) * ((np.sum(longitudinal_distance * suspension_force) + np.sum(sprung_mass) * hsp * (current_state.x_a.acc_x - self.static_parameters.gravity * sin_θ)) / Iy))
+        
+        current_state.x_a.wz_dot = Mz / Iz   #- h * (Fx * sin_Ф + Fy * sin_θ * cos_Ф)) / (Ix * (sin_θ**2) + (cos_θ**2) * (Iy * (sin_Ф**2) + Iz * (cos_Ф**2)))
+
+        current_state.x_a.wx += current_state.x_a.wx_dot * self.static_parameters.time_step
+        current_state.x_a.wy += current_state.x_a.wy_dot * self.static_parameters.time_step
+        current_state.x_a.wz += (current_state.x_a.wz_dot * self.static_parameters.time_step)
+
+        # Angular position
+        current_state.x_a.roll += (current_state.x_a.wx * self.static_parameters.time_step)
+        current_state.x_a.pitch += (current_state.x_a.wy * self.static_parameters.time_step)
+        current_state.x_a.yaw += (current_state.x_a.wz * self.static_parameters.time_step)
+
+        return current_state
+
+
+def main(static_parameters, current_state, data, logger, savefig=False):
+    import yaml
+    from tqdm import tqdm
+
+    body = Body(static_parameters, logger)
+
+    test_function = body.body
+    function_name = test_function.__name__
+
+    steering = data["steering"]
+    throttle = data["throttle"]
+    brake = data["brake"]
+    points = len(brake)
+
+    Fx_fl = data["Fx_fl"]
+    Fx_fr = data["Fx_fr"]
+    Fx_rl = data["Fx_rl"]
+    Fx_rr = data["Fx_rr"]
+
+    Fy_fl = data["Fy_fl"]
+    Fy_fr = data["Fy_fr"]
+    Fy_rl = data["Fy_rl"]
+    Fy_rr = data["Fy_rr"]
+
+    time = data["time"]
+    manoeuvre = Manoeuvre(steering, throttle, brake, time)
+    logger.info("loaded SimulationData")
+    simulation_range = range(0, len(time))
+    output_states = OutputStates()
+
+    for i in tqdm(simulation_range):
+        forces_in_x = [Fx_fl[i], Fx_rl[i], Fx_fr[i], Fx_rr[i]]
+        forces_in_y = [Fy_fl[i], Fy_rl[i], Fy_fr[i], Fy_rr[i]]
+        current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[0, :] = forces_in_x
+        current_state.x_rf.wheel_forces_transformed_force2vehicle_sys[1, :] = forces_in_y
+
+        output_states.set_states(test_function(current_state))
+    plot_function(output_states, manoeuvre, data, savefig, 
+                  plot_type="body",
+                  xlim=(-.25, data["time"][-1] - data["time"][0]+0.25),
+                  static_parameters=static_parameters)
+
+
+if __name__ == '__main__':
+    import pickle
+    test_function = Body
+    function_name = test_function.__name__
+    logger = LocalLogger(function_name).logger
+    logger.setLevel('INFO')
+
+    PATH_TO_DATA = "../../exampledata/Braking.pickle"
+    with open(PATH_TO_DATA,"rb")as handle:
+        data=pickle.load(handle)
+
+    from vehicle_dynamics.structures.StateVector import StateVector
+    from vehicle_dynamics.utils.StaticParameters import StaticParameters
+    static_parameters = StaticParameters("../../bmw_m8.yaml", freq = 1000)
+
+    initial_state = StateVector(x = np.array( data["Rel_pos_x"][0]),
+                                y = np.array(data["Rel_pos_y"][0]),
+                                vx = np.array( data["Velocity_X"][0]),
+                                yaw = np.array( data["Yaw"][0]))    
+                                #yaw = np.array( (sync_adma[0].ins_yaw+125)* 3.14/180.0))
+
+
+    current_state = CurrentStates(static_parameters, logger=logger, initial_state=initial_state)
+    logger.info("loaded current_state")
+
+    logger.info("loaded SimulationData")
+    main(static_parameters, current_state, data, logger, False)
---
+++ packages/vehicle-dynamics/vehicle_dynamics-0.0.1-py3-none-any.whl/vehicle_dynamics/modules/powertrain.py
@@ -0,0 +1,255 @@
+from vehicle_dynamics.utils.StaticParameters import StaticParameters
+from vehicle_dynamics.utils.LocalLogger import LocalLogger
+from vehicle_dynamics.utils.CurrentStates import CurrentStates
+from vehicle_dynamics.utils.import_data_CM import import_data_CM
+from vehicle_dynamics.utils.plot_function import plot_function
+from vehicle_dynamics.utils.StaticParameters import StaticParameters
+
+from vehicle_dynamics.structures.OutputStates import OutputStates
+from vehicle_dynamics.structures.Manoeuvre import Manoeuvre
+
+import logging
+from scipy.interpolate import interp1d
+import numpy as np
+
+
+class Powertrain(object):
+    """
+        Powertrain is a class calculates the current Torque delivered by the engine to the wheels.
+        For that is necessary select the proper gear and find the torque 
+        converter multiplicator.
+        gear_change() function uses engine angular velocity and car linear speed (vx)
+        torque_converter() multiplication is based on input and output rotation ratio.
+
+        Required current_state from static_parameters:
+            1.engine_w_table,
+            2.torque_max_table,
+            3.gear_ratio,
+            4.diff,
+            5.diff_ni,
+            6.transmition_ni,
+            7.gear_selection,
+            8.engine_inertia,
+            9.axel_inertia,
+            10.gearbox_inertia,
+            11.shaft_inertia,
+            12.wheel_inertia,
+            13.max_brake_torque,
+            14.brake_bias,
+
+        Required Arguments:
+            1. throttle
+            2. brake
+            3. acc_x
+            4.gear[t-1]
+            5. vx
+
+        Returns: (current_state)
+            1. Engine_w
+            2. Gear
+            3. Torque of the engine on the wheels
+            4. prev_gear
+            5. current_gear
+
+    """
+
+    def __init__(self, static_parameters: StaticParameters, logger: LocalLogger):
+        super(Powertrain, self).__init__()
+        self.logger = logger
+        self.torque_interpolation = interp1d(static_parameters.powertrain.engine.w_table, static_parameters.powertrain.engine.torque_max)
+        self.MIN_GEAR_CHANGE_INTERVAL = static_parameters.powertrain.gearbox.MIN_GEAR_CHANGE_INTERVAL
+        self.CONVERTER_SYNC_TIME = static_parameters.powertrain.torque_converter.CONVERTER_SYNC_TIME
+        self.current_sync = 0
+        self.current_grace_period = 0
+        self.static_parameters = static_parameters
+        self.μ_funct = interp1d(static_parameters.powertrain.torque_converter.speed_ratio,
+                                static_parameters.powertrain.torque_converter.ratio)
+        self.k_in_funct = interp1d(np.array(np.linspace(0., 1., len(static_parameters.powertrain.torque_converter.factor))), np.array(static_parameters.powertrain.torque_converter.factor))
+        self.previous_throtle = 0
+
+    def gear_change_rpm(self, current_state: CurrentStates, throttle: float):
+        if self.current_grace_period > 0:
+            self.current_grace_period -= 1
+            return False
+
+        # Gearbox up or down shifting
+        if current_state.engine_w > self.static_parameters.powertrain.gearbox.gear_max_rpm[round(throttle * 10)][current_state.gear]:
+            if current_state.gear + 1 >= self.static_parameters.powertrain.gearbox.gear_ratio.size:
+                return False
+            current_state.gear = current_state.gear + 1
+            self.current_grace_period = self.MIN_GEAR_CHANGE_INTERVAL
+            return True
+
+        elif current_state.engine_w < self.static_parameters.powertrain.gearbox.gear_min_rpm[round(throttle * 10)][current_state.gear]:
+            if current_state.gear - 1 < 1:
+                return False
+            self.current_grace_period = self.MIN_GEAR_CHANGE_INTERVAL
+            current_state.gear = current_state.gear - 1
+            return True        
+
+    def powertrain(self, current_state: CurrentStates, throttle: float, brake: float):
+        # Update Converter engine_w
+        # Based on page 3 from Generating Proper Integrated Dynamic Models for Vehicle Mobility (Loucas S. Louca)
+        # Based on the whell velocity and engine engine_w the torque on the
+
+        if self.gear_change_rpm(current_state, throttle):
+            self.current_sync = self.CONVERTER_SYNC_TIME
+
+        if self.current_sync > 0:
+            torque_converter_out = 0
+            self.current_sync -= 1
+            throttle = 0.2
+
+        # Calculate torque provided by the engine based on the engine engine_w
+        torque_available = self.torque_interpolation(current_state.engine_w)
+        
+        if self.previous_throtle - throttle != 0 :
+            self.current_grace_period += 2
+            self.previous_throtle = throttle
+        else:
+            self.previous_throtle = throttle
+        
+        gas_pedal = throttle
+
+        # checking for idle state
+        if current_state.engine_w < self.static_parameters.powertrain.engine.idle_rpm:
+            #throttle = 1
+            current_state.engine_w = self.static_parameters.powertrain.engine.idle_rpm
+
+        # find the torque delivered by the engine
+        engine_torque = (throttle * torque_available)
+
+        final_ratio = self.static_parameters.powertrain.gearbox.gear_ratio[current_state.gear] * self.static_parameters.powertrain.differential.ratio
+
+        turbine_w = final_ratio * np.mean(current_state.wheel_w_vel)
+
+        torque_converter_ratio = turbine_w / current_state.engine_w
+
+
+        if torque_converter_ratio >= self.static_parameters.powertrain.torque_converter.lock_up_ratio:
+            # Clutch Mode
+            current_state.engine_w = turbine_w
+            torque_converter_out = engine_torque
+        
+        else:
+            torque_converter_ratio = 0 if torque_converter_ratio < 0 else torque_converter_ratio
+            # Torque Converter
+            k_in = self.k_in_funct(torque_converter_ratio)
+            μ = self.μ_funct(torque_converter_ratio)
+
+            torque_converter_in = k_in * (current_state.engine_w ** 2)
+            torque_converter_out = μ * torque_converter_in
+
+            engine_wdot = (engine_torque - torque_converter_in) / self.static_parameters.powertrain.engine.inertia
+            current_state.engine_w = (current_state.engine_w + engine_wdot * self.static_parameters.time_step)
+
+        # Gillespie equation 2-7
+        traction_torque = torque_converter_out * final_ratio * self.static_parameters.powertrain.gearbox.efficiency
+        
+        #if traction_torque > 5000.0:
+        #    traction_torque = 5000.0
+
+        wheel_torque = traction_torque * self.static_parameters.powertrain.bias
+
+        #if brake > 0:
+        #    wheel_torque = np.zeros(4)
+
+        # --------------------Break Torque -------------------------
+        brake_torque = brake * self.static_parameters.brake.max_braking_torque * self.static_parameters.brake.brake_bias
+
+        # -------------------- Total Torque -------------------------
+        if np.mean((wheel_torque - brake_torque)) <= 0 and current_state.x_a.vx <= 1e-6:
+            current_state.powertrain_net_torque = np.zeros(4)
+            self.logger.debug(f"case 1")
+
+        elif current_state.x_a.vx <= 1e-6 and brake > 0:
+            # IDLE state
+            current_state.powertrain_net_torque = np.zeros(4)
+            self.logger.debug(f"case 2")
+
+        else:
+            current_state.powertrain_net_torque = wheel_torque - brake_torque 
+
+        # Unecessary but for code safety
+        if current_state.engine_w > self.static_parameters.powertrain.engine.maximum_rpm:
+            current_state.engine_w = self.static_parameters.powertrain.engine.maximum_rpm
+
+        return current_state
+
+
+def main(static_parameters, current_state, data, logger, savefig=False):
+    import yaml
+    from tqdm import tqdm
+
+    steering = data["steering"]
+    throttle = data["throttle"]
+    brake = data["brake"]
+
+    time = data["time"]
+
+    points = len(brake)
+
+    manoeuvre = Manoeuvre(steering, throttle, brake, time)
+    logger.info("loaded SimulationData")
+
+
+    fl,rl,fr,rr = (data["v_wheel_fl"],
+                   data["v_wheel_fr"],
+                   data["v_wheel_rl"],
+                   data["v_wheel_rr"])
+    
+    simulation_range = range(0, len(time))
+
+    wheel_w_vel = np.array([[fl[i],rl[i],fr[i],rr[i]] for i in range(len(fl))])
+    vx = data ["Velocity_X"]
+    vy = data ["Velocity_Y"]
+    
+    yawrate = data["Yaw_Velocity"]
+
+    powertrain = Powertrain(static_parameters, logger)
+    test_function = powertrain.powertrain
+    output_states = OutputStates()
+
+    for i in tqdm(simulation_range):
+        current_state.x_a.vx = vx[i]
+        current_state.wheel_w_vel = wheel_w_vel[i]
+        try:
+            current_state = test_function(current_state, 
+                                          throttle = manoeuvre[i].throttle,
+                                          brake = manoeuvre[i].brake)
+            output_states.set_states(current_state)
+        except ValueError as e:
+            print(e)
+            output_states.padding(len(manoeuvre) - len(output_states))
+            plot_function(output_states, manoeuvre, sync_adma, sync_zgw, savefig, plot_type="powertrain", xlim=(-.25,sync_zgw[-1].timestamp-sync_zgw[0].timestamp + 0.25))
+            exit()
+
+
+    plot_function(output_states, manoeuvre, data, savefig, plot_type="powertrain", xlim=(-.25,time[-1]-time[0] + 0.25))
+
+
+if __name__ == '__main__':
+    import pickle
+
+    PATH_TO_DATA = "../../exampledata/Braking.pickle"
+    with open(PATH_TO_DATA,"rb")as handle:
+        data=pickle.load(handle)
+
+    from vehicle_dynamics.structures.StateVector import StateVector
+    from vehicle_dynamics.utils.StaticParameters import StaticParameters
+    static_parameters = StaticParameters("../../bmw_m8.yaml", freq = 1000)
+    
+    function_name = "Powertrain"
+    logger = LocalLogger(function_name).logger
+    logger.info("loaded current_state")
+    from vehicle_dynamics.utils.StaticParameters import StaticParameters
+    static_parameters = StaticParameters("../../BMW_M8.yaml")
+    from vehicle_dynamics.structures.StateVector import StateVector    
+
+    initial_state = StateVector(x = np.array( data["Rel_pos_x"][0]),
+                                y = np.array(data["Rel_pos_y"][0]),
+                                vx = np.array( data["Velocity_X"][0]),
+                                yaw = np.array( data["Yaw"][0]))    
+
+    current_state = CurrentStates(static_parameters, logger=logger,initial_state=initial_state)
+    main(static_parameters, current_state, data, logger, False)
