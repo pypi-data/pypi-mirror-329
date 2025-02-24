8accf685d5fe402ca0f7cfa50f3e3973a4e687d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:04:42 +0000

    Add package pyvdv736/0.0.8/pyvdv736-0.0.8.tar.gz

---
+++ packages/pyvdv736/pyvdv736-0.0.8.tar.gz/pyvdv736-0.0.8/demo/demo_subscriber.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+import time
+
+from vdv736.subscriber import Subscriber
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
+
+with Subscriber('PY_TEST_SUBSCRIBER', './demo/demo_participants.yaml') as subscriber:
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'ps':
+    
+        time.sleep(15)
+        sid = subscriber.subscribe('PY_TEST_PUBLISHER')
+        time.sleep(10)
+        subscriber.status(sid)
+        time.sleep(10)
+        subscriber.unsubscribe(sid)
+        sid = subscriber.subscribe('PY_TEST_PUBLISHER')
+
+        while True:
+            time.sleep(60)
+            subscriber.status()
+
+    else:
+
+        time.sleep(25)
+        subscriber.request('PY_TEST_PUBLISHER')
+        print(subscriber.get_situations())
+
+        while True:
+            pass
