ad359997c50870e6727fe0bc7dcd5d814d6654d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 04:04:24 +0000

    Add package tinyprogress/1.1.0/tinyprogress-1.1.0.tar.gz

---
+++ packages/tinyprogress/tinyprogress-1.1.0.tar.gz/tinyprogress-1.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tinyprogress",  # Cambia esto por el nombre de tu paquete
+    version="1.1.0",
+    author="Croketillo",
+    author_email="croketillo@gmail.com",
+    description="A lightweight progress bar for Python without dependencies.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/croketillo/tinyprogress",  # Cambia por la URL de tu repositorio
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules', 
+        'Topic :: Utilities'
+    ],
+    python_requires='>=3.6',
+)
