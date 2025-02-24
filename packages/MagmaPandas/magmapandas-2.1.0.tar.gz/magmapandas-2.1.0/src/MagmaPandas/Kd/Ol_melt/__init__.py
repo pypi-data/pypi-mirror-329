11f7dd0fe072ea4b1391adb3bb4fe4e12e529275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:20 +0000

    Add package MagmaPandas/2.1.0/magmapandas-2.1.0.tar.gz

---
+++ packages/MagmaPandas/magmapandas-2.1.0.tar.gz/magmapandas-2.1.0/src/MagmaPandas/Kd/Ol_melt/__init__.py
@@ -0,0 +1,8 @@
+"""
+Module with models for calculating melt Fe-Mg partition coefficients between olivine and melt as:
+
+ (Fe\ :sup:`2+` / Mg)\ :sub:`ol` / (Fe\ :sup:`2+` / Mg)\ :sub:`melt`
+"""
+
+from MagmaPandas.Kd.Ol_melt.Kd_olmelt_FeMg_calculate import *
+from MagmaPandas.Kd.Ol_melt.Kd_olmelt_FeMg_models import Kd_olmelt_FeMg_models_dict
