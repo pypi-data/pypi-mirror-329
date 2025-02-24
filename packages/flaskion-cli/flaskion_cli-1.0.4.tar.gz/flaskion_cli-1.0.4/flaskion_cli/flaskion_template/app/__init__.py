eccbb804e7c303746ec5a41c3cadcca4d925bcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:46 +0000

    Add package flaskion-cli/1.0.4/flaskion_cli-1.0.4.tar.gz

---
+++ packages/flaskion-cli/flaskion_cli-1.0.4.tar.gz/flaskion_cli-1.0.4/flaskion_cli/flaskion_template/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.routes import register_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("app.config.Config")
+
+    # Register all routes
+    register_routes(app)
+
+    return app
