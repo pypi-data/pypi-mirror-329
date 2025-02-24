4df3978d7cd91dde998c2ae327f7906fa7d34cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 04:04:14 +0000

    Add package roboquant/1.0.0/roboquant-1.0.0-py3-none-any.whl

---
+++ packages/roboquant/roboquant-1.0.0-py3-none-any.whl/roboquant/__init__.py
@@ -0,0 +1,51 @@
+"""
+The `roboquant` package contains the `run` method and a number of shared classes
+like `Account`, `Asset` and `Event`.
+"""
+
+__version__ = "1.0.0"
+
+import logging
+
+from roboquant import brokers
+from roboquant import feeds
+from roboquant import journals
+from roboquant import strategies
+from roboquant import ml
+from .account import Account, Position
+from .config import Config
+from .event import Event, PriceItem, Bar, Trade, Quote
+from .order import Order
+from .monetary import Amount, Wallet
+from .asset import Asset, Stock, Crypto, Option
+from .run import run
+from .timeframe import Timeframe, utcnow
+
+logger = logging.getLogger(__name__)
+logger.info("roboquant version=%s", __version__)
+
+__all__ = [
+    "brokers",
+    "feeds",
+    "journals",
+    "strategies",
+    "ml",
+    "Account",
+    "Position",
+    "Config",
+    "Event",
+    "PriceItem",
+    "Bar",
+    "Trade",
+    "Quote",
+    "Order",
+    "Amount",
+    "Wallet",
+    "Asset",
+    "Stock",
+    "Crypto",
+    "Option",
+    "run",
+    "Timeframe",
+    "utcnow",
+]
