bb1cd02a2330c2cf7528fb841e77c2415afc3bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 04:04:35 +0000

    Add package diskstat/2025.2.23.2/diskstat-2025.2.23.2.tar.gz

---
+++ packages/diskstat/diskstat-2025.2.23.2.tar.gz/diskstat-2025.2.23.2/hatch_build.py
@@ -0,0 +1,40 @@
+import contextlib
+from typing import Any
+import subprocess
+import shutil
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+@contextlib.contextmanager
+def CD(d: str):
+    import os
+
+    old = os.getcwd()
+    os.chdir(d)
+    yield
+    os.chdir(old)
+
+
+def build():
+    with CD("diskstat_api"):
+        subprocess.run("go build diskstat-api.go".split(), check=True)
+        shutil.copy("diskstat-api.exe", "../diskstat/")
+    with CD("diskstat"):
+        subprocess.run("fyne package".split(), check=True)
+
+
+class CustomBuilder(BuildHookInterface):
+    def initialize(
+        self,
+        version: str,  # noqa: ARG002
+        build_data: dict[str, Any],
+    ) -> None:
+        build_data['tag'] = 'py3-none-win_amd64'
+        if self.target_name == "sdist":
+            return
+        build()
+
+
+if __name__ == "__main__":
+    build()
