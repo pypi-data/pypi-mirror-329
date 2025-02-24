ee694e4eed96d02d7a2391e89edf449e4b7447bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:36 +0000

    Add package orionis/0.85.0/orionis-0.85.0.tar.gz

---
+++ packages/orionis/orionis-0.85.0.tar.gz/orionis-0.85.0/orionis/luminate/providers/commands/reactor_commands_service_provider.py
@@ -0,0 +1,19 @@
+from orionis.luminate.providers.service_provider import ServiceProvider
+from orionis.luminate.services.commands.reactor_commands_service import ReactorCommandsService
+
+class ReactorCommandsServiceProvider(ServiceProvider):
+
+    def register(self) -> None:
+        """
+        Registers services or bindings into the given container.
+        """
+        self._container_id = self.app.singleton(ReactorCommandsService)
+
+    def boot(self,) -> None:
+        """
+        Boot the service provider.
+
+        This method is intended to be overridden by subclasses to perform
+        any necessary bootstrapping or initialization tasks.
+        """
+        self.app.make(self._container_id)
