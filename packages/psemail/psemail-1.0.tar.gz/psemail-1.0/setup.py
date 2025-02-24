48724c8b2a199a6710cb804795e11c0696ec6050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 04:04:34 +0000

    Add package psemail/1.0/psemail-1.0.tar.gz

---
+++ packages/psemail/psemail-1.0.tar.gz/psemail-1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="psemail",
+    version="1.0",
+    packages=find_packages(include=['psemail', 'psemail.*']),
+    install_requires=[
+        "python-dotenv"
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
