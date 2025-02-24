6a33e27026f0248386372aaab64788b6911cb44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:55 +0000

    Add package rclone-api/1.0.83/rclone_api-1.0.83-py2.py3-none-any.whl

---
+++ packages/rclone-api/rclone_api-1.0.83-py2.py3-none-any.whl/rclone_api/types.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class ModTimeStrategy(Enum):
+    USE_SERVER_MODTIME = "use-server-modtime"
+    NO_MODTIME = "no-modtime"
+
+
+class ListingOption(Enum):
+    DIRS_ONLY = "dirs-only"
+    FILES_ONLY = "files-only"
+    ALL = "all"
+
+
+class Order(Enum):
+    NORMAL = "normal"
+    REVERSE = "reverse"
+    RANDOM = "random"
+
+
+# class GroupingOption(Enum):
+#     BUCKET = "bucket"
+#     REMOTE = "remote"
+
+
+@dataclass
+class SizeResult:
+    """Size result dataclass."""
+
+    prefix: str
+    total_size: int
+    file_sizes: dict[str, int]
