4a29d1347bbcb8a5eaeda37d6ecf3b21848679ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:44 +0000

    Add package amsdal-glue/0.1.0/amsdal_glue-0.1.0-py3-none-any.whl

---
+++ packages/amsdal-glue/amsdal_glue-0.1.0-py3-none-any.whl/amsdal_glue/pipelines/services/router_mixin.py
@@ -0,0 +1,50 @@
+from typing import Any
+
+from amsdal_glue_core.common.data_models.results.data import DataResult
+
+from amsdal_glue.pipelines.manager import PipelineManager
+
+
+class PipelineServiceMixin:
+    def execute(self, *args: Any, **kwargs: Any) -> Any:
+        from amsdal_glue_core.containers import Container
+
+        service_type = Container.services.get_dependency_type(self)
+        pipeline = Container.managers.get(PipelineManager)
+        workflow = pipeline.iter_container(service_type)
+        first_container = next(workflow)
+
+        with Container.switch(first_container.name):
+            service = first_container.services.get(service_type)
+            result = service.execute(*args, **kwargs)
+
+        for container in workflow:
+            with Container.switch(container.name):
+                service = container.services.get(service_type)
+                _state_result = service.execute(*args, **kwargs)
+
+                if isinstance(_state_result, DataResult) and not _state_result.success:
+                    return _state_result
+
+        return result
+
+
+class AsyncPipelineServiceMixin:
+    async def execute(self, *args: Any, **kwargs: Any) -> Any:
+        from amsdal_glue_core.containers import Container
+
+        service_type = Container.services.get_dependency_type(self)
+        pipeline = Container.managers.get(PipelineManager)
+        workflow = pipeline.iter_container(service_type)
+        first_container = next(workflow)
+
+        with Container.switch(first_container.name):
+            service = first_container.services.get(service_type)
+            result = await service.execute(*args, **kwargs)
+
+        for container in workflow:
+            with Container.switch(container.name):
+                service = container.services.get(service_type)
+                await service.execute(*args, **kwargs)
+
+        return result
---
+++ packages/amsdal-glue/amsdal_glue-0.1.0-py3-none-any.whl/amsdal_glue/task_executors/parallel_thread_executor.py
@@ -0,0 +1,55 @@
+# mypy: disable-error-code="type-abstract"
+from concurrent.futures import ThreadPoolExecutor
+
+from amsdal_glue_core.common.executors.interfaces import ParallelExecutor
+from amsdal_glue_core.common.executors.interfaces import SequentialExecutor
+from amsdal_glue_core.common.workflows.chain import ChainTask
+from amsdal_glue_core.common.workflows.group import GroupTask
+from amsdal_glue_core.common.workflows.task import Task
+
+
+class ThreadParallelExecutor(ParallelExecutor):
+    """
+    ThreadParallelExecutor is responsible for executing tasks in parallel using threads.
+    """
+
+    def execute_parallel(
+        self,
+        tasks: list[Task],
+        transaction_id: str | None,
+        lock_id: str | None,
+    ):
+        """
+        Executes the given list of tasks in parallel using threads.
+
+        Args:
+            tasks (list[Task]): The list of tasks to be executed in parallel.
+            transaction_id (str | None): The transaction ID to be used during execution.
+            lock_id (str | None): The lock ID to be used during execution.
+        """
+        with ThreadPoolExecutor() as pool_executor:
+            pool_executor.map(lambda task: self.map_fn(task, transaction_id=transaction_id, lock_id=lock_id), tasks)
+
+    def map_fn(
+        self,
+        task: Task,
+        transaction_id: str | None,
+        lock_id: str | None,
+    ) -> None:
+        """
+        Maps and executes a single task, handling different task types.
+
+        Args:
+            task (Task): The task to be executed.
+            transaction_id (str | None): The transaction ID to be used during execution.
+            lock_id (str | None): The lock ID to be used during execution.
+        """
+        from amsdal_glue_core.containers import Container
+
+        if isinstance(task, ChainTask):
+            executor = Container.executors.get(SequentialExecutor)
+            executor.execute_sequential(task.tasks, task.final_task, transaction_id=transaction_id, lock_id=lock_id)
+        elif isinstance(task, GroupTask):
+            self.execute_parallel(task.tasks, transaction_id=transaction_id, lock_id=lock_id)
+        else:
+            task.execute(transaction_id=transaction_id, lock_id=lock_id)
