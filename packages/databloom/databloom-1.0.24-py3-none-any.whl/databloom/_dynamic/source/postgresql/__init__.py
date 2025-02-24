21a0945badb2908b9c7775e4b691b630cdf41f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:46 +0000

    Add package databloom/1.0.24/databloom-1.0.24-py3-none-any.whl

---
+++ packages/databloom/databloom-1.0.24-py3-none-any.whl/databloom/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,43 @@
+# --- render code block -----
+
+from .bumdata import bumdata
+from .marketing_source2 import marketing_source2
+from .sale_source import sale_source
+from .hainv4test2 import hainv4test2
+from .hainv4_source import hainv4_source
+from .neww222 import neww222
+from .chinhtt_marketing import chinhtt_marketing
+from .dep-1 import dep-1
+from .dep-2 import dep-2
+from .dep-3 import dep-3
+from .dep-4 import dep-4
+from .dep-5 import dep-5
+
+__all__ = [
+    
+    "bumdata",
+    
+    "marketing_source2",
+    
+    "sale_source",
+    
+    "hainv4test2",
+    
+    "hainv4_source",
+    
+    "neww222",
+    
+    "chinhtt_marketing",
+    
+    "dep-1",
+    
+    "dep-2",
+    
+    "dep-3",
+    
+    "dep-4",
+    
+    "dep-5",
+    
+]
+# --- render code block -----
---
+++ packages/databloom/databloom-1.0.24-py3-none-any.whl/databloom/_dynamic/source/postgresql/dep-3.py
@@ -0,0 +1,8 @@
+# --- render code block -----
+from databloom._core.postgres_core import PostgresqlBase
+
+class dep-3(PostgresqlBase):
+    def __init__(self, get_credential_from_server) -> None:
+        self.id = "67bc10cdf1ba9a2bf4a678bb"
+        self.credential = get_credential_from_server(self.id)
+# --- render code block -----
