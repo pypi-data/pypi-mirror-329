af04d0562f6860c27809db9f3e6ff837b37a9c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:52 +0000

    Add package e2L/0.2.1/e2l-0.2.1.tar.gz

---
+++ packages/e2L/e2l-0.2.1.tar.gz/e2l-0.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="e2L",
+    version="0.2.1",
+    license="MIT",
+    author="Raphaël Nussbaumer",
+    author_email="rafnuss@gmail.com",
+    description="Generate custom PDF checklists with eBird occurrence data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zoziologie/ebird2latex",
+    download_url="https://github.com/Zoziologie/ebird2latex/archive/refs/tags/v0.0.3.tar.gz",
+    py_modules=["e2L"],
+    install_requires=["requests", "lxml"],
+    keywords=["python", "eBird", "LaTeX"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+)
