05f85fadf475dffeebb5adcd9d0e8d57c5bd0e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:04:35 +0000

    Add package prettyfmt/0.1.7/prettyfmt-0.1.7.tar.gz

---
+++ packages/prettyfmt/prettyfmt-0.1.7.tar.gz/prettyfmt-0.1.7/src/prettyfmt/__init__.py
@@ -0,0 +1,20 @@
+from .prettyfmt import *  # noqa: F403
+from strif import abbrev_list, abbrev_str, quote_if_needed, single_line
+
+__all__ = (  # noqa: F405
+    "abbrev_obj",
+    "abbrev_on_words",
+    "abbrev_phrase_in_middle",
+    "fmt_age",
+    "fmt_time",
+    "fmt_size_human",
+    "fmt_size_dual",
+    "fmt_words",
+    "fmt_paras",
+    "sanitize_title",
+    # Re-export strif functions for convenience:
+    "abbrev_str",
+    "abbrev_list",
+    "single_line",
+    "quote_if_needed",
+)
