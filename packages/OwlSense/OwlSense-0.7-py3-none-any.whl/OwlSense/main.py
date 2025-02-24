d015d77fee1d94e579f5f67c8cff4b1b129088a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:46 +0000

    Add package OwlSense/0.7/OwlSense-0.7-py3-none-any.whl

---
+++ packages/OwlSense/OwlSense-0.7-py3-none-any.whl/OwlSense/main.py
@@ -0,0 +1,41 @@
+import openai
+
+def OwlSense():
+    print("Owl")
+    
+def ottaha():
+    camel_ascii = r"""
+     ___.-''''-.
+    /___  @    |
+    ',,,,.     |         _.'''''''._
+         '     |        /           \
+         |     \    _.-'             \
+         |      '.-'                  '-.
+         |                               ',
+         |                                '',
+          ',,-,                           ':;
+               ',,| ;,,                 ,' ;;
+                  ! ; !'',,,',',,,,'!  ;   ;:
+                 : ;  ! !       ! ! ;  ;   :;
+                 ; ;   ! !      ! !  ; ;   ;,
+                ; ;    ! !     ! !   ; ;     
+                ; ;    ! !    ! !     ; ;
+               ;,,      !,!   !,!     ;,;
+               /_I      L_I   L_I     /_I
+"""
+    print(camel_ascii)
+    print("Ottaha")
+
+    
+    
+    
+    
+# def gpt(OPENAI_API_KEY , models , promopt):
+
+#     openai.api_key = OPENAI_API_KEY
+#     response = openai.ChatCompletion.create(
+#         model=models,
+#         messages=[{"role": "user", "content": f"{promopt}"}]
+#     )
+#     return(response.choices[0].message.content)
+    
