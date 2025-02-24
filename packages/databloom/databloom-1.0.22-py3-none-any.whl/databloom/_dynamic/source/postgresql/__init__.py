1682dc2918a40fc75f521f81f0eef3c9a01f279f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:32 +0000

    Add package databloom/1.0.22/databloom-1.0.22-py3-none-any.whl

---
+++ packages/databloom/databloom-1.0.22-py3-none-any.whl/databloom/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,34 @@
+# --- render code block -----
+
+from .bumdata import bumdata
+from .marketing_source2 import marketing_source2
+from .sale_source import sale_source
+from .hainv4test2 import hainv4test2
+from .hainv4_source import hainv4_source
+from .neww222 import neww222
+from .chinhtt_marketing import chinhtt_marketing
+from .dep-1 import dep-1
+from .dep-2 import dep-2
+
+__all__ = [
+    
+    "bumdata",
+    
+    "marketing_source2",
+    
+    "sale_source",
+    
+    "hainv4test2",
+    
+    "hainv4_source",
+    
+    "neww222",
+    
+    "chinhtt_marketing",
+    
+    "dep-1",
+    
+    "dep-2",
+    
+]
+# --- render code block -----
---
+++ packages/databloom/databloom-1.0.22-py3-none-any.whl/databloom/_dynamic/source/postgresql/dep-1.py
@@ -0,0 +1,8 @@
+# --- render code block -----
+from databloom._core.postgres_core import PostgresqlBase
+
+class dep-1(PostgresqlBase):
+    def __init__(self, get_credential_from_server) -> None:
+        self.id = "67bc108cf1ba9a2bf4a678b9"
+        self.credential = get_credential_from_server(self.id)
+# --- render code block -----
