a37a48dbff6133c0ce9b48baef28cfa8accd8cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:48 +0000

    Add package orionis/0.93.0/orionis-0.93.0.tar.gz

---
+++ packages/orionis/orionis-0.93.0.tar.gz/orionis-0.93.0/orionis/luminate/providers/commands/scheduler_provider.py
@@ -0,0 +1,19 @@
+from orionis.luminate.providers.service_provider import ServiceProvider
+from orionis.luminate.services.commands.scheduler_service import ScheduleService
+
+class ScheduleServiceProvider(ServiceProvider):
+
+    def register(self) -> None:
+        """
+        Registers services or bindings into the given container.
+        """
+        self._container_id = self.app.bind(ScheduleService)
+
+    def boot(self,) -> None:
+        """
+        Boot the service provider.
+
+        This method is intended to be overridden by subclasses to perform
+        any necessary bootstrapping or initialization tasks.
+        """
+        self.app.make(self._container_id)
