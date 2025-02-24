778da6d47b59097db1f21d0fcba4673725e54940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 03:11:13 +0000

    Add package gapSolutions-client/0.1.25/gapSolutions_client-0.1.25.tar.gz

---
+++ packages/gapSolutions-client/gapSolutions_client-0.1.25.tar.gz/gapSolutions_client-0.1.25/gapSolutions_client/__init__.py
@@ -0,0 +1,7 @@
+from .file import file  
+from .folder.folder import list_files_in_folder,folder_create,folder_delete,folder_list,folder_update  
+
+
+def hello():
+    return "Hello from gapSolutions_client!"
+
