7af867242cbfc331084c59b2de8b37f8073500b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:09 +0000

    Add package ipybbycell/0.3.25/ipybbycell-0.3.25-py2.py3-none-any.whl

---
+++ packages/ipybbycell/ipybbycell-0.3.25-py2.py3-none-any.whl/ipybbycell-extension/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) boyuai.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (0, 3, 14)
+__version__ = ".".join(map(str, version_info))
