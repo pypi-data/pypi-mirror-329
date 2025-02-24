ff0f5dac9b75cddfb0bf9ddb50263b9d0a2c02f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 03:10:48 +0000

    Add package muqi/0.1.0/muqi-0.1.0.tar.gz

---
+++ packages/muqi/muqi-0.1.0.tar.gz/muqi-0.1.0/README.md
@@ -0,0 +1,6 @@
+# Muqi
+A simple Python package to say hello.
+
+## Installation
+```bash
+pip install muqi
---
+++ packages/muqi/muqi-0.1.0.tar.gz/muqi-0.1.0/muqi/greet.py
@@ -0,0 +1,4 @@
+# muqi/greet.py
+def say_hello(name):
+    """Print a greeting message."""
+    print(f"Hello, {name}!")
