66973a618b50dbe783aeaf4dacee2137234b24e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:04:41 +0000

    Add package telebot-components/0.12.13/telebot_components-0.12.13.tar.gz

---
+++ packages/telebot-components/telebot_components-0.12.13.tar.gz/telebot_components-0.12.13/telebot_components/feedback/trello_integration.py
@@ -0,0 +1,10 @@
+"""
+This module is kept for import compatibility only and will be removed in the future.
+
+Use telebot_components.feedback.integration.trello in new code.
+"""
+
+# flake8: noqa
+
+
+from telebot_components.feedback.integration.trello import *
