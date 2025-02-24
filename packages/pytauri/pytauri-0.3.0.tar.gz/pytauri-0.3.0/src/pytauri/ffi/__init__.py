137efebbaca9a64c7816c0413c4fdbc10b4e3b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:18 +0000

    Add package pytauri/0.3.0/pytauri-0.3.0.tar.gz

---
+++ packages/pytauri/pytauri-0.3.0.tar.gz/pytauri-0.3.0/src/pytauri/ffi/__init__.py
@@ -0,0 +1,56 @@
+"""Original FFI interface module.
+
+!!! warning
+    All APIs under this module should not be considered stable.
+    You should use the re-exported APIs under the top-level module.
+"""
+
+from pytauri.ffi._ext_mod import EXT_MOD
+from pytauri.ffi.lib import (
+    App,
+    AppHandle,
+    Assets,
+    Builder,
+    BuilderArgs,
+    Context,
+    Event,
+    EventId,
+    ImplListener,
+    ImplManager,
+    Listener,
+    Manager,
+    Position,
+    PositionType,
+    Rect,
+    RunEvent,
+    RunEventType,
+    Size,
+    SizeType,
+    builder_factory,
+    context_factory,
+)
+
+__all__ = (
+    "EXT_MOD",
+    "App",
+    "AppHandle",
+    "Assets",
+    "Builder",
+    "BuilderArgs",
+    "Context",
+    "Event",
+    "EventId",
+    "ImplListener",
+    "ImplManager",
+    "Listener",
+    "Manager",
+    "Position",
+    "PositionType",
+    "Rect",
+    "RunEvent",
+    "RunEventType",
+    "Size",
+    "SizeType",
+    "builder_factory",
+    "context_factory",
+)
