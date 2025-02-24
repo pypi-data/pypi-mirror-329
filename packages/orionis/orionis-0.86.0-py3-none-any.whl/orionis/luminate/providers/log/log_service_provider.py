a17c3f4b1e187f53797e93b69ec6f37c7540beec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:36 +0000

    Add package orionis/0.86.0/orionis-0.86.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.86.0-py3-none-any.whl/orionis/luminate/providers/log/log_service_provider.py
@@ -0,0 +1,19 @@
+from orionis.luminate.providers.service_provider import ServiceProvider
+from orionis.luminate.services.log.log_service import LogguerService
+
+class LogServiceProvider(ServiceProvider):
+
+    def register(self) -> None:
+        """
+        Registers services or bindings into the given container.
+        """
+        self._container_id = self.app.singleton(LogguerService)
+
+    def boot(self) -> None:
+        """
+        Boot the service provider.
+
+        This method is intended to be overridden by subclasses to perform
+        any necessary bootstrapping or initialization tasks.
+        """
+        self.app.make(self._container_id)
