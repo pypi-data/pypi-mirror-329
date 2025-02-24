76d0c2d0d668524119ee2316b69c2c7173bec6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 05:04:37 +0000

    Add package bdscriptes/0.3.4/bdscriptes-0.3.4-py3-none-any.whl

---
+++ packages/bdscriptes/bdscriptes-0.3.4-py3-none-any.whl/BDScriptES/utils/process_text.py
@@ -0,0 +1,21 @@
+import re
+
+def process_text(code):
+    """
+    Extrae el texto que no esté dentro de los marcadores $description o $title.
+    """
+    # Eliminar todos los marcadores $description y $title
+
+
+    plain_text = re.sub(
+    r'\$description\[.*?\]|\$title\[.*?\]|\$titleURL\[.*?\]|\$var\[.*?\]'
+    r'|\$color\[.*?\]|\$author\[.*?\]|\$authorURL\[.*?\]|\$authorIcon\[.*?\]'
+    r'|\$image\[.*?\]|\$thumbnail\[.*?\]|\$message(?:\[\d*?\])?', 
+    '', 
+    code)
+
+    
+    # Eliminar espacios en blanco innecesarios
+    plain_text = plain_text.strip()
+    
+    return plain_text
