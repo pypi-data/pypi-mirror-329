33a45210654209426627fa0342d32ab92eded8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:57 +0000

    Add package tradeflow/0.0.27/tradeflow-0.0.27-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/tradeflow/tradeflow-0.0.27-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tradeflow/enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class OrderSelectionMethodAR(Enum):
+    PACF = "pacf"
+
+    def __str__(self) -> str:
+        return self._value_
+
+
+class FitMethodAR(Enum):
+    YULE_WALKER = ("yule_walker", False)
+    BURG = ("burg", False)
+    OLS_WITH_CST = ("ols_with_cst", True)
+    MLE_WITHOUT_CST = ("mle_without_cst", False)
+    MLE_WITH_CST = ("mle_with_cst", True)
+
+    def __new__(cls, name: str, has_cst_parameter: bool):
+        obj = object.__new__(cls)
+        obj._value_ = name
+        obj.has_cst_parameter = has_cst_parameter
+        return obj
+
+    def __str__(self) -> str:
+        return self._value_
