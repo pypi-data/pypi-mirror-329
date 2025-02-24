73af39e1e812b1661c7b5fd505fdd23d8e576770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 04:04:25 +0000

    Add package blues-lib/1.3.7/blues_lib-1.3.7-py3-none-any.whl

---
+++ packages/blues-lib/blues_lib-1.3.7-py3-none-any.whl/blues_lib/plan/PublishPlanFactory.py
@@ -0,0 +1,17 @@
+from .PublishPlan import PublishPlan
+
+class PublishPlanFactory():
+
+  def create_baijia(self,excepted_quota=None):
+    platform = 'baijia'
+    limit_quota = {
+      'events':10,
+      'news':5,
+    }
+    default_excepted_quota = {
+      'events':1,
+      'news':1,
+    }
+    actual_excepted_quota = excepted_quota if excepted_quota else default_excepted_quota
+
+    return PublishPlan(platform,actual_excepted_quota,limit_quota)
