9fc7596536378baf177d688081a00c7dd830ab47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:07 +0000

    Add package suibshishi/0.0.1/suibshishi-0.0.1.tar.gz

---
+++ packages/suibshishi/suibshishi-0.0.1.tar.gz/suibshishi-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="suibshishi",
+    version="0.0.1",
+    author="my test",
+    author_email="suibiantian@gmail.com",
+    description="test package",
+    url="https://github.com/suibiantian/pytest",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
