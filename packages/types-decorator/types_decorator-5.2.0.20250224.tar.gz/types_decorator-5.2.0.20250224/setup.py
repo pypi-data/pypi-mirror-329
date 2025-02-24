eeddc6ef5d1e5c8ce58b768c0b4e2aaf7cd2adaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:40 +0000

    Add package types-decorator/5.2.0.20250224/types_decorator-5.2.0.20250224.tar.gz

---
+++ packages/types-decorator/types_decorator-5.2.0.20250224.tar.gz/types_decorator-5.2.0.20250224/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-decorator"
+description = "Typing stubs for decorator"
+long_description = '''
+## Typing stubs for decorator
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`decorator`](https://github.com/micheles/decorator) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `decorator`. This version of
+`types-decorator` aims to provide accurate annotations for
+`decorator==5.2.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/decorator`](https://github.com/python/typeshed/tree/main/stubs/decorator)
+directory.
+
+This package was tested with
+mypy 1.15.0,
+pyright 1.1.389,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`c27e41c33b47edc624ca456e673c7d29a46c7e5c`](https://github.com/python/typeshed/commit/c27e41c33b47edc624ca456e673c7d29a46c7e5c).
+'''.lstrip()
+
+setup(name=name,
+      version="5.2.0.20250224",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/decorator.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['decorator-stubs'],
+      package_data={'decorator-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.9",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
