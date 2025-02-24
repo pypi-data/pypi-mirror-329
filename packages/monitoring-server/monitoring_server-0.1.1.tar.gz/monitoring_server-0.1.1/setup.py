1557b349a8e8af4e264dd8888f59524b7101b75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 06:04:38 +0000

    Add package monitoring-server/0.1.1/monitoring_server-0.1.1.tar.gz

---
+++ packages/monitoring-server/monitoring_server-0.1.1.tar.gz/monitoring_server-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monitoring_server',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    description='Monitorear recursos (ram, procesador y disco) de un server',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ariel',
+    author_email='arielpespinosa@gmail.com',
+    url='https://github.com/arielespinosa/CloudWFS-ServerMonitoring',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'monitoring=monitoring_server.main:main',
+        ],
+    },
+)
