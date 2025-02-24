8c6eb19357109c29aa791c7476cc7c023a37780b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:46 +0000

    Add package norm-benchmark/0.5.0/norm_benchmark-0.5.0-py3-none-any.whl

---
+++ packages/norm-benchmark/norm_benchmark-0.5.0-py3-none-any.whl/norm_benchmark/norm_cli.py
@@ -0,0 +1,19 @@
+import fire
+
+from norm_benchmark.backend.app import main
+from norm_benchmark.frontend.dashboard import create_dashboard
+
+
+class Norm(object):
+    def benchmark(self, model_outputs_path, ground_truths_path, to_leaderboard=False):
+        main(model_outputs_path, ground_truths_path, to_leaderboard)
+
+    def dashboard(self):
+        create_dashboard()
+
+
+def run():
+    fire.Fire(Norm)
+
+if __name__ == "__main__":
+    run()
