273d08d9f72bdb32fd207f84a5f2b31987b8876b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 03:11:09 +0000

    Add package zmp-zcp-toolkit/0.0.8/zmp_zcp_toolkit-0.0.8-py3-none-any.whl

---
+++ packages/zmp-zcp-toolkit/zmp_zcp_toolkit-0.0.8-py3-none-any.whl/zmp_zcp_toolkit/__init__.py
@@ -0,0 +1,29 @@
+import logging
+
+from .wrapper.base_wrapper import AuthenticationType
+from .wrapper.api_wrapper import ZcpAnalysisAPIWrapper, ZcpReportAPIWrapper, ZcpSelfHealingAPIWrapper
+from .toolkits.toolkit import ZcpToolkit
+from .tools.tool import ZcpTool
+
+
+__all__ = [
+    "ZcpAnalysisAPIWrapper",
+    "ZcpReportAPIWrapper",
+    "ZcpSelfHealingAPIWrapper",
+    "ZcpToolkit",
+    "ZcpTool",
+    "AuthenticationType",
+]
+
+__logger_name = "zmp_zcp_toolkit"
+
+
+def configure_logging(level=logging.INFO):
+    """Configure logging for the library
+
+    Args:
+        level (logging.LEVEL, optional): The logging level to use. Defaults to logging.INFO.
+    """
+
+    logger = logging.getLogger(__logger_name)
+    logger.setLevel(level)
