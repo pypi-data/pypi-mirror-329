164458df20b48c7eb7ad2ad05bd3d9ba8f1a1b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:43 +0000

    Add package databloom/1.0.25/databloom-1.0.25.tar.gz

---
+++ packages/databloom/databloom-1.0.25.tar.gz/databloom-1.0.25/databloom/_dynamic/source/postgresql/dep-4.py
@@ -0,0 +1,8 @@
+# --- render code block -----
+from databloom._core.postgres_core import PostgresqlBase
+
+class dep-4(PostgresqlBase):
+    def __init__(self, get_credential_from_server) -> None:
+        self.id = "67bc10ecf1ba9a2bf4a678bc"
+        self.credential = get_credential_from_server(self.id)
+# --- render code block -----
---
+++ packages/databloom/databloom-1.0.25.tar.gz/databloom-1.0.25/databloom/_dynamic/source/postgresql/dep-5.py
@@ -0,0 +1,8 @@
+# --- render code block -----
+from databloom._core.postgres_core import PostgresqlBase
+
+class dep-5(PostgresqlBase):
+    def __init__(self, get_credential_from_server) -> None:
+        self.id = "67bc13c1f1ba9a2bf4a678bd"
+        self.credential = get_credential_from_server(self.id)
+# --- render code block -----
