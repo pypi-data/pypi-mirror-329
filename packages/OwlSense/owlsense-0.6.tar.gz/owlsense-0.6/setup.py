3c00647278c63ae7622be8663b98887165744e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 15:05:07 +0000

    Add package OwlSense/0.6/owlsense-0.6.tar.gz

---
+++ packages/OwlSense/owlsense-0.6.tar.gz/owlsense-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+ 
+setup(
+  name='OwlSense',
+  version='0.6',
+  description='Python lib for basic&advance AI,ML',
+  author='sebinmon.vr',
+  author_email='nthn8777@gmail.com',
+  license='MIT', 
+  packages=find_packages(),
+  install_requires=['openai==0.28'] ,
+  entry_points = {
+    'console_scripts': ['OwlSense=OwlSense.main:Owl'],
+  },
+  
+)
+
