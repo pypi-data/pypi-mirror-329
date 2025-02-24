ad3415e739351bb62231f2b78617384c9dd38df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 16:05:20 +0000

    Add package MagmaPandas/2.1.0/MagmaPandas-2.1.0-py3-none-any.whl

---
+++ packages/MagmaPandas/MagmaPandas-2.1.0-py3-none-any.whl/MagmaPandas/volatile_solubility/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module with |CO2|-|H2O| solublity models for silicate melts
+"""
+
+from MagmaPandas.volatile_solubility.calculate_volatiles import *
+from MagmaPandas.volatile_solubility.isobars import *
+from MagmaPandas.volatile_solubility.solubility_baseclass import Solubility_model
