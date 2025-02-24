1697ee111d11dabc2e5f45f386b9c6e30f39ce00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-24 14:04:57 +0000

    Add package itkwasm-compare-meshes/0.6.0/itkwasm_compare_meshes-0.6.0-py3-none-any.whl

---
+++ packages/itkwasm-compare-meshes/itkwasm_compare_meshes-0.6.0-py3-none-any.whl/itkwasm_compare_meshes/__init__.py
@@ -0,0 +1,8 @@
+# Generated file. To retain edits, remove this comment.
+
+"""itkwasm-compare-meshes: Compare meshes and polydata for regression testing."""
+
+from .compare_meshes_async import compare_meshes_async
+from .compare_meshes import compare_meshes
+
+from ._version import __version__
