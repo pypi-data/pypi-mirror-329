229fafaf7cead3ed9ff6ef134ff36b2004903974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 04:06:00 +0000

    Add package dcnum/0.25.6/dcnum-0.25.6.tar.gz

---
+++ packages/dcnum/dcnum-0.25.6.tar.gz/dcnum-0.25.6/src/dcnum/feat/feat_background/bg_copy.py
@@ -0,0 +1,31 @@
+import h5py
+
+from .base import Background
+
+
+class BackgroundCopy(Background):
+    @staticmethod
+    def check_user_kwargs():
+        pass
+
+    def process(self):
+        """Copy input data to output dataset"""
+        if self.h5in != self.h5out:
+            hin = self.hdin.h5
+            for feat in ["image_bg", "bg_off"]:
+                if feat in hin["events"]:
+                    h5py.h5o.copy(src_loc=hin["events"].id,
+                                  src_name=feat.encode("utf-8"),
+                                  dst_loc=self.h5out["events"].id,
+                                  dst_name=feat.encode("utf-8"),
+                                  )
+
+        # set progress to 100%
+        self.image_proc.value = 1
+
+    def process_approach(self):
+        # We do the copying in `process`, because we do not want to modify
+        # any metadata or delete datasets as is done in the base class.
+        # But we still have to implement this method, because it is an
+        # abstractmethod in the base class.
+        pass
---
+++ packages/dcnum/dcnum-0.25.6.tar.gz/dcnum-0.25.6/src/dcnum/read/const.py
@@ -0,0 +1,17 @@
+#: Scalar features that apply to all events in a frame and which are
+#: not computed for individual events.
+PROTECTED_FEATURES = [
+    "bg_off",
+    "flow_rate",
+    "frame",
+    "g_force",
+    "pressure",
+    "temp",
+    "temp_amb",
+    "time",
+]
+
+# User-defined features may be anything, but if the user needs something
+# very specific for the pipeline, having them protected is a nice feature.
+for ii in range(10):
+    PROTECTED_FEATURES.append(f"userdef{ii}")
