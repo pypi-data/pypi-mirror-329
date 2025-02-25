0404b6108d49ba4efe29388f45506d4fab8bdfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:07:42 +0000

    Add package RagPUREAI-libs/0.1.0/ragpureai_libs-0.1.0.tar.gz

---
+++ packages/RagPUREAI-libs/ragpureai_libs-0.1.0.tar.gz/ragpureai_libs-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RagPUREAI_libs',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,  # Informa para incluir os dados especificados no MANIFEST.in
+    package_data={
+        'bind_libs': ['*.so'],  # Inclui todos os arquivos .so dentro do pacote
+    },
+    author='Seu Nome',
+    author_email='seu.email@exemplo.com',
+    description='Binding de C++ e Python',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
