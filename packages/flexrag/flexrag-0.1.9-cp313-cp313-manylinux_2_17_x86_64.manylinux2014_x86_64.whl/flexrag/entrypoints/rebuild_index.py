361acec30e493f3a4e3f3316b04714541bcffe4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 16:05:42 +0000

    Add package flexrag/0.1.9/flexrag-0.1.9-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flexrag/flexrag-0.1.9-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flexrag/entrypoints/rebuild_index.py
@@ -0,0 +1,26 @@
+import hydra
+from hydra.core.config_store import ConfigStore
+from omegaconf import OmegaConf
+
+from flexrag.retriever import DenseRetriever, DenseRetrieverConfig
+
+
+cs = ConfigStore.instance()
+cs.store(name="default", node=DenseRetrieverConfig)
+
+
+@hydra.main(version_base="1.3", config_path=None, config_name="default")
+def main(cfg: DenseRetrieverConfig):
+    default_cfg = OmegaConf.structured(DenseRetrieverConfig)
+    cfg = OmegaConf.merge(default_cfg, cfg)
+
+    # rebuild index
+    retriever = DenseRetriever(
+        cfg, no_check=True
+    )  # do not check the index-retriever consistency
+    retriever.build_index()
+    return
+
+
+if __name__ == "__main__":
+    main()
