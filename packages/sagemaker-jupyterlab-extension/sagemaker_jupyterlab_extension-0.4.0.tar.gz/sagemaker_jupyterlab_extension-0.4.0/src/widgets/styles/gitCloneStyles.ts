55e90f66f9bcf70e4e2d8c594890375340616455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 16:05:48 +0000

    Add package sagemaker-jupyterlab-extension/0.4.0/sagemaker_jupyterlab_extension-0.4.0.tar.gz

---
+++ packages/sagemaker-jupyterlab-extension/sagemaker_jupyterlab_extension-0.4.0.tar.gz/sagemaker_jupyterlab_extension-0.4.0/src/widgets/styles/gitCloneStyles.ts
@@ -0,0 +1,20 @@
+import { css } from '@emotion/css';
+
+const increaseZIndex = css`
+  z-index: 2;
+`;
+const gitCloneContainer = css`
+  width: 460px;
+  input {
+    width: 460px;
+  }
+  .MuiFormControl-root.MuiTextField-root {
+    margin-bottom: 20px;
+  }
+`;
+
+const MarginBottom = css`
+  margin-bottom: 20px;
+`;
+
+export { gitCloneContainer, MarginBottom, increaseZIndex };
