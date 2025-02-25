9a8df6701f3d69ad5b9b566daa549af413375dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 05:05:08 +0000

    Add package GPUtilSimulator/0.1.4/gputilsimulator-0.1.4.tar.gz

---
+++ packages/GPUtilSimulator/gputilsimulator-0.1.4.tar.gz/gputilsimulator-0.1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GPUtilSimulator",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=["psutil", "setuptools"],
+    author="Bryan Rocha",
+    author_email="bryangomesrocha@gmail.com",
+    description="A library that simulates GPUtil behavior for users without a dedicated GPU. It enables the "
+                "simulation of multiple virtual GPUs, using CPU usage as a reference to generate dynamic load values. "
+                "The simulated GPU usage fluctuates based on real-time CPU load, providing a realistic approximation "
+                "of GPU workload behavior.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/BryanRO-SpTech/GPUtilSimulator",  # URL do projeto
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
