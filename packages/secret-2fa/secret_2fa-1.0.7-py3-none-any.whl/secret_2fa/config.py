9e154a2cdeab4736b9320b13a45d1e304be2c36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 15:05:34 +0000

    Add package secret-2fa/1.0.7/secret_2fa-1.0.7-py3-none-any.whl

---
+++ packages/secret-2fa/secret_2fa-1.0.7-py3-none-any.whl/secret_2fa/config.py
@@ -0,0 +1,11 @@
+browser = {
+    'headless': True,
+    'executablePath': "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe",
+    'defaultViewport': {'width': 1920, 'height': 1080},
+    'ignoreDefaultArgs': ['--enable-automation'],
+    'args': [
+        'https://lay2fa.com/',
+        '--window-size=1935,1050',
+        '--window-position=-8,0'
+    ],
+}
