cb3754eaf8c1d9ece1a201f61ab749104eb1c173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 15:05:39 +0000

    Add package Nexgram.py/0.0.5/nexgram_py-0.0.5.tar.gz

---
+++ packages/Nexgram.py/nexgram_py-0.0.5.tar.gz/nexgram_py-0.0.5/Nexgram/client.py
@@ -0,0 +1,38 @@
+import logging
+import struct
+import time
+import os
+import hashlib
+import asyncio
+from .connection import Connection
+
+log = logging.getLogger(__name__)
+
+class Client:
+  def __init__(
+    self,
+    name: str,
+    api_id: int,
+    api_hash: str,
+    bot_token: str = None,
+    session_string: str = None      
+  ):
+    self.name = name
+    self.api_id = api_id
+    self.api_hash = api_hash
+    self.bot_token = bot_token
+    self.session_string = session_string
+    self.auth_key = None
+    self.socket = Connection("149.154.167.50", 443)
+
+  async def connect(self):
+    await self.socket.connect()
+
+  async def req_pq(self):
+    random_bytes = os.urandom(16)
+    message_id = int(time.time() * (2**32))
+    payload = struct.pack("<Q", message_id) + random_bytes
+
+    await self.socket.send(payload)
+    response = await self.socket.receive()
+    return response
