f6adc04ae93b9187cdcd139d7fb3a7a76f4c7119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:06:46 +0000

    Add package link-shorteners/1.14.1/link_shorteners-1.14.1.tar.gz

---
+++ packages/link-shorteners/link_shorteners-1.14.1.tar.gz/link_shorteners-1.14.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="link-shorteners",
+    version="1.14.1",
+    description="List of all URL shorteners, such as bitly, tinyurl, shorturl, and many others.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={'link_shorteners': ['link-shorteners.txt']},
+    url="https://github.com/mayakyler/link-shorteners/py-link-shorteners",
+    license="MIT",
+)
