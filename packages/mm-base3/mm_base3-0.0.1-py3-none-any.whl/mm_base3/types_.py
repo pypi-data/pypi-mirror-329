5696e7dbed361276fe7162080f02e859c911e373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 16:05:51 +0000

    Add package mm-base3/0.0.1/mm_base3-0.0.1-py3-none-any.whl

---
+++ packages/mm-base3/mm_base3-0.0.1-py3-none-any.whl/mm_base3/types_.py
@@ -0,0 +1,15 @@
+from typing import TypeVar
+
+from litestar.enums import RequestEncodingType
+from litestar.params import Body
+
+from mm_base3 import BaseAppConfig
+from mm_base3.base_db import BaseDb
+from mm_base3.dconfig import DConfigDict
+
+FormBody = Body(media_type=RequestEncodingType.URL_ENCODED)
+
+
+APP_CONFIG_T = TypeVar("APP_CONFIG_T", bound=BaseAppConfig)
+DCONFIG_T = TypeVar("DCONFIG_T", bound=DConfigDict)
+DB_T = TypeVar("DB_T", bound=BaseDb)
