930f7d10ea05df2177593c64c1a09096a6ab6c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 05:05:21 +0000

    Add package onesource/0.1.0/onesource-0.1.0.tar.gz

---
+++ packages/onesource/onesource-0.1.0.tar.gz/onesource-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="onesource",
+    include_package_data=True,
+    packages=find_packages(
+        include=["onesource", "onesource.*"]
+    ),  # This will include all subpackages
+    version="0.1.0",
+    author="OneSource",
+    description="Trigger marketing, sales and operations actions from git commits",
+    install_requires=[
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "onesource=onesource.operator:cli",
+        ],
+    },
+)
