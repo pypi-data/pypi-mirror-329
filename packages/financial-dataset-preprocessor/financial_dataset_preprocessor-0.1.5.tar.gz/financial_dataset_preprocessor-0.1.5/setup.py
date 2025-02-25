8ab225d922795365f35422235140bb65a3293412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 03:10:35 +0000

    Add package financial-dataset-preprocessor/0.1.5/financial_dataset_preprocessor-0.1.5.tar.gz

---
+++ packages/financial-dataset-preprocessor/financial_dataset_preprocessor-0.1.5.tar.gz/financial_dataset_preprocessor-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='financial_dataset_preprocessor',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'financial_dataset_loader',
+    ],
+    author='June Young Park',
+    author_email='juneyoungpaak@gmail.com',
+    description='A package for preprocessing financial datasets, powering the Life Asset Management development team.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nailen1/financial_dataset_preprocessor',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.11',
+)
