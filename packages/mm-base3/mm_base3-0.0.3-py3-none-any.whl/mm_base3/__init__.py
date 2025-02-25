7b2ee62a4366af9ec874e63eb50ac66d1128850f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 17:05:45 +0000

    Add package mm-base3/0.0.3/mm_base3-0.0.3-py3-none-any.whl

---
+++ packages/mm-base3/mm_base3-0.0.3-py3-none-any.whl/mm_base3/__init__.py
@@ -0,0 +1,12 @@
+from .base_core import BaseCore as BaseCore
+from .base_core import BaseCoreAny as BaseCoreAny
+from .base_service import BaseService as BaseService
+from .config import BaseAppConfig as BaseAppConfig
+from .dconfig import DC as DC
+from .dconfig import DConfigDict as DConfigDict
+from .dvalue import DV as DV
+from .dvalue import DValueDict as DValueDict
+from .jinja import CustomJinja as CustomJinja
+from .jinja.render import render_html as render_html
+from .server import init_server as init_server
+from .types_ import FormBody as FormBody
