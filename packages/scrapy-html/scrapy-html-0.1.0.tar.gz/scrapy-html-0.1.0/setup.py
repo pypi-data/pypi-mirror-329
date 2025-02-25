360182bcfd1aa0587e21ee25f9abd09a3603730e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 03:10:48 +0000

    Add package scrapy-html/0.1.0/scrapy-html-0.1.0.tar.gz

---
+++ packages/scrapy-html/scrapy-html-0.1.0.tar.gz/scrapy-html-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scrapy-html",                      # 📛 Nome único para PyPI
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[                        # 🔗 Dependências do pacote
+        "beautifulsoup4>=4.12.0",
+        "requests>=2.31.0"
+    ],
+    author="Seu Nome",
+    author_email="seu@email.com",
+    description="🌐 Um simples scraper que retorna o HTML completo de uma URL usando BeautifulSoup",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/seuusuario/scrapy-html",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
