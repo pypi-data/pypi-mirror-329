1781c641d44e545ef5ff5503447a57e3604764fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 05:05:37 +0000

    Add package elicits/0.0.5/elicits-0.0.5.tar.gz

---
+++ packages/elicits/elicits-0.0.5.tar.gz/elicits-0.0.5/elicit/__init__.py
@@ -0,0 +1,16 @@
+# noqa SPDX-FileCopyrightText: 2024 Florence Bockting <florence.bockting@tu-dortmund.de>
+#
+# noqa SPDX-License-Identifier: Apache-2.0
+
+
+from . import initialization  # noqa
+from . import losses  # noqa
+from . import networks  # noqa
+from . import optimization  # noqa
+from . import simulations  # noqa
+from . import targets  # noqa
+from . import utils  # noqa
+from . import plots  # noqa
+from . import types  # noqa
+
+from .elicit import *  # noqa
