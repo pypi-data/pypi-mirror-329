8594e519fe6ec51d541e93c85bcf290848256ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 07:05:10 +0000

    Add package plain.models/0.16.0/plain_models-0.16.0-py3-none-any.whl

---
+++ packages/plain.models/plain_models-0.16.0-py3-none-any.whl/plain/models/config.py
@@ -0,0 +1,20 @@
+from importlib import import_module
+from importlib.util import find_spec
+
+from plain.packages import PackageConfig, packages_registry, register_config
+
+MODELS_MODULE_NAME = "models"
+
+
+@register_config
+class Config(PackageConfig):
+    # We want to use the "migrations" module
+    # in this package but not for the standard purpose
+    migrations_module = None
+
+    def ready(self):
+        # Trigger register calls to fire by importing the modules
+        for package_config in packages_registry.get_package_configs():
+            module_name = f"{package_config.name}.{MODELS_MODULE_NAME}"
+            if find_spec(module_name):
+                import_module(module_name)
