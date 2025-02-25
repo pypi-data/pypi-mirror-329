f25a2e36316748026c65ea8398bc0bffcf61c2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:07:00 +0000

    Add package lightning-pose/1.7.1/lightning_pose-1.7.1-py3-none-any.whl

---
+++ packages/lightning-pose/lightning_pose-1.7.1-py3-none-any.whl/lightning_pose/model.py
@@ -0,0 +1,285 @@
+from __future__ import annotations
+
+import copy
+from pathlib import Path
+from typing import TypedDict
+
+import pandas as pd
+from omegaconf import DictConfig, OmegaConf
+
+from lightning_pose.model_config import ModelConfig
+from lightning_pose.models import ALLOWED_MODELS
+from lightning_pose.utils.io import ckpt_path_from_base_path
+from lightning_pose.utils.predictions import (
+    export_predictions_and_labeled_video,
+    load_model_from_checkpoint,
+    predict_dataset,
+)
+
+from lightning_pose.utils.scripts import (
+    compute_metrics_single,
+    get_data_module,
+    get_dataset,
+    get_imgaug_transform,
+)
+
+__all__ = ["Model"]
+
+
+class Model:
+    model_dir: Path
+    """Directory the model is stored in."""
+
+    config: ModelConfig
+    """The model configuration stored as a `ModelConfig` object.
+    `ModelConfig` wraps the `omegaconf.DictConfig` and provides util functions
+    over it.
+    """
+
+    model: ALLOWED_MODELS | None = None
+
+    # Just a constant we can use as a default value for kwargs,
+    # to differentiate between user omitting a kwarg, vs explicitly passing None.
+    UNSPECIFIED = "unspecified"
+
+    @staticmethod
+    def from_dir(model_dir: str | Path):
+        """Create a `Model` instance for a model stored at `model_dir`."""
+        return Model.from_dir2(model_dir)
+
+    @staticmethod
+    def from_dir2(model_dir: str | Path, hydra_overrides: list[str] = None):
+        """Internal version of from_dir that supports hydra_overrides. Not sure whether to
+        promote this to public API yet."""
+
+        model_dir = Path(model_dir).absolute()
+
+        if hydra_overrides is not None:
+            import hydra
+
+            with hydra.initialize_config_dir(
+                version_base="1.1", config_dir=str(model_dir)
+            ):
+                cfg = hydra.compose(config_name="config", overrides=hydra_overrides)
+                config = ModelConfig(cfg)
+        else:
+            config = ModelConfig.from_yaml_file(model_dir / "config.yaml")
+
+        return Model(model_dir, config)
+
+    def __init__(self, model_dir: str | Path, config: ModelConfig):
+        self.model_dir = Path(model_dir).absolute()
+        self.config = config
+
+    @property
+    def cfg(self) -> DictConfig:
+        """The model configuration as an `omegaconf.DictConfig`."""
+        return self.config.cfg
+
+    def _load(self):
+        if self.model is None:
+            ckpt_file = ckpt_path_from_base_path(
+                base_path=str(self.model_dir), model_name=self.cfg.model.model_name
+            )
+            self.model = load_model_from_checkpoint(
+                cfg=self.cfg,
+                ckpt_file=ckpt_file,
+                eval=True,
+                skip_data_module=True,
+            )
+
+    def image_preds_dir(self) -> Path:
+        return self.model_dir / "image_preds"
+
+    def video_preds_dir(self) -> Path:
+        return self.model_dir / "video_preds"
+
+    def labeled_videos_dir(self) -> Path:
+        return self.model_dir / "video_preds" / "labeled_videos"
+
+    def cropped_data_dir(self):
+        return self.model_dir / "cropped_images"
+
+    def cropped_videos_dir(self):
+        return self.model_dir / "cropped_videos"
+
+    def cropped_csv_file_path(self, csv_file_path: str | Path):
+        csv_file_path = Path(csv_file_path)
+        return (
+            self.model_dir
+            / "image_preds"
+            / csv_file_path.name
+            / ("cropped_" + csv_file_path.name)
+        )
+
+    class PredictionResult(TypedDict):
+        predictions: pd.DataFrame
+        metrics: pd.DataFrame
+
+    def predict_on_label_csv(
+        self,
+        csv_file: str | Path,
+        data_dir: str | Path | None = None,
+        compute_metrics: bool = True,
+        generate_labeled_images: bool = False,
+        output_dir: str | Path | None = UNSPECIFIED,
+        add_train_val_test_set: bool = False,
+    ) -> PredictionResult:
+        """Predicts on a labeled dataset and computes error/loss metrics if applicable.
+
+        Args:
+            csv_file (str | Path): Path to the CSV file of images, keypoint locations.
+            data_dir (str | Path, optional): Root path for relative paths in the CSV file. Defaults to the
+                data_dir originally used when training.
+            compute_metrics (bool, optional): Whether to compute pixel error and loss metrics on
+                predictions.
+            generate_labeled_images (bool, optional): Whether to save labeled images. Defaults to False.
+            output_dir (str | Path, optional): The directory to save outputs to.
+                Defaults to `{model_dir}/image_preds/{csv_file_name}`. If set to None, outputs are not saved.
+            add_train_val_test_set (bool): When predicting on training dataset, set to true to add the `set`
+                column to the prediction output.
+        Returns:
+            PredictionResult: A PredictionResult object containing the predictions and metrics.
+        """
+        self._load()
+        # Convert this to absolute, because if relative, downstream will
+        # assume incorrectly assume its relative to the data_dir.
+        csv_file = Path(csv_file).absolute()
+        if data_dir is None:
+            data_dir = self.config.cfg.data.data_dir
+
+        if output_dir == self.__class__.UNSPECIFIED:
+            output_dir = self.image_preds_dir() / csv_file.name
+
+        elif output_dir is None:
+            raise NotImplementedError("Currently we must save predictions")
+
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        if generate_labeled_images:
+            raise NotImplementedError()
+
+        # Point predict_dataset to the csv_file and data_dir.
+        # HACK: For true multi-view model, trick predict_dataset and compute_metrics
+        # into thinking this is a single-view model.
+        cfg_overrides = {
+            "data": {
+                "data_dir": str(data_dir),
+                "csv_file": str(csv_file),
+            }
+        }
+
+        # Avoid annotating set=train/val/test for CSV file other than the training CSV file.
+        if not add_train_val_test_set:
+            cfg_overrides.update({"train_prob": 1, "val_prob": 0, "train_frames": 1})
+
+        cfg_pred = OmegaConf.merge(self.cfg, cfg_overrides)
+
+        # HACK: For true multi-view model, trick predict_dataset and compute_metrics
+        # into thinking this is a single-view model.
+        if self.config.is_multi_view():
+            del cfg_pred.data.view_names
+            # HACK: If we don't delete mirrored_column_matches, downstream
+            # interprets this as a mirrored multiview model, and compute_metrics fails.
+            del cfg_pred.data.mirrored_column_matches
+
+        data_module_pred = _build_datamodule_pred(cfg_pred)
+
+        preds_file_path = output_dir / "predictions.csv"
+        preds_file = str(preds_file_path)
+
+        df = predict_dataset(
+            cfg_pred, data_module_pred, model=self.model, preds_file=preds_file
+        )
+
+        if compute_metrics:
+            compute_metrics_single(
+                cfg=cfg_pred,
+                labels_file=str(csv_file),
+                preds_file=preds_file,
+                data_module=data_module_pred,
+            )
+
+        return self.PredictionResult(predictions=df)
+
+    def predict_on_video_file(
+        self,
+        video_file: str | Path,
+        output_dir: str | Path | None = UNSPECIFIED,
+        compute_metrics: bool = True,
+        generate_labeled_video: bool = False,
+    ) -> PredictionResult:
+        """Predicts on a video file and computes unsupervised loss metrics if applicable.
+
+        Args:
+            video_file (str | Path): Path to the video file.
+            compute_metrics (bool, optional): Whether to compute pixel error and loss metrics on
+                predictions.
+            generate_labeled_video (bool, optional): Whether to save a labeled video. Defaults to False.
+            output_dir (str | Path, optional): The directory to save outputs to.
+                Defaults to `{model_dir}/image_preds/{csv_file_name}`. If set to None, outputs are not saved.
+        Returns:
+            PredictionResult: A PredictionResult object containing the predictions and metrics.
+        """
+        self._load()
+        video_file = Path(video_file)
+
+        if output_dir == self.__class__.UNSPECIFIED:
+            output_dir = self.video_preds_dir()
+
+        elif output_dir is None:
+            raise NotImplementedError("Currently we must save predictions")
+
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        prediction_csv_file = output_dir / f"{video_file.stem}.csv"
+
+        labeled_mp4_file = None
+        if generate_labeled_video:
+            labeled_mp4_file = str(
+                self.labeled_videos_dir() / f"{video_file.stem}_labeled.mp4"
+            )
+
+        if self.config.cfg.eval.get("predict_vids_after_training_save_heatmaps", False):
+            raise NotImplementedError(
+                "Implement this after cleaning up _predict_frames: "
+                "Set a flag on the model to return heatmaps. "
+                "Use trainer.predict instead of side-stepping it."
+            )
+        df = export_predictions_and_labeled_video(
+            video_file=str(video_file),
+            cfg=self.config.cfg,
+            prediction_csv_file=str(prediction_csv_file),
+            labeled_mp4_file=labeled_mp4_file,
+            model=self.model,
+        )
+
+        if compute_metrics:
+            # FIXME: Data module is only used for computing PCA metrics.
+            data_module = _build_datamodule_pred(self.cfg)
+            compute_metrics_single(
+                cfg=self.cfg,
+                labels_file=None,
+                preds_file=str(prediction_csv_file),
+                data_module=data_module,
+            )
+
+        return self.PredictionResult(predictions=df)
+
+
+def _build_datamodule_pred(cfg: DictConfig):
+    cfg_pred = copy.deepcopy(cfg)
+    cfg_pred.training.imgaug = "default"
+    imgaug_transform_pred = get_imgaug_transform(cfg=cfg_pred)
+    dataset_pred = get_dataset(
+        cfg=cfg_pred,
+        data_dir=cfg_pred.data.data_dir,
+        imgaug_transform=imgaug_transform_pred,
+    )
+    data_module_pred = get_data_module(
+        cfg=cfg_pred, dataset=dataset_pred, video_dir=cfg_pred.data.video_dir
+    )
+
+    return data_module_pred
---
+++ packages/lightning-pose/lightning_pose-1.7.1-py3-none-any.whl/tests/test_train.py
@@ -0,0 +1,205 @@
+import contextlib
+import copy
+import os
+import subprocess
+
+import pytest
+import torch
+from omegaconf import OmegaConf, open_dict
+
+from lightning_pose.train import train
+
+
+# TODO: Replace with contextlib.chdir in python 3.11.
+@contextlib.contextmanager
+def chdir(dir):
+    pwd = os.getcwd()
+    os.chdir(dir)
+    try:
+        yield
+    finally:
+        os.chdir(pwd)
+
+
+def _test_cfg(cfg):
+    pwd = os.getcwd()
+    # copy config and update paths
+    cfg_tmp = copy.deepcopy(cfg)
+    cfg_tmp.eval.test_videos_directory = cfg_tmp.data.video_dir
+
+    # don't train for long
+    cfg_tmp.training.min_epochs = 2
+    cfg_tmp.training.max_epochs = 2
+    cfg_tmp.training.check_val_every_n_epoch = 1
+    cfg_tmp.training.log_every_n_steps = 1
+    cfg_tmp.training.limit_train_batches = 2
+
+    # train simple model
+    cfg_tmp.model.model_type = "heatmap"
+    cfg_tmp.model.losses_to_use = []
+
+    # predict on vid
+    cfg_tmp.eval.predict_vids_after_training = True
+    cfg_tmp.eval.save_vids_after_training = True
+    return cfg_tmp
+
+
+def test_train_singleview(cfg, tmp_path):
+    cfg = _test_cfg(cfg)
+
+    # temporarily change working directory to temp output directory
+    with chdir(tmp_path):
+        # train model
+        train(cfg)
+
+    # ensure labeled data was properly processed
+    assert (tmp_path / "config.yaml").is_file()
+    assert (tmp_path / "CollectedData.csv").is_file()
+    assert (tmp_path / "predictions.csv").is_file()
+    assert (tmp_path / "predictions_pca_multiview_error.csv").is_file()
+    assert (tmp_path / "predictions_pca_singleview_error.csv").is_file()
+    assert (tmp_path / "predictions_pixel_error.csv").is_file()
+
+    # ensure video data was properly processed
+    assert (tmp_path / "video_preds" / "test_vid.csv").is_file()
+    assert (tmp_path / "video_preds" / "test_vid_pca_multiview_error.csv").is_file()
+    assert (tmp_path / "video_preds" / "test_vid_pca_singleview_error.csv").is_file()
+    assert (tmp_path / "video_preds" / "test_vid_temporal_norm.csv").is_file()
+    assert (
+        tmp_path / "video_preds" / "labeled_videos" / "test_vid_labeled.mp4"
+    ).is_file()
+
+
+@pytest.mark.skip(reason="Not yet implemented in Model class.")
+def test_train_singleview_detector_outputs(cfg, tmp_path):
+    cfg = _test_cfg(cfg)
+    cfg.eval.predict_vids_after_training = False
+    with open_dict(cfg):
+        cfg.detector = OmegaConf.create(
+            {"crop_ratio": 1.5, "anchor_keypoints": ["nose_top", "tailMid_bot"]}
+        )
+
+    # temporarily change working directory to temp output directory
+    detector_model_dir = tmp_path / "detector_model"
+    detector_model_dir.mkdir()
+
+    with chdir(detector_model_dir):
+        # train model
+        detector_model = train(cfg)
+
+    # ensure cropped images were properly processed
+    assert (
+        detector_model_dir / "cropped_images" / "labeled-data" / "img00.png"
+    ).is_file()
+    assert (
+        detector_model_dir / "cropped_images" / "labeled-data" / "img92.png"
+    ).is_file()
+    image_pred_dir = detector_model_dir / "image_preds" / "CollectedData.csv"
+    assert (image_pred_dir / "bbox.csv").is_file()
+    assert (image_pred_dir / "predictions.csv").is_file()
+    assert (image_pred_dir / "predictions_pca_multiview_error.csv").is_file()
+    assert (image_pred_dir / "predictions_pca_singleview_error.csv").is_file()
+    assert (image_pred_dir / "predictions_pixel_error.csv").is_file()
+    assert (image_pred_dir / "cropped_CollectedData.csv").is_file()
+
+    # ensure cropped videos were properly processed
+    # assert (detector_model_dir / "cropped_videos" / "test_vid.mp4").is_file()
+    # assert (detector_model_dir / "cropped_videos" / "test_vid_bbox.csv").is_file()
+
+    del cfg.detector
+    pose_model_dir = tmp_path / "pose_model"
+    pose_model_dir.mkdir()
+    with chdir(pose_model_dir):
+        # train model
+        train(cfg, detector_model)
+
+    # ensure labeled data was properly processed
+    assert (pose_model_dir / "config.yaml").is_file()
+    assert (pose_model_dir / "cropped_CollectedData.csv").is_file()
+    image_pred_dir = pose_model_dir / "image_preds" / "cropped_CollectedData.csv"
+    assert (image_pred_dir / "predictions.csv").is_file()
+    assert (image_pred_dir / "predictions_pixel_error.csv").is_file()
+
+
+def test_train_multiview(cfg_multiview, tmp_path):
+    from lightning_pose.train import train
+
+    cfg = _test_cfg(cfg_multiview)
+
+    # temporarily change working directory to temp output directory
+    with chdir(tmp_path):
+        # train model
+        train(cfg)
+
+    assert (tmp_path / "config.yaml").is_file()
+    assert (tmp_path / "top.csv").is_file()
+    assert (tmp_path / "bot.csv").is_file()
+
+    for view in ["top", "bot"]:
+        # ensure labeled data was properly processed
+        assert (tmp_path / f"predictions_{view}.csv").is_file()
+        assert (tmp_path / f"predictions_{view}_pixel_error.csv").is_file()
+        # assert (tmp_path / f"predictions_{view}_pca_multiview_error.csv").is_file()
+        # assert (tmp_path / f"predictions_{view}_pca_singleview_error.csv").is_file()
+
+        # ensure video data was properly processed
+        assert (tmp_path / "video_preds" / f"test_vid_{view}.csv").is_file()
+        assert (
+            tmp_path / "video_preds" / f"test_vid_{view}_temporal_norm.csv"
+        ).is_file()
+        # assert (tmp_path / "video_preds", f"test_vid_{view}_pca_multiview_error.csv").is_file()
+        # assert (tmp_path / "video_preds", f"test_vid_{view}_pca_singleview_error.csv").is_file()
+        assert (
+            tmp_path / "video_preds" / "labeled_videos" / f"test_vid_{view}_labeled.mp4"
+        ).is_file()
+
+
+# Multi-GPU tests must be run as their own scripts due to DDP.
+# https://github.com/Lightning-AI/pytorch-lightning/issues/4397#issuecomment-722743582
+# Our multi-GPU tests currently just ensure the train script finishes with status code 0.
+def _execute_multi_gpu_test(cfg, tmp_path, pytestconfig):
+    # set output directory to {tmp_path}/output.
+    cfg = OmegaConf.merge(
+        cfg, OmegaConf.create({"hydra": {"run": {"dir": tmp_path / "output"}}})
+    )
+
+    # Saves config in {tmp_dir}/config.yaml so train_hydra can read from it.
+    OmegaConf.save(cfg, tmp_path / "config.yaml")
+
+    # Run train_hydra script.
+    process = subprocess.run(
+        [
+            "python",
+            pytestconfig.rootpath / "scripts" / "train_hydra.py",
+            f"--config-path={tmp_path}",
+            f"--config-name=config",
+        ],
+    )
+    assert process.returncode == 0
+
+
+@pytest.mark.multigpu  # allow running only multi_gpu tests with `pytest -m multigpu`
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="Multiple GPUs required.")
+def test_train_multi_gpu_supervised(cfg, tmp_path, pytestconfig):
+    cfg = _test_cfg(cfg)
+    # Make the batches large enough to test 2 batches on each GPU.
+    cfg.training.num_gpus = 2
+    cfg.training.train_batch_size = 4
+    cfg.training.val_batch_size = 8
+
+    _execute_multi_gpu_test(cfg, tmp_path, pytestconfig)
+
+
+@pytest.mark.multigpu  # allow running only multi_gpu tests with `pytest -m multigpu`
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="Multiple GPUs required.")
+def test_train_multi_gpu_unsupervised(cfg, tmp_path, pytestconfig):
+    cfg = _test_cfg(cfg)
+    # Make the batches large enough to test 2 batches on each GPU.
+    cfg.training.num_gpus = 2
+    cfg.training.train_batch_size = 4
+    cfg.training.val_batch_size = 8
+    cfg.dali.context.train.batch_size = 16
+    cfg.model.model_type = "heatmap_mhcrnn"
+    cfg.model.losses_to_use = ["pca_singleview", "pca_multiview", "temporal"]
+
+    _execute_multi_gpu_test(cfg, tmp_path, pytestconfig)
