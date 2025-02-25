efad6245bc0d482aea96864a5fbfbfc58619d694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 16:06:01 +0000

    Add package abilian-sbe/1.1.13/abilian_sbe-1.1.13.tar.gz

---
+++ packages/abilian-sbe/abilian_sbe-1.1.13.tar.gz/abilian_sbe-1.1.13/tests/sbe/apps/preferences/test_preferences.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2012-2024, Abilian SAS
+
+from __future__ import annotations
+
+import pytest
+from flask import Flask, url_for
+
+
+@pytest.mark.skip("FIXME ASAP")
+def test_sbe_notifications(app: Flask, client, login_user):
+    response = client.get(url_for("preferences.sbe_notifications"))
+    assert response.status_code == 200
