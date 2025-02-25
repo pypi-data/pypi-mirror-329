e663864d920a5b572f8296c0e0a0bc154385dc18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 14:06:02 +0000

    Add package pyorderbook/0.4.6/pyorderbook-0.4.6.tar.gz

---
+++ packages/pyorderbook/pyorderbook-0.4.6.tar.gz/pyorderbook-0.4.6/pyorderbook/order.py
@@ -0,0 +1,83 @@
+import operator
+from collections.abc import Callable
+from decimal import Decimal
+from enum import StrEnum, auto
+from functools import partial
+from typing import TypeAlias
+from uuid import UUID, uuid4
+
+Symbol: TypeAlias = str
+Price: TypeAlias = Decimal
+
+
+class Side(StrEnum):
+    """Enum to represent BID or ASK Order"""
+
+    BID = auto()
+    ASK = auto()
+
+    @property
+    def other(self) -> "Side":
+        """Return the opposite side of the order"""
+        return Side.BID if self == Side.ASK else Side.ASK
+
+    @property
+    def price_comparator(self) -> Callable[[Price, Price], bool]:
+        """Return the price comparator function for the side.
+        Best price for BID is the lowest price, and vice versa for ASK.
+        """
+        return operator.le if self == Side.ASK else operator.ge
+
+    @property
+    def calc_fill_price(self) -> Callable[[Price, Price], Price]:
+        """Return the fill price calculation function for the side.
+        For BID, the fill price is the max of the two prices.
+        For ASK, the fill price is the min of the two prices.
+        """
+        return max if self == Side.ASK else min
+
+
+class OrderStatus(StrEnum):
+    """Enum for Order Status after matching occurs"""
+
+    QUEUED = auto()
+    PARTIAL_FILL = auto()
+    FILLED = auto()
+
+
+class Order:
+    def __init__(self, side: Side, symbol: Symbol, price: float, quantity: int) -> None:
+        if quantity <= 0:
+            raise ValueError("Order quantity must be greater than zero")
+        self.id: UUID = uuid4()
+        self.price: Price = Decimal(str(price))
+        self.quantity: int = quantity
+        self.symbol: Symbol = symbol
+        self.side: Side = side
+        self.original_quantity: int = quantity
+
+    @property
+    def status(self) -> OrderStatus:
+        if self.quantity == 0:
+            return OrderStatus.FILLED
+        elif self.quantity < self.original_quantity:
+            return OrderStatus.PARTIAL_FILL
+        return OrderStatus.QUEUED
+
+
+bid = partial(Order, Side.BID)
+ask = partial(Order, Side.ASK)
+
+
+class OrderQueue(dict[UUID, Order]):
+    def append_order(self, order: Order) -> None:
+        self[order.id] = order
+
+    def peek(self) -> Order:
+        if not self:
+            raise ValueError("Order Queue is Empty!")
+        first_key = next(iter(self))
+        return self[first_key]
+
+    def popleft(self) -> None:
+        self.pop(self.peek().id)
---
+++ packages/pyorderbook/pyorderbook-0.4.6.tar.gz/pyorderbook-0.4.6/tests/test_order_book.py
@@ -0,0 +1,28 @@
+from pyorderbook import Book, ask, bid
+
+
+def test_bid() -> None:
+    book = Book()
+    blotter = book.match([ask("IBM", 3.5, 70), ask("IBM", 3.6, 70), bid("IBM", 54.3, 140)])
+    assert len(blotter[2].trades) == 2
+    assert blotter[2].average_price == 3.55
+
+
+def test_ask() -> None:
+    book = Book()
+    blotters = book.match([bid("GOOG", 3.5, 70), bid("GOOG", 3.6, 70), ask("GOOG", 54.3, 140)])
+    assert len(blotters[2].trades) == 0
+    assert blotters[2].average_price == 0
+    blotter = book.match(ask("GOOG", 3.1, 140))
+    assert len(blotter.trades) == 2
+    assert blotter.average_price == 3.55
+
+
+def test_cancel() -> None:
+    book = Book()
+    bid1, bid2 = bid("GOOG", 3.5, 70), bid("GOOG", 3.6, 70)
+    book.match([bid1, bid2])
+    book.cancel(bid1)
+    blotter = book.match(ask("GOOG", 3.1, 140))
+    assert len(blotter.trades) == 1
+    assert blotter.average_price == 3.6
