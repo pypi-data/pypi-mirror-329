90341d809ccbe65590597754c0f04fbe4764f0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 15:06:22 +0000

    Add package sensor-sdk/0.0.4/sensor-sdk-0.0.4.tar.gz

---
+++ packages/sensor-sdk/sensor-sdk-0.0.4.tar.gz/sensor-sdk-0.0.4/sensor/utils.py
@@ -0,0 +1,30 @@
+import asyncio
+import signal
+
+
+async def delay(time: float, function) -> any:
+    if time > 0:
+        await asyncio.sleep(time)
+    return await function
+
+
+def timer(_loop: asyncio.AbstractEventLoop, time: float, function):
+    try:
+        asyncio.run_coroutine_threadsafe(delay(time, function), _loop)
+    except Exception as e:
+        print(e)
+        pass
+
+
+def sync_timer(_loop: asyncio.AbstractEventLoop, time: float, function) -> any:
+    try:
+        f = asyncio.run_coroutine_threadsafe(delay(time, function), _loop)
+        return f.result()
+    except Exception as e:
+        print(e)
+        pass
+
+
+def start_loop(loop: asyncio.BaseEventLoop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
