fe3cb74ecd1d18955487312f49f8d1c42cfeda9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:08:39 +0000

    Add package orionis/0.126.0/orionis-0.126.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.126.0-py3-none-any.whl/orionis/luminate/providers/environment/environment__service_provider.py
@@ -0,0 +1,22 @@
+from orionis.luminate.providers.service_provider import ServiceProvider
+from orionis.luminate.services.environment.environment_service import EnvironmentService
+from orionis.luminate.services.files.path_resolver_service import PathResolverService
+
+class EnvironmentServiceProvider(ServiceProvider):
+
+    beferoBootstrapping = True
+
+    def register(self) -> None:
+        """
+        Registers services or bindings into the given container.
+        """
+        self._container_id = self.app.singleton(EnvironmentService)
+
+    def boot(self) -> None:
+        """
+        Boot the service provider.
+
+        This method is intended to be overridden by subclasses to perform
+        any necessary bootstrapping or initialization tasks.
+        """
+        self.app.make(self._container_id)
