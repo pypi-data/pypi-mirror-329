80caaad33fa89154b8b61cb799984332b46b7026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 03:10:28 +0000

    Add package trustpoint-core/0.0.2/trustpoint_core-0.0.2-py3-none-any.whl

---
+++ packages/trustpoint-core/trustpoint_core-0.0.2-py3-none-any.whl/trustpoint_core/serializer/base.py
@@ -0,0 +1,16 @@
+"""The base module provides abstract base Serializer classes."""
+
+import abc
+
+
+class Serializer(abc.ABC):
+    """Abstract Base Class for all Serializer classes.
+
+    Warnings:
+        Serializer classes do not include any type of validation.
+        They are merely converting between formats.
+    """
+
+    @abc.abstractmethod
+    def serialize(self) -> bytes:
+        """The default serialization method."""
