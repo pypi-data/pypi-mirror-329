f2ab0ae81f062f7167fd8210d829013d72069a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 14:06:02 +0000

    Add package pyorderbook/0.4.8/pyorderbook-0.4.8.tar.gz

---
+++ packages/pyorderbook/pyorderbook-0.4.8.tar.gz/pyorderbook-0.4.8/pyorderbook/__init__.py
@@ -0,0 +1,26 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pyorderbook")
+except PackageNotFoundError:
+    __version__ = "v0.4.8"
+
+from pyorderbook.book import Book
+from pyorderbook.level import PriceLevel
+from pyorderbook.order import Order, OrderQueue, OrderStatus, Side, ask, bid
+from pyorderbook.trade_blotter import Trade, TradeBlotter
+
+easter_egg = "artificial lake"
+__all__ = [
+    "Book",
+    "bid",
+    "ask",
+    "Order",
+    "OrderQueue",
+    "OrderStatus",
+    "Side",
+    "PriceLevel",
+    "Trade",
+    "TradeBlotter",
+    "easter_egg",
+]
---
+++ packages/pyorderbook/pyorderbook-0.4.8.tar.gz/pyorderbook-0.4.8/tests/demo.py
@@ -0,0 +1,15 @@
+from pyorderbook import Book, ask, bid
+
+
+def simulate_order_flow() -> None:
+    """Toy order flow simulation. Feel free to experiment and set
+    your desired log level at the top of the file!"""
+    book = Book()
+    book.match(ask("GOOG", 3.6, 70))
+    book.match(ask("GOOG", 3.5, 70))
+    blotter = book.match(bid("GOOG", 3.7, 70))
+    print(blotter)
+
+
+if __name__ == "__main__":
+    simulate_order_flow()
