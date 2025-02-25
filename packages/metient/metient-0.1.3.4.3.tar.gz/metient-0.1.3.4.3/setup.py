db6a148073409441c3080ddd9eb7ee67e64fdc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 05:05:17 +0000

    Add package metient/0.1.3.4.3/metient-0.1.3.4.3.tar.gz

---
+++ packages/metient/metient-0.1.3.4.3.tar.gz/metient-0.1.3.4.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    #print(requirements)
+
+setup(name='metient', version='0.1.3.4.3', url="https://github.com/divyakoyy/metient.git", packages=['metient', 'metient.util', 'metient.lib'], install_requires=requirements,)
