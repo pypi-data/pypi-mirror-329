60ccf0f137f29429fcc9579b739f44026554117a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 05:05:14 +0000

    Add package nlsam/0.7.2/nlsam-0.7.2-cp313-cp313-macosx_10_13_x86_64.whl

---
+++ packages/nlsam/nlsam-0.7.2-cp313-cp313-macosx_10_13_x86_64.whl/nlsam/tests/test_smoothing.py
@@ -0,0 +1,42 @@
+import numpy as np
+from numpy.testing import assert_array_less
+
+from nlsam.stabilizer import _test_xi as xi
+from nlsam.smoothing import local_standard_deviation
+
+
+def test_local_standard_deviation():
+
+    # SNR = 20
+    mean = 100
+    std = 5
+    shape = (30, 30, 30, 3)
+
+    for N in [1, 4, 8, 12]:
+        noise = 0
+        for _ in range(N):
+            noise += np.random.normal(mean, std, shape)**2 + np.random.normal(mean, std, shape)**2
+
+        noise = np.sqrt(noise)
+        corrected_std = local_standard_deviation(noise) / np.sqrt(xi(mean, std, N))
+
+        # everything less than 10% error of real value?
+        assert_array_less(np.abs(std - corrected_std.mean()) / std, 0.1)
+
+    # This estimation has a harder time at low SNR, high coils value, probably due
+    # to how the synthetic noise field is computed
+    # SNR = 5
+    mean = 250
+    std = 50
+    shape = (30, 30, 30, 3)
+
+    for N in [1, 4, 8, 12]:
+        noise = 0
+        for _ in range(N):
+            noise += np.random.normal(mean, std, shape)**2 + np.random.normal(mean, std, shape)**2
+
+        noise = np.sqrt(noise)
+        corrected_std = local_standard_deviation(noise) / np.sqrt(xi(mean, std, N))
+
+        # everything less than 10% error of real value?
+        assert_array_less(np.abs(std - corrected_std.mean()) / std, 0.1)
