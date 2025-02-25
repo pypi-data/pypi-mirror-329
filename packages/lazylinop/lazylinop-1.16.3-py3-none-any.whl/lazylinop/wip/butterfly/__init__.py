367149a4b02d28e62b844ad09ca172bd5b0eafa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 16:05:30 +0000

    Add package lazylinop/1.16.3/lazylinop-1.16.3-py3-none-any.whl

---
+++ packages/lazylinop/lazylinop-1.16.3-py3-none-any.whl/lazylinop/wip/butterfly/__init__.py
@@ -0,0 +1,3 @@
+from .ksm import ksm, clean, del_all_contexts
+from .chain import Chain
+from .ksd import ksd, bitrev_perm, read, save
---
+++ packages/lazylinop/lazylinop-1.16.3-py3-none-any.whl/lazylinop/wip/butterfly/kernels/template_ksmm.cuh
@@ -0,0 +1,490 @@
+// -*- c++ -*-
+
+#include <cuComplex.h>
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <stdexcept>
+#include <stdio.h>
+
+#ifndef KSMM
+#define KSMM
+
+#if defined(V1)
+#define VSIZE 1
+#elif defined(V2)
+#define VSIZE 2
+#elif defined(V3)
+#define VSIZE 3
+#elif defined(V4)
+#define VSIZE 4
+#endif
+
+#ifdef USE_FLOAT16
+#define SUPPORT_FLOAT16
+#endif
+
+#ifdef USE_FLOAT32
+#define SUPPORT_FLOAT32
+#endif
+
+#ifdef USE_COMPLEX64
+#define SUPPORT_COMPLEX64
+#define SUPPORT_COMPLEX
+#endif
+
+#ifdef USE_FLOAT64
+#define SUPPORT_FLOAT64
+#endif
+
+#ifdef USE_COMPLEX128
+#define SUPPORT_COMPLEX128
+#define SUPPORT_COMPLEX
+#endif
+
+#ifdef SUPPORT_FLOAT16
+typedef __half dtype;
+#if defined(V1)
+typedef __half dtypex;
+#endif
+#if defined(V2)
+typedef __half2 dtypex;
+#endif
+#if defined(V3)
+typedef half3 dtypex;
+#endif
+#if defined(V4)
+typedef half4 dtypex;
+#endif
+#endif
+
+#ifdef SUPPORT_FLOAT32
+typedef float dtype;
+#if defined(V1)
+typedef float dtypex;
+#endif
+#if defined(V2)
+typedef float2 dtypex;
+#endif
+#if defined(V3)
+typedef float3 dtypex;
+#endif
+#if defined(V4)
+typedef float4 dtypex;
+#endif
+#endif
+
+#ifdef SUPPORT_FLOAT64
+typedef double dtype;
+#if defined(V1)
+typedef double dtypex;
+#endif
+#if defined(V2)
+typedef double2 dtypex;
+#endif
+#if defined(V3)
+typedef double3 dtypex;
+#endif
+#if defined(V4)
+typedef double4 dtypex;
+#endif
+#endif
+
+#ifdef SUPPORT_COMPLEX64
+typedef cuFloatComplex dtype;
+typedef float elt_t;
+#if defined(V1) || defined(V3)
+typedef cuFloatComplex dtypex;
+#endif
+#if defined(V2)
+typedef float4 dtypex;
+#endif
+#if defined(V4)
+typedef float4 dtypex;
+#endif
+#endif
+
+#ifdef SUPPORT_COMPLEX128
+typedef cuDoubleComplex dtype;
+typedef double elt_t;
+#if defined(V1) || defined(V3)
+typedef cuDoubleComplex dtypex;
+#endif
+#if defined(V2)
+typedef double4 dtypex;
+#endif
+#if defined(V4)
+typedef double4 dtypex;
+#endif
+#endif
+
+#define loadx(a, b)                                                            \
+  reinterpret_cast<dtypex *>(&a)[0] = reinterpret_cast<dtypex *>(&b)[0]
+
+#ifdef SUPPORT_COMPLEX
+__device__ inline void make_cuXComplex(dtype &dst, elt_t r, elt_t i) {
+#if defined(SUPPORT_COMPLEX64)
+  dst = make_cuFloatComplex(r, i);
+#else
+  dst = make_cuDoubleComplex(r, i);
+#endif
+}
+#endif
+
+__device__ inline void kload(dtype *gmem, dtype *smem, int c, int row, int s,
+                             int col, dtypex src) {
+#if defined(SUPPORT_COMPLEX)
+#if defined(V2)
+  loadx(src, gmem[0]);
+  make_cuXComplex(smem[(col * VSIZE + 0) * xTILEYx + row + s], src.x, src.y);
+  make_cuXComplex(smem[(col * VSIZE + 1) * xTILEYx + row + s], src.z, src.w);
+#else
+#if defined(V4)
+  loadx(src, gmem[0]);
+  make_cuXComplex(smem[(col * VSIZE + 0) * xTILEYx + row + s], src.x, src.y);
+  make_cuXComplex(smem[(col * VSIZE + 1) * xTILEYx + row + s], src.z, src.w);
+  loadx(src, gmem[2]);
+  make_cuXComplex(smem[(col * VSIZE + 2) * xTILEYx + row + s], src.x, src.y);
+  make_cuXComplex(smem[(col * VSIZE + 3) * xTILEYx + row + s], src.z, src.w);
+#else
+  for (int v = 0; v < VSIZE; v++)
+    smem[(col * VSIZE + v) * xTILEYx + row + s] = gmem[v];
+#endif
+#endif
+#else
+#if defined(V1)
+ smem[col * VSIZE * xTILEYx + row + s] = gmem[0];
+#else
+  loadx(src, gmem[0]);
+  smem[(col * VSIZE + 0) * xTILEYx + row + s] = src.x;
+  smem[(col * VSIZE + 1) * xTILEYx + row + s] = src.y;
+#if defined(V3)
+  smem[(col * VSIZE + 2) * xTILEYx + row + s] = src.z;
+#elif defined(V4)
+  smem[(col * VSIZE + 2) * xTILEYx + row + s] = src.z;
+  smem[(col * VSIZE + 3) * xTILEYx + row + s] = src.w;
+#endif
+#endif
+#endif
+}
+
+__device__ inline void iload(dtype *gmem, dtype *smem, int batch_size,
+                             int row, int s, int col, int offset, dtypex src) {
+#if defined(SUPPORT_COMPLEX)
+#if defined(V2)
+  loadx(src, gmem[0]);
+  make_cuXComplex(smem[(row + s) * xTILEXx + col * VSIZE + 0], src.x, src.y);
+  make_cuXComplex(smem[(row + s) * xTILEXx + col * VSIZE + 1], src.z, src.w);
+#else
+#if defined(V4)
+  loadx(src, gmem[0]);
+  make_cuXComplex(smem[(row + s) * xTILEXx + col * VSIZE + 0], src.x, src.y);
+  make_cuXComplex(smem[(row + s) * xTILEXx + col * VSIZE + 1], src.z, src.w);
+  loadx(src, gmem[2]);
+  make_cuXComplex(smem[(row + s) * xTILEXx + col * VSIZE + 2], src.x, src.y);
+  make_cuXComplex(smem[(row + s) * xTILEXx + col * VSIZE + 3], src.z, src.w);
+#else
+  for (int v = 0; v < VSIZE; v++)
+    smem[(row + s) * xTILEXx + col * VSIZE + v] = gmem[v];
+#endif
+#endif
+#else
+#if defined(V1)
+  smem[(row + s) * xTILEXx + col * VSIZE] = gmem[0];
+#else
+  offset += col * VSIZE;
+  if ((batch_size - offset) >= VSIZE) {
+    loadx(smem[(row + s) * xTILEXx + col * VSIZE], gmem[0]);
+  } else {
+    for (int v = 0; v < (batch_size - offset); v++)
+      smem[(row + s) * xTILEXx + col * VSIZE + v] = gmem[v];
+  }
+#endif
+#endif
+}
+
+extern "C" {
+__global__ void ksmm(dtype *values, dtype *input, dtype *output, const int a,
+                     const int b, const int c, const int d, int batch_size) {
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int t_id = threadIdx.x * blockDim.y + threadIdx.y;
+  // Kronecker-sparse pattern (a, b, c, d)
+  // K = kron(Id_{a,a}, kron(1_{b,c}, Id_{d,d}))
+  // There is 'a' super-blocks of shape (b * d, c * d).
+  // Number of non-zero per super-block is
+  // b per column and c per row.
+  // We would like to compute K @ X.
+  // K (row-major format) shape is (a * b * d, a * c * d).
+  // X (row-major format) shape is (a * c * d, batch).
+  // TILEX / TX threads per column
+  // Get the current thread
+  int threadx = t_id % (xTILEXx / xTXx);
+  int thready = t_id / (xTILEXx / xTXx);
+  // To store input in shared memory
+  __shared__ dtype shared_input[2][xTILEKx * xTILEXx];
+  // To store sparse matrix in shared memory
+  __shared__ dtype shared_values[2][xTILEYx * xTILEKx];
+  // To store output in shared memory
+  __shared__ dtype shared_output[xTILEYx * xTILEXx];
+  dtype tmp_acc[xTYx * xTXx] = {0.0};
+  dtype regY[xTYx] = {0.0};
+  dtype regX[xTXx] = {0.0};
+
+  dtypex src;
+
+  // Current super-block.
+  int sb_id = (by * xTILEYx) / (b * d);
+  // Group and id inside the super-block.
+  int grp_id = (by * xTILEYx - sb_id * b * d) / b;
+  int off = ((by * xTILEYx - sb_id * b * d) % b) / xTILEYx;
+  // Move to the current super-block, group and id.
+  values = &values[(sb_id * b * d + grp_id + off * d * xTILEYx) * c];
+  input = &input[(c * d * sb_id + grp_id % d) * batch_size];
+  output = &output[(sb_id * b * d + grp_id + off * d * xTILEYx) * batch_size];
+  // Move bx * TILEX columns.
+  input += bx * xTILEXx;
+  output += bx * xTILEXx;
+
+  // int diff;
+  // int rem;
+
+  // Indices to load (Kronecker-sparse factor) in smem.
+  int ValuesSubRow = t_id / (xTILEKx / VSIZE);
+  int ValuesSubCol = t_id % (xTILEKx / VSIZE);
+  // Indices to load (input matrix) in smem.
+  int InputSubRow = t_id / (xTILEXx / VSIZE);
+  int InputSubCol = t_id % (xTILEXx / VSIZE);
+
+  // Use stride to load from GMEM to SMEM
+  const int StrideValues = VSIZE * xNTHREADSx / xTILEKx;
+  const int StrideInput = VSIZE * xNTHREADSx / xTILEXx;
+
+  // Load (dtypex) the first batch of Kronecker-sparse factor from global to
+  // shared memory TILEY * TILEK.
+#pragma unroll
+  for (int s = 0; s < xTILEYx; s += StrideValues)
+    kload(&values[d * (ValuesSubRow + s) * c + (ValuesSubCol * VSIZE) % c],
+          &shared_values[0][0], c, ValuesSubRow, s, ValuesSubCol, src);
+
+  // Load the first batch of input from global to shared memory TILEK * TILEX.
+#pragma unroll
+  for (int s = 0; s < xTILEKx; s += StrideInput)
+    iload(&input[d * (InputSubRow + s) * batch_size + InputSubCol * VSIZE],
+          &shared_input[0][0], batch_size, InputSubRow, s, InputSubCol,
+          bx * xTILEXx, src);
+
+  int load = 0;
+  int write = 1;
+
+  // Loop over non-zero entries by TILEK
+  for (int k = 0; k < xTILEKx * (c / xTILEKx); k += xTILEKx) {
+    __syncthreads();
+    // Load smem to register and compute accumulation.
+#pragma unroll
+    for (int i = 0; i < xTILEKx; i++) {
+      // Kronecker-sparse factor.
+#pragma unroll
+      for (int y = 0; y < xTYx; y += VSIZE) {
+#ifdef SUPPORT_COMPLEX
+	// No cuFloatComplexX and cuDoubleComplexX structs yet ?
+#if defined(V2)
+	loadx(src, shared_values[load][i * xTILEYx + thready * xTYx + y]);
+	make_cuXComplex(regY[y + 0], src.x, src.y);
+	make_cuXComplex(regY[y + 1], src.z, src.w);
+#else
+#if defined(V4)
+	loadx(src, shared_values[load][i * xTILEYx + thready * xTYx + y]);
+	make_cuXComplex(regY[y + 0], src.x, src.y);
+	make_cuXComplex(regY[y + 1], src.z, src.w);
+	loadx(src, shared_values[load][i * xTILEYx + thready * xTYx + y + 2]);
+	make_cuXComplex(regY[y + 2], src.x, src.y);
+	make_cuXComplex(regY[y + 3], src.z, src.w);
+#else
+#pragma unroll
+	for (int v = 0; v < VSIZE; v++)
+          regY[y + v] =
+	    shared_values[load][i * xTILEYx + thready * xTYx + y + v];
+#endif
+#endif
+#else
+#if defined(V1)
+	regY[y] = shared_values[load][i * xTILEYx + thready * xTYx + y];
+#else
+	loadx(regY[y], shared_values[load][i * xTILEYx + thready * xTYx + y]);
+#endif
+#endif
+      }
+      // Input.
+#pragma unroll
+      for (int x = 0; x < xTXx; x += VSIZE) {
+#ifdef SUPPORT_COMPLEX
+        // No cuFloatComplexX and cuDoubleComplexX structs yet ?
+#if defined(V2)
+	loadx(src, shared_input[load][i * xTILEXx + threadx * xTXx + x]);
+	make_cuXComplex(regX[x + 0], src.x, src.y);
+	make_cuXComplex(regX[x + 1], src.z, src.w);
+#else
+#if defined(V4)
+	loadx(src, shared_input[load][i * xTILEXx + threadx * xTXx + x]);
+	make_cuXComplex(regX[x + 0], src.x, src.y);
+	make_cuXComplex(regX[x + 1], src.z, src.w);
+	loadx(src, shared_input[load][i * xTILEXx + threadx * xTXx + x + 2]);
+	make_cuXComplex(regX[x + 2], src.x, src.y);
+	make_cuXComplex(regX[x + 3], src.z, src.w);
+#else
+#pragma unroll
+        for (int v = 0; v < VSIZE; v++)
+          regX[x + v] =
+	    shared_input[load][i * xTILEXx + threadx * xTXx + x + v];
+#endif
+#endif
+#else
+#if defined(V1)
+        regX[x] = shared_input[load][i * xTILEXx + threadx * xTXx + x];
+#else
+        loadx(regX[x], shared_input[load][i * xTILEXx + threadx * xTXx + x]);
+#endif
+#endif
+      }
+
+      // Compute accumulation.
+#pragma unroll
+      for (int y = 0; y < xTYx; y++) {
+#pragma unroll
+        for (int x = 0; x < xTXx; x++) {
+#if defined(SUPPORT_COMPLEX64)
+          tmp_acc[y * xTXx + x] =
+              cuCaddf(tmp_acc[y * xTXx + x], cuCmulf(regY[y], regX[x]));
+#elif defined(SUPPORT_COMPLEX128)
+          tmp_acc[y * xTXx + x] =
+              cuCadd(tmp_acc[y * xTXx + x], cuCmul(regY[y], regX[x]));
+#else
+          tmp_acc[y * xTXx + x] += regY[y] * regX[x];
+#endif
+        }
+      }
+    }
+
+    load = load ^ 1;
+    // Move xTILEKx columns (values is in row-major).
+    values += xTILEKx;
+    // Move d * xTILEKx rows (input is in row-major).
+    input += d * xTILEKx * batch_size;
+
+    // Condition on columns of values.
+    if ((k + xTILEKx) < (xTILEKx * (c / xTILEKx))) {
+      // Load the Kronecker-sparse factor in shared memory TILEY x TILEK.
+#pragma unroll
+      for (int s = 0; s < xTILEYx; s += StrideValues)
+        kload(&values[d * (ValuesSubRow + s) * c + (ValuesSubCol * VSIZE) % c],
+              &shared_values[write][0], c, ValuesSubRow, s, ValuesSubCol, src);
+        // Load next batch from global to shared memory TILEK x TILEX
+#pragma unroll
+      for (int s = 0; s < xTILEKx; s += StrideInput)
+        iload(&input[d * (InputSubRow + s) * batch_size + InputSubCol * VSIZE],
+              &shared_input[write][0], batch_size, InputSubRow, s, InputSubCol,
+              bx * xTILEXx, src);
+      write = write ^ 1;
+    }
+  }
+
+  // Store accumulation to shared memory
+#pragma unroll
+  for (int y = 0; y < xTYx; y++) {
+#pragma unroll
+    for (int x = 0; x < xTXx; x += VSIZE) {
+#ifdef SUPPORT_COMPLEX
+      // No cuFloatComplexX and cuDoubleComplexX structs yet ?
+#if defined(V2)
+      loadx(src, tmp_acc[y * xTXx + x]);
+      make_cuXComplex(shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + 0],
+		      src.x, src.y);
+      make_cuXComplex(shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + 1],
+		      src.z, src.w);
+#else
+#if defined(V4)
+      loadx(src, tmp_acc[y * xTXx + x]);
+      make_cuXComplex(shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + 0],
+		      src.x, src.y);
+      make_cuXComplex(shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + 1],
+		      src.z, src.w);
+      loadx(src, tmp_acc[y * xTXx + x + 2]);
+      make_cuXComplex(shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + 2],
+		      src.x, src.y);
+      make_cuXComplex(shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + 3],
+		      src.z, src.w);
+#else
+#pragma unroll
+      for (int v = 0; v < VSIZE; v++)
+	shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + v] =
+	  tmp_acc[y * xTXx + x + v];
+#endif
+#endif
+#else
+#if defined(V1)
+      shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x] =
+	tmp_acc[y * xTXx + x];
+#else
+      loadx(shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x],
+	    tmp_acc[y * xTXx + x]);
+#endif
+#endif
+    }
+  }
+
+  // Write out the accumulation (from shared to global memory).
+//   diff = batch_size - (bx * xTILEXx + threadx * xTXx);
+//   rem = diff >= xTXx ? xTXx : diff;
+// #pragma unroll
+//   for (int y = 0; y < xTYx; y++) {
+// #pragma unroll
+//     for (int x = 0; x < rem; x++)
+//       output[threadx * xTXx + x + d * (thready * xTYx + y) * batch_size] =
+//           shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x];
+//   }
+#pragma unroll
+  for (int y = 0; y < xTYx; y++) {
+#pragma unroll
+    for (int x = 0; x < xTXx; x += VSIZE) {
+#ifdef SUPPORT_COMPLEX
+#if defined(V2)
+      loadx(src, shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x]);
+      make_cuXComplex(output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x + 0],
+		      src.x, src.y);
+      make_cuXComplex(output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x + 1],
+		      src.z, src.w);
+#else
+#if defined(V4)
+      loadx(src, shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x]);
+      make_cuXComplex(output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x + 0],
+		      src.x, src.y);
+      make_cuXComplex(output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x + 1],
+		      src.z, src.w);
+      loadx(src, shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + 2]);
+      make_cuXComplex(output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x + 2],
+		      src.x, src.y);
+      make_cuXComplex(output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x + 3],
+		      src.z, src.w);
+#else
+#pragma unroll
+      for (int v = 0; v < VSIZE; v++)
+	output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x + v] =
+	  shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x + v];
+#endif
+#endif
+#else
+#if defined(V1)
+      output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x] =
+	shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x];
+#else
+      loadx(output[d * (thready * xTYx + y) * batch_size + threadx * xTXx + x],
+	    shared_output[(thready * xTYx + y) * xTILEXx + threadx * xTXx + x]);
+#endif
+#endif
+    }
+  }
+}
+}
+
+#endif
