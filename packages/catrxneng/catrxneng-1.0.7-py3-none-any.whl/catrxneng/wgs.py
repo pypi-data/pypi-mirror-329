846051b65e12c893fe49c6765a9031c26a560125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:08:11 +0000

    Add package catrxneng/1.0.7/catrxneng-1.0.7-py3-none-any.whl

---
+++ packages/catrxneng/catrxneng-1.0.7-py3-none-any.whl/catrxneng/wgs.py
@@ -0,0 +1,17 @@
+from catrxneng import CO, CO2, H2, H2O, Reaction
+
+
+class WGS(Reaction):
+    def __init__(self,temp_C):
+        self.T_C = temp_C
+        self.T_K = self.T_C + 273
+        self.co = CO(temp_C)
+        self.co2 = CO2(temp_C)
+        self.h2 = H2(temp_C)
+        self.h2o = H2O(temp_C)
+
+    def dH_rxn(self):
+        return self.co2.Hf() + self.h2.Hf() - self.co.Hf() - self.h2o.Hf()
+
+    def dS_rxn(self):
+        return self.co2.Sf() + self.h2.Sf() - self.co.Sf() - self.h2o.Sf()
