884fc07582f318d2b571923e4f02f70421199fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 14:05:59 +0000

    Add package boto3-stubs-full/1.37.0/boto3_stubs_full-1.37.0.tar.gz

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.37.0.tar.gz/boto3_stubs_full-1.37.0/mypy_boto3_batch/__init__.pyi
@@ -0,0 +1,52 @@
+"""
+Main interface for batch service.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_batch/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_batch import (
+        BatchClient,
+        Client,
+        DescribeComputeEnvironmentsPaginator,
+        DescribeJobDefinitionsPaginator,
+        DescribeJobQueuesPaginator,
+        ListJobsPaginator,
+        ListSchedulingPoliciesPaginator,
+    )
+
+    session = Session()
+    client: BatchClient = session.client("batch")
+
+    describe_compute_environments_paginator: DescribeComputeEnvironmentsPaginator = client.get_paginator("describe_compute_environments")
+    describe_job_definitions_paginator: DescribeJobDefinitionsPaginator = client.get_paginator("describe_job_definitions")
+    describe_job_queues_paginator: DescribeJobQueuesPaginator = client.get_paginator("describe_job_queues")
+    list_jobs_paginator: ListJobsPaginator = client.get_paginator("list_jobs")
+    list_scheduling_policies_paginator: ListSchedulingPoliciesPaginator = client.get_paginator("list_scheduling_policies")
+    ```
+"""
+
+from .client import BatchClient
+from .paginator import (
+    DescribeComputeEnvironmentsPaginator,
+    DescribeJobDefinitionsPaginator,
+    DescribeJobQueuesPaginator,
+    ListJobsPaginator,
+    ListSchedulingPoliciesPaginator,
+)
+
+Client = BatchClient
+
+__all__ = (
+    "BatchClient",
+    "Client",
+    "DescribeComputeEnvironmentsPaginator",
+    "DescribeJobDefinitionsPaginator",
+    "DescribeJobQueuesPaginator",
+    "ListJobsPaginator",
+    "ListSchedulingPoliciesPaginator",
+)
