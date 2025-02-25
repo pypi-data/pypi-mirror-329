9ae11807afe6ca06628aa5a893391072131bab20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 16:05:33 +0000

    Add package zeit.deploynotify/1.9.1/zeit_deploynotify-1.9.1.tar.gz

---
+++ packages/zeit.deploynotify/zeit_deploynotify-1.9.1.tar.gz/zeit_deploynotify-1.9.1/src/zeit/deploynotify/base.py
@@ -0,0 +1,7 @@
+class Notification:
+
+    def __init__(self, environment, project, version, previous_version):
+        self.environment = environment
+        self.project = project
+        self.version = version
+        self.previous_version = previous_version
---
+++ packages/zeit.deploynotify/zeit_deploynotify-1.9.1.tar.gz/zeit_deploynotify-1.9.1/src/zeit/deploynotify/grafana.py
@@ -0,0 +1,22 @@
+from .base import Notification
+import logging
+import requests
+
+
+log = logging.getLogger(__name__)
+
+
+class Grafana(Notification):
+
+    def __call__(self, url, token, text='{project} {version}', tags=None):
+        text = text.format(**self.__dict__)
+        if not tags:
+            tags = []
+
+        with requests.Session() as http:
+            r = http.post(
+                f'{url}/api/annotations', json={
+                    'text': text,
+                    'tags': tags,
+                }, headers={'Authorization': f'Bearer {token}'})
+            log.info('%s returned %s: %s', r.url, r.status_code, r.text)
