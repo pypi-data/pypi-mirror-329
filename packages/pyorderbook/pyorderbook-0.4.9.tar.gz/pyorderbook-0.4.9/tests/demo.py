160347f854ffe4272387a0c2a6d455544574cce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 15:06:16 +0000

    Add package pyorderbook/0.4.9/pyorderbook-0.4.9.tar.gz

---
+++ packages/pyorderbook/pyorderbook-0.4.9.tar.gz/pyorderbook-0.4.9/tests/demo.py
@@ -0,0 +1,15 @@
+"""Toy order flow simulation"""
+
+import pprint
+
+from pyorderbook import Book, ask, bid
+
+
+def simulate_order_flow() -> None:
+    """Toy order flow simulation"""
+    book = Book()
+    pprint.pprint(book.match([ask("GOOG", 3.5, 25), ask("GOOG", 3.6, 75), bid("GOOG", 3.7, 100)]))
+
+
+if __name__ == "__main__":
+    simulate_order_flow()
