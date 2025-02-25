62e3186460154a2e3b4fc24f1ad035459f9fe63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 03:11:22 +0000

    Add package nodestream/0.14.0/nodestream-0.14.0.tar.gz

---
+++ packages/nodestream/nodestream-0.14.0.tar.gz/nodestream-0.14.0/nodestream/interpreting/interpretations/interpretation.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from ...pipeline.value_providers import ProviderContext
+from ...pluggable import Pluggable
+from ...schema import ExpandsSchema
+from ...subclass_registry import SubclassRegistry
+
+INTERPRETATION_REGISTRY = SubclassRegistry()
+
+
+@INTERPRETATION_REGISTRY.connect_baseclass
+class Interpretation(ExpandsSchema, Pluggable, ABC):
+    entrypoint_name = "interpretations"
+    assigns_source_nodes = False
+
+    @abstractmethod
+    def interpret(self, context: ProviderContext):
+        raise NotImplementedError
+
+    @classmethod
+    def from_file_data(cls, **arguments) -> "Interpretation":
+        name = arguments.pop("type")
+        class_to_load = INTERPRETATION_REGISTRY.get(name)
+        return class_to_load(**arguments)
