27f02dd17c3333aff4e7ca3abb4204db347c6a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:08:10 +0000

    Add package plurally/0.3.47/plurally-0.3.47-py3-none-any.whl

---
+++ packages/plurally/plurally-0.3.47-py3-none-any.whl/plurally/models/action/ai.py
@@ -0,0 +1,5 @@
+from plurally.models.action.indexer import Indexer  # noqa: F401
+from plurally.models.action.instruct import Instruct  # noqa: F401
+from plurally.models.action.transcript import Transcript  # noqa: F401
+
+__all__ = ["Indexer", "Instruct", "Transcript"]
---
+++ packages/plurally/plurally-0.3.47-py3-none-any.whl/plurally/models/output/__init__.py
@@ -0,0 +1,5 @@
+from .crm_actions import CrmActionsOutput
+from .table import TableOutput
+from .text import HtmlOutput, TextOutput
+
+__all__ = ["TextOutput", "CrmActionsOutput", "HtmlOutput", "TableOutput"]
