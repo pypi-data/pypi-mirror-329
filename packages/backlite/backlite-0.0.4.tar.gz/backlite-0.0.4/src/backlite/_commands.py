07820c75e5f5c3aea908b6b1b1666b35e6f22f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:06:46 +0000

    Add package backlite/0.0.4/backlite-0.0.4.tar.gz

---
+++ packages/backlite/backlite-0.0.4.tar.gz/backlite-0.0.4/src/backlite/_commands.py
@@ -0,0 +1,118 @@
+import sqlite3
+from collections.abc import Collection
+from collections.abc import Mapping
+from datetime import UTC
+from datetime import datetime
+
+from backlite._metadata import total_value_size
+from backlite.types import CacheItem
+from backlite.types import EvictionPolicy
+
+
+def get_cache_keys(conn: sqlite3.Connection) -> list[str]:
+    """Get the keys in the cache."""
+    rows = conn.execute("SELECT key FROM cache").fetchall()
+    return [r[0] for r in rows]
+
+
+def get_cache_items(
+    conn: sqlite3.Connection,
+    keys: Collection[str] | None,
+) -> Mapping[str, CacheItem]:
+    """Get the values for the given keys."""
+    if keys is None:
+        rows = conn.execute(
+            """
+            SELECT key, value, expires_at
+            FROM cache
+            """
+        ).fetchall()
+        keys = [r[0] for r in rows]
+    else:
+        rows = conn.execute(
+            f"""
+            SELECT key, value, expires_at
+            FROM cache
+            WHERE key IN ({", ".join("?" for _ in keys)})
+            """,  # noqa: S608 (ok because values are not user input)
+            tuple(keys),
+        ).fetchall()
+    result = {
+        key: CacheItem(value=value, expires_at=datetime.fromtimestamp(expires_at, tz=UTC))
+        if expires_at is not None
+        else CacheItem(value=value)
+        for key, value, expires_at in rows
+    }
+    conn.execute(
+        f"""
+        UPDATE cache
+        SET accessed_at = unixepoch('subsec'),
+            accessed_count = accessed_count + 1
+        WHERE key IN ({", ".join("?" for _ in keys)})
+        """,  # noqa: S608
+        tuple(keys),
+    )
+    return result
+
+
+def set_cache_items(conn: sqlite3.Connection, items: Mapping[str, CacheItem]) -> None:
+    """Update the cache with the given values."""
+    conn.executemany(
+        """
+        INSERT OR REPLACE INTO cache (key, value, expires_at)
+        VALUES (?, ?, ?)
+        """,
+        [
+            (
+                key,
+                value["value"],
+                expires_at.timestamp()
+                if (expires_at := value.get("expires_at")) is not None
+                else None,
+            )
+            for key, value in items.items()
+        ],
+    )
+
+
+def evict_cache_items(
+    conn: sqlite3.Connection,
+    *,
+    size_limit: int,
+    policy: EvictionPolicy,
+) -> None:
+    """Evict items from the cache until the total size is less than the max size."""
+    # Cleanup expired items first
+    conn.execute(
+        "DELETE FROM cache WHERE expires_at IS NOT NULL AND expires_at < unixepoch('subsec')"
+    )
+
+    # Get the current size of the cache
+    current_size = total_value_size.get(conn)
+
+    # If the current size is already less than the limit, do nothing
+    if current_size <= size_limit:
+        return
+
+    # Pick the keys to evict based on the policy
+    keys_to_evict: list[str] = []
+    order_by = _SORT_BY_POLICY[policy]
+    for key, size in conn.execute(
+        f"SELECT key, LENGTH(value) FROM cache ORDER BY {order_by}"  # noqa: S608
+    ).fetchall():
+        keys_to_evict.append(key)
+        current_size -= size
+        if current_size <= size_limit:
+            break
+
+    # Evict the items
+    conn.execute(
+        f"DELETE FROM cache WHERE key IN ({', '.join('?' for _ in keys_to_evict)})",  # noqa: S608
+        tuple(keys_to_evict),
+    )
+
+
+_SORT_BY_POLICY: Mapping[EvictionPolicy, str] = {
+    "least-recently-used": "accessed_at ASC",
+    "least-frequently-used": "accessed_count ASC",
+}
---
+++ packages/backlite/backlite-0.0.4.tar.gz/backlite-0.0.4/src/backlite/decorators.py
@@ -0,0 +1,85 @@
+import pickle
+from collections.abc import Awaitable
+from collections.abc import Callable
+from collections.abc import Coroutine
+from datetime import UTC
+from datetime import datetime
+from datetime import timedelta
+from functools import wraps
+from inspect import Signature
+from inspect import signature
+from typing import Any
+from typing import ParamSpec
+from typing import TypeAlias
+from typing import TypeVar
+
+from paramorator import paramorator
+
+from backlite.cache import Cache
+
+P = ParamSpec("P")
+R = TypeVar("R")
+AsyncCallable: TypeAlias = Callable[P, Awaitable[R]]
+CoroCallable: TypeAlias = Callable[P, Coroutine[None, None, R]]
+
+
+@paramorator
+def cached(
+    func: Callable[P, R],
+    *,
+    storage: Cache,
+    expire_after: timedelta | None = None,
+) -> Callable[P, R]:
+    """Decorate a function to cache its result."""
+    sig = signature(func)
+
+    @wraps(func)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        key = _param_hash_func(sig, args, kwargs)
+        if (item := storage.get_one(key)) is not None:
+            value = pickle.loads(item["value"])
+        else:
+            value = func(*args, **kwargs)
+            storage.set_one(
+                key,
+                {
+                    "value": pickle.dumps(value),
+                    "expires_at": datetime.now(UTC) + expire_after if expire_after else None,
+                },
+            )
+        return value
+
+    return wrapper
+
+
+@paramorator
+def async_cached(
+    func: AsyncCallable[P, R],
+    storage: Cache,
+    *,
+    expire_after: timedelta | None = None,
+) -> CoroCallable[P, R]:
+    """Decorate an async function to cache its result."""
+    sig = signature(func)
+
+    @wraps(func)
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        key = _param_hash_func(sig, args, kwargs)
+        if (item := storage.get_one(key)) is not None:
+            value = pickle.loads(item["value"])
+        else:
+            value = await func(*args, **kwargs)
+            storage.set_one(
+                key,
+                {
+                    "value": pickle.dumps(value),
+                    "expires_at": datetime.now(UTC) + expire_after if expire_after else None,
+                },
+            )
+        return value
+
+    return wrapper
+
+
+def _param_hash_func(_: Signature, args: tuple[Any, ...], kwargs: dict[str, Any]) -> str:
+    return str(hash((args, frozenset(kwargs.items()))))
