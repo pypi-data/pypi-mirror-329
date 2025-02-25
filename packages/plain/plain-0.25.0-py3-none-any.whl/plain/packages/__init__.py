0619e3da0a971b4395ddee4df3b7e4bb36340ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 05:05:36 +0000

    Add package plain/0.25.0/plain-0.25.0-py3-none-any.whl

---
+++ packages/plain/plain-0.25.0-py3-none-any.whl/plain/packages/__init__.py
@@ -0,0 +1,4 @@
+from .config import PackageConfig
+from .registry import packages_registry
+
+__all__ = ["PackageConfig", "packages_registry"]
---
+++ packages/plain/plain-0.25.0-py3-none-any.whl/plain/utils/module_loading.py
@@ -0,0 +1,69 @@
+import os
+import sys
+from importlib import import_module
+from importlib.util import find_spec as importlib_find
+
+
+def cached_import(module_path, class_name):
+    # Check whether module is loaded and fully initialized.
+    if not (
+        (module := sys.modules.get(module_path))
+        and (spec := getattr(module, "__spec__", None))
+        and getattr(spec, "_initializing", False) is False
+    ):
+        module = import_module(module_path)
+    return getattr(module, class_name)
+
+
+def import_string(dotted_path):
+    """
+    Import a dotted module path and return the attribute/class designated by the
+    last name in the path. Raise ImportError if the import failed.
+    """
+    try:
+        module_path, class_name = dotted_path.rsplit(".", 1)
+    except ValueError as err:
+        raise ImportError(f"{dotted_path} doesn't look like a module path") from err
+
+    try:
+        return cached_import(module_path, class_name)
+    except AttributeError as err:
+        raise ImportError(
+            f'Module "{module_path}" does not define a "{class_name}" attribute/class'
+        ) from err
+
+
+def module_has_submodule(package, module_name):
+    """See if 'module' is in 'package'."""
+    try:
+        package_name = package.__name__
+        package_path = package.__path__
+    except AttributeError:
+        # package isn't a package.
+        return False
+
+    full_module_name = package_name + "." + module_name
+    try:
+        return importlib_find(full_module_name, package_path) is not None
+    except ModuleNotFoundError:
+        # When module_name is an invalid dotted path, Python raises
+        # ModuleNotFoundError.
+        return False
+
+
+def module_dir(module):
+    """
+    Find the name of the directory that contains a module, if possible.
+
+    Raise ValueError otherwise, e.g. for namespace packages that are split
+    over several directories.
+    """
+    # Convert to list because __path__ may not support indexing.
+    paths = list(getattr(module, "__path__", []))
+    if len(paths) == 1:
+        return paths[0]
+    else:
+        filename = getattr(module, "__file__", None)
+        if filename is not None:
+            return os.path.dirname(filename)
+    raise ValueError(f"Cannot determine directory containing {module}")
