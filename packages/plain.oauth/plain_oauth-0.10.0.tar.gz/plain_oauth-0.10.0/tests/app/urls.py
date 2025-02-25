ee08720a0a1892874beeac5764cd6db9274443e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 07:05:10 +0000

    Add package plain.oauth/0.10.0/plain_oauth-0.10.0.tar.gz

---
+++ packages/plain.oauth/plain_oauth-0.10.0.tar.gz/plain_oauth-0.10.0/tests/app/urls.py
@@ -0,0 +1,29 @@
+import plain.oauth.urls
+from plain.auth.views import AuthViewMixin, LogoutView
+from plain.oauth.providers import get_provider_keys
+from plain.urls import RouterBase, include, path, register_router
+from plain.views import TemplateView
+
+
+class LoggedInView(AuthViewMixin, TemplateView):
+    template_name = "index.html"
+
+    def get_template_context(self):
+        context = super().get_template_context()
+        context["oauth_provider_keys"] = get_provider_keys()
+        return context
+
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+
+@register_router
+class Router(RouterBase):
+    namespace = ""
+    urls = [
+        include("oauth/", plain.oauth.urls),
+        path("login/", LoginView, name="login"),
+        path("logout/", LogoutView, name="logout"),
+        path("", LoggedInView),
+    ]
