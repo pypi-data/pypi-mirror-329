842753ab82bad7f0643373ebdb1d446cbee45aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 06:06:57 +0000

    Add package lazyinput/0.1.1/lazyinput-0.1.1.tar.gz

---
+++ packages/lazyinput/lazyinput-0.1.1.tar.gz/lazyinput-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="lazyinput",
+    version="0.1.1",
+    description="I am very lazy",
+    url="https://github.com/justin-huiyun-lee/",
+    author="5nsou",
+    author_email="justin.huiyun.lee@gmail.com",
+    install_requires=[],
+)
