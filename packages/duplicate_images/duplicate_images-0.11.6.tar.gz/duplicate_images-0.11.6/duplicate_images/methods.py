2b81474a426f1128134dca19d93525a19d8a3f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 03:10:59 +0000

    Add package duplicate_images/0.11.6/duplicate_images-0.11.6.tar.gz

---
+++ packages/duplicate_images/duplicate_images-0.11.6.tar.gz/duplicate_images-0.11.6/duplicate_images/methods.py
@@ -0,0 +1,98 @@
+"""
+Definition of the possible actions run on sets of equal images
+"""
+__author__ = 'Lene Preuss <lene.preuss@gmail.com>'
+
+import logging
+from argparse import Namespace
+from pathlib import Path
+from shlex import quote
+from subprocess import call  # nosec
+from typing import Callable, Dict, List, Optional, Union
+
+import imagehash
+
+from duplicate_images.common import path_with_parent
+from duplicate_images.function_types import ActionFunction, HashFunction, ImageGroup
+
+__all__ = [
+    'call', 'quote', 'get_hash_size_kwargs', 'IMAGE_HASH_ALGORITHM', 'ALGORITHM_DEFAULTS',
+    'ACTIONS_ON_EQUALITY'
+]
+
+
+def ascending_by_size(group: ImageGroup) -> List[Path]:
+    return sorted(group, key=lambda path: (path.stat().st_size, str(path)))
+
+
+def delete_with_log_message(file: Path) -> None:
+    file.unlink()
+    logging.info('Deleted %s', path_with_parent(file))
+
+
+def symlink_to_biggest_file(group: ImageGroup):
+    biggest = ascending_by_size(group)[-1]
+    others = set(group) - {biggest}
+    for file in others:
+        delete_with_log_message(file)
+        file.symlink_to(biggest)
+
+
+def shell_exec(args: Namespace, group: ImageGroup) -> None:
+    cmd = args.exec
+    for num, path in enumerate(group):
+        cmd = cmd.replace(f"{'{'}{num + 1}{'}'}", f'{quote(str(path))}')
+    cmd = cmd.replace('{*}', ' '.join([quote(str(path)) for path in group]))
+    call(cmd, shell=True)  # nosec
+
+
+def get_hash_size_kwargs(algorithm: HashFunction, size: Optional[int]) -> Dict:
+    if size is None:
+        return ALGORITHM_DEFAULTS.get(algorithm, {'hash_size': 8})
+    kwarg = next(iter(ALGORITHM_DEFAULTS.get(algorithm, {'hash_size': 8})))
+    return {} if kwarg == 'hash_func' else {kwarg: size}
+
+
+IMAGE_HASH_ALGORITHM = {
+    'ahash': imagehash.average_hash,
+    'phash': imagehash.phash,
+    'phash_simple': imagehash.phash_simple,
+    'dhash': imagehash.dhash,
+    'dhash_vertical': imagehash.dhash_vertical,
+    'whash': imagehash.whash,
+    'colorhash': imagehash.colorhash,
+    'crop_resistant': imagehash.crop_resistant_hash,
+}  # type: Dict[str, HashFunction]
+
+ALGORITHM_DEFAULTS: Dict[Callable, Dict[str, Union[int, HashFunction]]] = {
+    imagehash.average_hash: {'hash_size': 8},
+    imagehash.phash: {'hash_size': 8},
+    imagehash.phash_simple: {'hash_size': 8},
+    imagehash.dhash: {'hash_size': 8},
+    imagehash.dhash_vertical: {'hash_size': 8},
+    imagehash.whash: {'hash_size': 8},
+    imagehash.colorhash: {'binbits': 3},
+    imagehash.crop_resistant_hash: {'hash_func': imagehash.phash},
+}
+
+ACTIONS_ON_EQUALITY: Dict[str, ActionFunction] = {
+    'delete-first': lambda args, group: delete_with_log_message(group[0]),
+    'd1': lambda args, group: delete_with_log_message(group[0]),
+    'delete-last': lambda args, group: delete_with_log_message(group[-1]),
+    'dl': lambda args, group: delete_with_log_message(group[-1]),
+    'delete-biggest': lambda args, group: delete_with_log_message(ascending_by_size(group)[-1]),
+    'd>': lambda args, group: delete_with_log_message(ascending_by_size(group)[-1]),
+    'delete-smallest': lambda args, group: delete_with_log_message(ascending_by_size(group)[0]),
+    'd<': lambda args, group: delete_with_log_message(ascending_by_size(group)[0]),
+    'symlink-smaller': lambda args, group: symlink_to_biggest_file(group),
+    'eog': lambda args, group: call(['eog'] + [str(pic) for pic in group]),  # nosec
+    'xv': lambda args, group: call(['xv', '-nolim'] + [str(pic) for pic in group]),  # nosec
+    'print': lambda args, group: print(*group),
+    'print_inline': lambda args, group: print(*group, end=' '),
+    'quote': lambda args, group: print(' '.join([quote(str(pic)) for pic in group])),
+    'quote_inline': lambda args, group: print(
+        ' '.join([quote(str(pic)) for pic in group]), end=' '
+    ),
+    'exec': lambda args, group: shell_exec(args, group),  # pylint: disable=unnecessary-lambda
+    'none': lambda args, group: None,
+}
---
+++ packages/duplicate_images/duplicate_images-0.11.6.tar.gz/duplicate_images-0.11.6/duplicate_images/parse_commandline.py
@@ -0,0 +1,94 @@
+"""
+Define and parse command line arguments for the `find-dups` command line tool
+"""
+__author__ = 'Lene Preuss <lene.preuss@gmail.com>'
+
+from os import cpu_count
+from argparse import ArgumentParser, Namespace
+from typing import List, Optional
+
+from PIL import Image
+
+from duplicate_images.methods import ACTIONS_ON_EQUALITY, IMAGE_HASH_ALGORITHM
+
+
+def is_power_of_2(n: int) -> bool:
+    # https://stackoverflow.com/questions/57025836/how-to-check-if-a-given-number-is-a-power-of-two
+    return (n != 0) and (n & (n - 1) == 0)
+
+
+def parse_command_line(args: Optional[List[str]] = None) -> Namespace:
+    parser = ArgumentParser(description='Find pairs of equal or similar images.')
+
+    parser.add_argument(
+        'root_directory', default='.', nargs='+',
+        help='The root of the directory tree under which images are compared'
+    )
+    parser.add_argument(
+        '--exclude-dir', default=None, nargs='*',
+        help='Directories to exclude from the search (can be given as regular expressions)'
+    )
+    parser.add_argument(
+        '--algorithm', choices=IMAGE_HASH_ALGORITHM.keys(),
+        default='phash',
+        help='Method used to determine if two images are considered equal'
+    )
+    parser.add_argument(
+        '--max-distance', type=int, default=0,
+        help='Maximum hash distance for images to be considered equal'
+    )
+    parser.add_argument(
+        '--hash-size', type=int,
+        help='Hash size (or number of bin bits for colorhash)'
+    )
+    parser.add_argument(
+        '--on-equal', choices=ACTIONS_ON_EQUALITY.keys(),
+        default='print', help='Command to be run on each pair of images found to be equal'
+    )
+    parser.add_argument(
+        '--exec', type=str,
+        help='Command to execute (replaces {1}, {2} with file paths)'
+    )
+    parser.add_argument(
+        '--parallel', nargs='?', type=int, default=None, const=cpu_count(),
+        help=f'Calculate hashes using PARALLEL threads (default: {cpu_count()})'
+    )
+    parser.add_argument(
+        '--parallel-actions', nargs='?', type=int, default=None, const=cpu_count(),
+        help=f'Execute actions on equal images using PARALLEL threads (default: {cpu_count()})'
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '--slow', action='store_true', help='Use slow (O(N^2)) algorithm'
+    )
+    group.add_argument(
+        '--group', action='store_true',
+        help='Handle equal images in a group instead of multiple pairs'
+    )
+    parser.add_argument(
+        '--progress', action='store_true', help='Show progress bars during processing'
+    )
+    parser.add_argument(
+        '--debug', action='store_true', help='Print lots of debugging info'
+    )
+    parser.add_argument(
+        '--quiet', '-q', action='count', default=0, help='Decrease log level by one for each'
+    )
+    parser.add_argument(
+        '--hash-db', default=None, help='File storing precomputed hashes'
+    )
+    parser.add_argument(
+        '--max-image-pixels', type=int, default=None,
+        help=f'Maximum size of image in pixels (default: {Image.MAX_IMAGE_PIXELS})'
+    )
+
+    namespace = parser.parse_args(args)
+    if namespace.on_equal == 'exec' and not namespace.exec:
+        parser.error('--exec argument is required with --on-equal exec')
+    if namespace.exec and namespace.on_equal != 'exec':
+        parser.error('--exec is only allowed with --on-equal exec')
+    if namespace.algorithm == 'whash' and not is_power_of_2(namespace.hash_size):
+        parser.error('whash requires hash_size to be a power of 2')
+    if namespace.group and namespace.max_distance:
+        parser.error('--max-distance: not allowed with argument --group')
+    return namespace
