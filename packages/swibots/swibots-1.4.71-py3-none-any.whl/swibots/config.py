4f137eccc0155acd37cc70251070c97f482c90e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 03:11:15 +0000

    Add package swibots/1.4.71/swibots-1.4.71-py3-none-any.whl

---
+++ packages/swibots/swibots-1.4.71-py3-none-any.whl/swibots/config.py
@@ -0,0 +1,70 @@
+import os
+
+
+APP_CONFIG = {
+    "CHAT_SERVICE": {
+        "BASE_URL": os.getenv("CHAT_SERVICE_BASE_URL") or "https://chat-api.switch.pe",
+        "WS_URL": os.getenv("CHAT_SERVICE_WS_URL")
+        or "wss://chat-api.switch.pe/v1/websocket/message/ws",
+    },
+    "BOT_SERVICE": {
+        "BASE_URL": os.getenv("BOT_SERVICE_BASE_URL") or "https://chat-api.switch.pe",
+    },
+    "AUTH_SERVICE": {
+        "BASE_URL": os.getenv("AUTH_SERVICE_BASE_URL")
+        or "https://gateway.switch.pe/user-service",
+    },
+    "AIRDROP_SERVICE": {
+        "BASE_URL": os.getenv("AIRDROP_SERVICE_BASE_URL")
+        or "https://gateway.switch.pe/airdrop-service"
+    },
+    "COMMUNITY_SERVICE": {
+        "BASE_URL": os.getenv("COMMUNITY_SERVICE_BASE_URL")
+        or "https://gateway.switch.pe/community-service",
+        "WS_URL": os.getenv("COMMUNITY_SERVICE_WS_URL")
+        or "wss://gateway.switch.pe/community-service/v1/websocket/community/ws",
+    },
+    "BACKBLAZE": {
+        "BUCKET_ID": os.getenv("BACKBLAZE_BUCKET_ID") or "6e4a0369d05018689a560c1e",
+        "ACCOUNT_ID": os.getenv("BACKBLAZE_ACCOUNT_ID") or "005ea390088a6ce0000000001",
+        "APPLICATION_KEY": os.getenv("BACKBLAZE_APPLICATION_KEY")
+        or "K005/dLxZIYBzJLms5IEZ7CnZJUBRCU",
+    },
+}
+
+
+
+
+def get_config():
+    return APP_CONFIG
+
+
+def reload_config():
+    APP_CONFIG["CHAT_SERVICE"]["BASE_URL"] = (
+        os.getenv("CHAT_SERVICE_BASE_URL") or "https://chat-api.switch.pe"
+    )
+    APP_CONFIG["CHAT_SERVICE"]["WS_URL"] = (
+        os.getenv("CHAT_SERVICE_WS_URL")
+        or "wss://chat-api.switch.pe/v1/websocket/message/ws"
+    )
+    APP_CONFIG["BOT_SERVICE"]["BASE_URL"] = (
+        os.getenv("BOT_SERVICE_BASE_URL") or "https://chat-api.switch.pe"
+    )
+    APP_CONFIG["AUTH_SERVICE"]["BASE_URL"] = (
+        os.getenv("AUTH_SERVICE_BASE_URL") or "https://gateway.switch.pe/user-service"
+    )
+    APP_CONFIG["COMMUNITY_SERVICE"]["BASE_URL"] = (
+        os.getenv("COMMUNITY_SERVICE_BASE_URL")
+        or "https://gateway.switch.pe/community-service"
+    )
+    APP_CONFIG["COMMUNITY_SERVICE"]["WS_URL"] = (
+        os.getenv("COMMUNITY_SERVICE_WS_URL")
+        or "wss://gateway.switch.pe/community-service/v1/websocket/community/ws"
+    )
+    APP_CONFIG["AIRDROP_SERVICE"]["BASE_URL"] = (
+        os.getenv("AIRDROP_SERVICE_BASE_URL")
+        or "https://gateway.switch.pe/airdrop-service"
+    )
+
+
+reload_config()
