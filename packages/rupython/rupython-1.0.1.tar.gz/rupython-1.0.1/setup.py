db900a2bc1b7c9bd8015a20559a6d623eac0a035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 15:06:38 +0000

    Add package rupython/1.0.1/rupython-1.0.1.tar.gz

---
+++ packages/rupython/rupython-1.0.1.tar.gz/rupython-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup as Установить
+
+Установить(
+    name = 'rupython',
+    version = '1.0.1',
+    description = "Исполнитель кода Русского Питона",
+    packages = [ 'rupython', 'rupython.Модули' ]
+)
