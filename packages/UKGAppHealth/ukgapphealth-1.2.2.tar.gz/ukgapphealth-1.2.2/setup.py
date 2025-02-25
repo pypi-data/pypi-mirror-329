0df58555ba74ee484b0423407eae2c9914e60d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 04:05:19 +0000

    Add package UKGAppHealth/1.2.2/ukgapphealth-1.2.2.tar.gz

---
+++ packages/UKGAppHealth/ukgapphealth-1.2.2.tar.gz/ukgapphealth-1.2.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UKGAppHealth',
+    version='1.2.2',
+    description='Python library for monitoring health checks on running apps',
+    author='Reinas Balakauskas',
+    packages=find_packages(),
+)
