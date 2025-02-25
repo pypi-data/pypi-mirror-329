bce7ecaae986675c013a757da9d25064bac0bb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 15:05:30 +0000

    Add package neodynamics/0.1.1/neodynamics-0.1.1-py3-none-any.whl

---
+++ packages/neodynamics/neodynamics-0.1.1-py3-none-any.whl/neodynamics/core.py
@@ -0,0 +1,13 @@
+def announce():
+    """
+    Prints the announcement message for NeoDynamics.
+    """
+    message = """
+    🚀 NeoDynamics is coming! 🚀
+
+    Get ready for a revolutionary approach to Reinforcement Learning.
+    Soon, we'll make RL accessible to everyone.
+
+    Stay tuned for updates!
+    """
+    print(message)
