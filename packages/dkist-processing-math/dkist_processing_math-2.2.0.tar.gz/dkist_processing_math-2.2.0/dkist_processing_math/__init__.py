c3897b584542dfd13af767cc0fb2839dfc1761bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 04:05:30 +0000

    Add package dkist-processing-math/2.2.0/dkist_processing_math-2.2.0.tar.gz

---
+++ packages/dkist-processing-math/dkist_processing_math-2.2.0.tar.gz/dkist_processing_math-2.2.0/dkist_processing_math/__init__.py
@@ -0,0 +1,9 @@
+"""init."""
+from pkg_resources import DistributionNotFound
+from pkg_resources import get_distribution
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
