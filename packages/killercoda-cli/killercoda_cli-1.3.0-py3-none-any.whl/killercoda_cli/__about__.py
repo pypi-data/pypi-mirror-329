4c862b562719e1249b5fae8dc3fdb01f9d3d5ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-25 07:05:04 +0000

    Add package killercoda-cli/1.3.0/killercoda_cli-1.3.0-py3-none-any.whl

---
+++ packages/killercoda-cli/killercoda_cli-1.3.0-py3-none-any.whl/killercoda_cli/__about__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2024-present Piotr Zaniewski <piotrzan@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+# This is setup in build process in CI, but also servers as a record for the --version flag
+__version__ = "1.3.0"
