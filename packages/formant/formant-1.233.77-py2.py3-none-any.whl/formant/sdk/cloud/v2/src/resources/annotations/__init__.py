99e5bae69935a5dd7b07f39e9b50c4fb16f02dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 05:05:19 +0000

    Add package formant/1.233.77/formant-1.233.77-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.233.77-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import TagParameters, AnnotationTemplate, SheetParameters, AnnotationTags, AnnotationFieldValuesRequest, UserParametersRolesItem, AnnotationTemplateTags, AnnotationFieldType, AnnotationMetadata, AnnotationFieldValueTags, ScopeFilterTypesItem, AnnotationTemplateListResponse, AnnotationField, AnnotationFieldValuesResponse, ScopeFilter, AnnotationType, AnnotationFieldValuesRequestTags, Annotation, AnnotationStreamType, FocusedDatapoint, UserParameters, AnnotationFieldValue
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.233.77-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/queries/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_query_api_client.models import StreamDataTags, Video, ImageAnnotation, Location, QueryAggregate, Query, Vector3, PointCloud, StreamDataPointsItem, QueryTypesItem, Twist, NumericSetEntry, TransformNode, Image, Odometry, Path, Battery, Bitset, BoundingBox, Health, Transform, File, StreamAggregateDataType, Video,VideoMimeType, HealthStatus, StreamDataListResponse, Localization, Map, Quaternion, Goal, StreamData, StreamDataType, StreamAggregateDataTags, StreamAggregateData, VideoMimeType, Health,HealthStatus
+from formant.sdk.cloud.v2.formant_query_api_client.api.query import query_controller_query
+from formant.sdk.cloud.v2.formant_query_api_client.types import *
