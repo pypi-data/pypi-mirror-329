e3503be81335b39c74982a9295ee3af8583acb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:06:36 +0000

    Add package ext-llm/0.1.2/ext_llm-0.1.2.tar.gz

---
+++ packages/ext-llm/ext_llm-0.1.2.tar.gz/ext_llm-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ext-llm",
+    version="0.1.2",
+    description="A wrapper library to abstract common llm providers",
+    author="Giovanni Pio Grieco",
+    author_email="gio.grieco@stud.uniroma3.it",
+    license="GPL3",
+    packages=find_packages(),  # Automatically find and include all packages
+    install_requires=[
+        # List your dependencies here
+    ],
+    url="https://github.com/giovanni-grieco/llmx",
+)
