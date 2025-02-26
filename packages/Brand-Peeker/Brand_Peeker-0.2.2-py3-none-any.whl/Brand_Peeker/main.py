14b32c883431073f032500482745758185c96aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 04:07:05 +0000

    Add package Brand-Peeker/0.2.2/Brand_Peeker-0.2.2-py3-none-any.whl

---
+++ packages/Brand-Peeker/Brand_Peeker-0.2.2-py3-none-any.whl/Brand_Peeker/main.py
@@ -0,0 +1,19 @@
+import requests
+
+from Brand_Peeker import getUrls
+
+def getIconByUrl(url, output):
+    faviconUrl = getUrls.getFaviconUrl(url)
+    if output:
+        Name = output
+    else:
+        Name = faviconUrl.split("/")[-1]
+
+    r = requests.get(faviconUrl, allow_redirects=True)
+    open(Name, 'wb').write(r.content)
+
+
+def getIconByName(name, output):
+    url = getUrls.getUrlByWebsearch(name)
+    getIconByUrl(url, output)
+
