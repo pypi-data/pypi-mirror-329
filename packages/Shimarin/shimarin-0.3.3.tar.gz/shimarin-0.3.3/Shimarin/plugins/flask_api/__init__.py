00bc864cc76288b7977a1a1ebc08cce77b48b942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:05:43 +0000

    Add package Shimarin/0.3.3/shimarin-0.3.3.tar.gz

---
+++ packages/Shimarin/shimarin-0.3.3.tar.gz/shimarin-0.3.3/Shimarin/plugins/flask_api/__init__.py
@@ -0,0 +1,4 @@
+from Shimarin.plugins.flask_api.server import (CONTEXT_PATH, LoginResponse,
+                                               ShimaApp, login)
+
+__all__ = ["login", "CONTEXT_PATH", "ShimaApp", "LoginResponse"]
