1f18d7406d5f5c4c66d47fe38effc47a13cbfd6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:10:41 +0000

    Add package safe-security-logger/1.8.0/safe_security_logger-1.8.0.tar.gz

---
+++ packages/safe-security-logger/safe_security_logger-1.8.0.tar.gz/safe_security_logger-1.8.0/CHANGELOG.md
@@ -0,0 +1,60 @@
+## v1.8.0 (2025-02-19)
+
+### Feat
+
+- **logger**: add support for tenantId and requestId (#15)
+
+## v1.7.0 (2022-10-12)
+
+### Feat
+
+- **logger**: add support for stack trace (#13)
+
+## v1.6.0 (2022-09-30)
+
+### Feat
+
+- **logger**: aded support to move additional fields to metadata (#9)
+
+## v1.5.0 (2022-09-29)
+
+### Feat
+
+- **logger**: added manual trigger and restrict access (#8)
+- **logger**: added line number to logs (#7)
+
+## v1.4.0 (2022-09-27)
+
+### Feat
+
+- **logger**: added support for additional fields in logger init (#6)
+
+## v1.3.0 (2022-09-26)
+
+### Feat
+
+- **logger**: Added support to provide multiple handlers during init (#5)
+
+## v1.2.0 (2022-09-26)
+
+### Feat
+
+- **logger**: renamed serviceName field to service (#4)
+
+## v1.1.0 (2022-09-23)
+
+### Feat
+
+- **logger**: added support to provide service name during logger init (#3)
+
+## v1.0.1 (2022-09-21)
+
+### Fix
+
+- add few spaces to test out auto release
+
+## v1.0.0 (2022-09-21)
+
+### Feat
+
+- **logger**: Add initial version of logger with boilerplate (#1)
---
+++ packages/safe-security-logger/safe_security_logger-1.8.0.tar.gz/safe_security_logger-1.8.0/src/safe_security_logger/logger.py
@@ -0,0 +1,91 @@
+import logging
+import sys
+import time
+
+from pythonjsonlogger import jsonlogger
+
+__author__ = "deepak.s@safe.security"
+__copyright__ = "Safe Security"
+__license__ = "MIT"
+
+ROOT_LEVEL_FIELDS = [
+    "level",
+    "service",
+    "timestamp",
+    "type",
+    "message",
+    "error",
+    "tenantId",
+    "requestId",
+]
+
+
+# Adding custom logger to support additional default field such as serviceName
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def __init__(self, *args, **kwargs):
+        self.additional_fields = kwargs.pop("additional_fields", None)
+        self.root_level_fields = kwargs.pop("root_level_fields", None)
+        super().__init__(*args, **kwargs)
+
+    def add_fields(self, log_record, record, message_dict):
+        super().add_fields(log_record, record, message_dict)
+        for key, value in self.additional_fields.items():
+            if (not log_record.get(key)) and value:
+                log_record[key] = value
+
+        # While using logger.exception inside except block, the stack trace would be added to exc_info field
+        # Porting that information to "error" field to maintain consistency with NPM package
+        if "error" not in log_record and "exc_info" in log_record:
+            log_record["error"] = log_record.pop("exc_info")
+
+        # all fields other than the root level fields would be moved to the metadata field to maintain consistency with NPM package
+        metadata = {}
+        log_record_copy = log_record.copy()
+        for key, value in log_record_copy.items():
+            if key not in self.root_level_fields:
+                metadata.update({key: value})
+                log_record.pop(key)
+        log_record["metadata"] = metadata
+
+
+def getLogger(
+    name,
+    service=None,
+    level=logging.INFO,
+    handlers=[],
+    additional_fields={},
+    root_level_fields=ROOT_LEVEL_FIELDS,
+):
+
+    logger = logging.getLogger(name)
+
+    if service:
+        additional_fields.update({"service": service})
+
+    additional_fields.update({"type": "application"})
+
+    formatter = CustomJsonFormatter(
+        "%(asctime)s %(levelname)s %(message)s %(name)s %(module)s %(funcName)s %(lineno)d",
+        rename_fields={
+            "levelname": "level",
+            "asctime": "timestamp",
+            "funcName": "functionName",
+            "name": "loggerName",
+        },
+        additional_fields=additional_fields,
+        root_level_fields=root_level_fields,
+    )
+    # Use UTC time
+    formatter.converter = time.gmtime
+
+    # console handler would be added by default
+    console_handler = logging.StreamHandler(sys.stdout)
+    handlers.append(console_handler)
+
+    for handler in handlers:
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # default level would be INFO if level is not provided
+    logger.setLevel(level)
+    return logger
