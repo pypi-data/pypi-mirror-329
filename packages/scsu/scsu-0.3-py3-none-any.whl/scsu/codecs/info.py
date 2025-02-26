e88c87d83fce8591bdf94c28c734739697345e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 05:06:33 +0000

    Add package scsu/0.3/scsu-0.3-py3-none-any.whl

---
+++ packages/scsu/scsu-0.3-py3-none-any.whl/scsu/codecs/info.py
@@ -0,0 +1,8 @@
+MODULE_NAME = "scsu"
+MODULE_VERSION = "0.3"
+
+ENCODING_NAME = "Standard Compression Scheme for Unicode"
+
+CODEC_NAME = "SCSU"
+SIGNATURE_SUFFIX = "_SIG"
+BOM_SCSU = b"\x0E\xFE\xFF"
