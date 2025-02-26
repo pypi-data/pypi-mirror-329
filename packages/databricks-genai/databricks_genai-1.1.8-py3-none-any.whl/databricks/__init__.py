c08cd438d3f6582021a7e08542dafed6172c3ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 08:05:50 +0000

    Add package databricks-genai/1.1.8/databricks_genai-1.1.8-py3-none-any.whl

---
+++ packages/databricks-genai/databricks_genai-1.1.8-py3-none-any.whl/databricks/__init__.py
@@ -0,0 +1,5 @@
+"""Databricks Gen AI Package, aliased as databricks"""
+# https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
+# This file should only contain the following line. Otherwise other sub-packages databricks.* namespace
+# may not be importable.
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
