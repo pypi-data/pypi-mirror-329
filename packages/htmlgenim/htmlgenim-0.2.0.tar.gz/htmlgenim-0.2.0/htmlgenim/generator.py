adb73285c92fa6e82042fd9a72c16967df48987a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:10:38 +0000

    Add package htmlgenim/0.2.0/htmlgenim-0.2.0.tar.gz

---
+++ packages/htmlgenim/htmlgenim-0.2.0.tar.gz/htmlgenim-0.2.0/htmlgenim/generator.py
@@ -0,0 +1,26 @@
+import re
+import os
+
+class HTMLGenerator:
+    def __init__(self, template):
+        self.template = template
+
+    def render(self, **context):
+        # Заменяем переменные в шаблоне на значения из контекста
+        def replace_match(match):
+            key = match.group(1)
+            return str(context.get(key, ""))
+
+        # Используем регулярное выражение для поиска переменных в шаблоне
+        pattern = r"\{\{(\w+)\}\}"
+        rendered_html = re.sub(pattern, replace_match, self.template)
+        return rendered_html
+
+    @staticmethod
+    def save_to_file(html_content, output_file):
+        output_path = os.path.abspath(output_file)
+        # Создаем директорию, если её нет
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        with open(output_path, "w", encoding="utf-8") as file:
+            file.write(html_content)
+        print(f"File save: {output_path}")
