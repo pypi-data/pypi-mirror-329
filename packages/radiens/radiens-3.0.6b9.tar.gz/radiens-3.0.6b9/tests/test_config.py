76e4884ff1da97cdaa5cf159b949e4ca5b5033d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:06:23 +0000

    Add package radiens/3.0.6b9/radiens-3.0.6b9.tar.gz

---
+++ packages/radiens/radiens-3.0.6b9.tar.gz/radiens-3.0.6b9/tests/test_config.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+import unittest
+from pathlib import Path
+
+import radiens.utils.config as cfg
+
+
+class base_utest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+    
+    def test_discover_server_unix(self):
+        pass
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
