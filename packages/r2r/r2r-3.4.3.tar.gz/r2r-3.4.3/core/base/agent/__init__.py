1873a3eede5c2a3d9a83910adc908f8a64a76cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 07:05:41 +0000

    Add package r2r/3.4.3/r2r-3.4.3.tar.gz

---
+++ packages/r2r/r2r-3.4.3.tar.gz/r2r-3.4.3/core/base/agent/__init__.py
@@ -0,0 +1,10 @@
+from .agent import Agent, AgentConfig, Conversation, Tool, ToolResult
+
+__all__ = [
+    # Agent abstractions
+    "Agent",
+    "AgentConfig",
+    "Conversation",
+    "Tool",
+    "ToolResult",
+]
