3ae84476d79a1ff5d3800ce149940510f8c8e406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:11:21 +0000

    Add package weightedpandas/0.1.0/weightedpandas-0.1.0-py3-none-any.whl

---
+++ packages/weightedpandas/weightedpandas-0.1.0-py3-none-any.whl/weightedpandas/__init__.py
@@ -0,0 +1,15 @@
+from weightedpandas.core import (
+    WeightedDataFrame,
+    WeightedSeries,
+    weighted_dataframe,
+    weighted_series,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "WeightedSeries",
+    "WeightedDataFrame",
+    "weighted_series",
+    "weighted_dataframe",
+]
