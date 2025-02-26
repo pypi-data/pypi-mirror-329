90cc65a2713e4d4d2a06c12a27d46ec23d74cb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:11:10 +0000

    Add package irrectum/0.1.1/irrectum-0.1.1.tar.gz

---
+++ packages/irrectum/irrectum-0.1.1.tar.gz/irrectum-0.1.1/irrectum/__init__.py
@@ -0,0 +1,1 @@
+from .abc_metrics import Metric, MAE, Corr, CoefDet, BIC, IQI, RMSE, NRMSE, CovMetric, MCoefDet, RSS, MAPE, AIC
---
+++ packages/irrectum/irrectum-0.1.1.tar.gz/irrectum-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="irrectum",
+    version="0.1.0",
+    author="Aksyanov Rustam",
+    author_email="akrustamru@gmail.com",
+    description="Metrics for Evaluating Model Quality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy>=1.26.0',
+        'setuptools>=69.1.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
