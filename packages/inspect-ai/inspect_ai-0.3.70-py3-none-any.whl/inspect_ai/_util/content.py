2103d1bb65215acbd78df903b30ea437675b4417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:11:01 +0000

    Add package inspect-ai/0.3.70/inspect_ai-0.3.70-py3-none-any.whl

---
+++ packages/inspect-ai/inspect_ai-0.3.70-py3-none-any.whl/inspect_ai/_util/content.py
@@ -0,0 +1,59 @@
+from typing import Literal, Union
+
+from pydantic import BaseModel, Field
+
+
+class ContentText(BaseModel):
+    """Text content."""
+
+    type: Literal["text"] = Field(default="text")
+    """Type."""
+
+    text: str
+    """Text content."""
+
+
+class ContentImage(BaseModel):
+    """Image content."""
+
+    type: Literal["image"] = Field(default="image")
+    """Type."""
+
+    image: str
+    """Either a URL of the image or the base64 encoded image data."""
+
+    detail: Literal["auto", "low", "high"] = Field(default="auto")
+    """Specifies the detail level of the image.
+
+    Currently only supported for OpenAI. Learn more in the    [Vision guide](https://platform.openai.com/docs/guides/vision/low-or-high-fidelity-image-understanding).
+    """
+
+
+class ContentAudio(BaseModel):
+    """Audio content."""
+
+    type: Literal["audio"] = Field(default="audio")
+    """Type."""
+
+    audio: str
+    """Audio file path or base64 encoded data URL."""
+
+    format: Literal["wav", "mp3"]
+    """Format of audio data ('mp3' or 'wav')"""
+
+
+class ContentVideo(BaseModel):
+    """Video content."""
+
+    type: Literal["video"] = Field(default="video")
+    """Type."""
+
+    video: str
+    """Audio file path or base64 encoded data URL."""
+
+    format: Literal["mp4", "mpeg", "mov"]
+    """Format of video data ('mp4', 'mpeg', or 'mov')"""
+
+
+Content = Union[ContentText, ContentImage, ContentAudio, ContentVideo]
+"""Content sent to or received from a model."""
