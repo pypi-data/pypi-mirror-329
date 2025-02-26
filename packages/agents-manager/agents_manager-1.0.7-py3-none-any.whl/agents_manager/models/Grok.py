476219c1131c863fa69ed9d279b6105209eb8660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:06:42 +0000

    Add package agents-manager/1.0.7/agents_manager-1.0.7-py3-none-any.whl

---
+++ packages/agents-manager/agents_manager-1.0.7-py3-none-any.whl/agents_manager/models/Grok.py
@@ -0,0 +1,25 @@
+from typing import Any, Optional
+
+from openai import OpenAI
+
+from agents_manager.models import OpenAi
+
+
+class Grok(OpenAi):
+    def __init__(self, name: str, **kwargs: Any) -> None:
+        """
+        Initialize the OpenAi model with a name and optional keyword arguments.
+
+        Args:
+            name (str): The name of the OpenAI model (e.g., "gpt-3.5-turbo").
+            **kwargs (Any): Additional arguments, including optional "api_key".
+        """
+        super().__init__(name, **kwargs)
+
+        if name is None:
+            raise ValueError("A valid  Grok model name is required")
+
+        self.client = OpenAI(
+            api_key=kwargs.get("api_key"),  # type: Optional[str]
+            base_url="https://api.x.ai/v1"
+        )
