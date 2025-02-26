baa006757fb069559678b09e9af57aa5277adfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:06:52 +0000

    Add package structifyai/1.103.0/structifyai-1.103.0.tar.gz

---
+++ packages/structifyai/structifyai-1.103.0.tar.gz/structifyai-1.103.0/src/structify/types/admin/training_dataset_suspicious_count_params.py
@@ -0,0 +1,30 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["TrainingDatasetSuspiciousCountParams"]
+
+
+class TrainingDatasetSuspiciousCountParams(TypedDict, total=False):
+    status: Required[
+        Literal[
+            "Unlabeled",
+            "NavLabeled",
+            "SaveLabeled",
+            "NavVerified",
+            "SaveVerified",
+            "Pending",
+            "Skipped",
+            "SuspiciousNav",
+            "SuspiciousSave",
+            "PotentialSuspiciousNav",
+            "PotentialSuspiciousSave",
+        ]
+    ]
+
+    dataset_name: Optional[str]
+
+    user_restriction: bool
