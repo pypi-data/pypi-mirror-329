70a9551938f67385359b05b55181c04af93c3901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 07:06:25 +0000

    Add package rustycalc/0.1.0/rustycalc-0.1.0.tar.gz

---
+++ packages/rustycalc/rustycalc-0.1.0.tar.gz/rustycalc-0.1.0/src/maths/factorial.rs
@@ -0,0 +1,11 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+/// Computes the factorial of a given integer `n`.
+pub fn factorial(n: u32) -> u32 {
+    if n == 0 {
+        return 1;
+    }
+    println!("Lund lele!");
+    return n * factorial(n - 1);
+}
