52bccc94ebb388cbc059bffc8e942e64cf787679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 04:06:53 +0000

    Add package kolzchut-ragbot/1.1.0/kolzchut_ragbot-1.1.0.tar.gz

---
+++ packages/kolzchut-ragbot/kolzchut_ragbot-1.1.0.tar.gz/kolzchut_ragbot-1.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kolzchut-ragbot',
+    version='1.1.0',
+    author='Shmuel Robinov',
+    author_email='shmuel_robinov@webiks.com',
+    description='A search engine using machine learning models and Elasticsearch for advanced document retrieval.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shmuelrob/ragbot',
+    packages=find_packages(),
+    install_requires=[
+        'elasticsearch==8.14.0',
+        'sentence-transformers==3.0.1',
+        'torch==2.3.1',
+        'transformers==4.42.3'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
