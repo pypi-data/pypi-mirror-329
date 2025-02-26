af4144d392611131c94f4d66a83618f8eb1e8d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 07:06:23 +0000

    Add package KgmiColors/0.1.1/kgmicolors-0.1.1.tar.gz

---
+++ packages/KgmiColors/kgmicolors-0.1.1.tar.gz/kgmicolors-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="KgmiColors",
+    version="0.1.1",
+    description="Un package simple pour colorer les textes dans la console.",
+    author="Kagami",
+    url="https://github.com/Uwu-Kagami",
+    packages=find_packages(),
+    readme = "README.md",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests>=2.25.1,<3.0.0"
+    ]
+)
+
