647242e77163e35b3bc09f129b6e28f0a3da8efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:05:43 +0000

    Add package axial-positional-embedding/0.3.12/axial_positional_embedding-0.3.12.tar.gz

---
+++ packages/axial-positional-embedding/axial_positional_embedding-0.3.12.tar.gz/axial_positional_embedding-0.3.12/axial_positional_embedding/__init__.py
@@ -0,0 +1,8 @@
+from axial_positional_embedding.axial_positional_embedding import (
+    AxialPositionalEmbedding,
+    AxialPositionalEmbeddingImage
+)
+
+from axial_positional_embedding.continuous_axial_positional_embedding import (
+    ContinuousAxialPositionalEmbedding
+)
