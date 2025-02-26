1c5210499f25f1b0eaf0a031c51b1e13a2e92c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 07:06:21 +0000

    Add package encrust/2025.2.25/encrust-2025.2.25-py3-none-any.whl

---
+++ packages/encrust/encrust-2025.2.25-py3-none-any.whl/encrust/_signing.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from typing import Iterable
+
+from twisted.python.filepath import FilePath
+
+from ._spawnutil import c
+
+
+async def signOneFile(
+    fileToSign: FilePath[str],
+    codesigningIdentity: str,
+    entitlements: FilePath[str],
+) -> None:
+    """
+    Code sign a single file.
+    """
+    fileStr = fileToSign.path
+    entitlementsStr = entitlements.path
+    print("✓", end="", flush=True)
+    await c.codesign(
+        "--sign",
+        codesigningIdentity,
+        "--entitlements",
+        entitlementsStr,
+        "--force",
+        "--options",
+        "runtime",
+        fileStr,
+    )
+
+
+def signablePathsIn(topPath: FilePath[str]) -> Iterable[FilePath[str]]:
+    """
+    What files need to be individually code-signed within a given bundle?
+    """
+    for p in topPath.walk(lambda subp: (not subp.islink() and subp.isdir())):
+        if p.islink():
+            continue
+        ext = p.splitext()[-1]
+        if ext in {".so", ".dylib", ".framework", ".a", ".app"}:
+            yield p
+        elif p.basename() == 'python' and p.parent().basename() == "MacOS":
+            yield p
+
+
+async def notarize(
+    *,
+    archivePath: FilePath[str],
+    applicationPath: FilePath[str],
+    appleID: str,
+    teamID: str,
+    notarizeProfile: str,
+) -> None:
+    """
+    Submit the signed bundle for notarization, wait for success, then notarize
+    it.
+    """
+    await c.xcrun(
+        "notarytool",
+        "submit",
+        archivePath.path,
+        f"--apple-id={appleID}",
+        f"--team-id={teamID}",
+        f"--keychain-profile={notarizeProfile}",
+        f"--wait",
+    )
+    await c.xcrun("stapler", "staple", applicationPath.path)
