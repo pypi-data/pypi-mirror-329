4733acff674a876a28072a483ea5297a507326fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 08:05:55 +0000

    Add package canonical.ext.google/0.0.1/canonical_ext_google-0.0.1.tar.gz

---
+++ packages/canonical.ext.google/canonical_ext_google-0.0.1.tar.gz/canonical_ext_google-0.0.1/canonical/ext/google/protocols/idatastorekey.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2023 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from typing import Protocol
+
+
+class IDatastoreKey(Protocol):
+    __module__: str = 'tensorshield.ext.google.protocols'
+    flat_path: tuple[int | str, ...]
+    id: int
+    name: str
+    path: list[dict[str, int | str]]
