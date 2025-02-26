ae9cf0275fcad2d0b4c0d179c66267423ea21f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:06:21 +0000

    Add package jf-ingest/0.0.184/jf_ingest-0.0.184.tar.gz

---
+++ packages/jf-ingest/jf_ingest-0.0.184.tar.gz/jf_ingest-0.0.184/jf_ingest/jf_git/exceptions.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+
+class GitProviderUnavailable(Exception):
+    pass
+
+
+class GqlRateLimitExceededException(Exception):
+    pass
+
+
+class GitAuthenticationException(Exception):
+    def __init__(self, *args, original_exception: Optional[Exception] = None):
+        self.original_exception = original_exception
+        super().__init__(*args)
