e4997c02d51a4d37b620c242e354ecf9553333e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:10:46 +0000

    Add package cvmcore/0.2.2/cvmcore-0.2.2.tar.gz

---
+++ packages/cvmcore/cvmcore-0.2.2.tar.gz/cvmcore-0.2.2/cvmcore/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+__title__ = 'cvmplot'
+__description__ = 'Major plot function used by SZQ lab from China Agricultural University'
+__url__ = 'https://github.com/hbucqp/cvmplot'
+__version__ = "0.2.2"
+__author__ = 'Qingpo Cui'
+__author_email__ = 'cqp@cau.edu.cn'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 Qingpo Cui'
