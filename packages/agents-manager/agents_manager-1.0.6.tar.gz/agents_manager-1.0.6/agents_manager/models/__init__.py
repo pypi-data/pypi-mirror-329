71bcb85a0559614f8caa261383684345a81d0487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:11:19 +0000

    Add package agents-manager/1.0.6/agents_manager-1.0.6.tar.gz

---
+++ packages/agents-manager/agents_manager-1.0.6.tar.gz/agents_manager-1.0.6/agents_manager/models/__init__.py
@@ -0,0 +1,6 @@
+
+from agents_manager.models.OpenAi import OpenAi
+from agents_manager.models.Grok import Grok
+from agents_manager.models.DeepSeek import DeepSeek
+
+__all__ = ["OpenAi", "Grok", "DeepSeek"]
