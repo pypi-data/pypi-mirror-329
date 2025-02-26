1c009263a86ee68fe0e3dd6cb3585c496b869a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 04:05:32 +0000

    Add package tqdm-prometheus-exporter/0.1.14/tqdm_prometheus_exporter-0.1.14.tar.gz

---
+++ packages/tqdm-prometheus-exporter/tqdm_prometheus_exporter-0.1.14.tar.gz/tqdm_prometheus_exporter-0.1.14/tests/test_proxy.py
@@ -0,0 +1,69 @@
+# pylint: disable=missing-function-docstring, missing-class-docstring, missing-module-docstring
+
+from io import StringIO
+from time import sleep
+import unittest
+
+from tqdmpromproxy.proxy import TqdmPrometheusProxy
+
+
+class ProxyIntegrationTest(unittest.TestCase):
+    def test_export(self):
+        try:
+            proxy = TqdmPrometheusProxy()
+            proxy.start()
+
+            instances = [proxy.tqdm(i, desc=f'Item#{i}', total=100, position=i)
+                         for i in range(10)]
+
+            counter = 0
+            for i in instances:
+                i.update(counter*10)
+                counter += 1
+
+            def test():
+                _buf = StringIO()
+                proxy.http_server.bucketer.export(_buf)
+                buf = _buf.getvalue()
+
+                return 'Item#0' in buf, f"Wanted Item#0 in '{buf}'"
+
+            self._retryable_assertion(test)
+        finally:
+            proxy.stop()
+
+    def test_long_task_not_duplicated(self):
+        try:
+            proxy = TqdmPrometheusProxy()
+            proxy.start()
+
+            duration_s = 30
+            total = 100
+
+            for _ in proxy.tqdm(range(total), desc='LongItem'):
+                sleep(duration_s/float(total))
+
+            _buf = StringIO()
+            proxy.http_server.bucketer.export(_buf)
+            buf = _buf.getvalue()
+
+            self.assertIn('LongItem_active_count 1', buf,
+                          f"Wanted single instance in \n---\n'{buf}'\n---")
+        finally:
+            proxy.stop()
+
+    def _retryable_assertion(self, test: callable, max_wait=10.0):
+        delay = 0.2
+        assertion = False
+        while not assertion:
+            assertion, message = test()
+            if not assertion:
+                delay *= 2
+                if delay > max_wait:
+                    break
+
+                print(
+                    f"Assertion failed. Recieved: {message}")
+                sleep(delay)
+
+        return self.assertTrue(assertion, message)
