cff978b655a482b567454f8fabac49b8a08bee61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 05:05:28 +0000

    Add package sso-config-generator/1.1.1/sso_config_generator-1.1.1-py3-none-any.whl

---
+++ packages/sso-config-generator/sso_config_generator-1.1.1-py3-none-any.whl/sso_config_generator/__init__.py
@@ -0,0 +1,7 @@
+"""SSO Config Generator - Generate AWS SSO configuration and directory structures."""
+
+__version__ = "1.1.1"
+
+from .core import SSOConfigGenerator
+
+__all__ = ["SSOConfigGenerator", "__version__"]
