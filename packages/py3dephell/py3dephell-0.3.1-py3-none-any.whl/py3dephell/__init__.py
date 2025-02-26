bc9492ed593170461a15c7284d5759d89a6dbf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:10:38 +0000

    Add package py3dephell/0.3.1/py3dephell-0.3.1-py3-none-any.whl

---
+++ packages/py3dephell/py3dephell-0.3.1-py3-none-any.whl/py3dephell/__init__.py
@@ -0,0 +1,12 @@
+'''
+
+py3dephell - helps you to control python3 dependencies and provides
+
+py3dephell.py3req - detect dependencies, filter them through self-provides
+
+py3dephell.py3prov - detect provides, process .pth files
+
+py3dephell.py3graph - separate package to subpackages according to their dependencies (not fully implemented yet)
+
+.. include:: ../../README.md
+'''
