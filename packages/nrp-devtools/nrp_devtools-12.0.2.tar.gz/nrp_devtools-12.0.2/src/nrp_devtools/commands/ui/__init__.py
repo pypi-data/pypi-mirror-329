5a6d4be1e29870e786b6ae9339488923d729dc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 07:05:57 +0000

    Add package nrp-devtools/12.0.2/nrp_devtools-12.0.2.tar.gz

---
+++ packages/nrp-devtools/nrp_devtools-12.0.2.tar.gz/nrp_devtools-12.0.2/src/nrp_devtools/commands/ui/__init__.py
@@ -0,0 +1,45 @@
+import json
+
+from ...config import OARepoConfig
+from ..check import check_failed
+from .assets import collect_assets, install_npm_packages
+from .build import build_production_ui
+from .less import register_less_components
+
+
+def check_ui(config: OARepoConfig, will_fix=False, **kwargs):
+    # check that there is a manifest.json there
+    manifest = config.invenio_instance_path / "static" / "dist" / "manifest.json"
+    if not manifest.exists():
+        check_failed(
+            f"manifest.json file is missing.",
+            will_fix=will_fix,
+        )
+
+    try:
+        json_data = json.loads(manifest.read_text())
+        if json_data.get("status") != "done":
+            check_failed(
+                f"manifest.json file is not ready. "
+                f"Either a build is in progress or it failed.",
+                will_fix=will_fix,
+            )
+    except:  # noqa
+        check_failed(
+            f"manifest.json file is not valid json file.",
+            will_fix=will_fix,
+        )
+
+
+def fix_ui(config: OARepoConfig, **kwargs):
+    collect_assets(config)
+    install_npm_packages(config)
+    build_production_ui(config)
+
+
+__all__ = [
+    "register_less_components",
+    "build_production_ui",
+    "collect_assets",
+    "install_npm_packages",
+]
