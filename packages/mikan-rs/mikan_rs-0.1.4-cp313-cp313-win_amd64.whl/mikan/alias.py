993bf80302ac4e6761c9b56889653f53bf29f0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:10:35 +0000

    Add package mikan-rs/0.1.4/mikan_rs-0.1.4-cp313-cp313-win_amd64.whl

---
+++ packages/mikan-rs/mikan_rs-0.1.4-cp313-cp313-win_amd64.whl/mikan/alias.py
@@ -0,0 +1,118 @@
+ALIAS_DICT = {
+
+    'accuracy': 'accuracy',
+    'acc': 'accuracy',
+    'Acc': 'accuracy',
+    'ACC': 'accuracy',
+    "Accuracy": "accuracy",
+    "RI": "accuracy",
+    "RandIndex": "accuracy",
+
+    'adjust_rand_score': 'adjust_rand_score',
+
+    'auc': 'auc',
+    'AUC': 'auc',
+    'AUC_trapezoid': 'auc',
+
+    'balanced_accuracy': 'balanced_accuracy',
+    'bacc': 'balanced_accuracy',
+    'BACC': 'balanced_accuracy',
+    'BalancedAccuracy':'balanced_accuracy',
+
+    'dice': 'dice',
+    'Dice': 'dice',
+    'dsc': 'dice',
+    'DSC': 'dice',
+
+    'f_score': 'f_score',
+
+    'jaccard_score': 'jaccard_score',
+    'iou': 'jaccard_score',
+    'IoU': 'jaccard_score',
+    'jaccard': 'jaccard_score',
+    'Jaccard': 'jaccard_score',
+    'IntersectionOverUnion': 'jaccard_score',
+
+    'kappa': 'kappa',
+    'KAP':'kappa',
+    'Kappa': 'kappa',
+    'CohensKappa': 'kappa',
+
+    'mcc': 'mcc',
+    'MCC': 'mcc',
+    'MatthewsCorrelationCoefficient': 'mcc',
+
+    'nmcc': 'nmcc',
+    'NMCC': 'nmcc',
+    'nMCC': 'nmcc',
+    'MCC_normalized': "nmcc",
+
+    'amcc': 'amcc',
+    'AMCC': 'amcc',
+    'aMCC': 'amcc',
+    'MCC_absolute': 'amcc',
+
+    'precision': 'precision',
+    'prec': 'precision',
+    'PREC': 'precision',
+    'Precision': 'precision',
+
+    'senstivity': 'senstivity',
+    'sens': 'senstivity',
+    'SENS': 'senstivity',
+    'Sensitivity': 'senstivity',
+    'Recall': 'senstivity',
+    'recall': 'senstivity',
+    'TPR': 'senstivity',
+    'tpr': 'senstivity',
+    'true_positive_rate': 'senstivity',
+    
+    'specificity': 'specificity',
+    'Specificity': 'specificity',
+    'spec': 'specificity',
+    'Spec': 'specificity',
+    'SPEC': 'specificity',
+    'TNR': 'specificity',
+    'tnr': 'specificity',
+    'true_negative_rate': 'specificity',
+    
+    'tn': 'tn',
+    'TN': 'tn',
+    'TrueNegative': 'tn',
+
+    'tp': 'tp',
+    'TP': 'tp',
+    'TruePositive': 'tp',
+
+    'fn': 'fn',
+    'FN': 'fn',
+    'FalseNegative': 'fn',
+
+    'fp': 'fp',
+    'FP': 'fp',
+    'FalsePositive': "fp",
+
+    'fnr': 'fnr',
+    'FNR': 'fnr',
+    'fpr': 'fpr',
+    'FPR': 'fpr',
+
+    'volume_similarity': 'volume_similarity',
+    'VolumetricSimilarity': 'volume_similarity',
+    'vs': "volume_similarity",
+    "VS": "volume_similarity",
+
+    'hausdorff_distance_95': 'hausdorff_distance_95',
+    'HD95': 'hausdorff_distance_95',
+    'hd95': 'hausdorff_distance_95',
+    
+    'hausdorff_distance': 'hausdorff_distance',
+    'hd': 'hausdorff_distance',
+    'HD': 'hausdorff_distance',
+    
+    'assd': 'assd',
+    'ASSD': 'assd',
+    
+    'masd': 'masd',
+    'MASD': 'masd',
+}
