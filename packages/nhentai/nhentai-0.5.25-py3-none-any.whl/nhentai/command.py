691973937ba85ac32047026488d3ce7636fb26ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 05:05:30 +0000

    Add package nhentai/0.5.25/nhentai-0.5.25-py3-none-any.whl

---
+++ packages/nhentai/nhentai-0.5.25-py3-none-any.whl/nhentai/command.py
@@ -0,0 +1,162 @@
+# coding: utf-8
+import os
+import shutil
+import sys
+import signal
+import platform
+import urllib3.exceptions
+
+from nhentai import constant
+from nhentai.cmdline import cmd_parser, banner, write_config
+from nhentai.parser import doujinshi_parser, search_parser, legacy_search_parser, print_doujinshi, favorites_parser
+from nhentai.doujinshi import Doujinshi
+from nhentai.downloader import Downloader
+from nhentai.logger import logger
+from nhentai.constant import BASE_URL
+from nhentai.utils import generate_html, generate_doc, generate_main_html, generate_metadata_file, \
+    paging, check_cookie, signal_handler, DB, move_to_folder
+
+
+def main():
+    banner()
+
+    if sys.version_info < (3, 0, 0):
+        logger.error('nhentai now only support Python 3.x')
+        sys.exit(1)
+
+    options = cmd_parser()
+    logger.info(f'Using mirror: {BASE_URL}')
+
+    # CONFIG['proxy'] will be changed after cmd_parser()
+    if constant.CONFIG['proxy']:
+        if isinstance(constant.CONFIG['proxy'], dict):
+            constant.CONFIG['proxy'] = constant.CONFIG['proxy'].get('http', '')
+            logger.warning(f'Update proxy config to: {constant.CONFIG["proxy"]}')
+            write_config()
+
+        logger.info(f'Using proxy: {constant.CONFIG["proxy"]}')
+
+    if not constant.CONFIG['template']:
+        constant.CONFIG['template'] = 'default'
+
+    logger.info(f'Using viewer template "{constant.CONFIG["template"]}"')
+
+    # check your cookie
+    check_cookie()
+
+    doujinshis = []
+    doujinshi_ids = []
+
+    page_list = paging(options.page)
+
+    if options.retry:
+        constant.RETRY_TIMES = int(options.retry)
+
+    if options.favorites:
+        if not options.is_download:
+            logger.warning('You do not specify --download option')
+
+        doujinshis = favorites_parser(page=page_list) if options.page else favorites_parser()
+
+    elif options.keyword:
+        if constant.CONFIG['language']:
+            logger.info(f'Using default language: {constant.CONFIG["language"]}')
+            options.keyword += f' language:{constant.CONFIG["language"]}'
+
+        _search_parser = legacy_search_parser if options.legacy else search_parser
+        doujinshis = _search_parser(options.keyword, sorting=options.sorting, page=page_list,
+                                    is_page_all=options.page_all)
+
+    elif options.artist:
+        doujinshis = legacy_search_parser(options.artist, sorting=options.sorting, page=page_list,
+                                          is_page_all=options.page_all, type_='ARTIST')
+
+    elif not doujinshi_ids:
+        doujinshi_ids = options.id
+
+    print_doujinshi(doujinshis)
+    if options.is_download and doujinshis:
+        doujinshi_ids = [i['id'] for i in doujinshis]
+
+    if options.is_save_download_history:
+        with DB() as db:
+            data = map(int, db.get_all())
+
+        doujinshi_ids = list(set(map(int, doujinshi_ids)) - set(data))
+
+    if not options.is_show:
+        downloader = Downloader(path=options.output_dir, threads=options.threads,
+                                timeout=options.timeout, delay=options.delay,
+                                exit_on_fail=options.exit_on_fail,
+                                no_filename_padding=options.no_filename_padding)
+
+        for doujinshi_id in doujinshi_ids:
+            doujinshi_info = doujinshi_parser(doujinshi_id)
+            if doujinshi_info:
+                doujinshi = Doujinshi(name_format=options.name_format, **doujinshi_info)
+            else:
+                continue
+
+            if not options.dryrun:
+                doujinshi.downloader = downloader
+
+                if doujinshi.check_if_need_download(options):
+                    doujinshi.download()
+                else:
+                    logger.info(f'Skip download doujinshi because a PDF/CBZ file exists of doujinshi {doujinshi.name}')
+                    continue
+
+            if options.generate_metadata:
+                generate_metadata_file(options.output_dir, doujinshi)
+
+            if options.is_save_download_history:
+                with DB() as db:
+                    db.add_one(doujinshi.id)
+
+            if not options.is_nohtml:
+                generate_html(options.output_dir, doujinshi, template=constant.CONFIG['template'])
+
+            if not options.no_metadata:
+                generate_doc('json', options.output_dir, doujinshi, options.regenerate)
+
+            if options.is_cbz:
+                generate_doc('cbz', options.output_dir, doujinshi, options.regenerate)
+
+            if options.is_pdf:
+                generate_doc('pdf', options.output_dir, doujinshi, options.regenerate)
+
+            if options.move_to_folder:
+                if options.is_cbz:
+                    move_to_folder(options.output_dir, doujinshi, 'cbz')
+                if options.is_pdf:
+                    move_to_folder(options.output_dir, doujinshi, 'pdf')
+
+            if options.rm_origin_dir:
+                if options.move_to_folder:
+                    logger.critical('You specified both --move-to-folder and --rm-origin-dir options, '
+                                    'you will not get anything :(')
+                shutil.rmtree(os.path.join(options.output_dir, doujinshi.filename), ignore_errors=True)
+
+        if options.main_viewer:
+            generate_main_html(options.output_dir)
+
+        if not platform.system() == 'Windows':
+            logger.log(16, '🍻 All done.')
+        else:
+            logger.log(16, 'All done.')
+
+    else:
+        for doujinshi_id in doujinshi_ids:
+            doujinshi_info = doujinshi_parser(doujinshi_id)
+            if doujinshi_info:
+                doujinshi = Doujinshi(name_format=options.name_format, **doujinshi_info)
+            else:
+                continue
+            doujinshi.show()
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/nhentai/nhentai-0.5.25-py3-none-any.whl/nhentai/downloader.py
@@ -0,0 +1,177 @@
+# coding: utf-
+
+import os
+import asyncio
+import httpx
+import urllib3.exceptions
+
+from urllib.parse import urlparse
+from nhentai import constant
+from nhentai.logger import logger
+from nhentai.utils import Singleton, async_request
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class NHentaiImageNotExistException(Exception):
+    pass
+
+
+def download_callback(result):
+    result, data = result
+    if result == 0:
+        logger.warning('fatal errors occurred, ignored')
+    elif result == -1:
+        logger.warning(f'url {data} return status code 404')
+    elif result == -2:
+        logger.warning('Ctrl-C pressed, exiting sub processes ...')
+    elif result == -3:
+        # workers won't be run, just pass
+        pass
+    else:
+        logger.log(16, f'{data} downloaded successfully')
+
+
+class Downloader(Singleton):
+    def __init__(self, path='', threads=5, timeout=30, delay=0, exit_on_fail=False,
+                 no_filename_padding=False):
+        self.threads = threads
+        self.path = str(path)
+        self.timeout = timeout
+        self.delay = delay
+        self.exit_on_fail = exit_on_fail
+        self.folder = None
+        self.semaphore = None
+        self.no_filename_padding = no_filename_padding
+
+    async def fiber(self, tasks):
+        self.semaphore = asyncio.Semaphore(self.threads)
+        for completed_task in asyncio.as_completed(tasks):
+            try:
+                result = await completed_task
+                if result[0] > 0:
+                    logger.info(f'{result[1]} download completed')
+                else:
+                    raise Exception(f'{result[1]} download failed, return value {result[0]}')
+            except Exception as e:
+                logger.error(f'An error occurred: {e}')
+                if self.exit_on_fail:
+                    raise Exception('User intends to exit on fail')
+
+    async def _semaphore_download(self, *args, **kwargs):
+        async with self.semaphore:
+            return await self.download(*args, **kwargs)
+
+    async def download(self, url, folder='', filename='', retried=0, proxy=None, length=0):
+        logger.info(f'Starting to download {url} ...')
+
+        if self.delay:
+            await asyncio.sleep(self.delay)
+
+        filename = filename if filename else os.path.basename(urlparse(url).path)
+        base_filename, extension = os.path.splitext(filename)
+
+        if not self.no_filename_padding:
+            filename = base_filename.zfill(length) + extension
+        else:
+            filename = base_filename + extension
+
+        save_file_path = os.path.join(self.folder, filename)
+
+        try:
+            if os.path.exists(save_file_path):
+                logger.warning(f'Skipped download: {save_file_path} already exists')
+                return 1, url
+
+            response = await async_request('GET', url, timeout=self.timeout, proxy=proxy)
+
+            if response.status_code != 200:
+                path = urlparse(url).path
+                for mirror in constant.IMAGE_URL_MIRRORS:
+                    logger.info(f"Try mirror: {mirror}{path}")
+                    mirror_url = f'{mirror}{path}'
+                    response = await async_request('GET', mirror_url, timeout=self.timeout, proxies=proxy)
+                    if response.status_code == 200:
+                        break
+
+            if not await self.save(filename, response):
+                logger.error(f'Can not download image {url}')
+                return -1, url
+
+        except (httpx.HTTPStatusError, httpx.TimeoutException, httpx.ConnectError) as e:
+            if retried < constant.RETRY_TIMES:
+                logger.warning(f'Download {filename} failed, retrying({retried + 1}) times...')
+                return await self.download(
+                    url=url,
+                    folder=folder,
+                    filename=filename,
+                    retried=retried + 1,
+                    proxy=proxy,
+                )
+            else:
+                logger.warning(f'Download {filename} failed with {constant.RETRY_TIMES} times retried, skipped')
+                return -2, url
+
+        except NHentaiImageNotExistException as e:
+            os.remove(save_file_path)
+            return -3, url
+
+        except Exception as e:
+            import traceback
+
+            logger.error(f"Exception type: {type(e)}")
+            traceback.print_stack()
+            logger.critical(str(e))
+            return -9, url
+
+        except KeyboardInterrupt:
+            return -4, url
+
+        return 1, url
+
+    async def save(self, save_file_path, response) -> bool:
+        if response is None:
+            logger.error('Error: Response is None')
+            return False
+        save_file_path = os.path.join(self.folder, save_file_path)
+        with open(save_file_path, 'wb') as f:
+            if response is not None:
+                length = response.headers.get('content-length')
+                if length is None:
+                    f.write(response.content)
+                else:
+                    async for chunk in response.aiter_bytes(2048):
+                        f.write(chunk)
+        return True
+
+    def start_download(self, queue, folder='') -> bool:
+        if not isinstance(folder, (str,)):
+            folder = str(folder)
+
+        if self.path:
+            folder = os.path.join(self.path, folder)
+
+        logger.info(f'Doujinshi will be saved at "{folder}"')
+        if not os.path.exists(folder):
+            try:
+                os.makedirs(folder)
+            except EnvironmentError as e:
+                logger.critical(str(e))
+        self.folder = folder
+
+        if os.getenv('DEBUG', None) == 'NODOWNLOAD':
+            # Assuming we want to continue with rest of process.
+            return True
+
+        digit_length = len(str(len(queue)))
+        logger.info(f'Total download pages: {len(queue)}')
+        coroutines = [
+            self._semaphore_download(url, filename=os.path.basename(urlparse(url).path), length=digit_length)
+            for url in queue
+        ]
+
+        # Prevent coroutines infection
+        asyncio.run(self.fiber(coroutines))
+
+        return True
