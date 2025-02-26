67d80e1d7d09f6f7bb27cb4c8c21bd2a125addcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 04:06:48 +0000

    Add package Nexgram.py/0.0.15/Nexgram.py-0.0.15-py3-none-any.whl

---
+++ packages/Nexgram.py/Nexgram.py-0.0.15-py3-none-any.whl/Nexgram/Methods/send_message.py
@@ -0,0 +1,10 @@
+import httpx
+
+class sendMessage:
+  async def send_message(self, chat_id, text):
+    if not self.connected: raise ConnectionError("Client is not connected, you must connect the client to send message.")
+    url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"
+    data = {"chat_id": chat_id, "text": text}
+    async with httpx.AsyncClient() as client:
+      r = await client.post(url, json=data)
+      return r.json()
