dcf478a9fbb54602e1fcc72bc8fe1a8757b2a81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 05:05:29 +0000

    Add package protovalidate/0.7.1/protovalidate-0.7.1.tar.gz

---
+++ packages/protovalidate/protovalidate-0.7.1.tar.gz/protovalidate-0.7.1/README.md
@@ -0,0 +1,165 @@
+# protovalidate-python
+
+[![CI](https://github.com/bufbuild/protovalidate-python/actions/workflows/ci.yaml/badge.svg)](https://github.com/bufbuild/protovalidate-python/actions/workflows/ci.yaml)
+[![Conformance](https://github.com/bufbuild/protovalidate-python/actions/workflows/conformance.yaml/badge.svg)](https://github.com/bufbuild/protovalidate-python/actions/workflows/conformance.yaml)
+[![PyPI version](https://badge.fury.io/py/protovalidate.svg)](https://badge.fury.io/py/protovalidate)
+
+`protovalidate-python` is the Python implementation of [`protovalidate`](https://github.com/bufbuild/protovalidate),
+designed to validate Protobuf messages at runtime based on user-defined validation constraints. Powered by Google's
+Common Expression Language ([CEL](https://github.com/google/cel-spec)), it provides a flexible and efficient foundation
+for defining and evaluating custom validation rules. The primary goal of `protovalidate` is to help developers ensure
+data consistency and integrity across the network without requiring generated code.
+
+## The `protovalidate` project
+
+Head over to the core [`protovalidate`](https://github.com/bufbuild/protovalidate/) repository for:
+
+- [The API definition](https://github.com/bufbuild/protovalidate/tree/main/proto/protovalidate/buf/validate/validate.proto):
+  used to describe validation constraints
+- [Documentation](https://github.com/bufbuild/protovalidate/tree/main/docs): how to apply `protovalidate` effectively
+- [Migration tooling](https://github.com/bufbuild/protovalidate/tree/main/docs/migrate.md): incrementally migrate
+  from `protoc-gen-validate`
+- [Conformance testing utilities](https://github.com/bufbuild/protovalidate/tree/main/docs/conformance.md): for
+  acceptance testing of `protovalidate` implementations
+
+Other `protovalidate` runtime implementations include:
+
+- Go: [`protovalidate-go`](https://github.com/bufbuild/protovalidate-go)
+- C++: [`protovalidate-cc`](https://github.com/bufbuild/protovalidate-cc)
+- Java: [`protovalidate-java`](https://github.com/bufbuild/protovalidate-java)
+
+And others coming soon:
+
+- TypeScript: `protovalidate-ts`
+
+## Installation
+
+To install the package, use pip:
+
+```shell
+pip install protovalidate
+```
+
+Make sure you have the latest version of `protovalidate-python` by checking the
+project's [PyPI page](https://pypi.org/project/protovalidate/).
+
+## Usage
+
+### Implementing validation constraints
+
+Validation constraints are defined directly within `.proto` files. Documentation for adding constraints can be found in
+the `protovalidate` project [README](https://github.com/bufbuild/protovalidate) and
+its [comprehensive docs](https://github.com/bufbuild/protovalidate/tree/main/docs).
+
+```protobuf
+syntax = "proto3";
+
+package my.package;
+
+import "google/protobuf/timestamp.proto";
+import "buf/validate/validate.proto";
+
+message Transaction {
+  uint64 id = 1 [(buf.validate.field).uint64.gt = 999];
+  google.protobuf.Timestamp purchase_date = 2;
+  google.protobuf.Timestamp delivery_date = 3;
+
+  string price = 4 [(buf.validate.field).cel = {
+    id: "transaction.price",
+    message: "price must be positive and include a valid currency symbol ($ or £)",
+    expression: "(this.startsWith('$') || this.startsWith('£')) && double(this.substring(1)) > 0"
+  }];
+
+  option (buf.validate.message).cel = {
+    id: "transaction.delivery_date",
+    message: "delivery date must be after purchase date",
+    expression: "this.delivery_date > this.purchase_date"
+  };
+}
+```
+
+### Generating Code with `buf`
+
+When using the runtime library after installing it with `pip`, it's necessary to generate the Python code for the core `buf.protovalidate` Protobuf package. `buf` provides an efficient method for this:
+
+1. **Initialize a New Configuration File**:
+   ```shell
+   buf config init
+   ```
+   This initializes the `buf.yaml` configuration file at the root of the Protobuf source files.
+
+2. **Module Configuration and Dependencies**:
+   ```yaml
+   # buf.yaml
+   version: v2
+   deps:
+     - buf.build/bufbuild/protovalidate
+   ```
+
+   Ensure your dependencies are up-to-date with:
+   ```shell
+   buf dep update
+   ```
+
+3. **Setup Code Generation**:
+   ```yaml
+   # buf.gen.yaml
+   version: v2
+   plugins:
+     - remote: buf.build/protocolbuffers/python
+       out: gen
+   ```
+
+4. **Generate Code**:
+   To generate the required Python code:
+   ```shell
+   buf generate --include-imports
+   ```
+
+5. **Specify import paths**:
+   Ensure that the generated code is importable by setting the `PYTHONPATH` environment variable:
+   ```shell
+   export PYTHONPATH=$PYTHONPATH:gen
+   ```
+
+If your goal is to generate code specifically for the `buf.protovalidate` Protobuf package, run:
+```shell
+buf generate buf.build/bufbuild/protovalidate
+```
+
+> **Note:** For users familiar with `protoc`, while it's an alternative to `buf`, it is recommended to use tooling or frameworks like Bazel for direct code generation, as it provides an encapsulated environment for such tasks.
+
+### Example
+
+```python
+import protovalidate
+from google.protobuf.timestamp_pb2 import Timestamp
+from my.package import Transaction
+
+transaction = Transaction()
+transaction.id = 1234
+transaction.price = "$5.67"
+transaction.purchase_date.CopyFrom(Timestamp())
+transaction.delivery_date.CopyFrom(Timestamp())
+
+try:
+    protovalidate.validate(transaction)
+except protovalidate.ValidationError as e:
+# Report the violations
+```
+
+### Ecosystem
+
+- [`protovalidate`](https://github.com/bufbuild/protovalidate) core repository
+- [Buf](https://buf.build)
+- [CEL Spec](https://github.com/google/cel-spec)
+
+## Legal
+
+Offered under the [Apache 2 license][license].
+
+[license]: LICENSE
+[buf]: https://buf.build
+[buf-mod]: https://buf.build/bufbuild/protovalidate
+[cel-go]: https://github.com/google/cel-go
+[cel-spec]: https://github.com/google/cel-spec
---
+++ packages/protovalidate/protovalidate-0.7.1.tar.gz/protovalidate-0.7.1/protovalidate/internal/extra_func.py
@@ -0,0 +1,261 @@
+# Copyright 2023 Buf Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+import typing
+from email.utils import parseaddr
+from ipaddress import IPv4Address, IPv4Network, IPv6Address, IPv6Network, ip_address, ip_network
+from urllib import parse as urlparse
+
+import celpy  # type: ignore
+from celpy import celtypes  # type: ignore
+
+from protovalidate.internal import string_format
+
+
+def _validate_hostname(host):
+    if not host:
+        return False
+    if len(host) > 253:
+        return False
+
+    if host[-1] == ".":
+        host = host[:-1]
+
+    all_digits = True
+    for part in host.split("."):
+        if len(part) == 0 or len(part) > 63:
+            return False
+
+        # Host names cannot begin or end with hyphens
+        if part[0] == "-" or part[-1] == "-":
+            return False
+        all_digits = True
+        for r in part:
+            if (r < "A" or r > "Z") and (r < "a" or r > "z") and (r < "0" or r > "9") and r != "-":
+                return False
+            all_digits = all_digits and "0" <= r <= "9"
+    return not all_digits
+
+
+def validate_email(addr):
+    parts = parseaddr(addr)
+    if addr != parts[1]:
+        return False
+
+    addr = parts[1]
+    if len(addr) > 254:
+        return False
+
+    parts = addr.split("@")
+    if len(parts) != 2:
+        return False
+    if len(parts[0]) > 64:
+        return False
+    return _validate_hostname(parts[1])
+
+
+def validate_host_and_port(string: str, *, port_required: bool) -> bool:
+    if not string:
+        return False
+
+    split_idx = string.rfind(":")
+    if string[0] == "[":
+        end = string.find("]")
+        after_end = end + 1
+        if after_end == len(string):  # no port
+            return not port_required and validate_ip(string[1:end], 6)
+        if after_end == split_idx:  # port
+            return validate_ip(string[1:end]) and validate_port(string[split_idx + 1 :])
+        return False  # malformed
+
+    if split_idx == -1:
+        return not port_required and (_validate_hostname(string) or validate_ip(string, 4))
+
+    host = string[:split_idx]
+    port = string[split_idx + 1 :]
+    return (_validate_hostname(host) or validate_ip(host, 4)) and validate_port(port)
+
+
+def validate_port(val: str) -> bool:
+    try:
+        port = int(val)
+        return port <= 65535
+    except ValueError:
+        return False
+
+
+def validate_ip(val: typing.Union[str, bytes], version: typing.Optional[int] = None) -> bool:
+    try:
+        if version is None:
+            ip_address(val)
+        elif version == 4:
+            IPv4Address(val)
+        elif version == 6:
+            IPv6Address(val)
+        else:
+            msg = "invalid argument, expected 4 or 6"
+            raise celpy.CELEvalError(msg)
+        return True
+    except ValueError:
+        return False
+
+
+def is_ip(val: celtypes.Value, version: typing.Optional[celtypes.Value] = None) -> celpy.Result:
+    if not isinstance(val, (celtypes.BytesType, celtypes.StringType)):
+        msg = "invalid argument, expected string or bytes"
+        raise celpy.CELEvalError(msg)
+    if not isinstance(version, celtypes.IntType) and version is not None:
+        msg = "invalid argument, expected int"
+        raise celpy.CELEvalError(msg)
+    return celtypes.BoolType(validate_ip(val, version))
+
+
+def is_ip_prefix(val: celtypes.Value, *args) -> celpy.Result:
+    if not isinstance(val, (celtypes.BytesType, celtypes.StringType)):
+        msg = "invalid argument, expected string or bytes"
+        raise celpy.CELEvalError(msg)
+    version = None
+    strict = celtypes.BoolType(False)
+    if len(args) == 1 and isinstance(args[0], celtypes.BoolType):
+        strict = args[0]
+    elif len(args) == 1 and isinstance(args[0], celtypes.IntType):
+        version = args[0]
+    elif len(args) == 1 and (not isinstance(args[0], celtypes.BoolType) or not isinstance(args[0], celtypes.IntType)):
+        msg = "invalid argument, expected bool or int"
+        raise celpy.CELEvalError(msg)
+    elif len(args) == 2 and isinstance(args[0], celtypes.IntType) and isinstance(args[1], celtypes.BoolType):
+        version = args[0]
+        strict = args[1]
+    elif len(args) == 2 and (not isinstance(args[0], celtypes.IntType) or not isinstance(args[1], celtypes.BoolType)):
+        msg = "invalid argument, expected int and bool"
+        raise celpy.CELEvalError(msg)
+    try:
+        if version is None:
+            ip_network(val, strict=bool(strict))
+        elif version == 4:
+            IPv4Network(val, strict=bool(strict))
+        elif version == 6:
+            IPv6Network(val, strict=bool(strict))
+        else:
+            msg = "invalid argument, expected 4 or 6"
+            raise celpy.CELEvalError(msg)
+        return celtypes.BoolType(True)
+    except ValueError:
+        return celtypes.BoolType(False)
+
+
+def is_email(string: celtypes.Value) -> celpy.Result:
+    if not isinstance(string, celtypes.StringType):
+        msg = "invalid argument, expected string"
+        raise celpy.CELEvalError(msg)
+    return celtypes.BoolType(validate_email(string))
+
+
+def is_uri(string: celtypes.Value) -> celpy.Result:
+    url = urlparse.urlparse(str(string))
+    # urlparse correctly reads the scheme from URNs but parses everything
+    # after (except the query string) as the path.
+    if url.scheme == "urn":
+        if not (url.path):
+            return celtypes.BoolType(False)
+    elif not all([url.scheme, url.netloc, url.path]):
+        return celtypes.BoolType(False)
+
+    # If the query string contains percent-encoding, then try to decode it.
+    # unquote will return the same string if it is improperly encoded.
+    if "%" in url.query:
+        return celtypes.BoolType(urlparse.unquote(url.query) != url.query)
+
+    return celtypes.BoolType(True)
+
+
+def is_uri_ref(string: celtypes.Value) -> celpy.Result:
+    url = urlparse.urlparse(str(string))
+    if not all([url.scheme, url.path]) and url.fragment:
+        return celtypes.BoolType(False)
+    return celtypes.BoolType(True)
+
+
+def is_hostname(string: celtypes.Value) -> celpy.Result:
+    if not isinstance(string, celtypes.StringType):
+        msg = "invalid argument, expected string"
+        raise celpy.CELEvalError(msg)
+    return celtypes.BoolType(_validate_hostname(string))
+
+
+def is_host_and_port(string: celtypes.Value, port_required: celtypes.Value) -> celpy.Result:
+    if not isinstance(string, celtypes.StringType):
+        msg = "invalid argument, expected string"
+        raise celpy.CELEvalError(msg)
+    if not isinstance(port_required, celtypes.BoolType):
+        msg = "invalid argument, expected bool"
+        raise celpy.CELEvalError(msg)
+    return celtypes.BoolType(validate_host_and_port(string, port_required=bool(port_required)))
+
+
+def is_nan(val: celtypes.Value) -> celpy.Result:
+    if not isinstance(val, celtypes.DoubleType):
+        msg = "invalid argument, expected double"
+        raise celpy.CELEvalError(msg)
+    return celtypes.BoolType(math.isnan(val))
+
+
+def is_inf(val: celtypes.Value, sign: typing.Optional[celtypes.Value] = None) -> celpy.Result:
+    if not isinstance(val, celtypes.DoubleType):
+        msg = "invalid argument, expected double"
+        raise celpy.CELEvalError(msg)
+    if sign is None:
+        return celtypes.BoolType(math.isinf(val))
+
+    if not isinstance(sign, celtypes.IntType):
+        msg = "invalid argument, expected int"
+        raise celpy.CELEvalError(msg)
+    if sign > 0:
+        return celtypes.BoolType(math.isinf(val) and val > 0)
+    elif sign < 0:
+        return celtypes.BoolType(math.isinf(val) and val < 0)
+    else:
+        return celtypes.BoolType(math.isinf(val))
+
+
+def unique(val: celtypes.Value) -> celpy.Result:
+    if not isinstance(val, celtypes.ListType):
+        msg = "invalid argument, expected list"
+        raise celpy.CELEvalError(msg)
+    return celtypes.BoolType(len(val) == len(set(val)))
+
+
+def make_extra_funcs(locale: str) -> dict[str, celpy.CELFunction]:
+    # TODO(#257): Fix types and add tests for StringFormat.
+    # For now, ignoring the type.
+    string_fmt = string_format.StringFormat(locale)  # type: ignore
+    return {
+        # Missing standard functions
+        "format": string_fmt.format,
+        # protovalidate specific functions
+        "isNan": is_nan,
+        "isInf": is_inf,
+        "isIp": is_ip,
+        "isIpPrefix": is_ip_prefix,
+        "isEmail": is_email,
+        "isUri": is_uri,
+        "isUriRef": is_uri_ref,
+        "isHostname": is_hostname,
+        "isHostAndPort": is_host_and_port,
+        "unique": unique,
+    }
+
+
+EXTRA_FUNCS = make_extra_funcs("en_US")
