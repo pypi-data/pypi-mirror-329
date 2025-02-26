6b5e268ec7dd00d5f7b203af3c45bf792bf69b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 07:05:48 +0000

    Add package actbench/0.0.1a3/actbench-0.0.1a3-py3-none-any.whl

---
+++ packages/actbench/actbench-0.0.1a3-py3-none-any.whl/actbench/datasets/__init__.py
@@ -0,0 +1,18 @@
+from typing import Dict, Any, List
+
+from .base import BaseDataset
+from .json import JsonDataset
+
+_DATASET_INSTANCE: BaseDataset = JsonDataset("dataset.jsonl")
+
+
+def load_task_data(task_id: str | int) -> Dict[str, Any]:
+    return _DATASET_INSTANCE.load_task_data(task_id)
+
+
+def get_all_task_ids() -> List[str]:
+    return _DATASET_INSTANCE.get_all_task_ids()
+
+
+def get_all_tasks() -> List[dict[str, Any]]:
+    return _DATASET_INSTANCE.get_all_tasks()
