1915a6c8856e0bca7538e70eb3065046708439a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 04:06:13 +0000

    Add package pyromark/0.9.1/pyromark-0.9.1.tar.gz

---
+++ packages/pyromark/pyromark-0.9.1.tar.gz/pyromark-0.9.1/src/lib.rs
@@ -0,0 +1,17 @@
+mod class_api;
+mod common;
+mod function_api;
+use pyo3::prelude::*;
+
+#[pymodule]
+fn _pyromark(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    m.add_function(wrap_pyfunction!(crate::function_api::events, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        crate::function_api::events_with_range,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(crate::function_api::html, m)?)?;
+    m.add_class::<crate::class_api::Markdown>()?;
+    Ok(())
+}
