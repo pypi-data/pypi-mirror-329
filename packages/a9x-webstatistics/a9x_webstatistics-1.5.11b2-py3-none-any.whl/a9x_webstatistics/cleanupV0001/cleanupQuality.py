512bd42588a86a9412d0e50de225bd6958da5e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:05:43 +0000

    Add package a9x-webstatistics/1.5.11b2/a9x_webstatistics-1.5.11b2-py3-none-any.whl

---
+++ packages/a9x-webstatistics/a9x_webstatistics-1.5.11b2-py3-none-any.whl/a9x_webstatistics/cleanupV0001/cleanupQuality.py
@@ -0,0 +1,25 @@
+from copy import deepcopy
+from datetime import datetime, timedelta
+from operator import itemgetter
+import json
+
+def cleanupQualityV0001(d, statfile):
+    
+    # delete quality older than 92 days:
+
+    clnupUntilDay = datetime.now() - timedelta(days=92)
+    clnupTill = sumUntilDay.strftime("%Y%m")
+    print("cleanupQualityV0001: accumulate including month: "  + clnupTill)
+    
+    # iterate over days to accumulate months:
+    dtmp = deepcopy(d)
+    for x in dtmp['v0001']['days']:
+        # if date has a day:
+        if len(x) > 6:
+            continue
+        if x >= clnupTill:
+            continue
+        if 'quality' in dtmp['v0001']['days'][x]:
+            print("cleanupQuality: delete quality in: " + str(x))
+    return d
+            
