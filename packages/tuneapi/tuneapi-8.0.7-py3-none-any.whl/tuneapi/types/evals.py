cc7122f210af7ebadd210970fd9d1b25ae2096d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:10:44 +0000

    Add package tuneapi/8.0.7/tuneapi-8.0.7-py3-none-any.whl

---
+++ packages/tuneapi/tuneapi-8.0.7-py3-none-any.whl/tuneapi/types/evals.py
@@ -0,0 +1,23 @@
+# Copyright © 2024-2025 Frello Technology Private Limited
+
+
+class Evals:
+    """
+    A simple class containing different evaluation metrics. Each function is self explanatory and returns a JSON logic
+    object.
+    """
+
+    # small things like unit tests
+    def exactly(x):
+        return {"==": [{"var": "response"}, x]}
+
+    def contains(x):
+        return {"in": [{"var": "response"}, x]}
+
+    def is_function(name: str, **kwargs: dict):
+        if not kwargs:
+            return {"==": [{"var": "response.name"}, name]}
+        args = []
+        for k, v in kwargs.items():
+            args.append({"==": [{"var": f"response.arguments.{k}"}, v]})
+        return {"and": [{"==": [{"var": "response.name"}, name]}, *args]}
---
+++ packages/tuneapi/tuneapi-8.0.7-py3-none-any.whl/tuneapi/utils/env.py
@@ -0,0 +1,30 @@
+# Copyright © 2024-2025 Frello Technology Private Limited
+# REMEMBER: nothing from outside tune should be imported in utils
+
+import os
+
+
+class _ENV:
+    def __init__(self):
+        self.vars_called = set()
+
+    TUNEAPI_TOKEN = lambda _, x="": os.getenv("TUNEAPI_TOKEN", x)
+    TUNEORG_ID = lambda _, x="": os.getenv("TUNEORG_ID", x)
+
+    def __getattr__(self, __name: str):
+        self.vars_called.add(__name)
+        return lambda x="": os.getenv(__name, x)
+
+
+ENV = _ENV()
+"""
+Convinience class to get environment variables
+
+Usage:
+
+.. code-block:: python
+
+    from tuneapi.utils.env import ENV
+    TUNEAPI_TOKEN = ENV.TUNEAPI_TOKEN()   # get defined values
+    MY_ENV = ENV.MY_ENV()               # get your arbitrary env var
+"""
