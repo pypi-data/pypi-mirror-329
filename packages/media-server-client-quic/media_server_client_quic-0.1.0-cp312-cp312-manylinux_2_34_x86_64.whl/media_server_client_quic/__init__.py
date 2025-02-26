5796aca3f176b38b5a6d7de887850faa6e1c0a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 08:05:51 +0000

    Add package media-server-client-quic/0.1.0/media_server_client_quic-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/media-server-client-quic/media_server_client_quic-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl/media_server_client_quic/__init__.py
@@ -0,0 +1,5 @@
+from .media_server_client_quic import *
+
+__doc__ = media_server_client_quic.__doc__
+if hasattr(media_server_client_quic, "__all__"):
+    __all__ = media_server_client_quic.__all__
