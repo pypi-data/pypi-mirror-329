675038e29ed2e142630b06e51aa3eecf36bc80a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:11:02 +0000

    Add package rllama/0.1.4/rllama-0.1.4-py3-none-any.whl

---
+++ packages/rllama/rllama-0.1.4-py3-none-any.whl/rllama/scripts.py
@@ -0,0 +1,9 @@
+def post_install_message():
+    print("""
+    🦙 Installation complete! Llama llama hehe 🦙
+    
+    Get started with:
+    >>> from rllama import RLlamaAgent
+    
+   🦙🦙🦙🦙 Happy training! 🎮✨ llama llama llaaammmmaaa !!!!! 🦙🦙🦙🦙
+    """)
