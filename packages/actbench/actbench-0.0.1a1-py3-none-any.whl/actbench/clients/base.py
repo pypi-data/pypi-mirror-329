48794e7ea0c3a7c1c076168ac9c8d98074cf4a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 04:06:41 +0000

    Add package actbench/0.0.1a1/actbench-0.0.1a1-py3-none-any.whl

---
+++ packages/actbench/actbench-0.0.1a1-py3-none-any.whl/actbench/clients/base.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+
+
+class BaseClient(ABC):
+    @abstractmethod
+    def set_api_key(self, api_key: str) -> None:
+        pass
+
+    @abstractmethod
+    def run(self, task_data: Dict[str, Any]) -> Dict[str, Any]:
+        pass
+
