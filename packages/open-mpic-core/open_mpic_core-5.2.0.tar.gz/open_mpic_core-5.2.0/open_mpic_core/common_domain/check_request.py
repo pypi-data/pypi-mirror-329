3b8e935771d4faa31c077eeee1ff412fbc00f052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 06:05:46 +0000

    Add package open-mpic-core/5.2.0/open_mpic_core-5.2.0.tar.gz

---
+++ packages/open-mpic-core/open_mpic_core-5.2.0.tar.gz/open_mpic_core-5.2.0/open_mpic_core/common_domain/check_request.py
@@ -0,0 +1,21 @@
+from abc import ABC
+from typing import Union
+
+from pydantic import BaseModel
+
+from open_mpic_core import CaaCheckParameters, DcvCheckParameters
+
+
+class BaseCheckRequest(BaseModel, ABC):
+    domain_or_ip_target: str
+
+
+class CaaCheckRequest(BaseCheckRequest):
+    caa_check_parameters: CaaCheckParameters | None = None
+
+
+class DcvCheckRequest(BaseCheckRequest):
+    dcv_check_parameters: DcvCheckParameters
+
+
+CheckRequest = Union[CaaCheckRequest, DcvCheckRequest]
