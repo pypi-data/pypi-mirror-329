e1e4b0d4c89553f2df07db04a8d1630400ebf57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 08:05:52 +0000

    Add package UAVReaders/1.0.4/uavreaders-1.0.4.tar.gz

---
+++ packages/UAVReaders/uavreaders-1.0.4.tar.gz/uavreaders-1.0.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+from glob import glob
+
+cppfiles = glob("./*.cpp")
+cfiles = glob("./*.c")
+
+exclude = {"./mavlinkreader.cpp", "./dataflashreader.cpp", "./pyinterop.cpp"}
+
+extrafiles = [x for x in cppfiles + cfiles if x not in exclude]
+
+ext_modules = [
+    Extension (
+        "UAVReaders",
+        sources=["pyinterop.cpp"] + extrafiles
+    )
+]
+
+setup(
+    ext_modules = cythonize(ext_modules),
+    name="UAVReaders",
+    version="1.0.4",
+    author="Christian Clifford",
+    author_email="cjclifford@alaska.edu",
+    description="Parsers for UAV/Aerial Drone-related formats like MavLink and DataFlash",
+    py_modules=[]
+)
