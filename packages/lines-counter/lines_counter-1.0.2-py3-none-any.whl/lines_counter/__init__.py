8adc8d0358c229134db8b26ba8fd6bf41453c702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 08:05:59 +0000

    Add package lines-counter/1.0.2/lines_counter-1.0.2-py3-none-any.whl

---
+++ packages/lines-counter/lines_counter-1.0.2-py3-none-any.whl/lines_counter/__init__.py
@@ -0,0 +1,9 @@
+"""Lines counter library"""
+
+from src.usecases.count_logical_lines_from_project import (
+    count_logical_lines_from_project as count_logical_lines_from_project,
+)
+
+from src.usecases.count_physical_lines_from_project import (
+    count_physical_lines_from_project as count_physical_lines_from_project,
+)
