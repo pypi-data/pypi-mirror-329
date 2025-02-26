6ea6fc72e4afc0fa0a1a0f6a400f75458719ef37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 08:05:47 +0000

    Add package metaflow-stubs/2.15.0/metaflow_stubs-2.15.0-py2.py3-none-any.whl

---
+++ packages/metaflow-stubs/metaflow_stubs-2.15.0-py2.py3-none-any.whl/metaflow-stubs/plugins/airflow/__init__.pyi
@@ -0,0 +1,14 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.0                                                                                 #
+# Generated on 2025-02-25T21:21:51.347829                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import airflow_utils as airflow_utils
+from . import airflow_decorator as airflow_decorator
+from . import exception as exception
+from . import sensors as sensors
+
---
+++ packages/metaflow-stubs/metaflow_stubs-2.15.0-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/batch/__init__.pyi
@@ -0,0 +1,13 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.0                                                                                 #
+# Generated on 2025-02-25T21:21:51.366854                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import batch_client as batch_client
+from . import batch as batch
+from . import batch_decorator as batch_decorator
+
