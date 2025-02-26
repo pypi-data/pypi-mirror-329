dcc2ed9680d7d9a4cf1c7a3ce14ae203a9dbf5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 03:11:05 +0000

    Add package ai-guard-dbp/0.1.3/ai_guard_dbp-0.1.3-py3-none-any.whl

---
+++ packages/ai-guard-dbp/ai_guard_dbp-0.1.3-py3-none-any.whl/ai_guard/__init__.py
@@ -0,0 +1,12 @@
+
+from .evaluator import (
+    health_check,
+    detect_profanity,
+    evaluate_csv,
+    process_query,
+    guardrail_check,
+    calculate_bertscore,
+    calculate_rougel,
+    calculate_ragass
+)
+
